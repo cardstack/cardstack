9bc76a0862e66af96c99bdf4a6d2cf3132d8cdd6
Edward Faulkner <ef@alum.mit.edu>
2017-02-20 09:26:07 -0500

    lint the packages

--- tests/eslint-test.js
+++ tests/eslint-test.js
@@ -2,6 +2,6 @@ const lint = require('mocha-eslint');
 const path = require('path');
 
 lint([
-  path.join(__dirname, '..', 'src'),
+  path.join(__dirname, '..', 'packages'),
   path.join(__dirname, '..', 'tests')
 ]);
