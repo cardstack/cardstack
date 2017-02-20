8ff64866ad3fcfbba1a83f157ecfeaaa86108aad
Edward Faulkner <ef@alum.mit.edu>
2017-02-20 12:16:04 -0500

    splitting searcher out from indexer

---
+++ packages/server/searcher.js
@@ -0,0 +1,126 @@
+const makeClient = require('./elastic-client');
+const logger = require('heimdalljs-logger');
+
+class Searcher {
+  constructor({ elasticsearch }) {
+    this.es = makeClient(elasticsearch);
+    this.log = logger('searcher');
+  }
+
+  async search(branch, { queryString, filter }) {
+    let esBody = {
+      query: {
+        bool: {
+          must: [],
+          // All searches exclude `meta` documents, because those are
+          // internal to our system.
+          must_not: [{
+            term: {
+              _type: 'meta'
+            }
+          }]
+        }
+      }
+    };
+    if (queryString) {
+      esBody.query.bool.must.push({
+        match: {
+          _all: queryString
+        }
+      });
+    }
+    if (filter) {
+      for (let expression of this._filterToES(filter)) {
+        esBody.query.bool.must.push(expression);
+      }
+    }
+    this.log.debug('search %j', esBody);
+    let result = await this.es.search({
+      index: branch,
+      body: esBody
+    });
+    this.log.debug('searchResult %j', result);
+    return result.hits.hits.map(entry => ({ type: entry._type, id: entry._id, document: entry._source}));
+  }
+
+  _filterToES(filter) {
+    let result = [];
+    Object.keys(filter).forEach(key => {
+      let value = filter[key];
+      switch(key) {
+      case 'not':
+        result.push({ bool: { must_not: this._filterToES(value) } });
+        break;
+      case 'or':
+        result.push({ bool: { should: this._filterToES(value) } });
+        break;
+      case 'and':
+        // 'and' is not strictly needed, since we already conjoin all
+        // top-level conditions. But for completeness, it works.
+        result.push({ bool: { must: this._filterToES(value) } });
+        break;
+      default:
+        // Any keys that aren't one of the predefined operations are
+        // field names.
+        result.push(this._fieldFilter(key, value));
+      }
+    });
+    return result;
+  }
+
+  _fieldFilter(key, value) {
+    let field = fieldNameFromKey(key);
+
+    if (typeof value === 'string') {
+      // Bare strings are shorthand for a single term filter
+      return { term: { [field] : value.toLowerCase() } };
+    }
+
+    if (Array.isArray(value)) {
+      // Bare arrays are shorthand for a multi term filter
+      return { terms: { [field] : value.map(elt => elt.toLowerCase()) } };
+    }
+
+    if (value.range) {
+      let limits = {};
+      ['lte', 'gte', 'lt', 'gt'].forEach(limit => {
+        if (value.range[limit]) {
+          limits[limit] = value.range[limit];
+        }
+      });
+      return {
+        range: {
+          [field]: limits
+        }
+      };
+    }
+
+    if (value.exists != null) {
+      if (String(value.exists) === 'false') {
+        return {
+          bool: { must_not: { exists: { field } } }
+        };
+      } else {
+        return {
+          exists: { field }
+        };
+      }
+    }
+
+    throw new Error("Unimplemented");
+  }
+}
+
+// We use elastic search's built-in _type and _id to store JSONAPI's
+// type and id. We don't want clients to need to add the underscores.
+function fieldNameFromKey(key) {
+  if (key === 'type') {
+    return '_type';
+  }
+  if (key === 'id') {
+    return '_id';
+  }
+  return key;
+}
+
+module.exports = Searcher;
---
+++ packages/server/tests/searcher-test.js
@@ -0,0 +1,186 @@
+const temp = require('./temp-helper');
+const Indexer = require('@cardstack/server/indexer');
+const Searcher = require('@cardstack/server/searcher');
+const { inES, host } = require('./elastic-assertions');
+const elasticsearch = host();
+const { makeRepo } = require('./git-assertions');
+const REALTIME = true;
+
+describe('searcher', function() {
+
+  let root, searcher;
+  let fixtures = [
+    {
+      type: 'articles',
+      id: '1',
+      content: {
+        hello: 'magic words'
+      }
+    },
+    {
+      type: 'people',
+      id: '1',
+      content: {
+        firstName: 'Quint',
+        lastName: 'Faulkner',
+        age: 6
+      }
+    },
+    {
+      type: 'people',
+      id: '2',
+      content: {
+        firstName: 'Arthur',
+        lastName: 'Faulkner',
+        age: 1
+      }
+    }
+  ];
+
+  before(async function() {
+    root = await temp.mkdir('cardstack-server-test');
+    let indexer = new Indexer({
+      elasticsearch,
+      repoPath: root
+    });
+    searcher = new Searcher({
+      elasticsearch
+    });
+
+    await makeRepo(root, [
+      {
+        changes: fixtures.map(f => ({
+          filename: `contents/${f.type}/${f.id}.json`,
+          buffer: Buffer.from(JSON.stringify(f.content), 'utf8')
+        }))
+      }
+    ]);
+    await indexer.update(REALTIME);
+  });
+
+  after(async function() {
+    await temp.cleanup();
+    await inES(elasticsearch).deleteAllIndices();
+  });
+
+
+  it('can be searched for all content', async function() {
+    let results = await searcher.search('master', {});
+    expect(results).to.have.length(fixtures.length);
+  });
+
+  it('can be searched via queryString', async function() {
+    let results = await searcher.search('master', {
+      queryString: 'magic'
+    });
+    expect(results).to.have.length(1);
+    expect(results).includes.something.with.deep.property('document.hello', 'magic words');
+  });
+
+  it('can be searched via queryString, negative result', async function() {
+    let results = await searcher.search('master', {
+      queryString: 'this-is-an-unused-term'
+    });
+    expect(results).to.have.length(0);
+  });
+
+  it('can filter by type', async function() {
+    let results = await searcher.search('master', {
+      filter: {
+        type: 'articles'
+      }
+    });
+    expect(results).to.have.length(1);
+    expect(results).includes.something.with.deep.property('document.hello', 'magic words');
+  });
+
+  it('can filter by id', async function() {
+    let results = await searcher.search('master', {
+      filter: {
+        id: '1'
+      }
+    });
+    expect(results).to.have.length(2);
+    expect(results).includes.something.with.property('type', 'articles');
+    expect(results).includes.something.with.property('type', 'people');
+  });
+
+  it('can filter a field by one term', async function() {
+    let results = await searcher.search('master', {
+      filter: {
+        firstName: 'Quint'
+      }
+    });
+    expect(results).to.have.length(1);
+    expect(results).includes.something.with.deep.property('document.firstName', 'Quint');
+  });
+
+  it('can filter a field by multiple terms', async function() {
+    let results = await searcher.search('master', {
+      filter: {
+        firstName: ['Quint', 'Arthur']
+      }
+    });
+    expect(results).to.have.length(2);
+  });
+
+  it('can filter by range', async function() {
+    let results = await searcher.search('master', {
+      filter: {
+        age: {
+          range: {
+            lt: '2'
+          }
+        }
+      }
+    });
+    expect(results).to.have.length(1);
+    expect(results).includes.something.with.deep.property('document.firstName', 'Arthur');
+  });
+
+  it('can filter by field existence (string)', async function() {
+    let results = await searcher.search('master', {
+      filter: {
+        age: {
+          exists: 'true'
+        }
+      }
+    });
+    expect(results).to.have.length(2);
+  });
+
+  it('can filter by field nonexistence (string)', async function() {
+    let results = await searcher.search('master', {
+      filter: {
+        age: {
+          exists: 'false'
+        }
+      }
+    });
+    expect(results).to.have.length(fixtures.length - 2);
+  });
+
+  it('can filter by field existence (bool)', async function() {
+    let results = await searcher.search('master', {
+      filter: {
+        age: {
+          exists: true
+        }
+      }
+    });
+    expect(results).to.have.length(2);
+  });
+
+  it('can filter by field nonexistence (bool)', async function() {
+    let results = await searcher.search('master', {
+      filter: {
+        age: {
+          exists: false
+        }
+      }
+    });
+    expect(results).to.have.length(fixtures.length - 2);
+  });
+
+
+});
