ca498b7c10234b689e42d136c12f60fdfd03a92d
Edward Faulkner <ef@alum.mit.edu>
2017-03-05 15:57:56 -0500

    test support for emitting schema records

--- packages/server/tests/add-records.js
+++ packages/server/tests/add-records.js
@@ -1,15 +1,27 @@
 const IndexerEngine = require('@cardstack/server/indexer-engine');
+const Searcher = require('@cardstack/elasticsearch/searcher');
+const Schema = require('@cardstack/server/schema');
+const Plugins = require('@cardstack/server/plugins');
 
 module.exports = async function addRecords(records) {
+  let plugins = await Plugins.load();
   let stub = new StubIndexer();
-  let engine = new IndexerEngine([stub]);
-  stub.records = records;
+  let engine = new IndexerEngine([stub], new Searcher(), plugins);
+  let schemaTypes = Schema.ownTypes();
+  for (let record of records) {
+    if (schemaTypes.indexOf(record.type)) {
+      stub.schemaRecords.push(record);
+    } else {
+      stub.records.push(record);
+    }
+  }
   await engine.update({ realTime: true });
 };
 
 class StubIndexer {
   constructor() {
     this.records = [];
+    this.schemaRecords = [];
   }
   async branches() {
     return ['master'];
@@ -24,10 +36,10 @@ class StubUpdater {
     this.source = source;
     this.name = 'stub';
   }
-  async mappings() {
-    return {};
-  }
-  async updateSchema(/* meta, hints, ops */) {
+  async updateSchema(meta, hints, ops) {
+    for (let record of this.source.schemaRecords) {
+      await ops.save(record.type, record.id, record);
+    }
   }
   async updateContent(meta, hints, ops) {
     for (let record of this.source.records) {
