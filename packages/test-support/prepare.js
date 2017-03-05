b1b5218315feb1149a802e904ac9b92ad100a819
Edward Faulkner <ef@alum.mit.edu>
2017-03-05 15:57:35 -0500

    improved error reporting in test suite

--- packages/test-support/prepare.js
+++ packages/test-support/prepare.js
@@ -1,3 +1,11 @@
 let chai = require('chai');
 global.expect = chai.expect;
 chai.use(require('chai-things'));
+
+// Without this, we can't see stack traces for certain failures within
+// promises during the test suite.
+process.on('warning', (warning) => {
+  /* eslint-disable no-console */
+  console.warn(warning.stack);
+  /* eslint-enable no-console */
+});
