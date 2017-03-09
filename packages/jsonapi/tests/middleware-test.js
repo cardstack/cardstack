04c6a859a72b035662705b0051fdc15896120138
Edward Faulkner <ef@alum.mit.edu>
2017-03-09 11:45:09 -0500

    add failing test for first jsonapi endpoint

--- packages/jsonapi/tests/middleware-test.js
+++ packages/jsonapi/tests/middleware-test.js
@@ -1,21 +1,32 @@
 const jsonapi = require('@cardstack/jsonapi/middleware');
 const supertest = require('supertest');
 const Koa = require('koa');
+const addRecords = require('@cardstack/server/tests/add-records');
 
 describe('jsonapi', function() {
 
   let request;
 
   beforeEach(async function() {
+    await addRecords([
+      {
+        type: 'articles',
+        id: '0',
+        attributes: {
+          title: "Hello world"
+        }
+      }
+    ]);
     let app = new Koa();
     app.use(jsonapi());
     request = supertest(app.callback());
   }),
 
-  it('can access the built-in fields', async function() {
-    let response = await request.get('/fields');
+  it('can get a single resource', async function() {
+    let response = await request.get('/articles/0');
     expect(response).to.have.property('status', 200);
-    expect(response).deep.property('body.data').includes.something.with.deep.property('attributes.name', 'string');
+    expect(response.body).deep.property('data.id', '0');
+    expect(response.body).deep.property('data.attributes.title', 'Hello world');
   });
 
 });
