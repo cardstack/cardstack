fc0b3131bb9c608ce0fd5fb431a44e601df2cfab
Edward Faulkner <ef@alum.mit.edu>
2017-03-16 13:00:20 -0400

    backfilling tests for cardstack-tools-launcher

--- tests/integration/components/cardstack-tools-launcher-test.js
+++ tests/integration/components/cardstack-tools-launcher-test.js
@@ -1,25 +1,50 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('cardstack-tools-launcher', 'Integration | Component | cardstack tools launcher', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    this.register('service:cardstack-tools', Ember.Service.extend({
+      available: true,
+      active: false,
+      setActive(value) {
+        this.set('active', value);
+      }
+    }));
+    this.inject.service('cardstack-tools', { as: 'tools' });
+  }
 });
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
+test('it renders with default implementation', function(assert) {
   this.render(hbs`{{cardstack-tools-launcher}}`);
+  assert.equal(this.$('svg').length, 1, "found svg icon");
+  assert.equal(this.$('svg.active').length, 0, "not active");
+});
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
+test('it renders with custom implementation', function(assert) {
   this.render(hbs`
-    {{#cardstack-tools-launcher}}
-      template block text
+    {{#cardstack-tools-launcher as |launcher|}}
+      <div class="outer {{if launcher.active 'active'}}">
+        <button {{action launcher.setActive true}}>Open</button>
+        <button {{action launcher.setActive false}}>Close</button>
+        <button {{action launcher.toggleActive}}>Toggle</button>
+      </div>
     {{/cardstack-tools-launcher}}
   `);
+  assert.equal(this.$('.outer').length, 1, 'found provided element');
+});
 
-  assert.equal(this.$().text().trim(), 'template block text');
+test('it does not render when tools are not available', function(assert) {
+  this.get('tools').set('available', false);
+  this.render(hbs`{{cardstack-tools-launcher}}`);
+  assert.equal(this.$('svg').length, 0, "no icon");
+});
+
+test('clicking icon toggles tools', function(assert) {
+  this.render(hbs`{{cardstack-tools-launcher}}`);
+  Ember.run(() => {
+    this.$('svg').click();
+  });
+  assert.equal(this.$('svg.active').length, 1, "found active icon");
 });
