26dd10c4b187a9bc3a3f9d28765244b493fbd801
Edward Faulkner <ef@alum.mit.edu>
2017-03-16 18:01:35 -0400

    toolbox sizing

--- addon/services/cardstack-tools.js
+++ addon/services/cardstack-tools.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   available: true,
   active: false,
+  activePanel: 'cardstack-toolbox',
   setActive(isActive) {
     this.set('active', isActive);
   }
