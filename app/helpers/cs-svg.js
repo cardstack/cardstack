a0dacdd6f4c2eb3ecc2e0dbb10dd2bce13ee550f
Edward Faulkner <ef@alum.mit.edu>
2017-03-17 17:35:53 -0400

    svg symbol storage
    I wanted to use ember-svg-jar, but it doesn't handle our use case well. I can get it to work within an addon, but not when the consuming app also wants to use it.

---
+++ app/helpers/cs-svg.js
@@ -0,0 +1,1 @@
+export { default, csSvg } from 'cardstack-suite/helpers/cs-svg';
--- index.js
+++ index.js
@@ -17,5 +17,20 @@ module.exports = {
         ]
       }
     }
+  },
+  contentFor(type) {
+    if (type === 'body-footer') {
+      return `<script>
+  var ajax = new XMLHttpRequest();
+  ajax.open('GET', '/cardstack-suite/symbols.svg', true);
+  ajax.send();
+  ajax.onload = function(e) {
+    var div = document.createElement('div');
+    div.innerHTML = ajax.responseText;
+    document.body.insertBefore(div, document.body.childNodes[0]);
+  };
+</script>`;
+
+    }
   }
 };
