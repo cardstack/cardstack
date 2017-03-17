c2927be15162626e302737c8880041d9ea664eae
Edward Faulkner <ef@alum.mit.edu>
2017-03-17 19:43:03 -0400

    view mode button states

--- addon/components/cs-view-mode-button.js
+++ addon/components/cs-view-mode-button.js
@@ -3,5 +3,21 @@ import layout from '../templates/components/cs-view-mode-button';
 
 export default Ember.Component.extend({
   layout,
-  tagName: ''
+  classNames: ['cs-view-mode-button'],
+  classNameBindings: ['active'],
+  active: Ember.computed('mode', 'currentMode', function() {
+    return this.get('mode') === this.get('currentMode');
+  }),
+  iconWidth: Ember.computed('mode', function() {
+    return this.get('mode') === 'cards' ? 20 : 18;
+  }),
+  description: Ember.computed('mode', function() {
+    return this.get('mode') === 'cards' ? 'Tile' : 'Page';
+  }),
+  icon: Ember.computed('mode', function() {
+    return this.get('mode') === 'cards' ? 'tiles' : 'page';
+  }),
+  click() {
+    this.get('setMode')(this.get('mode'));
+  }
 });
--- addon/components/cs-view-mode-buttons.js
+++ addon/components/cs-view-mode-buttons.js
@@ -4,18 +4,14 @@ import layout from '../templates/components/cs-view-mode-buttons';
 export default Ember.Component.extend({
   layout,
   tagName: '',
-  modes: [
-    {
-      mode: 'page',
-      icon: 'page',
-      iconWidth: 18,
-      description: 'Page'
-    },
-    {
-      mode: 'tiles',
-      icon: 'tiles',
-      iconWidth: 20,
-      description: 'Tile'
+  tools: Ember.inject.service('cardstack-tools'),
+  currentMode: Ember.computed.alias('tools.viewMode'),
+
+  actions: {
+    setMode(which) {
+      this.get('tools').setViewMode(which);
     }
-  ]
+  },
+
+  modes: [ 'page', 'cards' ]
 });
