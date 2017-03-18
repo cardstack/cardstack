39ae237a5f0c5b2c3aa5fa4051e0ff5cc440e324
Edward Faulkner <ef@alum.mit.edu>
2017-03-18 10:57:21 -0400

    finish rename/cleanup/test adjustment

--- addon/components/cs-mode-button.js
+++ addon/components/cs-mode-button.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
-import layout from '../templates/components/cs-toolbox-mode-button';
+import layout from '../templates/components/cs-mode-button';
 
 export default Ember.Component.extend({
   layout,
-  classNames: ['cs-toolbox-mode-button'],
+  classNames: ['cs-mode-button'],
   classNameBindings: ['active', 'iconOnly'],
   iconOnly: Ember.computed.not('mode.description'),
   active: Ember.computed.alias('mode.active'),
--- tests/integration/components/cs-mode-choices-test.js
+++ tests/integration/components/cs-mode-choices-test.js
@@ -1,25 +1,49 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('cs-mode-choices', 'Integration | Component | cs mode choices', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    this.register('service:cardstack-tools', Ember.Service.extend({
+
+      // This is the basic protocol required by cs-mode-choices: a
+      // property to control, choices, and a setter function:
+      favoriteColor: 'blue',
+      favoriteColorChoices: [
+        { id: 'blue', description: 'Blue' },
+        { id: 'red', description: 'Red' }
+      ],
+      setFavoriteColor(color) {
+        this.set('favoriteColor', color);
+      }
+
+    }));
+    this.inject.service('cardstack-tools', { as: 'tools' });
+  }
+
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{cs-mode-choices}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
   this.render(hbs`
-    {{#cs-mode-choices}}
-      template block text
+    {{#cs-mode-choices for="favoriteColor" as |choice|}}
+      <section>
+        {{cs-mode-button mode=choice}}
+      </section>
     {{/cs-mode-choices}}
   `);
+  assert.equal(this.$('section').length, 2);
+  assert.equal(this.$('.cs-mode-button.active').length, 1);
+});
 
-  assert.equal(this.$().text().trim(), 'template block text');
+test('it can update value', function(assert) {
+  this.render(hbs`
+    {{#cs-mode-choices for="favoriteColor" as |choice|}}
+      <section>
+        {{cs-mode-button mode=choice}}
+      </section>
+    {{/cs-mode-choices}}
+  `);
+  Ember.run(() => this.$('.cs-mode-button:contains(Red)').click());
+  assert.equal(this.get('tools.favoriteColor'), 'red');
 });
