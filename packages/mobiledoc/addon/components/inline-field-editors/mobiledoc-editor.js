ac19f0401b57a565491a3c94697bddbafb9bc534
Edward Faulkner <ef@alum.mit.edu>
2017-03-20 19:57:10 -0400

    inline mobiledoc editor

---
+++ packages/mobiledoc/addon/components/inline-field-editors/mobiledoc-editor.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import layout from '../../templates/components/inline-field-editors/mobiledoc-editor';
+
+export default Ember.Component.extend({
+  layout
+});
---
+++ packages/mobiledoc/app/components/inline-field-editors/mobiledoc-editor.js
@@ -0,0 +1,1 @@
+export { default } from '@cardstack/mobiledoc/components/inline-field-editors/mobiledoc-editor';
