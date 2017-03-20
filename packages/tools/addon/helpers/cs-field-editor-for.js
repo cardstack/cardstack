a8208618ce07d962e84710b58b16bc258235c208
Edward Faulkner <ef@alum.mit.edu>
2017-03-20 19:44:38 -0400

    split field editor lookup into own helper

---
+++ packages/tools/addon/helpers/cs-field-editor-for.js
@@ -0,0 +1,47 @@
+import Ember from 'ember';
+
+export function csFieldEditorFor([content, fieldName], { variant }) {
+  if (!content) { return; }
+  let meta;
+
+  try {
+    meta = content.constructor.metaForProperty(fieldName);
+  } catch (err) {
+    return;
+  }
+
+  // meta.options.fieldType is our convention for annotating
+  // models. meta.type is the name of the transform that ember-data
+  // is using, which we keep as a fallback.
+  let type = meta.options && meta.options.fieldType;
+
+  if (!type && meta.type) {
+    // lift the default ember-data transform names into our core
+    // types
+    type = `@cardstack/core-types::${meta.type}`;
+  }
+
+  if (!type) {
+    return;
+  }
+
+  type = stripNamespace(type);
+
+  let prefix = '';
+  if (variant === 'inline') {
+    prefix = 'inline-';
+  }
+  return `${prefix}field-editors/${type}-editor`;
+}
+
+export default Ember.Helper.helper(csFieldEditorFor);
+
+function stripNamespace(type) {
+  // Right now the actual field editor components get flattened down
+  // out of their namespaces, so we throw away everything but the
+  // last bit of their names here. This problem is easier to solve
+  // once I can integrate a module-unification resolver, so I'm
+  // leaving it like this for now.
+  let parts = type.split(/[/:]/g);
+  return parts[parts.length - 1];
+}
---
+++ packages/tools/app/helpers/cs-field-editor-for.js
@@ -0,0 +1,1 @@
+export { default, csFieldEditorFor } from '@cardstack/tools/helpers/cs-field-editor-for';
