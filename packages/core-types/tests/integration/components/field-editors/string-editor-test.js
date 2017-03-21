d5ddd9d1caeab310f9eaaf24e57a923019ef16f7
Edward Faulkner <ef@alum.mit.edu>
2017-03-21 10:15:49 -0400

    fix test

--- packages/core-types/tests/integration/components/field-editors/string-editor-test.js
+++ packages/core-types/tests/integration/components/field-editors/string-editor-test.js
@@ -6,20 +6,9 @@ moduleForComponent('field-editors/string-editor', 'Integration | Component | fie
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{field-editors/string-editor}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#field-editors/string-editor}}
-      template block text
-    {{/field-editors/string-editor}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.set('model', {
+    title: 'hello'
+  });
+  this.render(hbs`{{field-editors/string-editor content=model field="title"}}`);
+  assert.equal(this.$('input').val(), 'hello');
 });
