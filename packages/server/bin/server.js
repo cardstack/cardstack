5d0a3852f63a104fe27e93f32986d83746ad4e32
Edward Faulkner <ef@alum.mit.edu>
2017-03-22 10:08:39 -0400

    initial seed model loader for server

--- packages/server/bin/server.js
+++ packages/server/bin/server.js
@@ -1,10 +1,45 @@
 const Koa = require('koa');
-const app = new Koa();
 const Searcher = require('@cardstack/elasticsearch/searcher');
 const Writers = require('@cardstack/server/writers');
 const SchemaCache = require('@cardstack/server/schema-cache');
+const logger = require('heimdalljs-logger');
+const log = logger('server');
+const commander = require('commander');
+const path = require('path');
 
-let schemaCache = new SchemaCache();
+function runServer(port, seedModels) {
+  let schemaCache = new SchemaCache(seedModels);
+  let app = new Koa();
+  app.use(require('@cardstack/jsonapi/middleware')(new Searcher(schemaCache), new Writers(schemaCache)));
+  app.listen(port);
+  log.info("server listening on %s", port);
+}
 
-app.use(require('@cardstack/jsonapi/middleware')(new Searcher(schemaCache), new Writers(schemaCache)));
-app.listen(3000);
+
+function commandLineOptions() {
+  commander
+    .usage('[options] <seed-config-file>')
+    .option('-p --port <port>', 'Server listen port.', 3000)
+    .parse(process.argv);
+
+  if (commander.args.length < 1) {
+    commander.outputHelp();
+    process.exit(-1);
+  }
+
+  commander.seedConfigFile = path.resolve(commander.args[0]);
+  return commander;
+}
+
+function loadSeedModels(options) {
+  try {
+    return require(options.seedConfigFile);
+  } catch (err) {
+    process.stderr.write(`Unable to load models from your seed-config-file (${options.seedConfigFile}), ${err}\n`);
+    process.exit(-1);
+  }
+}
+
+let options = commandLineOptions();
+let seedModels = loadSeedModels(options);
+runServer(options.port, seedModels);
