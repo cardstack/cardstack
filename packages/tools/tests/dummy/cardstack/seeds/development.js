31c228d3db714f0d0a7c22357d8b1418accdd217
Edward Faulkner <ef@alum.mit.edu>
2017-03-22 21:29:00 -0400

    update comment

--- packages/tools/tests/dummy/cardstack/seeds/development.js
+++ packages/tools/tests/dummy/cardstack/seeds/development.js
@@ -35,15 +35,11 @@ module.exports = [
 
         // Keep all of the branches we manage segregated under this
         // prefix. So when the user queries 'master' they will really
-        // be querying cs-master in our repo. If we're trying to write
-        // to a prefixed branch that doesn't exist yet, we will try to
-        // create it based off the non-prefixed branch of the same
-        // name. So your first write to `master` will make a new
-        // `cs-master` branch that stars at your real `master` branch.
+        // be querying cs-master in our repo.
         //
         // This is intended as a development-mode feature for people
         // who are actively managing their own local git
-        // repository. In production or in friendly desktop mode, this
+        // repository. In production or in friendly-desktop mode, this
         // feature should not be needed.
         branchPrefix: 'cs-'
       }
