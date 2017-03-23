ae046ec669b949365241bacff16120af951b4242
Edward Faulkner <ef@alum.mit.edu>
2017-03-23 14:34:08 -0400

    point at real server!

--- packages/tools/tests/dummy/app/routes/application.js
+++ packages/tools/tests/dummy/app/routes/application.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findRecord('page', 0);
+    return this.get('store').findRecord('page', 1);
   }
 });
