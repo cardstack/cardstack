74a06e4694f595dfdc8530ae8ac9df7a58573615
Edward Faulkner <ef@alum.mit.edu>
2017-03-22 21:44:18 -0400

    wide-open dev grant

--- packages/tools/tests/dummy/cardstack/seeds/development.js
+++ packages/tools/tests/dummy/cardstack/seeds/development.js
@@ -44,5 +44,15 @@ module.exports = [
         branchPrefix: 'cs-'
       }
     }
+  },
+  {
+    type: 'grants',
+    id: 0,
+    attributes: {
+      'may-create-resource': true,
+      'may-update-resource': true,
+      'may-delete-resource': true,
+      'may-write-field': true
+    }
   }
 ];
