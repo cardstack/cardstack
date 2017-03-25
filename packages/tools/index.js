988ac35b6fd36bb452a54bdc55a82abebf2c562f
Edward Faulkner <ef@alum.mit.edu>
2017-03-25 11:43:41 -0400

    back out URL polyfill because we didn't end up needing it

--- packages/tools/index.js
+++ packages/tools/index.js
@@ -1,52 +1,15 @@
 /* eslint-env node */
 
-const CssImport = require('postcss-import');
-const CssNext = require('postcss-cssnext');
-const path = require('path');
-const mergeTrees = require('broccoli-merge-trees');
-const Funnel = require('broccoli-funnel');
+var CssImport = require('postcss-import');
+var CssNext = require('postcss-cssnext');
 
 'use strict';
 
 module.exports = {
   name: '@cardstack/tools',
-
   isDevelopingAddon() {
     return process.env.CARDSTACK_DEV;
   },
-
-  init: function() {
-    if (this._super.init) {
-      this._super.init.apply(this, arguments);
-    }
-    // Shim this.import for support in older versions of ember-cli
-    if (!this.import) {
-      this._findHost = function findHostShim() {
-        var current = this;
-        var app;
-
-        // Keep iterating upward until we don't have a grandparent.
-        // Has to do this grandparent check because at some point we hit the project.
-        do {
-          app = current.app || app;
-        } while (current.parent.parent && (current = current.parent));
-
-        return app;
-      };
-      this.import = function importShim(asset, options) {
-        var app = this._findHost();
-        app.import(asset, options);
-      };
-    }
-  },
-
-  included: function(){
-    if (this._super.included) {
-      this._super.included.apply(this, arguments);
-    }
-    this.import('vendor/js-polyfills/url.js');
-  },
-
   options: {
     postcssOptions: {
       compile: {
@@ -58,16 +21,6 @@ module.exports = {
       }
     }
   },
-
-  treeForVendor(tree){
-    var polyfillsPath = path.dirname(require.resolve('js-polyfills'));
-    var polyfillsTree = new Funnel(this.treeGenerator(polyfillsPath), {
-      include: ['url.js'],
-      destDir: 'js-polyfills'
-    });
-    return mergeTrees([tree, polyfillsTree]);
-  },
-
   contentFor(type) {
     if (type === 'body-footer') {
       return `<script>
