2029688bbcb5644538465386338469c1afce992c
Edward Faulkner <ef@alum.mit.edu>
2017-03-25 11:45:44 -0400

    not needed

--- packages/tools/addon/adapters/cardstack-generic.js
+++ packages/tools/addon/adapters/cardstack-generic.js
@@ -1,8 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  host: 'http://localhost:3000',
-
   findRecord(store, type, id /*, snapshot */) {
     let [branch, realType, realId] = id.split('/');
     let adapter = store.adapterFor(realType);
