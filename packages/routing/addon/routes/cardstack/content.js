740d90b2ec7d9a880c406c0876429c0192c6bccb
Edward Faulkner <ef@alum.mit.edu>
2017-03-25 16:03:18 -0400

    slug-based routing works for cardstack/content

--- packages/routing/addon/routes/cardstack/content.js
+++ packages/routing/addon/routes/cardstack/content.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model({ type, slug }) {
     return this.store.queryRecord(Ember.String.singularize(type), {
-      filter: { slug },
+      filter: { slug: { exact: slug } },
       branch: this.modelFor('cardstack').branch
     });
   }
