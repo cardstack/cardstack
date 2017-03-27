e062105be80f0e82aeae90190e1dbff76caa123f
Edward Faulkner <ef@alum.mit.edu>
2017-03-27 16:42:57 -0700

    ember-cli server middleware

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,3 +1,21 @@
+const { makeServer } = require('./main');
+const path = require('path');
+
 module.exports = {
-  name: '@cardstack/server'
+  name: '@cardstack/server',
+  serverMiddleware({ app, options }) {
+    let { project, environment } = options;
+
+    let seedPath = path.join(path.dirname(project.configPath()), '..', 'cardstack', 'seeds');
+    let seedFile = path.join(seedPath, environment + '.js');
+    let seedModels = [];
+    try {
+      seedModels = require(seedFile);
+    } catch (err) {
+      project.ui.writeWarnLine(`Unable to load your seed models (looking for ${seedFile})`);
+    }
+    makeServer(seedModels).then(server => {
+      app.use('/cardstack', server.callback());
+    });
+  }
 };
