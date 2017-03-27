ded5b8f400f55dad6884f2b20ab4dfcec1099b2e
Edward Faulkner <ef@alum.mit.edu>
2017-03-27 16:13:21 -0700

    splitting server startup

--- packages/server/bin/server.js
+++ packages/server/bin/server.js
@@ -1,38 +1,14 @@
-const Koa = require('koa');
-const Searcher = require('@cardstack/elasticsearch/searcher');
-const Writers = require('@cardstack/server/writers');
-const SchemaCache = require('@cardstack/server/schema-cache');
-const Indexers = require('@cardstack/server/indexers');
-const logger = require('heimdalljs-logger');
-const log = logger('server');
+const { makeServer } = require('../main');
 const commander = require('commander');
 const path = require('path');
-
-async function wireItUp(seedModels) {
-  let schemaCache = new SchemaCache(seedModels);
-  let indexers = new Indexers(schemaCache);
-  setInterval(() => indexers.update(), 1000);
-  let writers = new Writers(schemaCache);
-  writers.addListener('changed', what => indexers.update({ hints: [ what ] }));
-  await indexers.update();
-  let searcher = new Searcher(schemaCache);
-  return { searcher, writers };
-}
+const logger = require('heimdalljs-logger');
+const log = logger('server');
 
 async function runServer(port, seedModels) {
-  let { searcher, writers } = await wireItUp(seedModels);
-  let app = new Koa();
-  app.use(httpLogging);
-  app.use(require('@cardstack/jsonapi/middleware')(searcher, writers));
+  let app = await makeServer(port, seedModels);
   app.listen(port);
   log.info("server listening on %s", port);
 }
-
-async function httpLogging(ctxt, next) {
-  await next();
-  log.info('%s %s %s', ctxt.request.method, ctxt.request.url, ctxt.response.status);
-}
-
 
 function commandLineOptions() {
   commander
---
+++ packages/server/main.js
@@ -0,0 +1,33 @@
+const Koa = require('koa');
+const Searcher = require('@cardstack/elasticsearch/searcher');
+const Writers = require('@cardstack/server/writers');
+const SchemaCache = require('@cardstack/server/schema-cache');
+const Indexers = require('@cardstack/server/indexers');
+const logger = require('heimdalljs-logger');
+const log = logger('server');
+
+async function wireItUp(seedModels) {
+  let schemaCache = new SchemaCache(seedModels);
+  let indexers = new Indexers(schemaCache);
+  setInterval(() => indexers.update(), 1000);
+  let writers = new Writers(schemaCache);
+  writers.addListener('changed', what => indexers.update({ hints: [ what ] }));
+  await indexers.update();
+  let searcher = new Searcher(schemaCache);
+  return { searcher, writers };
+}
+
+async function makeServer(port, seedModels) {
+  let { searcher, writers } = await wireItUp(seedModels);
+  let app = new Koa();
+  app.use(httpLogging);
+  app.use(require('@cardstack/jsonapi/middleware')(searcher, writers));
+  return app;
+}
+
+async function httpLogging(ctxt, next) {
+  await next();
+  log.info('%s %s %s', ctxt.request.method, ctxt.request.url, ctxt.response.status);
+}
+
+exports.makeServer = makeServer;
