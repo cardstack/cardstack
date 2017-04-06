b17a3fd54653975ad6c103ca4164763623ed2b5d
Edward Faulkner <ef@alum.mit.edu>
2017-04-06 00:08:53 -0400

    use simpler layering strategy for stub cardstack-routing service
    tools needs the service to exists but doesn't want to just blow up unhelpfully if you haven't made one yet. So we provide a stub that emits warnings. Users who are doing their own routing can define the service in their app, or they can install @cardstack/routing which provides our full implementation.

--- packages/tools/index.js
+++ packages/tools/index.js
@@ -2,8 +2,6 @@
 
 var CssImport = require('postcss-import');
 var CssNext = require('postcss-cssnext');
-var path = require('path');
-var mergeTrees = require('broccoli-merge-trees');
 
 'use strict';
 
@@ -21,21 +19,6 @@ module.exports = {
           { module: CssNext }
         ]
       }
-    }
-  },
-
-  treeForApp(tree) {
-    let haveRouting;
-    try {
-      require.resolve('@cardstack/routing')
-      haveRouting = true;
-    } catch(err) {
-      haveRouting = false;
-    }
-    if (haveRouting) {
-      return tree;
-    } else {
-      return mergeTrees([tree, path.join(__dirname, 'lib/routing-stub')]);
     }
   },
 
