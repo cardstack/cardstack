23207a2b39a66891f6785d3c00adf7d91aaf55f0
Edward Faulkner <ef@alum.mit.edu>
2017-04-07 01:53:48 -0400

    better structure via koa-better-router

--- packages/oauth2-client/bin/server.js
+++ packages/oauth2-client/bin/server.js
@@ -1,5 +1,7 @@
 const Koa = require('koa');
+const mount = require('koa-mount');
+
 let app = new Koa();
-app.use(require('../middleware')());
+app.use(mount('/oauth2', require('../middleware')()));
 app.listen(3001);
 console.log("listening on 3001");
--- packages/oauth2-client/middleware.js
+++ packages/oauth2-client/middleware.js
@@ -1,57 +1,58 @@
-//const logger = require('heimdalljs-logger');
 const koaJSONBody = require('koa-json-body');
+const Router = require('koa-better-router');
 const Error = require('@cardstack/plugin-utils/error');
 const https = require('https');
 
 module.exports = function() {
-  //let log = logger('oauth2-client');
-  let bodyParser = koaJSONBody({ limit: '1mb' });
+  let router = new Router();
 
-  return async function(ctxt) {
-    ctxt.response.set('Access-Control-Allow-Origin', '*');
-    if (ctxt.request.method === 'OPTIONS') {
+  router.addRoute('OPTIONS', '/', [
+    async function(ctxt) {
+      ctxt.response.set('Access-Control-Allow-Origin', '*');
       ctxt.response.set('Access-Control-Allow-Methods', 'POST,OPTIONS');
       ctxt.response.set('Access-Control-Allow-Headers', 'Content-Type');
       ctxt.status = 200;
-      return;
     }
+  ]);
 
-    await bodyParser(ctxt, err => {
-      if (err) {
-        throw err;
-      }
-    });
-    let body = ctxt.request.body;
+  router.addRoute('POST', '/', [
+    koaJSONBody({ limit: '1mb' }),
+    async function(ctxt) {
+      ctxt.response.set('Access-Control-Allow-Origin', '*');
+      let body = ctxt.request.body;
 
-    if (!body.authorizationCode) {
-      ctxt.response.set('Content-Type', 'application/json');
-      ctxt.status = 400;
-      ctxt.body = { errors: [ new Error("missing required field 'authorizationCode'", { status: 400 }) ] };
-      return;
-    }
-    let payload = {
-      client_id: '2680c97f309a904b41b0',
-      client_secret: process.env.GITHUB_CLIENT_SECRET,
-      code: ctxt.request.body.authorizationCode
-    };
-    if (body.state) {
-      payload.state = body.state;
-    }
-    let data = JSON.stringify(payload);
-    let options = {
-      hostname: 'github.com',
-      port: 443,
-      path: '/login/oauth/access_token',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(data),
-        'Accept': 'application/json',
-        'User-Agent': '@cardstack/oauth2-client'
+      if (!body.authorizationCode) {
+        ctxt.response.set('Content-Type', 'application/json');
+        ctxt.status = 400;
+        ctxt.body = { errors: [ new Error("missing required field 'authorizationCode'", { status: 400 }) ] };
+        return;
       }
-    };
-    await forwardHttpsRequest(ctxt, options, data);
-  };
+      let payload = {
+        client_id: '2680c97f309a904b41b0',
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code: ctxt.request.body.authorizationCode
+      };
+      if (body.state) {
+        payload.state = body.state;
+      }
+      let data = JSON.stringify(payload);
+      let options = {
+        hostname: 'github.com',
+        port: 443,
+        path: '/login/oauth/access_token',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(data),
+          'Accept': 'application/json',
+          'User-Agent': '@cardstack/oauth2-client'
+        }
+      };
+      await forwardHttpsRequest(ctxt, options, data);
+    }
+  ]);
+
+  return router.middleware();
 };
 
 function forwardHttpsRequest(ctxt, options, data) {
