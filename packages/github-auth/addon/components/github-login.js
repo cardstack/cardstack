ebb03d8db7f9bc392ed9b326d716f7cb4ee0500c
Edward Faulkner <ef@alum.mit.edu>
2017-04-19 17:08:34 -0400

    refactor for clarity

--- packages/github-auth/addon/components/github-login.js
+++ packages/github-auth/addon/components/github-login.js
@@ -8,20 +8,23 @@ export default Ember.Component.extend({
   session: Ember.inject.service(),
   torii: Ember.inject.service(),
 
+  _extendToriiProviders(newConfig) {
+    let toriiConfig = Object.assign({}, getConfiguration());
+    if (!toriiConfig.providers) {
+      toriiConfig.providers = {};
+    }
+    Object.assign(toriiConfig.providers, newConfig)
+    configure(toriiConfig);
+  },
+
   actions: {
     login() {
-      let toriiConfig = Object.assign({}, getConfiguration());
-      if (!toriiConfig.providers) {
-        toriiConfig.providers = {};
-      }
-      Object.assign(toriiConfig.providers, {
+      this._extendToriiProviders({
         'github-oauth2': {
           apiKey: '2680c97f309a904b41b0',
-          redirectUri: 'http://localhost:4200',
-          scope: 'user:email',
+          scope: 'user:email'
         }
-      })
-      configure(toriiConfig);
+      });
       this.get('torii').open('github').then(({ authorizationCode }) => {
         // TODO this "github" should actually be the configurable
         // server authentication source id
