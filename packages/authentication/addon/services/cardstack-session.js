4817bf0d7e6a5c251774951b5a87f16549c7f08b
Edward Faulkner <ef@alum.mit.edu>
2017-04-19 13:45:08 -0400

    splitting more code between general authentication and github-auth

--- packages/authentication/addon/services/cardstack-session.js
+++ packages/authentication/addon/services/cardstack-session.js
@@ -3,6 +3,7 @@
 
 import Ember from 'ember';
 import { singularize } from 'ember-inflector';
+import fetch from 'fetch';
 
 export default Ember.Service.extend({
   session: Ember.inject.service(),
@@ -21,5 +22,19 @@ export default Ember.Service.extend({
         return this.get('store').peekRecord(singularize(rawSession.userDocument.data.type), rawSession.userDocument.data.id);
       }
     }
-  })
+  }),
+
+  // authentication plugins should call this as their bridge to the
+  // server side. authenticationSource is the name of the configured authentication-source
+  authenticate(authenticationSource, payload) {
+    let config = Ember.getOwner(this).resolveRegistration('config:environment');
+    let tokenExchangeUri = config.cardstack.apiURL + '/auth/' + authenticationSource;
+    return fetch(tokenExchangeUri, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload)
+    }).then(response => response.json());
+  }
 });
--- packages/authentication/addon/torii-authenticator.js
+++ packages/authentication/addon/torii-authenticator.js
@@ -1,28 +1,19 @@
 import Ember from 'ember';
 import ToriiAuthenticator from 'ember-simple-auth/authenticators/torii';
-import fetch from 'fetch';
 
 export default ToriiAuthenticator.extend({
   torii: Ember.inject.service(),
+  cardstackSession: Ember.inject.service(),
 
   authenticate() {
-    let config = Ember.getOwner(this).resolveRegistration('config:environment');
-    let tokenExchangeUri = config.cardstack.tokenURL;
-    return this._super(...arguments).then((data) => {
-      return fetch(tokenExchangeUri, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          authorizationCode: data.authorizationCode
-        })
-      }).then(response => response.json()).then(response => {
-        return {
-          userDocument: response,
-          provider: data.provider
-        };
-      });
+    return this._super(...arguments).then(data => {
+      return this.get('cardstackSession').authenticate('github', data)
+        .then(userDocument => {
+          return {
+            userDocument,
+            provider: data.provider
+          };
+        });
     });
   }
 });
