54bd26f30e8ed1d21c161ca797297d16deb33342
Edward Faulkner <ef@alum.mit.edu>
2017-04-19 21:56:50 -0400

    improve publish script

--- scripts/publish-all.sh
+++ scripts/publish-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in `find packages -name package.json`; do
+for f in `find packages -name package.json -maxdepth 2`; do
     pushd `dirname $f`;
     npm publish --access=public;
     popd;
