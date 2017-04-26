c38c2ea1ca71e7e17de238a464ee0915de5a0359
Edward Faulkner <ef@alum.mit.edu>
2017-04-26 09:31:28 -0400

    admin menu markup

---
+++ packages/tools/addon/components/cs-admin-menu.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../templates/components/cs-admin-menu';
+
+export default Ember.Component.extend({
+  layout,
+  classNames: ['cs-admin-menu']
+});
--- packages/tools/tests/integration/components/cs-admin-launcher-test.js
+++ packages/tools/tests/integration/components/cs-admin-launcher-test.js
@@ -1,4 +1,5 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('cs-admin-launcher', 'Integration | Component | cs admin launcher', {
@@ -8,4 +9,13 @@ moduleForComponent('cs-admin-launcher', 'Integration | Component | cs admin laun
 test('it renders', function(assert) {
   this.render(hbs`{{cs-admin-launcher}}`);
   assert.equal(this.$('svg').length, 1, 'found icon');
+});
+
+
+test('it opens menu', function(assert) {
+  this.render(hbs`{{#toolbar-manager}}{{cs-admin-launcher}}{{/toolbar-manager}}`);
+  Ember.run(() => {
+    this.$('svg').click();
+  });
+  assert.equal(this.$('.cs-admin-menu').length, 1, 'found menu');
 });
