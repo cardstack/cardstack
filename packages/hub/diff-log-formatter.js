202cfa43f40ba612f5184719f184ad893617b980
Edward Faulkner <ef@alum.mit.edu>
2017-05-25 17:13:29 -0400

    improved log formatters

--- packages/hub/diff-log-formatter.js
+++ packages/hub/diff-log-formatter.js
@@ -1,9 +1,15 @@
 const createDebug = require('debug');
 const jsondiffpatch = require('jsondiffpatch').create();
 
-createDebug.formatters.p = (v) => {
+function format(v) {
   if (!v) {
     return '[invalid input to diff-log-formatter]';
   }
   return JSON.stringify(jsondiffpatch.diff(v.left, v.right));
-};
+}
+format.isDiffLogFormatter = true;
+
+if (createDebug.formatters.p && !createDebug.formatters.p.isDiffLogFormatter) {
+  throw new Error("namespace collision in log formatters for %p");
+}
+createDebug.formatters.p = format;
---
+++ packages/hub/table-log-formatter.js
@@ -0,0 +1,36 @@
+const createDebug = require('debug');
+
+if (createDebug.formatters.t && !createDebug.formatters.t.isTableLogFormatter) {
+  throw new Error("namespace collision in log formatters for %t");
+}
+createDebug.formatters.t = format;
+
+function format (v){
+  if (!v) {
+    return 'none';
+  }
+  if (typeof v === 'function') {
+    v = v();
+  }
+  let colWidths = [];
+  for (let row of v) {
+    for (let [colIndex, col] of row.entries()) {
+      colWidths[colIndex] = Math.max(colWidths[colIndex] || 0, String(col).length);
+    }
+  }
+  let output = '';
+  for (let row of v) {
+    for (let [colIndex, col] of row.entries()) {
+      let s = String(col);
+      output += s;
+      for (let i = s.length; i < colWidths[colIndex]; i++) {
+        output += ' ';
+      }
+      output += ' ';
+    }
+    output += "\n";
+  }
+  return output;
+}
+
+format.isTableLogFormatter = true;
