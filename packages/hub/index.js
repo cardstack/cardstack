71ab87209c6a87d121d2ccc39fe128142a826533
Edward Faulkner <ef@alum.mit.edu>
2017-06-28 16:52:54 -0400

    build separate bundles for generated code, to support hot loading

--- packages/hub/index.js
+++ packages/hub/index.js
@@ -3,8 +3,10 @@ const path = require('path');
 const crypto = require('crypto');
 const fs = require('fs');
 const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
 const log = require('@cardstack/plugin-utils/logger')('hub/main');
 const BroccoliConnector = require('./broccoli-connector');
+const buildBranches = require('./branch-builder');
 
 // TODO: move into configuration
 const defaultBranch = 'master';
@@ -94,7 +96,9 @@ module.exports = {
       return;
     }
 
-    return new Funnel(this._broccoliConnector.tree, { include: ['.cardstack-build'] });
+    let nonce = new Funnel(this._broccoliConnector.tree, { include: ['.cardstack-build'] });
+    let branchBuilds = buildBranches(this._broccoliConnector.tree, { compiler: this.compileAddon.bind(this) });
+    return mergeTrees([ nonce, branchBuilds ], { annotation: 'cardstack-hub-public' });
   },
 
   // The serverMiddleware hook is well-behaved and will wait for us to
