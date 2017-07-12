fc1950eaca596b8b4db7ac1863fdff02708250bf
Edward Faulkner <ef@alum.mit.edu>
2017-07-12 15:56:06 -0400

    add some initial models to dev mode

--- packages/models/tests/dummy/cardstack/seeds/development/ephemeral.js
+++ packages/models/tests/dummy/cardstack/seeds/development/ephemeral.js
@@ -1,4 +1,25 @@
 /* eslint-env node */
+const JSONAPIFactory = require('@cardstack/test-support/jsonapi-factory');
+
+function initialModels() {
+  let initial = new JSONAPIFactory();
+
+  initial.addResource('content-types', 'posts')
+    .withRelated('fields', [
+      initial.addResource('fields', 'title').withAttributes({
+        fieldType: '@cardstack/core-types::string'
+      })
+    ]);
+  initial.addResource('posts', '1')
+    .withAttributes({
+      title: 'hello world'
+    });
+  initial.addResource('posts', '2')
+    .withAttributes({
+      title: 'second'
+    });
+  return initial.getModels();
+}
 
 module.exports = [
   {
@@ -12,7 +33,10 @@ module.exports = [
     type: 'data-sources',
     id: 'default',
     attributes: {
-      'source-type': '@cardstack/ephemeral'
+      'source-type': '@cardstack/ephemeral',
+      params: {
+        initialModels: initialModels()
+      }
     }
   },
   {
