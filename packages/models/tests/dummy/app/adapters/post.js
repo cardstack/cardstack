20e087cda5bf8a8d4a9fe7e8fbac8ad1525482b1
Edward Faulkner <ef@alum.mit.edu>
2017-07-12 11:22:06 -0400

    restoring these to keep the tests passing
    will eventually remove them, but not ready yet

---
+++ packages/models/tests/dummy/app/adapters/post.js
@@ -0,0 +1,5 @@
+import Adapter from '@cardstack/models/adapter';
+
+export default Adapter.extend({
+  namespace: 'cardstack/api'
+});
---
+++ packages/models/tests/dummy/app/serializers/post.js
@@ -0,0 +1,4 @@
+import Serializer from '@cardstack/models/serializer';
+
+export default Serializer.extend({
+});
