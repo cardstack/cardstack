af49dca04ca42675c4479c9a3dd6a52e447fd9a2
Edward Faulkner <ef@alum.mit.edu>
2017-07-15 17:14:45 -0400

    expose hub's middleware hooks to parent app
    ember-cli does not treat these as recursive by default

--- packages/test-support/index.js
+++ packages/test-support/index.js
@@ -2,5 +2,13 @@ module.exports = {
   name: '@cardstack/test-support',
   isDevelopingAddon() {
     return process.env.CARDSTACK_DEV;
+  },
+  serverMiddleware({ app }) {
+    let hub = this.addons.find(a => a.name === '@cardstack/hub');
+    return hub.serverMiddleware({ app });
+  },
+  testemMiddleware(app) {
+    let hub = this.addons.find(a => a.name === '@cardstack/hub');
+    return hub.testemMiddleware(app);
   }
 };
