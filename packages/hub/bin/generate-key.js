5e5ce4f75db6c9cf4dccdec3f1410252503ee150
Aaron Sikes <aaron@sikes.io>
2017-07-21 13:31:31 -0400

    Optionally accept session key as a file path instead

--- packages/hub/bin/generate-key.js
+++ packages/hub/bin/generate-key.js
@@ -1,3 +1,3 @@
 const crypto = require('crypto');
 let key = crypto.randomBytes(32);
-process.stdout.write(`CARDSTACK_SESSIONS_KEY=${key.toString('base64')}\n`);
+process.stdout.write(key.toString('base64'));
