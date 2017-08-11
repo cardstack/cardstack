b26685df2ed84c9e47718c18b0760e6e1888d6a8
Edward Faulkner <ef@alum.mit.edu>
2017-08-11 16:09:49 -0400

    partial implementation of postgres writer

---
+++ packages/postgresql/node-tests/writer-test.js
@@ -0,0 +1,113 @@
+const {
+  createDefaultEnvironment,
+  destroyDefaultEnvironment
+} = require('@cardstack/test-support/env');
+const { Client } = require('pg');
+const JSONAPIFactory = require('@cardstack/test-support/jsonapi-factory');
+
+describe('postgresql/writer', function() {
+  let pgClient, client, env, writer;
+
+  beforeEach(async function() {
+    pgClient = new Client({ database: 'postgres' });
+    await pgClient.connect();
+    await pgClient.query(`create database test1`);
+
+    client = new Client({ database: 'test1' });
+    await client.connect();
+    await client.query('create table articles (id serial primary key, title varchar, length integer, published boolean)');
+    await client.query('insert into articles values ($1, $2, $3, $4)', ['0', 'hello world', 100, true]);
+
+    let factory = new JSONAPIFactory();
+
+    factory.addResource('plugin-configs').withAttributes({
+      module: '@cardstack/postgresql'
+    });
+
+    factory.addResource('data-sources')
+      .withAttributes({
+        'source-type': '@cardstack/postgresql',
+        params: {
+          branches: {
+            master: {
+              database: 'test1'
+            }
+          }
+        }
+      });
+
+
+    env = await createDefaultEnvironment(`${__dirname}/..`, factory.getModels());
+    await env.lookup('hub:indexers').update({ realTime: true });
+    writer = env.lookup('hub:writers');
+  });
+
+  afterEach(async function() {
+    await destroyDefaultEnvironment(env);
+    await client.end();
+    await pgClient.query(`select pg_drop_replication_slot(slot_name) from pg_replication_slots;`);
+    await pgClient.query(`drop database test1`);
+    await pgClient.end();
+  });
+
+  it('can create new record', async function() {
+    let created = await writer.create('master', env.session, 'articles', {
+      type: 'articles',
+      attributes: {
+        title: 'I was created',
+        length: 200,
+        published: false
+      }
+    });
+    let result = await client.query('select title, length, published from articles where id=$1', [created.id]);
+    expect(result.rows).has.length(1);
+    expect(result.rows[0].title).to.equal('I was created');
+    expect(result.rows[0].length).to.equal(200);
+    expect(result.rows[0].published).to.equal(false);
+  });
+
+  it('can create new record with user-provided id', async function() {
+    let created = await writer.create('master', env.session, 'articles', {
+      type: 'articles',
+      id: '42',
+      attributes: {
+        title: 'I was created',
+        length: 200,
+        published: false
+      }
+    });
+    expect(created.id).to.equal('42');
+    let result = await client.query('select title, length, published from articles where id=$1', ['42']);
+    expect(result.rows).has.length(1);
+    expect(result.rows[0].title).to.equal('I was created');
+    expect(result.rows[0].length).to.equal(200);
+    expect(result.rows[0].published).to.equal(false);
+  });
+
+  it('can update a record', async function() {
+    await writer.update('master', env.session, 'articles', '0', {
+      type: 'articles',
+      id: '0',
+      attributes: {
+        length: 101
+      }
+    });
+    let result = await client.query('select title, length, published from articles where id=$1', ['0']);
+    expect(result.rows).has.length(1);
+    expect(result.rows[0].title).to.equal('hello world');
+    expect(result.rows[0].length).to.equal(101);
+  });
+
+  it('returns full record from update', async function() {
+    let updated = await writer.update('master', env.session, 'articles', '0', {
+      type: 'articles',
+      id: '0',
+      attributes: {
+        length: 101
+      }
+    });
+    expect(updated).has.deep.property('attributes.length', 101);
+    expect(updated).has.deep.property('attributes.title', 'hello world');
+  });
+
+});
--- packages/postgresql/writer.js
+++ packages/postgresql/writer.js
@@ -1,7 +1,13 @@
+const PendingChange = require('@cardstack/plugin-utils/pending-change');
+const Error = require('@cardstack/plugin-utils/error');
+const { Pool } = require('pg');
+const { range }  = require('lodash');
+
+const safeIdentifier = /^[a-zA-Z0-9._]+$/;
+const pendingChanges = new WeakMap();
+
 /*
 const logger = require('@cardstack/plugin-utils/logger');
-const Error = require('@cardstack/plugin-utils/error');
-const PendingChange = require('@cardstack/plugin-utils/pending-change');
 */
 
 module.exports = class Writer {
@@ -10,16 +16,111 @@ module.exports = class Writer {
   }
   constructor({ branches }) {
     this.branchConfig = branches;
+    this.pools = Object.create(null);
   }
 
-  async prepareCreate(/* branch, session, type, document, isSchema */) {
-
+  _getPool(branch) {
+    if (!this.pools[branch]) {
+      let config = this.branchConfig[branch];
+      this.pools[branch] = new Pool({
+        user: config.user,
+        host: config.host,
+        database: config.database,
+        password: config.password,
+        port: config.port
+      });
+    }
+    return this.pools[branch];
   }
 
-  async prepareUpdate(/* branch, session, type, id, document, isSchema */) {
+  async teardown() {
+    for (let pool of Object.values(this.pools)) {
+      await pool.end();
+    }
+  }
+
+  async prepareCreate(branch, session, type, document, isSchema) {
+    let { tableName, args, columns } = this._prepareQuery(isSchema, branch, type, document);
+    if (document.id != null) {
+      args.push(document.id);
+      columns.push('id');
+    }
+    let placeholders = range(1, args.length + 1).map(n => `$${n}`).join(',');
+    let client = await this._getPool(branch).connect();
+    try {
+      await client.query('begin');
+      let result = await client.query(`insert into ${tableName} (${columns.join(',')}) values (${placeholders}) returning id`, args);
+      let finalDocument = {
+        id: String(result.rows[0].id),
+        type,
+        attributes: document.attributes
+      };
+      let change = new PendingChange(null, finalDocument, finalize, abort);
+      pendingChanges.set(change, { client });
+      return change;
+    } catch (err) {
+      await client.release();
+      throw err;
+    }
+  }
+
+  async prepareUpdate(branch, session, type, id, document, isSchema) {
+    let { tableName, args, columns } = this._prepareQuery(isSchema, branch, type, document);
+    let client = await this._getPool(branch).connect();
+    args.push(id);
+    try {
+      await client.query('begin');
+      let result = await client.query(`update ${tableName} set ${columns.map((name,index) => `${name}=$${index+1}`).join(',')} where id=$${args.length} returning *`, args);
+      let finalDocument = {
+        id: String(result.rows[0].id),
+        type,
+        attributes: document.attributes
+      };
+      let change = new PendingChange(null, finalDocument, finalize, abort);
+      pendingChanges.set(change, { client });
+      return change;
+    } catch (err) {
+      await client.release();
+      throw err;
+    }
 
   }
 
   async prepareDelete(/* branch, session, version, type, id, isSchema */) {
   }
+
+  _prepareQuery(isSchema, branch, type, document) {
+    if (isSchema) {
+      throw new Error("The @cardstack/postgresql data source does not support schema creation", { status: 400 });
+    }
+    if (!this.branchConfig[branch]) {
+      throw new Error(`No such configured branch ${branch}`, { status: 400 });
+    }
+
+    let tableName = type;
+    if (!safeIdentifier.test(tableName)) {
+      throw new Error(`Disallowed table name ${tableName}`);
+    }
+    let args = [];
+    let columns = Object.entries(document.attributes).map(([key, value]) => {
+      if (!safeIdentifier.test(key)) {
+        throw new Error(`Disallowed column name ${key}`);
+      }
+      args.push(value);
+      return key;
+    });
+    return { tableName, args, columns };
+  }
 };
+
+async function finalize(pendingChange) {
+  let { client } = pendingChanges.get(pendingChange);
+  await client.query('commit');
+  await client.release();
+}
+
+async function abort(pendingChange) {
+  let { client } = pendingChanges.get(pendingChange);
+  await client.query('rollback');
+  await client.release();
+}
