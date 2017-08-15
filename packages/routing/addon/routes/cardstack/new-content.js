9dce569e1e2b1a6b0ebbef85da4bb65454eb6e16
Edward Faulkner <ef@alum.mit.edu>
2017-08-15 01:43:19 -0400

    routing package should not depend on any of this

--- packages/routing/addon/routes/cardstack/new-content.js
+++ packages/routing/addon/routes/cardstack/new-content.js
@@ -3,11 +3,8 @@ import { singularize }  from 'ember-inflector';
 
 export default Ember.Route.extend({
   cardstackRouting: Ember.inject.service(),
-  resourceMetadata: Ember.inject.service(),
   model({ type }) {
-    let branch = this.modelFor('cardstack').branch;
-    let model = this.store.createRecord(singularize(type));
-    this.get('resourceMetadata').write(model, { branch });
-    return model;
+    // let branch = this.modelFor('cardstack').branch;
+    return this.store.createRecord(singularize(type));
   }
 });
