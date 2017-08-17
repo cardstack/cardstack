109bb8193c19c4d658301eb3264380f0faf76330
Edward Faulkner <ef@alum.mit.edu>
2017-08-17 02:12:06 -0400

    progress on search package

--- packages/search/addon/components/cardstack-search.js
+++ packages/search/addon/components/cardstack-search.js
@@ -1,16 +1,25 @@
 import Ember from 'ember';
 import layout from '../templates/components/cardstack-search';
 import { task } from 'ember-concurrency';
+import { singularize } from 'ember-inflector';
 
 export default Ember.Component.extend({
   layout,
+  tagName: '',
   store: Ember.inject.service(),
   search: task(function * () {
-    if (!this.get('query')) {
+    let query = this.get('query');
+    if (!query) {
       this.set('items', []);
       return;
     }
-    let records = yield this.get('store').query('pokemon', this.get('query'));
-    this.set('items', records);
+    if (!query.type) {
+      Ember.warn("cardstack-search queries must have a `type`", false, { id: 'cardstack-search-needs-type' });
+      this.set('items', []);
+    } else {
+      let restQuery = Object.assign({}, query, { type: undefined });
+      let records = yield this.get('store').query(singularize(query.type), restQuery);
+      this.set('items', records);
+    }
   }).observes('query').on('init')
 });
