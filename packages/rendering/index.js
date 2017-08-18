a69d5c5aabfdf300ad0b39a2031cd04cc9c77086
Edward Faulkner <ef@alum.mit.edu>
2017-08-18 16:05:31 -0400

    adding a basic AST transform for annotating cardstack fields

--- packages/rendering/index.js
+++ packages/rendering/index.js
@@ -2,5 +2,16 @@
 'use strict';
 
 module.exports = {
-  name: '@cardstack/rendering'
+  name: '@cardstack/rendering',
+
+  included(app){
+    while (app.app) {
+      app = app.app;
+    }
+    app.registry.add("htmlbars-ast-plugin", {
+      name: "cardstack-transform",
+      plugin: require("./lib/transform"),
+      baseDir() { return __dirname; }
+    });
+  }
 };
---
+++ packages/rendering/lib/transform.js
@@ -0,0 +1,44 @@
+/* eslint-env node */
+"use strict";
+
+/*
+  This is the beginning of an experimental code transform for cardstack templates.
+
+  It runs on any template that appears under
+  templatse/components/cardstack. It transforms direct property
+  bindings like:
+
+    {{content.title}}
+
+  Into simple cs-field calls like:
+
+    {{cs-field content "title"}}
+
+  In order to annotate those fields for editable by the cardstack tools.
+
+*/
+
+function BindTransform({ moduleName }) {
+  this.moduleName = moduleName;
+  this.syntax = null;
+}
+
+BindTransform.prototype.transform = function(ast) {
+  if (/\btemplates\/components\/cardstack\//.test(this.moduleName)){
+    console.log("Rewriting inside " + this.moduleName);
+    var b = this.syntax.builders;
+
+    this.syntax.traverse(ast, {
+      MustacheStatement(node) {
+        if (node.path.parts.length === 2 && node.path.parts[0] === 'content') {
+          return b.mustache(b.path("cs-field"), [b.path("content"), b.string(node.path.parts[1])]);
+        }
+      }
+    });
+  } else {
+    console.log("skipping " + this.moduleName);
+  }
+  return ast;
+};
+
+module.exports = BindTransform;
