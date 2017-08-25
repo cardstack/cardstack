c96dfbfee5cf5fb708ce4ec7dcefb40f5fe7738f
Balint Erdi <balint.erdi@gmail.com>
2017-08-25 12:36:52 +0200

    Add per-category counters to workflow service

--- packages/workflow/addon/services/cardstack-workflow.js
+++ packages/workflow/addon/services/cardstack-workflow.js
@@ -1,5 +1,32 @@
 import Ember from 'ember';
 
+export const REQUEST_TO_PUBLISH_LIVE = 'Request to publish live';
+export const READY_FOR_COPYEDITING = 'Ready for copyediting';
+export const COURSE_INFORMATION_SYNCED = 'Course information synced';
+
+const categories = [
+  REQUEST_TO_PUBLISH_LIVE,
+  READY_FOR_COPYEDITING,
+  COURSE_INFORMATION_SYNCED
+];
+
 export default Ember.Service.extend({
   notificationCount: 0,
+  isOpen: false,
+  items: Ember.A(),
+
+  notificationCount: Ember.computed('items.@each.isHandled', function() {
+    return this.get('items').filterBy('isHandled', false).length;
+  }),
+
+  changesByCategory: Ember.computed('items.@each.{isHandled,category}', function() {
+    return this.get('items').reduce((changes, change) => {
+      let category = Ember.get(change, 'category');
+      if (!changes[category]) {
+        changes[category] = 0;
+      }
+      changes[category] += Ember.get(change, 'isHandled') ? 0 : 1;
+      return changes;
+    }, {});
+  }),
 });
--- packages/workflow/tests/unit/services/cardstack-workflow-test.js
+++ packages/workflow/tests/unit/services/cardstack-workflow-test.js
@@ -1,12 +1,42 @@
+import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
+import {
+  REQUEST_TO_PUBLISH_LIVE,
+  READY_FOR_COPYEDITING,
+  COURSE_INFORMATION_SYNCED
+} from '@cardstack/workflow/services/cardstack-workflow';
 
-moduleFor('service:cardstack-workflow', 'Unit | Service | cardstack workflow', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+moduleFor('service:cardstack-workflow', 'Unit | Service | cardstack-workflow', {
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let service = this.subject();
-  assert.ok(service);
+test('it gets notification count from unhandled items', function(assert) {
+  let items = Ember.A([
+    { status: 'pending', isHandled: false },
+    { status: 'approved', isHandled: true },
+    { status: 'denied', isHandled: false },
+  ]);
+  let service = this.subject({
+    items
+  });
+  assert.equal(service.get('notificationCount'), 2);
+});
+
+test('it extracts categories from the items', function(assert) {
+  let items = Ember.A([
+    { category: REQUEST_TO_PUBLISH_LIVE, isHandled: false },
+    { category: REQUEST_TO_PUBLISH_LIVE, isHandled: false },
+    { category: REQUEST_TO_PUBLISH_LIVE, isHandled: true },
+    { category: READY_FOR_COPYEDITING, isHandled: false },
+    { category: READY_FOR_COPYEDITING, isHandled: true },
+    { category: COURSE_INFORMATION_SYNCED, isHandled: true },
+    { category: COURSE_INFORMATION_SYNCED, isHandled: true },
+  ]);
+  let service = this.subject({
+    items
+  });
+  let expected = {};
+  expected[REQUEST_TO_PUBLISH_LIVE] = 2;
+  expected[READY_FOR_COPYEDITING] = 1;
+  expected[COURSE_INFORMATION_SYNCED] = 0;
+  assert.deepEqual(service.get('changesByCategory'), expected);
 });
