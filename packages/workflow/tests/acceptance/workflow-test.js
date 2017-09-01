3d23c3edf66ed6b0ac598491d42e15017d108c22
Balint Erdi <balint.erdi@gmail.com>
2017-09-01 15:25:38 +0200

    Remove async weirdness from workflow acceptance test

--- packages/workflow/tests/acceptance/workflow-test.js
+++ packages/workflow/tests/acceptance/workflow-test.js
@@ -1,12 +1,5 @@
-import Ember from 'ember';
 import { test } from 'qunit';
 import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
-
-function waitFor(time) {
-  return new Ember.RSVP.Promise((resolve) => {
-    setTimeout(resolve, time);
-  });
-}
 
 moduleForAcceptance('Acceptance | Workflow');
 
@@ -17,12 +10,11 @@ test('The first-level workflow sidebar', function(assert) {
     assertTrimmedText(assert, '[data-test-total-notification-count]', "3");
   });
 
-  click('.cardstack-workflow-launcher')
-    // This is a hack to work around a probable timing issue in ember-toolbars
-    .then(() => waitFor(2000))
-    .then(() => {
-      assertTrimmedText(assert, '[data-test-tag-counter="Request to publish live"]', "2");
-      assertTrimmedText(assert, '[data-test-tag-counter="Ready for copyediting"]', "1");
-      assertTrimmedText(assert, '[data-test-tag-counter="Course information synced"]', "0");
-    })
+  click('.cardstack-workflow-header');
+
+  andThen(function() {
+    assertTrimmedText(assert, '[data-test-tag-counter="Request to publish live"]', "2");
+    assertTrimmedText(assert, '[data-test-tag-counter="Ready for copyediting"]', "1");
+    assertTrimmedText(assert, '[data-test-tag-counter="Course information synced"]', "0");
+  });
 });
