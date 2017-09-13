b97ae91ec5c92eec7d16dc2dbeb7138acf8ad14c
Balint Erdi <balint.erdi@gmail.com>
2017-09-13 12:40:07 +0200

    Remove unused model data in route from dummy app

--- packages/workflow/tests/dummy/app/routes/index.js
+++ packages/workflow/tests/dummy/app/routes/index.js
@@ -1,20 +1,4 @@
 import Ember from 'ember';
-import {
-  REQUEST_TO_PUBLISH_LIVE,
-  READY_FOR_COPYEDITING,
-  COURSE_INFORMATION_SYNCED
-} from '@cardstack/workflow/services/cardstack-workflow';
 
 export default Ember.Route.extend({
-  model() {
-    return [
-      { tag: REQUEST_TO_PUBLISH_LIVE, isHandled: false },
-      { tag: REQUEST_TO_PUBLISH_LIVE, isHandled: false },
-      { tag: REQUEST_TO_PUBLISH_LIVE, isHandled: true },
-      { tag: READY_FOR_COPYEDITING, isHandled: false },
-      { tag: READY_FOR_COPYEDITING, isHandled: true },
-      { tag: COURSE_INFORMATION_SYNCED, isHandled: true },
-      { tag: COURSE_INFORMATION_SYNCED, isHandled: true },
-    ]
-  }
 });
