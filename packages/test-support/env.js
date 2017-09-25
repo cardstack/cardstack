61bb0af0ff642625be722cf828561292968243bd
Edward Faulkner <ef@alum.mit.edu>
2017-09-25 13:38:19 -0500

    cleanup if createDefaultEnvironment fails

--- packages/test-support/env.js
+++ packages/test-support/env.js
@@ -6,81 +6,93 @@ const { wireItUp } = require('@cardstack/hub/main');
 const Session = require('@cardstack/plugin-utils/session');
 
 exports.createDefaultEnvironment = async function(projectDir, initialModels = []) {
-  let factory = new JSONAPIFactory();
+  let container;
+  try {
+    let factory = new JSONAPIFactory();
 
-  let user = factory.addResource('users', 'the-default-test-user').withAttributes({
-    fullName: 'Default Test Environment',
-    email: 'test@example.com'
-  });
+    let user = factory.addResource('users', 'the-default-test-user').withAttributes({
+      fullName: 'Default Test Environment',
+      email: 'test@example.com'
+    });
 
-  let session = new Session(
-    { id: 'the-default-test-user', type: 'users'},
-    null,
-    user
-  );
-
-  factory.addResource('plugin-configs', '@cardstack/hub')
-    .withRelated(
-      'default-data-source',
-      factory.addResource('data-sources')
-        .withAttributes({
-          'source-type': '@cardstack/ephemeral'
-        })
+    let session = new Session(
+      { id: 'the-default-test-user', type: 'users'},
+      null,
+      user
     );
 
-  factory.addResource('plugin-configs', '@cardstack/ephemeral');
+    factory.addResource('plugin-configs', '@cardstack/hub')
+      .withRelated(
+        'default-data-source',
+        factory.addResource('data-sources')
+          .withAttributes({
+            'source-type': '@cardstack/ephemeral'
+          })
+      );
 
-  factory.addResource('grants')
-    .withAttributes({
-      mayCreateResource: true,
-      mayUpdateResource: true,
-      mayDeleteResource: true,
-      mayWriteField: true
-    }).withRelated('who', factory.addResource('groups', user.id));
+    factory.addResource('plugin-configs', '@cardstack/ephemeral');
 
-  let container = await wireItUp(projectDir, crypto.randomBytes(32), factory.getModels(), {
-    allowDevDependencies: true,
-    disableAutomaticIndexing: true
-  });
+    factory.addResource('grants')
+      .withAttributes({
+        mayCreateResource: true,
+        mayUpdateResource: true,
+        mayDeleteResource: true,
+        mayWriteField: true
+      }).withRelated('who', factory.addResource('groups', user.id));
 
-  let writers = container.lookup('hub:writers');
+    container = await wireItUp(projectDir, crypto.randomBytes(32), factory.getModels(), {
+      allowDevDependencies: true,
+      disableAutomaticIndexing: true
+    });
 
-  for (let model of initialModels) {
-    // TODO: this one-by-one creation is still slower than is nice for
-    // tests -- each time we write a schema model it invalidates the
-    // schema cache, which needs to get rebuilt before we can write
-    // the next one.
-    //
-    // On the other hand, this is a high-quality test of our ability
-    // to build up the entire state in the same way an end user would
-    // via JSONAPI requests. If we optimize this away, we should also
-    // add some tests like that that are similarly comprehensive.
-    await writers.create('master', session, model.type, model);
-  }
+    let writers = container.lookup('hub:writers');
 
-  await container.lookup('hub:indexers').update({ realTime: true });
-
-  Object.assign(container, {
-    session,
-    user: user.data,
-    async setUserId(id) {
-      let schema = await this.lookup('hub:schema-cache').schemaForControllingBranch();
-      let m = schema.plugins.lookupFeatureAndAssert('middleware', '@cardstack/test-support/authenticator');
-      m.userId = id;
+    for (let model of initialModels) {
+      // TODO: this one-by-one creation is still slower than is nice for
+      // tests -- each time we write a schema model it invalidates the
+      // schema cache, which needs to get rebuilt before we can write
+      // the next one.
+      //
+      // On the other hand, this is a high-quality test of our ability
+      // to build up the entire state in the same way an end user would
+      // via JSONAPI requests. If we optimize this away, we should also
+      // add some tests like that that are similarly comprehensive.
+      await writers.create('master', session, model.type, model);
     }
-  });
-  return container;
-};
 
-exports.destroyDefaultEnvironment = async function(env) {
-  if (env) {
-    await env.teardown();
-    await destroyIndices();
-    await temp.cleanup();
+    await container.lookup('hub:indexers').update({ realTime: true });
+
+    Object.assign(container, {
+      session,
+      user: user.data,
+      async setUserId(id) {
+        let schema = await this.lookup('hub:schema-cache').schemaForControllingBranch();
+        let m = schema.plugins.lookupFeatureAndAssert('middleware', '@cardstack/test-support/authenticator');
+        m.userId = id;
+      }
+    });
+    return container;
+  } catch (err) {
+    // don't leave a half-constructed environment lying around if we
+    // failed. Cleanup whatever parts were already created.
+    if (container) {
+      container.teardown();
+    }
+    destroyIndices();
+    temp.cleanup();
+    throw err;
   }
 };
 
-async function destroyIndices() {
-  let ea = new ElasticAssert();
-  await ea.deleteContentIndices();
-}
+  exports.destroyDefaultEnvironment = async function(env) {
+    if (env) {
+      await env.teardown();
+      await destroyIndices();
+      await temp.cleanup();
+    }
+  };
+
+  async function destroyIndices() {
+    let ea = new ElasticAssert();
+    await ea.deleteContentIndices();
+  }
