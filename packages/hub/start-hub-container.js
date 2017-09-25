c3abc04d1ebd5314aaea9e1bc2dc02d1b4e51c21
Aaron Sikes <aaron@sikes.io>
2017-09-25 11:32:34 -0400

    Build the cardstack-app image dynamically

--- packages/hub/start-hub-container.js
+++ packages/hub/start-hub-container.js
@@ -1,13 +1,16 @@
 const crypto = require('crypto');
 const path = require('path');
 const {promisify} = require('util');
-const execFile = promisify(require('child_process').execFile);
+const child_process = require('child_process');
+const {spawn} = child_process;
+const execFile = promisify(child_process.execFile);
 const realpath = promisify(require('fs').realpath);
 
 const Koa = require('koa');
 const proxy = require('koa-proxy');
 const nssocket = require('nssocket');
 const path_is_inside = require('path-is-inside');
+const Dockerfile = require('dockerfilejs').Dockerfile;
 const resolve = promisify(require('resolve'));
 const log = require('@cardstack/plugin-utils/logger')('hub/spawn-hub');
 
@@ -27,6 +30,8 @@ module.exports = async function(projectRoot) {
   }
 
   let key = crypto.randomBytes(32).toString('base64');
+
+  await buildAppImage();
 
   await execFile('docker', [
     'run',
@@ -61,6 +66,44 @@ module.exports = async function(projectRoot) {
   }));
   return app.callback();
 };
+
+async function buildAppImage() {
+  let proc = spawn('docker', [
+      'build',
+      '-t', 'cardstack-app',
+      '-f', '-',
+      '.'
+  ],{
+    cwd: '/Users/aaron/dev/basic-cardstack',
+    stdio: 'pipe'
+  });
+
+
+  let file = new Dockerfile();
+
+  file.from('cardstack/hub')
+    .workdir('/hub/app')
+    .copy({src: ['package.json', 'yarn.lock'], dest: '/hub/app/'})
+    .run('yarn install --frozen-lockfile')
+    .copy({src: '.', dest: '/hub/app'})
+    .env({
+      ELASTICSEARCH: 'http://elasticsearch:9200',
+      DEBUG: 'cardstack/*'
+    })
+    .cmd({command:'node', params: [
+      '/hub/app/node_modules/@cardstack/hub/bin/server.js',
+      '/hub/app/cardstack/seeds/development',
+      '-d',
+      '--containerized'
+    ]});
+
+  proc.stdin.end(file.render());
+
+  await new Promise(function(resolve, reject) {
+    proc.on('error', reject);
+    proc.on('exit', resolve);
+  });
+}
 
 async function linkedHubPath(projectRoot) {
   let hubPath = path.dirname(await resolve('@cardstack/hub/package.json', {basedir: projectRoot}));
