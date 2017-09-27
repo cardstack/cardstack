341a629e1e6c477984fc808c375b637919c9a6ae
Balint Erdi <balint.erdi@gmail.com>
2017-09-27 15:43:26 +0200

    Create list versions of domain-specific components

---
+++ packages/workflow/tests/dummy/app/components/cardstack/song-license-request-list-card.js
@@ -0,0 +1,16 @@
+import Component from "@ember/component";
+import { inject } from "@ember/service";
+
+export default Component.extend({
+  workflow: inject('cardstack-workflow'),
+
+  actions: {
+    approve() {
+      this.get('workflow').approveMessage(this.get('content'));
+    },
+    deny() {
+      this.get('workflow').denyMessage(this.get('content'));
+    }
+  }
+});
+
