9778c0f3b70417e8cfa1323a3af25bde3da433a9
Balint Erdi <balint.erdi@gmail.com>
2017-09-27 18:07:00 +0200

    Refactor accessing the message from domain-specific models

--- packages/workflow/tests/dummy/app/models/chat-message.js
+++ packages/workflow/tests/dummy/app/models/chat-message.js
@@ -1,20 +1,6 @@
 import ChatMessage from '@cardstack/models/generated/chat-message';
-import { computed } from '@ember/object';
+import hasMessage from 'dummy/utils/has-message';
 
 export default ChatMessage.extend({
-  message: computed(function() {
-    let messages = this.get('store').peekAll('message');
-    return messages.find((message) => {
-      return message.get('cardId') === this.get('id') &&
-        message.get('cardType') === 'chat-messages';
-     });
-  }),
-
-  // _loadMessage: task(function * () {
-  //   let message = yield this.get('store').queryRecord('message', {
-  //     cardId: this.get('id'),
-  //     cardType: 'song-change-requests'
-  //   });
-  //   this.set('message', message);
-  // }),
+  message: hasMessage('chat-messages'),
 })
--- packages/workflow/tests/dummy/app/models/song-change-request.js
+++ packages/workflow/tests/dummy/app/models/song-change-request.js
@@ -1,20 +1,6 @@
 import SongChangeRequest from '@cardstack/models/generated/song-change-request';
-import { computed } from '@ember/object';
+import hasMessage from 'dummy/utils/has-message';
 
 export default SongChangeRequest.extend({
-  message: computed(function() {
-    let messages = this.get('store').peekAll('message');
-    return messages.find((message) => {
-      return message.get('cardId') === this.get('id') &&
-        message.get('cardType') === 'song-change-requests';
-     });
-  }),
-
-  // _loadMessage: task(function * () {
-  //   let message = yield this.get('store').queryRecord('message', {
-  //     cardId: this.get('id'),
-  //     cardType: 'song-change-requests'
-  //   });
-  //   this.set('message', message);
-  // }),
+  message: hasMessage('song-change-requests'),
 })
