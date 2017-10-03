35421e665487ef51f1f9fe6b51c80a9abfc6764e
Edward Faulkner <ef@alum.mit.edu>
2017-10-03 15:30:33 -0400

    improving the github-auth tests/dummy app

---
+++ packages/github-auth/tests/dummy/cardstack/seeds/development/ephemeral.js
@@ -0,0 +1,24 @@
+/* eslint-env node */
+
+module.exports = [
+  {
+    type: 'plugin-configs',
+    id: '@cardstack/ephemeral'
+  },
+  {
+    type: 'data-sources',
+    id: 'default',
+    attributes: {
+      'source-type': '@cardstack/ephemeral'
+    }
+  },
+  {
+    type: 'plugin-configs',
+    id: '@cardstack/hub',
+    relationships: {
+      'default-data-source': {
+        data: { type: 'data-sources', id: 'default' }
+      }
+    }
+  }
+];
