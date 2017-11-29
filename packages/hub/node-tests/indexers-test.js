9df8469999714451ef2de9fe84c45de66d7fe25e
Edward Faulkner <ef@alum.mit.edu>
2017-11-29 14:55:46 -0500

    add a failing test

--- packages/hub/node-tests/indexers-test.js
+++ packages/hub/node-tests/indexers-test.js
@@ -6,40 +6,71 @@ const {
 describe('hub/indexers', function() {
   let env;
 
-  before(async function () {
+  async function setup () {
     env = await createDefaultEnvironment(`${__dirname}/../../../tests/stub-project`);
-  });
+  }
 
-  after(async function() {
+  async function teardown() {
     await destroyDefaultEnvironment(env);
+  }
+
+  describe('read-only', function() {
+    before(setup);
+    after(teardown);
+
+
+    it("indexes seed models", async function() {
+      // this seed model comes from createDefaultEnvironment
+      let response = await env.lookup('hub:searchers').search('master', { filter: { type: 'plugin-configs' }});
+      expect(response.data.map(m => m.id)).includes('@cardstack/hub');
+    });
+
+    it("indexes bootstrap models", async function() {
+      let response = await env.lookup('hub:searchers').search('master', { filter: { type: 'content-types' }});
+      expect(response.data.map(m => m.id)).includes('content-types');
+    });
+
+    it("indexes plugins", async function() {
+      let doc = await env.lookup('hub:searchers').get('master', 'plugins', 'sample-plugin-one');
+      expect(doc).is.ok;
+    });
+
+    it("includes features within plugins", async function() {
+      let doc = await env.lookup('hub:searchers').get('master', 'plugins', 'sample-plugin-one');
+      expect(doc).has.property('included');
+      expect(doc.included.map(r => r.id)).deep.equals(['sample-plugin-one::x']);
+    });
+
+    it("indexes plugin features", async function() {
+      let doc = await env.lookup('hub:searchers').get('master', 'field-types', 'sample-plugin-one::x');
+      expect(doc).is.ok;
+    });
   });
 
+  describe('read-write', function() {
+    beforeEach(setup);
+    afterEach(teardown);
 
-  it("indexes seed models", async function() {
-    // this seed model comes from createDefaultEnvironment
-    let response = await env.lookup('hub:searchers').search('master', { filter: { type: 'plugin-configs' }});
-    expect(response.data.map(m => m.id)).includes('@cardstack/hub');
+    it("indexes plugin-config changes", async function() {
+      // this test is deliberately writing directly to the ephemeral
+      // backend instead of going through hub:writeres. That ensures
+      // we aren't relying on side-effects from the writers.
+      let doc = await env.lookup('hub:searchers').get('master', 'plugins', 'sample-plugin-one');
+      expect(doc).has.deep.property('data.attributes.enabled', true);
+      let config = {
+        id: 'sample-plugin-one',
+        type: 'plugin-configs',
+        attributes: {
+          enabled: false
+        }
+      };
+      let schema = await env.lookup('hub:schema-cache').schemaForControllingBranch();
+      let source = [...schema.dataSources.values()].find(s => s.sourceType === '@cardstack/ephemeral');
+      let storage = await source.writer.storage();
+      storage.store(config.type, config.id, config, false, null);
+      await env.lookup('hub:indexers').update({ realTime: true });
+      doc = await env.lookup('hub:searchers').get('master', 'plugins', 'sample-plugin-one');
+      expect(doc).has.deep.property('data.attributes.enabled', false);
+    });
   });
-
-  it("indexes bootstrap models", async function() {
-    let response = await env.lookup('hub:searchers').search('master', { filter: { type: 'content-types' }});
-    expect(response.data.map(m => m.id)).includes('content-types');
-  });
-
-  it("indexes plugins", async function() {
-    let doc = await env.lookup('hub:searchers').get('master', 'plugins', 'sample-plugin-one');
-    expect(doc).is.ok;
-  });
-
-  it("includes features within plugins", async function() {
-    let doc = await env.lookup('hub:searchers').get('master', 'plugins', 'sample-plugin-one');
-    expect(doc).has.property('included');
-    expect(doc.included.map(r => r.id)).deep.equals(['sample-plugin-one::x']);
-  });
-
-  it("indexes plugin features", async function() {
-    let doc = await env.lookup('hub:searchers').get('master', 'field-types', 'sample-plugin-one::x');
-    expect(doc).is.ok;
-  });
-
 });
