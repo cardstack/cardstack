38fd00ecfeb389bb69723978830cf0b2e3c0278e
Balint Erdi <balint.erdi@gmail.com>
2017-12-07 16:04:11 +0100

    Fix handlebars expressions in cardstack components inside attribute assignments

--- packages/rendering/lib/transform.js
+++ packages/rendering/lib/transform.js
@@ -28,6 +28,33 @@ BindTransform.prototype.transform = function(ast) {
     var b = this.syntax.builders;
 
     this.syntax.traverse(ast, {
+      // BlockStatement(node) {
+      //   console.log('BLOCK', node);
+      //   return node;
+      // },
+      ElementNode(node) {
+        //FIXME: Go through all the attributes and stop at one
+        // which is of the type MustacheStatement
+        var tag = node.tag;
+        var value = node.attributes[0].value;
+        if (value.type === 'MustacheStatement') {
+          var path = value.path;
+          var parts = path.parts;
+          if (parts.length === 2 && parts[0] === 'content') {
+            var imageTag = b.element(tag, [
+              b.attr("src", b.mustache(b.path('url')))
+            ], []);
+            var block = b.program([imageTag], ['url']);
+            return b.block(b.path('cs-field'), [
+              b.path("content"), b.string(parts[1])
+            ], b.hash(), block);
+          }
+        }
+      },
+      /*
+      AttrNode(node) {
+      },
+      */
       MustacheStatement(node) {
         if (node.path.parts.length === 2 && node.path.parts[0] === 'content') {
           return b.mustache(b.path("cs-field"), [b.path("content"), b.string(node.path.parts[1])]);
--- packages/rendering/node-tests/transform-test.js
+++ packages/rendering/node-tests/transform-test.js
@@ -6,4 +6,9 @@ describe('rendering/transform', function() {
     let result = processTemplate("{{content.foo}}", { moduleName });
     expect(result).to.equal('{{cs-field content "foo"}}');
   });
+
+  it('it rewrites attribute assignments', function() {
+    let result = processTemplate("<img src={{content.imageUrl}} />", { moduleName });
+    expect(result).to.equal('{{#cs-field content "imageUrl" as |url|}}<img src={{url}}></img>{{/cs-field}}');
+  });
 });
