01c770af4cb3a2ef2888679f9de7de948eeaa92d
Aaron Sikes <aaron@sikes.io>
2017-12-11 10:10:41 -0500

    Just remove plugin-utils/logger

--- packages/plugin-utils/logger.js
+++
@@ -1,5 +0,0 @@
-const logger = require('@cardstack/logger');
-
-module.exports = logger;
-
-logger('cardstack/logger').warn('@cardstack/plugin-utils/logger is deprecated - please use @cardstack/logger instead');
