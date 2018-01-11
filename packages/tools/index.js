7a1ada07babeb5102067e23f7229c2c0cb3fa6be
Edward Faulkner <ef@alum.mit.edu>
2018-01-11 17:40:22 -0500

    standardize svg handling
    now that we have two packages that want to provide svgs, we needed to do something smarter. I took the best community addon for svg packaging and extended it so it plays nicely with multiple simultaneous addons.

--- packages/tools/index.js
+++ packages/tools/index.js
@@ -20,21 +20,6 @@ module.exports = whenEnabled({
         ]
       }
     }
-  },
-
-  contentFor(type) {
-    if (type === 'body-footer') {
-      return `<script>
-  var ajax = new XMLHttpRequest();
-  ajax.open('GET', '/@cardstack/tools/symbols.svg', true);
-  ajax.send();
-  ajax.onload = function(e) {
-    var div = document.createElement('div');
-    div.innerHTML = ajax.responseText;
-    document.body.insertBefore(div, document.body.childNodes[0]);
-  };
-</script>`;
-
-    }
   }
+
 });
