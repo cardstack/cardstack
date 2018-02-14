e0db7d6bd70fa929e31eaff4203a8c8e7120918a
Edward Faulkner <ef@alum.mit.edu>
2018-02-14 02:36:03 -0500

    updating some test seeds to match new grant schema

--- packages/hub/blueprints/cardstack-hub-allow-all/files/cardstack/seeds/development/grant.js
+++ packages/hub/blueprints/cardstack-hub-allow-all/files/cardstack/seeds/development/grant.js
@@ -6,15 +6,19 @@ module.exports = [
     type: 'grants',
     id: 'wide-open',
     attributes: {
-      'may-read-fields': true,
       'may-write-fields': true,
+      'may-read-fields': true,
       'may-create-resource': true,
       'may-update-resource': true,
-      'may-delete-resource': true
+      'may-delete-resource': true,
+      'may-read-resource': true
     },
     relationships: {
       who: {
-        data: { type: 'groups', id: 'everyone' }
+        data: {
+          type: 'groups',
+          id: 'everyone'
+        }
       }
     }
   }
