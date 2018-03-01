39539222f00e5e8490d44bed079d44e85d3788eb
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2018-03-01 14:11:36 -0500

    truffle running with sample contract

---
+++ packages/ethereum/test/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": require.resolve("@cardstack/eslint-config/test"),
+  "globals": {
+    "web3": true,
+    "contract": true,
+    "artifacts": true,
+    "assert": true
+  },
+};
