8866cddfd6625c0ca5fb3efb88484e4f358ad4f2
Edward Faulkner <ef@alum.mit.edu>
2018-04-01 00:05:22 -0400

    update models package to ember & ember-cli 3.0

--- packages/models/tests/acceptance/show-post-test.js
+++ packages/models/tests/acceptance/show-post-test.js
@@ -1,29 +1,32 @@
-import { test } from 'qunit';
-import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { visit, currentURL } from '@ember/test-helpers';
 import Fixtures from '@cardstack/test-support/fixtures';
 
-moduleForAcceptance('Acceptance | show post', {
-  async beforeEach(){
+module('Acceptance | show post', function(hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(async function() {
     await scenario.setup();
-    await this.application.__container__.lookup('service:cardstack-codegen').refreshCode();
-  }
-});
+    await this.owner.lookup('service:cardstack-codegen').refreshCode();
+  });
 
-let scenario = new Fixtures('default', factory => {
-  factory.addResource('content-types', 'posts')
-    .withRelated('fields', [
-      factory.addResource('fields', 'title').withAttributes({
-        fieldType: '@cardstack/core-types::string'
-      })
-    ]);
-  factory.addResource('posts', '1')
-    .withAttributes({
-      title: 'hello world'
-    });
-});
+  let scenario = new Fixtures('default', factory => {
+    factory.addResource('content-types', 'posts')
+      .withRelated('fields', [
+        factory.addResource('fields', 'title').withAttributes({
+          fieldType: '@cardstack/core-types::string'
+        })
+      ]);
+    factory.addResource('posts', '1')
+      .withAttributes({
+        title: 'hello world'
+      });
+  });
 
-test('visiting /show-post', async function(assert) {
-  await visit('/posts/1');
-  assert.equal(currentURL(), '/posts/1');
-  findWithAssert('h1:contains(hello world)');
+  test('visiting /show-post', async function(assert) {
+    await visit('/posts/1');
+    assert.equal(currentURL(), '/posts/1');
+    assert.equal(this.element.querySelector('h1').textContent.trim(), 'hello world');
+  });
 });
