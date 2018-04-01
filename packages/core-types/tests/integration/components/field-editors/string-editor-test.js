6c6fa789dd899ce63458b3a262958a345788ac65
Edward Faulkner <ef@alum.mit.edu>
2018-04-01 01:17:14 -0400

    ember-cli-update on core-types

--- packages/core-types/tests/integration/components/field-editors/string-editor-test.js
+++ packages/core-types/tests/integration/components/field-editors/string-editor-test.js
@@ -1,23 +1,25 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('field-editors/string-editor', 'Integration | Component | field editors/string editor', {
-  integration: true
-});
+module('Integration | Component | field editors/string editor', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  this.set('model', {
-    title: 'hello'
+  test('it renders', async function(assert) {
+    this.set('model', {
+      title: 'hello'
+    });
+    await render(hbs`{{field-editors/string-editor content=model field="title" enabled=true}}`);
+    assert.equal(this.$('input').val(), 'hello');
+    assert.equal(this.$('input[disabled]').length, 0);
   });
-  this.render(hbs`{{field-editors/string-editor content=model field="title" enabled=true}}`);
-  assert.equal(this.$('input').val(), 'hello');
-  assert.equal(this.$('input[disabled]').length, 0);
-});
 
-test('it can be disabled', function(assert) {
-  this.set('model', {
-    title: 'hello'
+  test('it can be disabled', async function(assert) {
+    this.set('model', {
+      title: 'hello'
+    });
+    await render(hbs`{{field-editors/string-editor content=model field="title" enabled=false}}`);
+    assert.equal(this.$('input[disabled]').length, 1);
   });
-  this.render(hbs`{{field-editors/string-editor content=model field="title" enabled=false}}`);
-  assert.equal(this.$('input[disabled]').length, 1);
 });
