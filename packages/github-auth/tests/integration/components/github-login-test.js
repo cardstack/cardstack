0dbfdcfb61fcff6da6a1308ab273e041142d9891
Edward Faulkner <ef@alum.mit.edu>
2018-04-01 01:24:51 -0400

    ember-cli-update github-auth

--- packages/github-auth/tests/integration/components/github-login-test.js
+++ packages/github-auth/tests/integration/components/github-login-test.js
@@ -1,25 +1,27 @@
-import { moduleForComponent, skip } from 'ember-qunit';
+import { module, skip } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('github-login', 'Integration | Component | github login', {
-  integration: true
-});
+module('Integration | Component | github login', function(hooks) {
+  setupRenderingTest(hooks);
 
-skip('it renders', function(assert) {
+  skip('it renders', function(assert) {
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{github-login}}`);
+    this.render(hbs`{{github-login}}`);
 
-  assert.equal(this.$().text().trim(), '');
+    assert.equal(this.element.textContent.trim(), '');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#github-login}}
-      template block text
-    {{/github-login}}
-  `);
+    // Template block usage:
+    this.render(hbs`
+      {{#github-login}}
+        template block text
+      {{/github-login}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
 });
