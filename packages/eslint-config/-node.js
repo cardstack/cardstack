fead2318343817022ec5cfeea850ff8e19f47672
Edward Faulkner <ef@alum.mit.edu>
2018-04-04 16:59:54 -0400

    need to restore all eslint recommended rules here

--- packages/eslint-config/-node.js
+++ packages/eslint-config/-node.js
@@ -9,16 +9,19 @@ module.exports = {
   },
   // I'm doing this instead of `extends` so that this config module
   // can be used within override blocks of other config modules.
-  rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-    'no-constant-condition': ["error", { checkLoops: false }],
-    'require-yield': 0,
-    'no-var': "error",
-    semi: ["error", "always"],
-    'node/no-extraneous-require': ['error', {
-      'allowModules': []
-    }],
-    'node/no-missing-require': ['error'],
-    'no-undef': 'error'
-  }),
+  rules: Object.assign(
+    {},
+    require('eslint-plugin-node').configs.recommended.rules,
+    require('eslint/conf/eslint-recommended').rules,
+    {
+      'no-constant-condition': ["error", { checkLoops: false }],
+      'require-yield': 0,
+      semi: ["error", "always"],
+      'node/no-extraneous-require': ['error', {
+        'allowModules': []
+      }],
+      'node/no-missing-require': ['error'],
+      'no-undef': 'error'
+    }),
   plugins: ['node']
 };
