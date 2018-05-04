7f87d63d7deb4069c29d5ee0962311a3d12bb61f
Alex Speller <alex@alexspeller.com>
2018-05-04 04:09:46 +0100

    Allow email sink to use SES configured automatically from env vars

--- packages/email/messenger.js
+++ packages/email/messenger.js
@@ -7,20 +7,37 @@ module.exports = class EmailMessenger {
   }
   constructor(params) {
     this.params = params;
-    ['smtpPasswordEnvVar', 'smtpHost', 'smtpUser', 'defaultFrom'].forEach( prop => {
-      if (!params[prop]) {
-        throw new Error(`Could not find ${prop} in message sink params`);
+
+    if(params.useSes) {
+      // If SES is required, you must add aws-sdk to the consuming project
+      const aws = require("aws-sdk");  // eslint-disable-line node/no-missing-require
+      if (!params.defaultFrom) {
+        throw new Error(`Could not find defaultFrom in message sink params`);
       }
-    });
-    this.transporter = nodemailer.createTransport({
-      host: params.smtpHost,
-      auth: {
-        user: params.smtpUser,
-        pass: process.env[params.smtpPasswordEnvVar]
-      }
-    }, {
-      from: params.defaultFrom
-    });
+
+      this.transporter = nodemailer.createTransport({
+        SES: new aws.SES({ apiVersion: '2010-12-01' })
+      }, {
+        from: params.defaultFrom
+      });
+
+    } else {
+      ['smtpPasswordEnvVar', 'smtpHost', 'smtpUser', 'defaultFrom'].forEach( prop => {
+        if (!params[prop]) {
+          throw new Error(`Could not find ${prop} in message sink params`);
+        }
+      });
+      this.transporter = nodemailer.createTransport({
+        host: params.smtpHost,
+        auth: {
+          user: params.smtpUser,
+          pass: process.env[params.smtpPasswordEnvVar]
+        }
+      }, {
+        from: params.defaultFrom
+      });
+    }
+
   }
   async send(message) {
     log.info(`Sending mail to ${message.to}: ${message.subject}`);
