8eae33c7c49e0e08c729f5bf9cb6cfcd9a38f287
Will Bagby <bagsbee@gmail.com>
2018-06-07 20:49:16 -0400

    Implement basic markup functionality for Mobiloedoc editor

---
+++ packages/mobiledoc/tests/acceptance/mobiledoc-markup-test.js
@@ -0,0 +1,46 @@
+import { module, test } from 'qunit';
+import { currentURL, visit, find, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { insertText } from '../helpers/ember-mobiledoc-editor';
+
+module('Acceptance | mobiledoc markup', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('mobiledoc field without cardstack-tools', async function(assert) {
+    await visit('/no-tools');
+
+    assert.equal(currentURL(), '/no-tools');
+    
+    let editorEl = find('.mobiledoc-editor__editor');
+    assert.dom('.mobiledoc-editor').hasText('I am a dongle.');
+
+    await insertText(editorEl, 'I am a paragraph.');
+    
+    assert.dom('.mobiledoc-editor p').hasTextContaining('I am a paragraph.', 'p tag is present');
+    
+    await click('.current-block .cs-icon');
+    await click('.block-options .editor-h2');
+    await insertText(editorEl, 'I am a header.');
+
+    assert.dom('.mobiledoc-editor h2').hasTextContaining('I am a header.', 'h2 tag is present');
+  });
+
+  test('mobiledoc field with cardstack-tools', async function(assert) {
+    await visit('/tools');
+
+    assert.equal(currentURL(), '/tools');
+    
+    let editorEl = find('.mobiledoc-editor__editor');
+    assert.dom('.mobiledoc-editor').hasText('I am a dongle.');
+
+    await insertText(editorEl, 'I am a paragraph.');
+    
+    assert.dom('.mobiledoc-editor p').hasTextContaining('I am a paragraph.', 'p tag is present');
+    
+    await click('.current-block .cs-icon');
+    await click('.block-options .editor-h2');
+    await insertText(editorEl, 'I am a header.');
+
+    assert.dom('.mobiledoc-editor h2').hasTextContaining('I am a header.', 'h2 tag is present');
+  });
+});
