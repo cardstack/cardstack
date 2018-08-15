38cd267fd686b249b82eeab0c9b4b7fe713eef31
Will Bagby <bagsbee@gmail.com>
2018-08-15 17:07:02 -0400

    WIP support multiple select VERY MUCH WIP

--- packages/core-types/addon/components/field-editors/dropdown-choices-editor.js
+++ packages/core-types/addon/components/field-editors/dropdown-choices-editor.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import layout from '../../templates/components/field-editors/dropdown-choices-editor';
+import { singularize } from 'ember-inflector';
 
 export default Component.extend({
   layout,
@@ -9,7 +10,7 @@ export default Component.extend({
   didReceiveAttrs() {
     let field = this.get('field');
 
-    return this.get('store').findAll(field).then((types) => {
+    return this.get('store').findAll(singularize(field)).then((types) => {
       if (!this.isDestroyed) {
         this.set('options', types);
       }
