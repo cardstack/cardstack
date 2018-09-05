cc6eb418ba64d60062904d16ec4560035b650d0f
Balint Erdi <balint.erdi@gmail.com>
2018-09-05 12:05:31 +0200

    Add support for relationship tracking & saving
    Where relationship tracking is provided by the ember-data-relationship-tracker add-on

--- packages/models/addon/model.js
+++ packages/models/addon/model.js
@@ -1,3 +1,29 @@
 import DS from 'ember-data';
 import RelationshipTracker from "ember-data-relationship-tracker";
-export default DS.Model.extend(RelationshipTracker);
+
+export default DS.Model.extend(RelationshipTracker, {
+  async save() {
+    // this._super is not safe to use asynchronously
+    // see https://github.com/ember-cli/ember-cli/issues/6282
+    let modelSave = this._super.bind(this);
+    await this.saveRelated();
+    await modelSave();
+  },
+
+  async saveRelated() {
+    let relationshipsByName = this.constructor.relationshipsByName;
+    let relatedSaves = this.dirtyRelationships.map(field => {
+      let { kind } = relationshipsByName.get(field);
+      let relatedRecords = kind === 'hasMany' ? this[field] : [ this.field ];
+      let dirtyRecords = relatedRecords.filter(record => record.hasDirtyFields);
+      return dirtyRecords.invoke('save');
+    });
+    return Promise.all(flatten(relatedSaves));
+  },
+});
+
+function flatten(arrays) {
+  return arrays.reduce((flattened, array) => {
+    return flattened.concat(array);
+  }, []);
+}
