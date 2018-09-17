ce894e45440ab3258d3e5995d34444f193d3fdb0
Chris Manson <chris.manson@simplabs.com>
2018-09-17 17:14:51 +0100

    implementing basic user flow

--- packages/card-picker/addon/components/card-picker-toolbox.js
+++ packages/card-picker/addon/components/card-picker-toolbox.js
@@ -1,7 +1,21 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
 import layout from '../templates/components/card-picker-toolbox';
 
 export default Component.extend({
+  tools: service('cardstack-card-picker'),
+
   classNames: ['cardstack-card-picker', 'cardstack-tools'],
   layout,
+
+  actions: {
+    select(model, event) {
+      event.stopPropagation();
+      event.preventDefault()
+
+      this.tools.resolveCard(model);
+      return false;
+    }
+  }
 });
--- packages/card-picker/addon/services/cardstack-card-picker.js
+++ packages/card-picker/addon/services/cardstack-card-picker.js
@@ -8,4 +8,16 @@ export default Service.extend({
     // Register items for edges
     this.get('cardstackEdges').registerTopLevelComponent('card-picker-edges');
   },
+
+  pickCard() {
+    return new Promise((resolve, reject) => {
+      this.setProperties({ resolve, reject });
+      this.set('active', true);
+    });
+  },
+
+  resolveCard(model) {
+    this.get('resolve')(model);
+    this.set('active', false);
+  }
 });
