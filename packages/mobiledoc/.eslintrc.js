f16e4ac071d965179fc4bf75b1b7bf39a965290a
Chris Manson <chris.manson@simplabs.com>
2018-09-19 08:31:20 +0100

    fixing eslint-config by using npmignore for node-tests

--- packages/mobiledoc/.eslintrc.js
+++ packages/mobiledoc/.eslintrc.js
@@ -1,12 +1,4 @@
 module.exports = {
   root: true,
-  extends: '@cardstack/eslint-config/browser',
-  overrides: [{
-    files: 'node-tests/**/*.js',
-    rules: {
-      'node/no-unpublished-require': ['error', {
-        'allowModules': ['@cardstack/test-support'],
-      }]
-    }
-  }]
+  extends: '@cardstack/eslint-config/browser'
 };
