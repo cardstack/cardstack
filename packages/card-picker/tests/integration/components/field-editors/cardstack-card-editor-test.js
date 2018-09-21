6f626393ae1788e3330916fd092b3393ec7190ab
Chris Manson <chris.manson@simplabs.com>
2018-09-21 13:36:00 +0100

    adding tests for cardstack-cards-editor

--- packages/card-picker/tests/integration/components/field-editors/cardstack-card-editor-test.js
+++ packages/card-picker/tests/integration/components/field-editors/cardstack-card-editor-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, pauseTest } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | field-editors/cardstack-card-editor', function(hooks) {
--- packages/card-picker/tests/integration/components/field-editors/cardstack-cards-editor-test.js
+++ packages/card-picker/tests/integration/components/field-editors/cardstack-cards-editor-test.js
@@ -1,4 +1,4 @@
-import { module, test } from 'qunit';
+import { module, test, skip } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
@@ -6,21 +6,47 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | field-editors/cardstack-cards-editor', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  test('it does not render edit controls if it is not enabled', async function(assert) {
     await render(hbs`{{field-editors/cardstack-cards-editor}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom('[data-test-add-card]').doesNotExist();
+  });
 
-    // Template block usage:
-    await render(hbs`
-      {{#field-editors/cardstack-cards-editor}}
-        template block text
-      {{/field-editors/cardstack-cards-editor}}
-    `);
+  test('it renders edit controls if it is enabled', async function(assert) {
+    this.set('enabled', true);
+    await render(hbs`{{field-editors/cardstack-cards-editor enabled=enabled}}`);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('[data-test-add-card]').exists();
+    assert.dom('[data-test-add-card]').hasText('Add Card');
+    assert.dom('[data-test-delete-card]').doesNotExist();
+  });
+
+  // TODO: figure out how to get this to work with jQuery turned off
+  skip('it allows you to delete card if enabled and has field content', async function(assert) {
+    this.set('enabled', true);
+    this.set('actions', {
+      orderChanged() {
+
+      }
+    })
+
+    this.set('content', {
+      articles: [{
+        id: '55',
+        type: 'super-fake-card'
+      }, {
+        id: '57',
+        type: 'super-fake-card'
+      }],
+    });
+
+    this.set('field', 'articles');
+
+    await render(hbs`{{field-editors/cardstack-cards-editor enabled=enabled content=content field=field}}`);
+
+    assert.dom('[data-test-add-card]').exists();
+    assert.dom('[data-test-add-card]').hasText('Add Card');
+    assert.dom('[data-test-delete-card]').exists(2);
+    assert.dom('[data-test-reorder-card]').exists(2);
   });
 });
