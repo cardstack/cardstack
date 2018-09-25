ada500bd93eeae6098e7f548be977313d48fa4f3
Balint Erdi <balint.erdi@gmail.com>
2018-09-25 19:06:34 +0200

    Upgrade ember-data version to 3.4.2

--- packages/tools/tests/acceptance/tools-test.js
+++ packages/tools/tests/acceptance/tools-test.js
@@ -2,6 +2,11 @@ import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import { visit, click } from '@ember/test-helpers';
 
+function findInputWithValue(value) {
+  return Array.from(this.element.querySelectorAll('input'))
+    .find(element => element.value === value);
+}
+
 module('Acceptance | tools', function(hooks) {
   setupApplicationTest(hooks);
 
@@ -19,7 +24,14 @@ module('Acceptance | tools', function(hooks) {
     await click('.cardstack-tools-launcher');
     let element = [...this.element.querySelectorAll('.cs-toolbox-section label')].find(element => /Title/.test(element.textContent));
     await click(element);
-    let matching = Array.from(this.element.querySelectorAll('input')).find(element => element.value === 'hello world');
+
+    let matching = findInputWithValue.call(this, 'hello world');
     assert.ok(matching, 'found field editor for title');
+
+    matching = findInputWithValue.call(this, 'Look behind you, a Three-Headed Monkey!');
+    assert.ok(matching, 'found field editor for comment body');
+
+    matching = findInputWithValue.call(this, 'Guybrush Threepwood');
+    assert.ok(matching, 'found field editor for comment author name');
   });
 });
