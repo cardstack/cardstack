585dad8c7217027496df882d99689459d374cb55
Will Bagby <bagsbee@gmail.com>
2018-10-02 13:45:40 -0400

    Fetch dropdown options by content type not field name

--- packages/core-types/tests/acceptance/editor-test.js
+++ packages/core-types/tests/acceptance/editor-test.js
@@ -17,30 +17,36 @@ module('Acceptance | field editors', async function(hooks) {
 
     this.store = this.owner.lookup('service:store');
     let model = await run(() => {
-      return this.store.findRecord('driver', 'metalmario');
+      return this.store.find('driver', 'metalmario');
     });
-    let vehicle = await model.get('vehicle.name');
     let feeling = await model.get('feeling.title');
+    let vehicle = await model.get('vehicle.name');
+    let alternateVehicle = await model.get('alternateVehicle.name');
 
     assert.equal(model.get('name'), 'Metal Mario', 'driver name is correct');
     assert.equal(model.get('dob'), '1999-01-01', 'driver dob is correct');
-    assert.equal(vehicle, 'Sport Bike', 'driver is using a sport bike');
     assert.equal(feeling, 'Happy', 'driver is feeling happy');
+    assert.equal(vehicle, 'Sport Bike', 'driver is using a sport bike');
+    assert.equal(alternateVehicle, 'Standard Kart', 'drivers alternate vehicle is a standard kart');
 
-    await fillIn('.ember-text-field:nth-of-type(1)', 'METAL Mario')
-    await fillIn('.ember-text-field:nth-of-type(2)', '1998-01-01')
-    await selectChoose('.vehicle-selector', 'Honeycoupe');
+    await fillIn('.ember-text-field:nth-of-type(1)', 'METAL Mario');
+    await fillIn('.ember-text-field:nth-of-type(2)', '1998-01-01');
     await selectChoose('.feeling-selector', 'Sad');
+    await selectChoose('.vehicle-selector', 'Honeycoupe');
+    await selectChoose('.alternate-vehicle-selector', 'Wild Wiggler');
 
-    assert.dom('.vehicle-selector .ember-power-select-selected-item').hasText('Honeycoupe');
     assert.dom('.feeling-selector .ember-power-select-selected-item').hasText('Sad');
+    assert.dom('.vehicle-selector .ember-power-select-selected-item').hasText('Honeycoupe');
+    assert.dom('.alternate-vehicle-selector .ember-power-select-selected-item').hasText('Wild Wiggler');
 
-    vehicle = await model.get('vehicle.name');
     feeling = await model.get('feeling.title');
+    vehicle = await model.get('vehicle.name');
+    alternateVehicle = await model.get('alternateVehicle.name');
 
     assert.equal(model.get('name'), 'METAL Mario', 'metal mario is more metal');
     assert.equal(model.get('dob'), '1998-01-01', 'metal mario was born earlier');
-    assert.equal(vehicle, 'Honeycoupe', 'metal mario is driving a honeycoupe');
     assert.equal(feeling, 'Sad', 'metal mario is sad');
+    assert.equal(vehicle, 'Honeycoupe', 'metal mario is driving a honeycoupe');
+    assert.equal(alternateVehicle, 'Wild Wiggler', 'metal marios alternate vehicle is a wild wiggler');
   });
 });
--- packages/core-types/tests/dummy/cardstack/seeds/ephemeral.js
+++ packages/core-types/tests/dummy/cardstack/seeds/ephemeral.js
@@ -4,11 +4,13 @@ const JSONAPIFactory = require('@cardstack/test-support/jsonapi-factory');
 function initialModels() {
   let initial = new JSONAPIFactory();
 
+
   initial.addResource('content-types', 'drivers')
   .withAttributes({
     defaultIncludes: [
       'feeling',
-      'vehicle'
+      'vehicle',
+      'alternate-vehicle'
     ]
   })
   .withRelated('fields', [
@@ -36,18 +38,32 @@ function initialModels() {
       .withRelated('fields', [
         initial.addResource('fields', 'name').withAttributes({ fieldType: '@cardstack/core-types::string' })
       ])
-    ])
+    ]),
+    initial.addResource('fields', 'alternate-vehicle').withAttributes({
+      fieldType: '@cardstack/core-types::belongs-to',
+      editorComponent: 'field-editors/dropdown-choices-editor',
+      editorOptions: { displayFieldName: 'name' }
+    }).withRelated('related-types', [{ type: 'content-types', id: 'vehicles' }])
   ]);
 
   let happyFeeling = initial.addResource('feelings', '1').withAttributes({ title: 'Happy' });
-  initial.addResource('feelings', '2').withAttributes({ title: 'Sad' });
-  initial.addResource('feelings', '3').withAttributes({ title: 'Exuberant' });
+  let sadFeeling = initial.addResource('feelings', '2').withAttributes({ title: 'Sad' });
+  let exuberantFeeling = initial.addResource('feelings', '3').withAttributes({ title: 'Exuberant' });
   initial.addResource('feelings', '4').withAttributes({ title: 'Melancholy' });
 
   let sportBikeVehicle = initial.addResource('vehicles', '1').withAttributes({ name: 'Sport Bike' });
-  initial.addResource('vehicles', '2').withAttributes({ name: 'Standard Kart' });
+  let standardKartVehicle = initial.addResource('vehicles', '2').withAttributes({ name: 'Standard Kart' });
   initial.addResource('vehicles', '3').withAttributes({ name: 'Honeycoupe' });
   initial.addResource('vehicles', '4').withAttributes({ name: 'Wild Wiggler' });
+
+
+  initial.addResource('drivers', 'kingboo')
+    .withAttributes({
+      name: 'King Boo',
+      dob: '1998-01-21'
+    })
+    .withRelated('feeling', exuberantFeeling)
+    .withRelated('vehicle', standardKartVehicle);
 
   initial.addResource('drivers', 'metalmario')
     .withAttributes({
@@ -55,6 +71,23 @@ function initialModels() {
       dob: '1999-01-01'
     })
     .withRelated('feeling', happyFeeling)
+    .withRelated('vehicle', sportBikeVehicle)
+    .withRelated('alternate-vehicle', standardKartVehicle);
+
+  initial.addResource('drivers', 'link')
+    .withAttributes({
+      name: 'Link',
+      dob: '2003-01-01'
+    })
+    .withRelated('feeling', happyFeeling)
+    .withRelated('vehicle', sportBikeVehicle);
+
+  initial.addResource('drivers', 'shyguy')
+    .withAttributes({
+      name: 'Shy Guy',
+      dob: '2001-01-01'
+    })
+    .withRelated('feeling', sadFeeling)
     .withRelated('vehicle', sportBikeVehicle);
 
   return initial.getModels();
