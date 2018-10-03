24c0342fbb3b3b422cc6fd85cdb301777ee181f7
Balint Erdi <balint.erdi@gmail.com>
2018-10-03 19:22:06 +0200

    Pass validation arguments to all field-editors

--- packages/core-types/addon/components/field-editors/string-editor.js
+++ packages/core-types/addon/components/field-editors/string-editor.js
@@ -5,21 +5,4 @@ import layout from '../../templates/components/field-editors/string-editor';
 
 export default Component.extend({
   layout,
-
-  disabled: not('enabled'),
-
-  isContentInvalid: not('content.isValid'),
-
-  hasValidationError: computed('isContentInvalid', 'content.errors', function() {
-    if (this.isContentInvalid) {
-      return this.content.errors.has(this.field);
-    }
-  }),
-
-  firstValidationError: computed('hasValidationError', function() {
-    if (this.hasValidationError) {
-      let errorsForField = this.content.errors.errorsFor(this.field);
-      return errorsForField.firstObject.message;
-    }
-  }),
 });
--- packages/tools/addon/components/cs-field-editor.js
+++ packages/tools/addon/components/cs-field-editor.js
@@ -1,7 +1,31 @@
 import Component from '@ember/component';
+import { not } from '@ember/object/computed';
+import { computed } from '@ember/object';
 import layout from '../templates/components/cs-field-editor';
 
 export default Component.extend({
   layout,
-  tagName: ''
+  tagName: '',
+
+  // @args
+  content: null,
+  field: '',
+  enabled: true,
+
+  disabled: not('enabled'),
+
+  isContentInvalid: not('content.isValid'),
+
+  hasValidationError: computed('isContentInvalid', 'content.errors', function() {
+    if (this.isContentInvalid) {
+      return this.content.errors.has(this.field);
+    }
+  }),
+
+  firstValidationError: computed('hasValidationError', function() {
+    if (this.hasValidationError) {
+      let errorsForField = this.content.errors.errorsFor(this.field);
+      return errorsForField.firstObject.message;
+    }
+  }),
 });
