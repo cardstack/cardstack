f23fa253e23be87c00a4e2dc5cb00cc19ddb4c45
Will Bagby <bagsbee@gmail.com>
2018-10-03 13:40:19 -0400

    Boolean field editor

---
+++ packages/core-types/addon/components/field-editors/boolean-editor.js
@@ -0,0 +1,16 @@
+import { get, set } from '@ember/object';
+import Component from '@ember/component';
+import layout from '../../templates/components/field-editors/boolean-editor';
+
+export default Component.extend({
+  layout,
+
+  actions: {
+    toggleValue(value) {
+      let content = get(this, 'content');
+      let field = get(this, 'field');
+
+      set(content, field, value);
+    }
+  }
+});
