65e6aa31e201b4bc82be78a8105d7430eb668ab7
Balint Erdi <balint.erdi@gmail.com>
2018-10-08 14:56:32 +0200

    Remove unused imports

--- packages/core-types/addon/components/field-editors/string-editor.js
+++ packages/core-types/addon/components/field-editors/string-editor.js
@@ -1,6 +1,4 @@
-import { not } from '@ember/object/computed';
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 import layout from '../../templates/components/field-editors/string-editor';
 
 export default Component.extend({
