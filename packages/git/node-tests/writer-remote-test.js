666b02cf120a93d1c3a85c097f2c1198cddd1f2e
Chris Manson <chris.manson@simplabs.com>
2018-10-10 12:36:49 +0100

    removing unused tests

--- packages/git/node-tests/writer-remote-test.js
+++ packages/git/node-tests/writer-remote-test.js
@@ -133,42 +133,7 @@ describe('git/writer with remote', function() {
       });
     });
 
-    it.skip('reports merge conflict', async function() {
-      await writers.update('master', env.session, 'articles', '1', {
-        data: {
-          id: '1',
-          type: 'articles',
-          attributes: {
-            title: 'Updated title'
-          },
-          meta: {
-            version: head
-          }
-        }
-      });
-
-      try {
-        await writers.update('master', env.session, 'articles', '1', {
-          data: {
-            id: '1',
-            type: 'articles',
-            attributes: {
-              title: 'Conflicting title'
-            },
-            meta: {
-              version: head
-            }
-          }
-        });
-        throw new Error("should not get here");
-      } catch (err) {
-        if (!err.status) {
-          throw err;
-        }
-        expect(err.status).to.equal(409);
-        expect(err.detail).to.match(/merge conflict/i);
-      }
-    });
+    // TODO: come up with testing scenarios for conflicts
   });
 
   describe('delete', function() {
@@ -181,30 +146,6 @@ describe('git/writer with remote', function() {
       expect(articles).to.not.contain('event-1.json');
     });
 
-    it.skip('reports merge conflict', async function() {
-      await writers.update('master', env.session, 'articles', '1', {
-        data: {
-          id: '1',
-          type: 'articles',
-          attributes: {
-            title: 'Updated title'
-          },
-          meta: {
-            version: head
-          }
-        }
-      });
-
-      try {
-        await writers.delete('master', env.session, head, 'articles', '1');
-        throw new Error("should not get here");
-      } catch (err) {
-        if (!err.status) {
-          throw err;
-        }
-        expect(err.status).to.equal(409);
-        expect(err.detail).to.match(/merge conflict/i);
-      }
-    });
+    // TODO: come up with testing scenarios for conflicts
   });
 });
