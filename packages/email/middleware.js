ed33ee3bb2f9b1f8f862299a2694970f8833e01c
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2018-10-17 16:33:00 +0200

    Middleware using messages

---
+++ packages/email/middleware.js
@@ -0,0 +1,44 @@
+const { declareInjections } = require('@cardstack/di');
+const route = require('koa-better-route');
+
+module.exports = declareInjections({
+  messengers: 'hub:messengers'
+},
+
+class CodeGenMiddleware {
+  constructor() {
+    this.before = 'authentication';
+  }
+
+  middleware() {
+    //TODO: Get these from config
+    let messageSinkId = 'the-sink';
+    let defaultMailTo = 'contact@cardstack.com';
+
+    return route.post('/email/send', async (ctxt) => {
+      let { req } = ctxt;
+      let body = '';
+      req.on('data', chunk => {
+        body += chunk.toString();
+      });
+      req.on('end', () => {
+        console.log(body);
+        res.end('ok');
+      });
+      
+      await this.messengers.send(messageSinkId, {
+        to: defaultMailTo,
+        subject: 'Hello from cardstack',
+        from: 'jorge.lainfiesta@cardstack.com',
+        text: 'Hello my friend',
+        html: '<h1>Hello my friend</h1>'
+      });
+
+      ctxt.body = `console.log('Hello world: ${messageSinkId}')`;
+      ctxt.response.set('Access-Control-Allow-Origin', '*');
+      ctxt.response.set('Content-Type', 'application/javascript');
+      ctxt.status = 200;
+    });
+  }
+
+});
