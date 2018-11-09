6cdd50116b22022dc1b92fa3c6f1ea6b642e203e
Burcu Noyan <burcunoyan@gmail.com>
2018-11-09 16:12:51 -0500

    Add acceptance test

--- packages/core-types/tests/acceptance/editor-test.js
+++ packages/core-types/tests/acceptance/editor-test.js
@@ -22,6 +22,9 @@ module('Acceptance | field editors', async function(hooks) {
     let feeling = await model.get('feeling.title');
     let vehicle = await model.get('vehicle.name');
     let alternateVehicle = await model.get('alternateVehicle.name');
+    let tracks = await model.get('tracks.length');
+    let firstTrack = await model.get('tracks.firstObject.name');
+    let lastTrack = await model.get('tracks.lastObject.name');
 
     assert.equal(model.get('name'), 'Metal Mario', 'driver name is correct');
     assert.equal(model.get('dob'), '1999-01-01', 'driver dob is correct');
@@ -30,6 +33,9 @@ module('Acceptance | field editors', async function(hooks) {
     assert.equal(feeling, 'Happy', 'driver is feeling happy');
     assert.equal(vehicle, 'Sport Bike', 'driver is using a sport bike');
     assert.equal(alternateVehicle, 'Standard Kart', 'drivers alternate vehicle is a standard kart');
+    assert.equal(tracks, '3', 'drivers has 3 tracks');
+    assert.equal(firstTrack, 'Rainbow Road', 'drivers first track is rainbow road');
+    assert.equal(lastTrack, 'Koopa City', 'drivers final track is koopa city');
 
     await fillIn('.ember-text-field:nth-of-type(1)', 'METAL Mario');
     await fillIn('input[type=date]', '1998-01-01');
@@ -38,14 +44,19 @@ module('Acceptance | field editors', async function(hooks) {
     await selectChoose('.feeling-selector', 'Sad');
     await selectChoose('.vehicle-selector', 'Honeycoupe');
     await selectChoose('.alternate-vehicle-selector', 'Wild Wiggler');
+    await selectChoose('.tracks-selector', 'Twisted Mansion');
 
     assert.dom('.feeling-selector .ember-power-select-selected-item').hasText('Sad');
     assert.dom('.vehicle-selector .ember-power-select-selected-item').hasText('Honeycoupe');
     assert.dom('.alternate-vehicle-selector .ember-power-select-selected-item').hasText('Wild Wiggler');
+    assert.dom('.tracks-selector .ember-power-select-multiple-option').exists({ count: 4 });
+    assert.dom('.tracks-selector .ember-power-select-multiple-option:nth-of-type(1)').hasText('× Rainbow Road');
+    assert.dom('.tracks-selector .ember-power-select-multiple-option:nth-of-type(4)').hasText('× Twisted Mansion');
 
     feeling = await model.get('feeling.title');
     vehicle = await model.get('vehicle.name');
     alternateVehicle = await model.get('alternateVehicle.name');
+    lastTrack = await model.get('tracks.lastObject.name');
 
     assert.equal(model.get('name'), 'METAL Mario', 'metal mario is more metal');
     assert.equal(model.get('dob'), '1998-01-01', 'metal mario was born earlier');
@@ -54,5 +65,6 @@ module('Acceptance | field editors', async function(hooks) {
     assert.equal(feeling, 'Sad', 'metal mario is sad');
     assert.equal(vehicle, 'Honeycoupe', 'metal mario is driving a honeycoupe');
     assert.equal(alternateVehicle, 'Wild Wiggler', 'metal marios alternate vehicle is a wild wiggler');
+    assert.equal(lastTrack, 'Twisted Mansion', 'drivers final track is twisted mansion');
   });
 });
