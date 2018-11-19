d3d819bf5adbb4fc3e4012e5aa2ce4faf7ec3c39
Balint Erdi <balint.erdi@gmail.com>
2018-11-19 13:56:17 +0100

    Fix integer editor setting string values

--- packages/core-types/addon/components/field-editors/integer-editor.js
+++ packages/core-types/addon/components/field-editors/integer-editor.js
@@ -3,4 +3,16 @@ import layout from '../../templates/components/field-editors/integer-editor';
 
 export default Component.extend({
   layout,
+
+  actions: {
+    update(value) {
+      let content = this.get('content');
+      let field = this.get('field');
+      if (value == null || value === '') {
+        content.set(field, null);
+      } else {
+        content.set(field, +value);
+      }
+    }
+  }
 });
