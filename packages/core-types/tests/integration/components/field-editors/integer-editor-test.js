d7d7fe2236a552e567c7a72b2f89acd7f246710b
Balint Erdi <balint.erdi@gmail.com>
2018-11-19 17:57:10 +0100

    Simplify test assertions and helpers

--- packages/core-types/tests/integration/components/field-editors/integer-editor-test.js
+++ packages/core-types/tests/integration/components/field-editors/integer-editor-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, triggerEvent, waitFor } from '@ember/test-helpers';
-import Object from '@ember/object';
+import { render, triggerEvent, fillIn } from '@ember/test-helpers';
+import EmberObject from '@ember/object';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | field editors/integer editor', function(hooks) {
@@ -9,7 +9,7 @@ module('Integration | Component | field editors/integer editor', function(hooks)
 
   test('it renders', async function(assert) {
     this.setProperties({
-      model: Object.create({ rating: 3 }),
+      model: EmberObject.create({ rating: 3 }),
       onchange() {}
     });
     await render(hbs`
@@ -19,12 +19,12 @@ module('Integration | Component | field editors/integer editor', function(hooks)
         onchange=onchange
       }}`);
     assert.dom('input[type=number]').hasValue('3', 'number input has correct value');
-    assert.dom('input').isNotDisabled('input is not disabled');
+    assert.dom('input').isNotDisabled();
   });
 
   test('it updates the value correctly', async function(assert) {
     this.setProperties({
-      model: Object.create({ rating: 3 }),
+      model: EmberObject.create({ rating: 3 }),
       onchange() {}
     });
     await render(hbs`
@@ -33,16 +33,15 @@ module('Integration | Component | field editors/integer editor', function(hooks)
         field="rating"
         onchange=onchange
       }}`);
-    this.set('model.rating', 5);
-    await triggerEvent('.field-editor > input', 'input');
+    await fillIn('.field-editor > input', '5');
     assert.dom('input[type=number]').hasValue('5', 'input is updated');
-    assert.dom('input').isNotDisabled('input is not disabled');
+    assert.dom('input').isNotDisabled();
     assert.equal(this.get('model.rating'), '5', 'model attribute is updated');
   });
 
   test('it can be disabled', async function(assert) {
     this.setProperties({
-      model: Object.create({ rating: 3 }),
+      model: EmberObject.create({ rating: 3 }),
       onchange() {}
     });
     await render(hbs`
@@ -52,6 +51,6 @@ module('Integration | Component | field editors/integer editor', function(hooks)
         onchange=onchange
         disabled=true
       }}`);
-    assert.dom('input').isDisabled('integer input is disabled');
+    assert.dom('input').isDisabled();
   });
 });
