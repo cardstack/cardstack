5afd7f7cb76789926337fb42aece41faf7550881
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2018-11-19 14:57:14 +0100

    Implements close button

--- packages/card-picker/addon/services/cardstack-card-picker.js
+++ packages/card-picker/addon/services/cardstack-card-picker.js
@@ -5,11 +5,13 @@ export default Service.extend({
   init() {
     this._super();
 
+    console.log('Register FACE');
     // Register items for edges
     this.get('cardstackEdges').registerTopLevelComponent('card-picker-edges');
   },
 
   pickCard() {
+    console.log('FACE');
     return new Promise((resolve, reject) => {
       this.setProperties({ resolve, reject });
       this.set('active', true);
@@ -18,6 +20,11 @@ export default Service.extend({
 
   resolveCard(model) {
     this.get('resolve')(model);
+    this.set('active', false);
+  },
+
+  closePicker() {
+    this.get('reject')();
     this.set('active', false);
   }
 });
