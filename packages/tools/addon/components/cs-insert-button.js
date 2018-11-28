5cfdb7d7e33f4fe12bc9a6d31c85014fb28988f9
Burcu Noyan <burcunoyan@gmail.com>
2018-11-28 15:22:09 -0500

    Make insert button a component

---
+++ packages/tools/addon/components/cs-insert-button.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import layout from '../templates/components/cs-insert-button';
+
+export default Component.extend({
+  layout,
+  tagName: 'button',
+  classNames: ['cs-insert-button']
+});
