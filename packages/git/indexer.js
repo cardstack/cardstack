53335a63c561c70b2a39affa400835e25b14e552
Derrick Camerino <robustdj@gmail.com>
2019-02-26 13:09:53 -0800

    Fix most of git indexer tests

--- packages/git/indexer.js
+++ packages/git/indexer.js
@@ -139,7 +139,9 @@ module.exports = class Indexer {
   async branches() {
     await this._ensureRepo();
 
-    await service.fetchAllFromRemote(this.remote.url);
+    if (this.remote) {
+      await service.fetchAllFromRemote(this.remote.url);
+    }
 
     // nodegit docs show a Branch.iteratorNew method that would be
     // more appropriate than this, but as far as I can tell it is not
