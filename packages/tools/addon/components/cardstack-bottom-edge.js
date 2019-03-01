8c2ae07c526cc1625cd721485b6723c278ddbfc6
Burcu Noyan <burcunoyan@gmail.com>
2019-03-01 11:47:46 -0500

    Add left and bottom edges

---
+++ packages/tools/addon/components/cardstack-bottom-edge.js
@@ -0,0 +1,4 @@
+import Component from '@ember/component';
+import layout from '../templates/components/cardstack-bottom-edge';
+
+export default Component.extend({ layout });
---
+++ packages/tools/app/components/cardstack-bottom-edge.js
@@ -0,0 +1,1 @@
+export { default } from '@cardstack/tools/components/cardstack-bottom-edge';
