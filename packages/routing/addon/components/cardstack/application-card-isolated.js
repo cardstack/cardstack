3f82be7b8ad510d811d21ae94d9ac2fd76b52922
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2019-03-15 21:16:05 -0400

    styling updates

--- packages/routing/addon/components/cardstack/application-card-isolated.js
+++ packages/routing/addon/components/cardstack/application-card-isolated.js
@@ -1,4 +1,7 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/cardstack/application-card-isolated';
 
-export default Component.extend({ layout });
+export default Component.extend({
+  layout,
+  classNames: ['cs-application-card']
+});
--- packages/routing/addon/components/cardstack/cardstack-placeholder-isolated.js
+++ packages/routing/addon/components/cardstack/cardstack-placeholder-isolated.js
@@ -2,5 +2,6 @@ import Component from '@ember/component';
 import layout from '../../templates/components/cardstack/cardstack-placeholder-isolated';
 
 export default Component.extend({
-  layout
+  layout,
+  classNames: ['cs-error-card']
 });
