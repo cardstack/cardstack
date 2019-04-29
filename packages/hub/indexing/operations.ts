16d9f5cd7ea31619422e93daff5d17682717e6d2
Edward Faulkner <ef@alum.mit.edu>
2019-04-29 15:19:56 -0400

    successfully converted first file to typescript

--- packages/hub/indexing/operations.ts
+++ packages/hub/indexing/operations.ts
@@ -1,33 +1,49 @@
-const opsPrivate = new WeakMap();
+import { Document } from 'jsonapi-typescript';
+import { todo } from '@cardstack/plugin-utils/todo-any';
 
-module.exports = class Operations {
-  static create(sourcesUpdate, sourceId) {
+const opsPrivate: WeakMap<Operations, PrivateOperations> = new WeakMap();
+
+interface PrivateOperations {
+  sourceId: string;
+  sourcesUpdate: todo;
+  nonce: Number | null;
+}
+
+function getPriv(instance: Operations): PrivateOperations {
+  // safe because we always populate opsPrivate at construction
+  return opsPrivate.get(instance)!;
+}
+
+class Operations {
+  static create(sourcesUpdate: todo, sourceId: string) {
     return new this(sourcesUpdate, sourceId);
   }
 
-  constructor(sourcesUpdate, sourceId) {
+  constructor(sourcesUpdate: todo, sourceId: string) {
     opsPrivate.set(this, {
       sourceId,
       sourcesUpdate,
       nonce: null
     });
   }
-  async save(type, id, doc){
-    let { sourceId, sourcesUpdate, nonce } = opsPrivate.get(this);
+  async save(type: string, id: string, doc: Document){
+    let { sourceId, sourcesUpdate, nonce } = getPriv(this);
     await sourcesUpdate.add(type, id, doc, sourceId, nonce);
   }
-  async delete(type, id) {
-    let { sourcesUpdate } = opsPrivate.get(this);
+  async delete(type: string, id: string) {
+    let { sourcesUpdate } = getPriv(this);
     await sourcesUpdate.delete(type, id);
   }
   async beginReplaceAll() {
-    opsPrivate.get(this).nonce = Math.floor(Number.MAX_SAFE_INTEGER * Math.random());
+    getPriv(this).nonce = Math.floor(Number.MAX_SAFE_INTEGER * Math.random());
   }
   async finishReplaceAll() {
-    let { sourcesUpdate, sourceId, nonce } = opsPrivate.get(this);
+    let { sourcesUpdate, sourceId, nonce } = getPriv(this);
     if (!nonce) {
       throw new Error("tried to finishReplaceAll when there was no beginReplaceAll");
     }
     await sourcesUpdate.deleteAllWithoutNonce(sourceId, nonce);
   }
 };
+
+export = Operations;
