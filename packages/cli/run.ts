2da5033a6052c14a0d49e0c0748446f255170b90
Edward Faulkner <ef@alum.mit.edu>
2019-04-30 16:04:13 -0400

    configure data-sources

--- packages/cli/run.ts
+++ packages/cli/run.ts
@@ -2,9 +2,9 @@ import { createHash } from "crypto";
 import { join, dirname } from "path";
 import { tmpdir } from "os";
 import hashForDep from "hash-for-dep";
-import UI from 'console-ui';
+import UI from "console-ui";
 import { execFileSync } from "child_process";
-import { ensureDirSync } from "fs-extra";
+import { ensureDirSync, pathExistsSync, writeFileSync, mkdirpSync } from "fs-extra";
 
 const appName = "cardstack-standard-app";
 
@@ -15,7 +15,11 @@ interface Options {
 
 function getWorkDir(cardDir: string): string {
   let hash = createHash("md5");
-  hash.update(cardDir + "\0" + (process.env.CARDSTACK_DEV_NOCACHE ? 'nocache' : hashForDep(__dirname)));
+  hash.update(
+    cardDir +
+      "\0" +
+      (process.env.CARDSTACK_DEV_NOCACHE ? "nocache" : hashForDep(__dirname))
+  );
   return join(tmpdir(), "cardstack", hash.digest("hex").slice(0, 6));
 }
 
@@ -24,23 +28,64 @@ export default async function run({ dir, ui }: Options) {
   ensureDirSync(workDir);
   ui.writeInfoLine(`running the card in ${dir}`);
   ui.writeInfoLine(`using temp dir ${workDir}`);
-  generateEmberApp(workDir);
-  installDependencies(join(workDir, appName));
+  let appDir = join(workDir, appName);
+  if (!pathExistsSync(appDir)) {
+    generateEmberApp(workDir);
+    installDependencies(appDir);
+    enhanceApp(appDir);
+  }
 }
 
 function generateEmberApp(workDir: string) {
-  let ember = require.resolve('ember-cli/bin/ember');
-  let blueprint = dirname(require.resolve('@ember/octane-app-blueprint/package.json'));
-  execFileSync(ember, ["new", appName, "--blueprint", blueprint, "--skip-npm", "--skip-git"], { cwd: workDir });
+  let ember = require.resolve("ember-cli/bin/ember");
+  let blueprint = dirname(
+    require.resolve("@ember/octane-app-blueprint/package.json")
+  );
+  execFileSync(
+    ember,
+    ["new", appName, "--blueprint", blueprint, "--skip-npm", "--skip-git"],
+    { cwd: workDir }
+  );
 }
 
 function installDependencies(appDir: string) {
-  let yarn = require.resolve('yarn/bin/yarn');
-  execFileSync(yarn, [
-    "add",
-    "--dev",
-    "@cardstack/hub",
-    "@cardstack/jsonapi",
-    "@cardstack/ephemeral"
-  ], { cwd: appDir });
+  let yarn = require.resolve("yarn/bin/yarn");
+  execFileSync(
+    yarn,
+    [
+      "add",
+      "--dev",
+      "@cardstack/hub",
+      "@cardstack/jsonapi",
+      "@cardstack/ephemeral",
+    ],
+    { cwd: appDir }
+  );
+}
+
+function enhanceApp(appDir: string) {
+  mkdirpSync(join(appDir, "cardstack", "data-sources"));
+  writeFileSync(
+    join(appDir, "cardstack", "data-sources", "ephemeral.js"),
+    `
+  module.exports = [
+    {
+      type: 'data-sources',
+      id: 'default',
+      attributes: {
+        'source-type': '@cardstack/ephemeral'
+      }
+    },
+    {
+      type: 'plugin-configs',
+      id: '@cardstack/hub',
+      relationships: {
+        'default-data-source': {
+          data: { type: 'data-sources', id: 'default' }
+        }
+      }
+    }
+  ];
+  `, 'utf8'
+  );
 }
