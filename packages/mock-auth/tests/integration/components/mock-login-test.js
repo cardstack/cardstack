1b8891c05a91dcf17e3963781c53f052ab5c61e8
Jen Weber <j@jenweber.me>
2019-05-08 13:30:17 -0500

    update mock-auth to Ember 3.8

--- packages/mock-auth/tests/integration/components/mock-login-test.js
+++ packages/mock-auth/tests/integration/components/mock-login-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | mock login', function(hooks) {
@@ -13,7 +13,7 @@ module('Integration | Component | mock login', function(hooks) {
 
     await render(hbs`{{mock-login}}`);
 
-    assert.equal(this.$().text().trim(), '');
+    assert.dom('*').hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -22,6 +22,6 @@ module('Integration | Component | mock login', function(hooks) {
       {{/mock-login}}
     `);
 
-    assert.equal(this.$().text().trim(), 'template block text');
+    assert.dom('*').hasText('template block text');
   });
 });
