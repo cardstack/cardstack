9459b8acde8c922ec9226ba937914462c7aabf07
Jen Weber <j@jenweber.me>
2019-05-08 15:09:05 -0500

    implement generateModules in hub generator

--- packages/hub/code-generators.ts
+++ packages/hub/code-generators.ts
@@ -1,6 +1,7 @@
 import { declareInjections }  from '@cardstack/di';
 import createLog from '@cardstack/logger';
 import { todo } from '@cardstack/plugin-utils/todo-any';
+import { transform } from '@babel/core';
 const log = createLog('cardstack/code-gen');
 
 export = declareInjections({
@@ -15,7 +16,19 @@ class CodeGenerators {
     for (let feature of activePlugins.featuresOfType('code-generators')) {
       log.debug(`Running code generator %s `, feature.id);
       let codeGenerator = activePlugins.lookupFeatureAndAssert('code-generators', feature.id);
-      results.push(await codeGenerator.generateCode(modulePrefix));
+      if (typeof codeGenerator.generateModules === 'function') {
+        let namedModules = await codeGenerator.generateModules();
+        for (let [name, code] of namedModules) {
+          results.push(
+            transform(code, {
+              plugins: ['@babel/plugin-transform-modules-amd'],
+              moduleId: name
+            })!.code
+          );
+        }
+      } else {
+        results.push(await codeGenerator.generateCode(modulePrefix));
+      }
     }
     return results.join("");
   }
