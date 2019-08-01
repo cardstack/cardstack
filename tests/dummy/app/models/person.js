8bb6d191e6224ef2a4cb1e55f051356177e74099
Will Bagby <bagsbee@gmail.com>
2019-08-01 16:35:07 -0400

    Add ember-cli-mirage for mocking data

--- tests/dummy/app/models/person.js
+++ tests/dummy/app/models/person.js
@@ -1,11 +1,11 @@
 import DS from 'ember-data';
 import { computed } from '@ember/object';
 
-const { Model } = DS;
+const { Model, attr } = DS;
 
 export default Model.extend({
-  firstName: Attr(),
-  lastName: Attr(),
+  firstName: attr(),
+  lastName: attr(),
   fullName: computed('firstName', 'lastName', function() {
     return `${this.firstName} ${this.lastName}`;
   })
