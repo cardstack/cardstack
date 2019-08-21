2b4be7cd621a2d2e36ff0c225998da748e26132a
Will Bagby <bagsbee@gmail.com>
2019-08-21 11:54:07 -0400

    use Cardstack sample images instead of faker

--- tests/dummy/mirage/fixtures/articles.js
+++ tests/dummy/mirage/fixtures/articles.js
@@ -1,6 +1,6 @@
 import faker from 'faker';
 
-const imageTypes = ["animals", "business", "cats", "city", "food", "nightlife", "fashion", "people", "nature", "sports", "technics", "transport"];
+const imageTypes = ["community", "developer", "feature", "token"];
 
 export default [
   {
@@ -8,7 +8,7 @@ export default [
     title: `Sample Article`,
     description: faker.random.words(5),
     publishedDate: faker.date.past().toLocaleDateString(),
-    imageUrl: faker.image.imageUrl(640, 480, imageTypes[Math.floor(Math.random()*imageTypes.length)], true, true),
+    imageUrl: `/images/${imageTypes[Math.floor(Math.random()*imageTypes.length)]}-${Math.ceil(Math.random()*4)}.png`,
     authorId: '1',
     body: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit , morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.'
   }
