005fd6dc8b5477de7e86630eb2d381127721c3a5
Burcu Noyan <burcunoyan@gmail.com>
2019-08-30 12:03:10 -0400

    Fix fading effect

--- tests/dummy/app/controllers/cards.js
+++ tests/dummy/app/controllers/cards.js
@@ -1,5 +1,6 @@
 import Controller from '@ember/controller';
 import { action, set } from '@ember/object';
+import { filterBy } from '@ember/object/computed';
 import move from 'ember-animated/motions/move';
 import resize from 'ember-animated/motions/resize';
 import adjustCSS from 'ember-animated/motions/adjust-css';
@@ -7,14 +8,14 @@ import { printSprites } from 'ember-animated';
 import { easeOut } from 'ember-animated/easings/cosine';
 
 export default class CardsController extends Controller {
+  @filterBy('model', 'expanded', true) expandedCards;
+
   @action toggle(id) {
     for (let elt of this.model) {
       if (elt.id === id) {
         set(elt, 'expanded', !elt.expanded);
-        set(elt, 'faded', false);
       } else {
         set(elt, 'expanded', false);
-        set(elt, 'faded', !elt.faded);
       }
     }
   }
