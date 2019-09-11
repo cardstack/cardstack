96ad30044e7b7a54f37acae8e7d26acadcc6316c
Edward Faulkner <ef@alum.mit.edu>
2019-09-11 14:28:01 -0400

    use a tmpdir that's writable on travis

--- config/deploy.js
+++ config/deploy.js
@@ -1,10 +1,15 @@
 /* eslint-env node */
 'use strict';
 
+const os = require('os');
+
 module.exports = function(/* deployTarget */) {
   let ENV = {
     build: {
       environment: 'production'
+    },
+    git: {
+      worktreePath: `${os.tmpdir()}/deploy-box`
     }
   };
   return ENV;
