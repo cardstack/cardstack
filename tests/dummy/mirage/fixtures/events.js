549f875dcfe9f3a627e01219ad57d3be375020eb
Edward Faulkner <ef@alum.mit.edu>
2019-09-11 12:46:01 -0400

    make image URLs portable

--- tests/dummy/mirage/fixtures/events.js
+++ tests/dummy/mirage/fixtures/events.js
@@ -1,4 +1,5 @@
 import faker from 'faker';
+import ENV from "dummy/config/environment";
 
 const imageTypes = ["community", "developer", "feature", "token"];
 
@@ -8,7 +9,7 @@ export default [
     title: `Sample Event - ${faker.random.words(3)}`,
     description: faker.random.words(7),
     publishedDate: faker.date.past().toLocaleDateString(),
-    imageUrl: `/images/${imageTypes[Math.floor(Math.random()*imageTypes.length)]}-${Math.ceil(Math.random()*4)}.png`,
+    imageUrl: `${ENV.rootURL}images/${imageTypes[Math.floor(Math.random()*imageTypes.length)]}-${Math.ceil(Math.random()*4)}.png`,
     authorId: '2',
     body: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit , morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.'
   }
