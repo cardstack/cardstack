d8954ce9497448be40db330056951a3e038f96c4
Will Bagby <bagsbee@gmail.com>
2019-09-12 17:10:39 -0400

    Animate title demo

---
+++ addon/components/field-modes/view.js
@@ -0,0 +1,8 @@
+import { tagName, layout as templateLayout } from '@ember-decorators/component';
+import Component from '@ember/component';
+import layout from '../../templates/components/field-modes/view';
+
+@templateLayout(layout)
+@tagName('')
+export default class FieldViewModeComponent extends Component {
+}
---
+++ app/components/field-modes/edit.js
@@ -0,0 +1,1 @@
+export { default } from 'boxel/components/field-modes/edit';
