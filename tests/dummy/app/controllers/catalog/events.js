e0ec5bad888297ff1e70558b508b4df738d09910
Burcu Noyan <burcunoyan@gmail.com>
2019-09-23 11:49:23 -0400

    Tray animation clean up

--- tests/dummy/app/controllers/catalog/events.js
+++ tests/dummy/app/controllers/catalog/events.js
@@ -2,8 +2,7 @@ import Controller from '@ember/controller';
 import { action, set } from '@ember/object';
 import resize from 'ember-animated/motions/resize';
 import move from 'ember-animated/motions/move';
-import adjustCSS from 'ember-animated/motions/adjust-css';
-import { printSprites } from 'ember-animated';
+// import { printSprites } from 'ember-animated';
 
 export default class CatalogEventsController extends Controller {
   @action
@@ -18,24 +17,18 @@ export default class CatalogEventsController extends Controller {
     }
   }
 
-
   * trayAnimation({ keptSprites }) {
     // printSprites(arguments[0]);
-
     keptSprites.forEach(sprite => {
       move(sprite);
       resize(sprite);
-      // adjustCSS(sprite, 'padding');
     });
   }
 
-  * contentAnimation({ keptSprites }) {
-    printSprites(arguments[0]);
-
+  * holdContent({ keptSprites }) {
+    // printSprites(arguments[0]);
     keptSprites.forEach(sprite => {
       move(sprite);
-      // resize(sprite);
-      // sprite.applyStyles({ position: 'fixed' })
     });
   }
 }
