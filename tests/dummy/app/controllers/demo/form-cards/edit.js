252aa6bd253ff01611b5d369916a9d2990e2810d
Edward Faulkner <ef@alum.mit.edu>
2019-09-30 16:33:26 -0400

    improved interruptions and simplified shadows

--- tests/dummy/app/controllers/demo/form-cards/edit.js
+++ tests/dummy/app/controllers/demo/form-cards/edit.js
@@ -4,8 +4,8 @@ import { action, set } from '@ember/object';
 // import { printSprites } from 'ember-animated';
 import move from 'ember-animated/motions/move';
 import resize from 'ember-animated/motions/resize';
-import opacity from 'ember-animated/motions/opacity';
 import { easeInAndOut } from 'ember-animated/easings/cosine';
+import boxShadow from 'ember-animated/motions/box-shadow';
 
 import { duration } from './index';
 
@@ -18,11 +18,17 @@ export default class DemoFormCardsEditController extends Controller {
   * backgroundTransition ({ insertedSprites, removedSprites, receivedSprites }) {
     // printSprites(arguments[0], 'edit background transition');
 
-    insertedSprites.concat(receivedSprites).forEach(sprite => {
+    insertedSprites.forEach(sprite => {
       sprite.startAtPixel({ y: -1.5 * window.innerHeight });
       move(sprite, { easing: easeInAndOut, duration });
       sprite.applyStyles({ 'z-index': 2 });
     });
+
+    receivedSprites.forEach(sprite => {
+      move(sprite, { easing: easeInAndOut, duration });
+      sprite.applyStyles({ 'z-index': 2 });
+    });
+
 
     removedSprites.forEach(sprite => {
       sprite.endAtPixel({ y: -1.5 * window.innerHeight });
@@ -37,6 +43,7 @@ export default class DemoFormCardsEditController extends Controller {
     sentSprites.forEach(sprite => {
       move(sprite, { easing: easeInAndOut, duration });
       resize(sprite, { easing: easeInAndOut, duration });
+      boxShadow(sprite);
       sprite.applyStyles({
         'z-index': 3
       });
@@ -45,6 +52,7 @@ export default class DemoFormCardsEditController extends Controller {
     receivedSprites.forEach(sprite => {
       move(sprite, { easing: easeInAndOut, duration });
       resize(sprite, { easing: easeInAndOut, duration });
+      boxShadow(sprite);
       sprite.applyStyles({
         'z-index': 3
       });
@@ -57,32 +65,6 @@ export default class DemoFormCardsEditController extends Controller {
         y: sprite.initialBounds.top
       });
       move(sprite, { easing: easeInAndOut, duration });
-      sprite.applyStyles({
-        'z-index': 1
-      });
-    });
-  }
-
-  * shadowTransition({ sentSprites, removedSprites }) {
-    // printSprites(arguments[0], 'edit shadow transition');
-
-    sentSprites.forEach(sprite => {
-      move(sprite, { easing: easeInAndOut, duration });
-      resize(sprite, { easing: easeInAndOut, duration });
-      opacity(sprite, { to: 0, easing: easeInAndOut, duration });
-      sprite.applyStyles({
-        'z-index': 3
-      });
-    });
-
-    // TODO: ideally there wouldn't be removedSprites, only sentSprites
-    removedSprites.forEach(sprite => {
-      sprite.endAtPixel({
-        x: sprite.initialBounds.left,
-        y: sprite.initialBounds.top
-      });
-      move(sprite, { easing: easeInAndOut, duration });
-      opacity(sprite, { to: 0, easing: easeInAndOut, duration });
       sprite.applyStyles({
         'z-index': 1
       });
--- tests/dummy/app/controllers/demo/form-cards/index.js
+++ tests/dummy/app/controllers/demo/form-cards/index.js
@@ -1,26 +1,20 @@
 import Controller from '@ember/controller';
-import { action, set } from '@ember/object';
+import { action } from '@ember/object';
 
-// import { printSprites } from 'ember-animated';
+import { printSprites } from 'ember-animated';
 import move from 'ember-animated/motions/move';
 import opacity from 'ember-animated/motions/opacity';
-import scale from '../../../motions/scale';
-import keep from '../../../motions/keep';
-import resize from 'ember-animated/motions/resize';
+import scale from 'ember-animated/motions/scale';
 import { easeInAndOut } from 'ember-animated/easings/cosine';
+
+import keep from '../../motions/keep';
+import scaleBy from '../../motions/scale';
 
 export let duration = 600;
 
 export default class DemoFormCardsIndexController extends Controller {
   @action edit(card) {
-    for (let elt of this.model) {
-      if (elt.id === card.id) {
-        set(elt, 'expanded', !elt.expanded);
-        this.transitionToRoute('form-cards.edit', elt);
-      } else {
-        set(elt, 'expanded', false);
-      }
-    }
+    this.transitionToRoute('form-cards.edit', card.id);
   }
 
   * wait({ removedSprites, receivedSprites }) {
@@ -28,8 +22,8 @@ export default class DemoFormCardsIndexController extends Controller {
     removedSprites.concat(receivedSprites).forEach(keep);
   }
 
-  * backgroundTransition({ removedSprites, insertedSprites, receivedSprites }) {
-    // printSprites(arguments[0], 'index background transition');
+  * backgroundTransition({ removedSprites, insertedSprites, sentSprites }) {
+    printSprites(arguments[0], 'index background transition');
 
     let factor = 0.8;
 
@@ -39,38 +33,28 @@ export default class DemoFormCardsIndexController extends Controller {
         y: sprite.initialBounds.top + ((1 - factor) / 2 * sprite.initialBounds.height)
       });
       opacity(sprite, { to: 0, easing: easeInAndOut, duration });
-      scale(sprite, { by: factor, easing: easeInAndOut, duration });
+      scaleBy(sprite, { by: factor, easing: easeInAndOut, duration });
       move(sprite, { easing: easeInAndOut, duration });
       sprite.applyStyles({ 'z-index': 1 });
     });
 
-    insertedSprites.concat(receivedSprites).forEach(sprite => {
+    insertedSprites.forEach(sprite => {
       sprite.startAtPixel({
         x: sprite.finalBounds.left + ((1 - factor) / 2 * sprite.finalBounds.width),
         y: sprite.finalBounds.top + ((1 - factor) / 2 * sprite.finalBounds.height)
       });
       sprite.scale(factor, factor);
       opacity(sprite, { from: 0, easing: easeInAndOut, duration });
-      scale(sprite, { by: 1 / factor, easing: easeInAndOut, duration });
+      scaleBy(sprite, { by: 1 / factor, easing: easeInAndOut, duration });
       move(sprite, { easing: easeInAndOut, duration });
       sprite.applyStyles({ 'z-index': 1 });
     });
-  }
-
-  /*
-    TODO: unlike the box transition animation, the animation for the div that supplies box-shadow
-    doesn't work as expected when moved to `receivedSprites` in `edit.js`
-  */
-  * shadowTransition({ sentSprites }) {
-    // printSprites(arguments[0], 'index shadow transition');
 
     sentSprites.forEach(sprite => {
-      move(sprite, { easing: easeInAndOut, duration });
-      resize(sprite, { easing: easeInAndOut, duration });
-      opacity(sprite, { from: 0, easing: easeInAndOut, duration });
-      sprite.applyStyles({
-        'z-index': 3
-      });
+      opacity(sprite, { easing: easeInAndOut, duration });
+      scale(sprite, { easing: easeInAndOut, duration });
+      move(sprite, { disableMomentum: true, easing: easeInAndOut, duration });
     });
+
   }
 }
