39015b0452b188f00416a44c5c488fc184b2c6c8
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2019-10-01 15:05:44 -0400

    comment cleanup

--- packages/test-support/addon/environment.js
+++ packages/test-support/addon/environment.js
@@ -1,3 +1,3 @@
 // This is just a stub so that embroider doesn't get mad that this file doesn't exist.
-// When the HUB_ENVIRONMENT is not "test", then we'll spit out this file, which is just a no-op.
+// When the EMBER_ENV is not "test", then we'll spit out this file, which is just a no-op.
 export const __iDontDoAnything__ = null;
