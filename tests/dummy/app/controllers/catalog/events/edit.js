2be1c666717c7d2285a882c0888ed0ec0d41e6a5
Burcu Noyan <burcunoyan@gmail.com>
2019-10-02 14:19:29 -0400

    Edit page v1 transitions and styles

--- tests/dummy/app/controllers/catalog/events/edit.js
+++ tests/dummy/app/controllers/catalog/events/edit.js
@@ -2,9 +2,9 @@ import Controller from '@ember/controller';
 import { action, set } from '@ember/object';
 import resize from 'ember-animated/motions/resize';
 import move from 'ember-animated/motions/move';
-// import adjustCSS from 'ember-animated/motions/adjust-css';
-// import { default as opacity } from 'ember-animated/motions/opacity';
-import { printSprites } from 'ember-animated';
+import adjustCSS from 'ember-animated/motions/adjust-css';
+import { default as opacity } from 'ember-animated/motions/opacity';
+// import { printSprites } from 'ember-animated';
 
 export default class CatalogEventsEditController extends Controller {
   @action
@@ -13,19 +13,66 @@ export default class CatalogEventsEditController extends Controller {
     this.transitionToRoute('catalog.events');
   }
 
-  * trayAnimation({ receivedSprites, sentSprites }) {
-    printSprites(arguments[0], 'edit tray');
-
+  * trayAnimation({ receivedSprites }) {
     receivedSprites.forEach(sprite => {
       move(sprite);
       resize(sprite);
       sprite.applyStyles({ 'z-index': 1 });
     });
+  }
 
+  * cardTransition({ sentSprites, receivedSprites }) {
     sentSprites.forEach(sprite => {
       move(sprite);
       resize(sprite);
-      sprite.applyStyles({ 'z-index': 1 });
+      sprite.applyStyles({ 'z-index': 2 });
     });
+
+    receivedSprites.forEach(sprite => {
+      move(sprite);
+      resize(sprite);
+      sprite.applyStyles({ 'z-index': 2 });
+    });
+  }
+
+  * imageTransition({ sentSprites }) {
+    // printSprites(arguments[0], 'image transition');
+    sentSprites.forEach(sprite => {
+      move(sprite);
+      resize(sprite);
+      sprite.applyStyles({ 'z-index': 3 });
+    });
+  }
+
+  * headerTransition({ sentSprites }) {
+    sentSprites.forEach(sprite => {
+      move(sprite);
+      sprite.applyStyles({ 'z-index': 3 });
+    });
+  }
+
+  * bodyTransition({ sentSprites, receivedSprites, duration }) {
+    sentSprites.forEach(sprite => {
+      move(sprite);
+      resize(sprite);
+      opacity(sprite, { to: 0,  duration: duration / 3 });
+      sprite.applyStyles({ 'z-index': 4 });
+    });
+
+    receivedSprites.forEach(sprite => {
+      move(sprite);
+      resize(sprite);
+      opacity(sprite, { from: 0 });
+      sprite.applyStyles({ 'z-index': 4 });
+    });
+  }
+
+  * tweenTitle({ sentSprites }) {
+    sentSprites.forEach(sprite => {
+      move(sprite);
+      resize(sprite);
+      adjustCSS('font-size', sprite);
+      sprite.applyStyles({ 'z-index': 4 });
+    })
   }
 }
