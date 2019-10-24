63d693b9bfc214c6f4e088caf949d19a31a21718
Edward Faulkner <ef@alum.mit.edu>
2019-10-24 10:54:15 -0400

    only follow devDeps at top level

--- packages/test-support/lib/find-dependencies.ts
+++ packages/test-support/lib/find-dependencies.ts
@@ -19,7 +19,7 @@ function loadPackages() {
   return result;
 }
 
-function deps(pkg: PackageJSON) {
+function deps(pkg: PackageJSON, includeDev: boolean) {
   let result: Set<string> = new Set();
 
   if (pkg.dependencies) {
@@ -27,9 +27,11 @@ function deps(pkg: PackageJSON) {
       result.add(dep);
     }
   }
-  if (pkg.devDependencies) {
-    for (let dep of Object.keys(pkg.devDependencies)) {
-      result.add(dep);
+  if (includeDev) {
+    if (pkg.devDependencies) {
+      for (let dep of Object.keys(pkg.devDependencies)) {
+        result.add(dep);
+      }
     }
   }
 
@@ -45,19 +47,28 @@ export default function findDependencies(startPackage: string) {
   }
 
   let used: Set<PackageJSON> = new Set();
-  crawl(pkg, packages, used);
+  crawl(pkg, packages, used, 0);
   return [...used].map(p => p.name);
 }
 
-function crawl(pkg: PackageJSON, packages: Map<string, PackageJSON>, used: Set<PackageJSON>) {
+function crawl(
+  pkg: PackageJSON,
+  packages: Map<string, PackageJSON>,
+  used: Set<PackageJSON>,
+  depth: number
+) {
   if (used.has(pkg)) {
     return;
   }
   used.add(pkg);
-  for (let dep of deps(pkg)) {
+  for (let dep of deps(pkg, depth === 0)) {
     let depPkg = packages.get(dep);
     if (depPkg) {
-      crawl(depPkg, packages, used);
+      crawl(depPkg, packages, used, depth + 1);
     }
   }
+}
+
+if (require.main === module) {
+  process.stdout.write(findDependencies(process.argv[2]).sort().join("\n"));
 }
