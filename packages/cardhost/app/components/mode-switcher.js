ced0cf701c2ec0f6048230da3135a19c710d890d
Burcu Noyan <burcunoyan@gmail.com>
2019-10-31 10:29:02 -0400

    Use this.args instead of properties

--- packages/cardhost/app/components/mode-switcher.js
+++ packages/cardhost/app/components/mode-switcher.js
@@ -1,5 +1,4 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
@@ -22,21 +21,15 @@ export default class ModeSwitcher extends Component {
   modes = modes;
 
   @service router;
-  @tracked currentCardName;
-  @tracked currentMode;
 
   constructor(...args) {
     super(...args);
-
-    this.currentCardName = this.args.name;
-    this.currentMode = this.args.mode;
   }
 
   @action
   selectMode(mode) {
-    if (!mode || !this.currentCardName) { return; }
+    if (!mode || !this.args.name) { return; }
 
-    this.currentMode = mode;
-    this.router.transitionTo(`cards.${this.currentMode}`, this.currentCardName);
+    this.router.transitionTo(`cards.${mode}`, this.args.name);
   }
 }
