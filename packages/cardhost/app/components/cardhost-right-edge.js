68af5e48264d97a1d1aec239acade7feefaea497
Derrick Camerino <robustdj@gmail.com>
2019-11-01 14:10:26 -0700

    Make right edge section title dynamic; add helper text field

---
+++ packages/cardhost/app/components/cardhost-right-edge.js
@@ -0,0 +1,15 @@
+import Component from '@glimmer/component';
+
+const typeToInputTypeMap = {
+  '@cardstack/core-types::string': 'Text Field',
+  '@cardstack/core-types::case-insensitive': 'Text Field',
+  '@cardstack/core-types::boolean': 'Checkbox',
+  '@cardstack/core-types::date': 'Date',
+  '@cardstack/core-types::integer': 'Text Field',
+}
+
+export default class CardhostRightEdge extends Component {
+  get sectionTitle() {
+    return typeToInputTypeMap[this.args.selectedField.type] || 'Text Field';
+  }
+}
