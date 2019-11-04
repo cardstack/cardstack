661749ed9c85f7e2a01c74123ec9a96e405c5cae
Derrick Camerino <robustdj@gmail.com>
2019-11-04 13:23:14 -0800

    Remove unneeded import

--- packages/cardhost/app/components/card-creator.js
+++ packages/cardhost/app/components/card-creator.js
@@ -1,5 +1,4 @@
 import { action } from '@ember/object';
-import { dasherize } from '@ember/string';
 import CardManipulator from "./card-manipulator";
 
 export default class CardCreator extends CardManipulator {
