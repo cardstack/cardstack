515eca37591e32741fa55c2a6245e3a4fc8e1256
Will Bagby <bagsbee@gmail.com>
2019-11-06 15:05:43 -0500

    Welcome page links scroll to appropriate section

---
+++ packages/cardhost/app/controllers/index.js
@@ -0,0 +1,20 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+export default class IndexController extends Controller {
+  @service scroller;
+
+  @tracked selectedSection = 'recent-cards';
+
+  @action
+  scrollToSection(sectionId) {
+    if (!sectionId) { return; }
+    this.scroller.scrollToSection({
+      selector: `.cardhost-section--${sectionId}`,
+      elementOffset: 160,
+      doneScrolling: () => this.selectedSection = sectionId
+    });
+  }
+}
