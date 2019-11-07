0144c902d9ef6c64b0643326400cb03b1575bed0
Jen Weber <j@jenweber.me>
2019-11-06 21:45:09 -0500

    depend on github repo for requirejs polyfill

--- packages/cardhost/app/components/code-editor.js
+++ packages/cardhost/app/components/code-editor.js
@@ -1,10 +1,14 @@
 import Component from '@glimmer/component';
+import requirejsContext from 'requirejs-monaco-ember-polyfill';
 import * as monaco from 'monaco-editor';
 import { action } from '@ember/object';
+
+
 
 export default class CodeEditor extends Component {
   @action
   renderEditor(el) {
+    requirejsContext();
     monaco.editor.create(el, {
       value: this.args.code,
       language: this.args.language,
