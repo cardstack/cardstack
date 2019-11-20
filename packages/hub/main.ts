dcca47d2075f8007c7b8388b9914491023e9c2b7
Edward Faulkner <ef@alum.mit.edu>
2019-11-20 13:50:14 -0500

    http logging

--- packages/hub/main.ts
+++ packages/hub/main.ts
@@ -1,5 +1,15 @@
 import Koa from 'koa';
+import logger from '@cardstack/logger';
+const log = logger('cardstack/server');
 
 export async function makeServer() {
-  return new Koa();
+  let app = new Koa();
+  app.use(httpLogging);
+  return app;
+}
+
+async function httpLogging(ctxt: Koa.Context, next: Koa.Next) {
+  log.info('start %s %s', ctxt.request.method, ctxt.request.originalUrl);
+  await next();
+  log.info('finish %s %s %s', ctxt.request.method, ctxt.request.originalUrl, ctxt.response.status);
 }
