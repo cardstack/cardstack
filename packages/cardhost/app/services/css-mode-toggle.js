89bb2cfce31ecf2c16eb0b53fc4a49b01ef96c44
Derrick Camerino <robustdj@gmail.com>
2019-11-20 15:01:54 -0800

    track editingCss

--- packages/cardhost/app/services/css-mode-toggle.js
+++ packages/cardhost/app/services/css-mode-toggle.js
@@ -1,6 +1,9 @@
 import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class CssModeToggleService extends Service {
+  @tracked editingCss;
+
   setEditingCss(value) {
     this.editingCss = value;
   }
