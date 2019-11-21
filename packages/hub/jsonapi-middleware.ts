6e5a73c7d30bbcd10ef44b810348d13d1efd08ed
Edward Faulkner <ef@alum.mit.edu>
2019-11-21 17:31:22 -0500

    mime-type support & expanded test

--- packages/hub/jsonapi-middleware.ts
+++ packages/hub/jsonapi-middleware.ts
@@ -1,21 +1,57 @@
 import compose from "koa-compose";
 import route from "koa-better-route";
 import Koa from "koa";
+// @ts-ignore
+import mimeMatch from "mime-match";
+import { Memoize } from "typescript-memoize";
+
+const apiPrefix = /^\/api\/(.*)/;
 
 export default class JSONAPIMiddleware {
   middleware() {
+    return (ctxt: Koa.Context, next: Koa.Next) => {
+      let m = apiPrefix.exec(ctxt.request.path);
+      if (!m) {
+        return next();
+      }
+      ctxt.request.path = `/${m[1]}`;
+
+      if (this.isJSONAPI(ctxt)) {
+        return this.jsonHandlers(ctxt, next);
+      } else {
+        throw new Error(`not implemented`);
+      }
+    };
+  }
+
+  @Memoize()
+  get jsonHandlers() {
     return compose([
       //route.get("/cards", getCards),
-      route.post("/api/cards", this.createCard.bind(this))
+      route.post("/cards", this.createCard.bind(this))
       // route.get("/cards/:id", getCard),
       // route.patch("/cards/:id", updateCard),
       // route.delete("/cards/:id", deleteCard)
     ]);
   }
 
+  isJSONAPI(ctxt: Koa.Context) {
+    let contentType = ctxt.request.headers["content-type"];
+    let isJsonApi =
+      contentType && contentType.includes("application/vnd.api+json");
+    let [acceptedTypes]: string[] = (
+      ctxt.request.headers["accept"] || ""
+    ).split(";");
+    let types = acceptedTypes.split(",");
+    let acceptsJsonApi = types.some(t =>
+      mimeMatch(t, "application/vnd.api+json")
+    );
+    return isJsonApi || acceptsJsonApi;
+  }
+
   createCard(ctxt: Koa.Context) {
     ctxt.body = "Hello world";
-    ctxt.status = 200;
+    ctxt.status = 201;
   }
 }
 
--- packages/hub/node-tests/jsonapi-test.ts
+++ packages/hub/node-tests/jsonapi-test.ts
@@ -1,7 +1,6 @@
-
-import Koa from 'koa';
-import { wireItUp } from '../main';
-import supertest from 'supertest';
+import Koa from "koa";
+import { wireItUp } from "../main";
+import supertest from "supertest";
 
 describe("hub/jsonapi", function() {
   let request: supertest.SuperTest<supertest.Test>;
@@ -9,13 +8,49 @@ describe("hub/jsonapi", function() {
   beforeEach(async function() {
     let app = new Koa();
     let container = await wireItUp();
-    let jsonapi = await container.lookup('jsonapi-middleware');
+    let jsonapi = await container.lookup("jsonapi-middleware");
     app.use(jsonapi.middleware());
     request = supertest(app.callback());
   });
 
   it("can create card", async function() {
-    let response = await request.post('/api/cards');
-    expect(response.status).to.equal(200);
+    let response = await request
+      .post("/api/cards")
+      .set("Content-Type", "application/vnd.api+json")
+      .send({
+        data: {
+          id: "local-hub::new-card-476650",
+          type: "cards",
+          relationships: {
+            "adopted-from": {
+              data: { type: "cards", id: "core-catalog::@cardstack/base-card" }
+            }
+          },
+          attributes: {
+            model: {
+              attributes: {
+                "new-field-0": "hello",
+              }
+            },
+            "field-order": ["new-field-0"],
+            fields: {
+              "new-field-0": {
+                attributes: {
+                  caption: "Your New Field"
+                },
+                relationships: {
+                  definition: {
+                    data: {
+                      type: "cards",
+                      id: "core-catalog::@cardstack/string"
+                    }
+                  }
+                }
+              },
+            }
+          }
+        }
+      });
+    expect(response.status).to.equal(201);
   });
 });
