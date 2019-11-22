cf02bc474dbbf3c2eba553e67755f43e1880a9e7
Derrick Camerino <robustdj@gmail.com>
2019-11-22 14:13:14 -0800

    Code Editor layout fixes (#1062)
    * Add css edit mode to layout view; make code editor editable
    * Add resizable functionality to code-editor
    * cardhost - update babel-eslint, closes #1060
    * Fix editor keeps growing bug
    * Add resizable param to code editor in other modes in card builder

--- packages/cardhost/app/components/code-editor.js
+++ packages/cardhost/app/components/code-editor.js
@@ -2,8 +2,8 @@ import Component from '@glimmer/component';
 import requirejsPolyfill from '@cardstack/requirejs-monaco-ember-polyfill';
 import * as monaco from 'monaco-editor';
 import { action } from '@ember/object';
+import { restartableTask } from "ember-concurrency-decorators";
 import { timeout } from "ember-concurrency";
-import { task } from "ember-concurrency";
 
 /**
  * <CodeEditor> takes the following arguments:
@@ -25,11 +25,23 @@ export default class CodeEditor extends Component {
   editor; // value is set by renderEditor
   editorIsReady = false; // set to true when the first code block renders
 
+  constructor(...args) {
+    super(...args);
+
+    this.resizable = (this.args.resizable === true) ? true : false;
+  }
+
   // Set default debounce in milliseconds.
   // This limits how often updateCode is called.
   get debounceMs() {
     let ms = this.args.debounceMs
     return (ms !== undefined) ? this.args.debounceMs : 500;
+  }
+
+  // Sets default resize interval check in milliseconds.
+  // This limits how often updatedDimensions is called.
+  get resizeCheckIntervalMs() {
+    return this.args.resizeCheckIntervalMs || 2000;
   }
 
   // readOnly defaults to true
@@ -44,6 +56,10 @@ export default class CodeEditor extends Component {
 
   get updateCode() {
     return this.args.updateCode || function() {};
+  }
+
+  updateDimensions(opts) {
+    this.editor.layout(opts);
   }
 
   @action
@@ -66,8 +82,10 @@ export default class CodeEditor extends Component {
     // `create` constructs a code editor and inserts it into the DOM.
     // el is the element that {{did-insert}} was used on.
     let codeModel = monaco.editor.createModel(this.args.code, this.args.language)
-    let height = codeModel.getLineCount() * 20;
-    el.style.height = height.toString() + "px";
+    if (!this.resizable) {
+      let height = codeModel.getLineCount() * 20;
+      el.style.height = height.toString() + "px";
+    }
     let editor = monaco.editor.create(el, {
       model: codeModel,
       theme: 'vs-dark',
@@ -81,6 +99,10 @@ export default class CodeEditor extends Component {
     editor.onDidChangeModelContent(this.onUpdateCode)
     // Save editor instance locally, so we can reference it in other methods
     this.editor = editor;
+
+    if (this.resizable) {
+      this.startResizeWatcher.perform(el);
+    }
   }
 
   @action
@@ -89,12 +111,32 @@ export default class CodeEditor extends Component {
     this.debounceAndUpdate.perform()
   }
 
-  debounceAndUpdate = task(function * () {
+  @restartableTask
+  * debounceAndUpdate() {
     // This is a rate limiter so that fast typing doesn't wreck things.
     yield timeout(this.debounceMs)
     let code = this.editor.getValue() // get the current text contents of the code editor
     if (this.validate(code)) {
       this.updateCode(code);
     }  
-  }).restartable()
+  }
+
+  @restartableTask
+  * startResizeWatcher (wrapper) {
+    wrapper.style['padding-bottom'] = '20px';
+
+    let { offsetWidth, offsetHeight } = wrapper;
+
+    while (true) {
+      yield timeout(this.resizeCheckIntervalMs);
+
+      let { offsetWidth: newOffsetWidth, offsetHeight: newOffsetHeight } = wrapper;
+      if (offsetHeight !== newOffsetHeight || offsetWidth !== newOffsetWidth) {
+        offsetHeight = newOffsetHeight;
+        offsetWidth = newOffsetWidth;
+        let editorHeight = offsetHeight - 15; // So that the editor doesn't cover the resize corner
+        this.updateDimensions({height: editorHeight, width: offsetWidth});
+      }
+    }
+  }
 }
--- packages/cardhost/app/controllers/cards/view.js
+++ packages/cardhost/app/controllers/cards/view.js
@@ -1,5 +1,9 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+
 export default class ViewCardController extends Controller {
+  @service cssModeToggle
+
   get cardJson() {
     if (!this.model) { return null; }
     return JSON.stringify(this.model.json, null, 2);
