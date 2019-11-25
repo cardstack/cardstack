7a38ecbc6f37976d370db4a2d03b55fb55a1dcec
Jen Weber <j@jenweber.me>
2019-11-25 18:27:53 -0500

    show card CSS and markup in themer
    closes #1065

--- packages/cardhost/app/components/editor-pane.js
+++ packages/cardhost/app/components/editor-pane.js
@@ -4,6 +4,17 @@ import { action } from '@ember/object';
 
 export default class EditorPane extends Component {
   @tracked dockLocation = "bottom";
+  @tracked markup;
+  @tracked css = this.args.model.isolatedCss;
+
+
+  constructor() {
+    super(...arguments)
+    // getting markup must be done in the constuctor to guarantee that it
+    // resolves before the code editor is rendered.
+    let cardMarkup = document.querySelector('.card-renderer-isolated--main')
+    this.markup = cardMarkup ? cardMarkup.innerHTML : "";
+  }
 
   @action
   dockRight() {
@@ -15,4 +26,26 @@ export default class EditorPane extends Component {
     this.dockLocation = "bottom";
   }
 
+  @action
+  preview() {
+    var css = this.css
+    let newStyle = document.createElement('style')
+    newStyle.setAttribute('id', 'card-styles')
+
+    document.querySelector('#card-styles').replaceWith(newStyle)
+
+    newStyle.type = 'text/css';
+    if (newStyle.styleSheet){
+      // This is required for IE8 and below.
+      newStyle.styleSheet.cssText = css;
+    } else {
+      newStyle.appendChild(document.createTextNode(css));
+    }
+  }
+
+  @action
+  updateCode(code) {
+    this.css = code
+    this.preview()
+  }
 }
