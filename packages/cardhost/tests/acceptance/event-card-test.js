b0565fd888f56a57c4ac79ef32777bae33ee5159
Jen Weber <j@jenweber.me>
2019-11-26 10:00:54 -0500

    event card test

--- packages/cardhost/tests/acceptance/event-card-test.js
+++ packages/cardhost/tests/acceptance/event-card-test.js
@@ -1,14 +1,54 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import Fixtures from '@cardstack/test-support/fixtures'
+import { createCards } from '@cardstack/test-support/card-ui-helpers';
 import { percySnapshot } from 'ember-percy';
+import { setupMockUser, login } from '../helpers/login';
+
+const eventData = [
+  ['image', 'string', true, "https',//images.unsplash.com/]photo-1542296140-47fd7d838e76"],
+  ['title','string', true,  "Ember Meetup NYC"],
+  ['date','date', true,  "2019-09-26"],
+  ['location','string', true,  "One World Trade Center"],
+  ['city','string', true,  "New York, NY"],
+  ['admission','string', true,  'Free'],
+  ['description','string', true,  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed scelerisque ex, sed elementum lorem."],
+  ['cta','string', true, 'RSVP']
+]
+
+const card1Id = 'event-card';
+const qualifiedCard1Id = `local-hub::${card1Id}`;
+
+const scenario = new Fixtures({
+  create(factory) {
+    setupMockUser(factory);
+  },
+  destroy() {
+    return [
+      { type: 'cards', id: qualifiedCard1Id }
+    ];
+  }
+});
 
 module('Acceptance | event card', function(hooks) {
   setupApplicationTest(hooks);
+  scenario.setupTest(hooks);
+
+  hooks.beforeEach(async function () {
+    this.owner.lookup('service:data')._clearCache();
+    // Until we have searching capabilities, we'll just render the contents of the
+    // local store. So the first step is to warm up the store.
+    await login();
+    await createCards({
+      [card1Id]: eventData
+    })
+    await visit(`/cards/${card1Id}`);
+  })
 
   test('visiting /event-card', async function(assert) {
     await visit('cards/event-card/');
-    assert.equal(currentURL(), 'cards/event-card/');
+    assert.equal(currentURL(), '/cards/event-card');
     await percySnapshot(assert)
   });
 });
