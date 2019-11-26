491e8e14948abd52d13b2acf4d94013921e18b8e
Jen Weber <j@jenweber.me>
2019-11-26 16:04:29 -0500

    set up actions

--- packages/cardhost/app/controllers/cards/view.js
+++ packages/cardhost/app/controllers/cards/view.js
@@ -1,5 +1,6 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class ViewCardController extends Controller {
   @service cssModeToggle
@@ -7,5 +8,11 @@ export default class ViewCardController extends Controller {
   get cardJson() {
     if (!this.model) { return null; }
     return JSON.stringify(this.model.json, null, 2);
+  }
+
+  saveCss(css) {
+    this.model.isolatedCss = css;
+    console.log(this.model)
+    // this.model.save()
   }
 }
