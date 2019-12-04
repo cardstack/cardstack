85c545b3eca9b1e68270b2809b2334ce54456913
Jen Weber <j@jenweber.me>
2019-12-04 16:16:14 -0500

    Render and persist CSS changes, closes #1065
    * Fix type error for editingCss query param
    
    * Render and persist CSS, closes #1065

--- packages/cardhost/app/components/editor-pane.js
+++ packages/cardhost/app/components/editor-pane.js
@@ -1,7 +1,23 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class EditorPane extends Component {
-  @service cssModeToggle
+  @service cssModeToggle;
+  @tracked markup;
+  @tracked css = this.args.model.isolatedCss;
 
+  constructor() {
+    super(...arguments)
+    // getting markup must be done in the constuctor to guarantee that it
+    // resolves before the code editor is rendered.
+    let cardMarkup = document.querySelector('.card-renderer-isolated--content')
+    this.markup = cardMarkup ? cardMarkup.innerHTML.toString().trim() : "";
+  }
+
+  @action
+  updateCode(code) {
+    this.args.model.setIsolatedCss(code);
+  }
 }
