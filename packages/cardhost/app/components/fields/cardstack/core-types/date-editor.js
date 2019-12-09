3799f1ba0d4fc388630a6d7916fdfeb36125c6e3
Burcu Noyan <burcunoyan@gmail.com>
2019-12-09 17:26:27 -0500

    Use TextField ui-component in more places

--- packages/cardhost/app/components/fields/cardstack/core-types/date-editor.js
+++ packages/cardhost/app/components/fields/cardstack/core-types/date-editor.js
@@ -1,9 +1,2 @@
 import BaseEditor from './base-editor';
-import { action } from '@ember/object';
-export default class DateEditor extends BaseEditor {
-  @action
-  updateFieldValue({ target: { value } }) {
-    this.fieldValue = value;
-    this.args.setFieldValue(value);
-  }
-}
+export default class DateEditor extends BaseEditor {}
