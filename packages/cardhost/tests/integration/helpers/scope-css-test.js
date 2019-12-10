9880c06fe04b76920b58e8c4884765489ecf3430
Jen Weber <j@jenweber.me>
2019-12-10 15:18:00 -0500

    fix tests

--- packages/cardhost/tests/integration/helpers/scope-css-test.js
+++ packages/cardhost/tests/integration/helpers/scope-css-test.js
@@ -3,13 +3,16 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
+let scoped = `.my-card .a {}
+.my-card .b {}`;
+
 module('Integration | Helper | scope-css', function(hooks) {
   setupRenderingTest(hooks);
 
   // Replace this with your real tests.
   test('it renders', async function(assert) {
-    await render(hbs`{{scope-css ".some-class { font-size: 10px; }" "my-card"}}`);
-
-    assert.equal(this.element.textContent.trim(), '.my-card .some-class { font-size: 10px; }');
+    await render(hbs`{{scope-css ".a {}
+.b {}" "my-card"}}`);
+    assert.equal(this.element.textContent.trim(), scoped);
   });
 });
