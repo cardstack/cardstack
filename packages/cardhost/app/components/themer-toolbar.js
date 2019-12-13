c0419fd0790b5871fdd582712ec7e2af7a113320
Jen Weber <j@jenweber.me>
2019-12-13 11:44:07 -0500

    Move themer toolbar buttons to their own component

---
+++ packages/cardhost/app/components/themer-toolbar.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+
+export default class ThemerToolbarComponent extends Component {
+  @service cssModeToggle;
+}
