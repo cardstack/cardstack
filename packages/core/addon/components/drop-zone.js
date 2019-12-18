297358188efcf3035141cf43817fceccaa2acdb9
Will Bagby <bagsbee@gmail.com>
2019-12-18 14:13:06 -0500

    WIP css transitions

--- packages/core/addon/components/drop-zone.js
+++ packages/core/addon/components/drop-zone.js
@@ -3,30 +3,42 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import fade from 'ember-animated/transitions/fade';
 import resize from 'ember-animated/motions/resize';
+import { fadeIn, fadeOut } from 'ember-animated/motions/opacity';
 import { easeInAndOut } from 'ember-animated/easings/cosine';
 import { printSprites } from 'ember-animated';
 
-export default class DropZone extends Component {
-  @tracked isOverDropZone = false;
+const duration = 250;
 
+export default class DropZone extends Component {
+  @tracked dropZoneStatus = 'outside';
   fade = fade;
 
   get stubField() {
     return {
       name: 'new field',
       label: 'New Field',
-      type: '@cardstack/core-types::string'
+      type: '@cardstack/core-types::string',
     };
+  }
+
+  get isOverDropZone() {
+    return this.dropZoneStatus === 'dragging';
   }
 
   @action dragOver(event) {
     event.preventDefault();
   }
 
-  *expandPreview({ keptSprites }) {
+  *expandPreview({ insertedSprites, removedSprites, keptSprites }) {
     printSprites(arguments[0], 'outerTransition');
-    keptSprites.forEach(sprite => {
-      resize(sprite, { easing: easeInAndOut });
+    insertedSprites.forEach(sprite => {
+      fadeIn(sprite, { duration });
     });
+    removedSprites.forEach(sprite => {
+      fadeOut(sprite, { duration });
+    });
+    // keptSprites.forEach(sprite => {
+    //   resize(sprite, { easing: easeInAndOut });
+    // });
   }
 }
