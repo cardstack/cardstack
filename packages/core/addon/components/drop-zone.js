84c59e9db948f96730db3213e2020b9f4fc42a0f
Will Bagby <bagsbee@gmail.com>
2019-12-19 10:58:13 -0500

    remove cruft

--- packages/core/addon/components/drop-zone.js
+++ packages/core/addon/components/drop-zone.js
@@ -1,15 +1,12 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import fade from 'ember-animated/transitions/fade';
 import { fadeIn, fadeOut } from 'ember-animated/motions/opacity';
-import { printSprites } from 'ember-animated';
 
 const duration = 250;
 
 export default class DropZone extends Component {
   @tracked dropZoneStatus = 'outside';
-  fade = fade;
 
   get stubField() {
     return {
@@ -28,15 +25,11 @@ export default class DropZone extends Component {
   }
 
   *expandPreview({ insertedSprites, removedSprites }) {
-    printSprites(arguments[0], 'outerTransition');
     insertedSprites.forEach(sprite => {
       fadeIn(sprite, { duration });
     });
     removedSprites.forEach(sprite => {
       fadeOut(sprite, { duration });
     });
-    // keptSprites.forEach(sprite => {
-    //   resize(sprite, { easing: easeInAndOut });
-    // });
   }
 }
