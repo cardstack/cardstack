5449dcf1801943c0e6c9cfe581503ed83b4fa751
Will Bagby <bagsbee@gmail.com>
2019-12-19 14:18:32 -0500

    Animate drag & drop fields in schema mode (#1152)
    Animate drag & drop fields in schema mode

--- packages/core/addon/components/drop-zone.js
+++ packages/core/addon/components/drop-zone.js
@@ -1,11 +1,35 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { fadeIn, fadeOut } from 'ember-animated/motions/opacity';
+
+const duration = 250;
 
 export default class DropZone extends Component {
-  @tracked isOverDropZone = false;
+  @tracked dropZoneStatus = 'outside';
+
+  get stubField() {
+    return {
+      name: 'new field',
+      label: 'New Field',
+      type: '@cardstack/core-types::string',
+    };
+  }
+
+  get isOverDropZone() {
+    return this.dropZoneStatus === 'dragging';
+  }
 
   @action dragOver(event) {
     event.preventDefault();
+  }
+
+  *expandPreview({ insertedSprites, removedSprites }) {
+    insertedSprites.forEach(sprite => {
+      fadeIn(sprite, { duration });
+    });
+    removedSprites.forEach(sprite => {
+      fadeOut(sprite, { duration });
+    });
   }
 }
