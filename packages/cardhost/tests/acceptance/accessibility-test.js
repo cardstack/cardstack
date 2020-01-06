03dc950a9e82b44f0940a6b7e36d3aee43643542
Jen Weber <j@jenweber.me>
2020-01-06 10:34:27 -0500

    Fix test

--- packages/cardhost/tests/acceptance/accessibility-test.js
+++ packages/cardhost/tests/acceptance/accessibility-test.js
@@ -59,34 +59,4 @@ module('Acceptance | accessibility', function(hooks) {
     assert.equal(currentURL(), `/`);
     assert.ok(true, 'no a11y errors found for index');
   });
-
-  test(`can navigate to card editor`, async function(assert) {
-    await login();
-    await createCards({
-      [card1Id]: [['body', 'string', false, 'test body']],
-    });
-    await visit(`/cards/${card1Id}/schema`);
-    await click('[data-test-mode-switcher] .ember-power-select-trigger');
-    await click('[data-test-mode-switcher-mode="edit"]');
-    await waitFor(`[data-test-card-edit="${card1Id}"]`, { timeout });
-
-    assert.equal(currentURL(), `/cards/${card1Id}/edit`);
-    assert.dom(`[data-test-mode-switcher] .ember-power-select-selected-item`).hasText('Edit View');
-    assert.dom(`[data-test-card-edit="${card1Id}"]`).exists();
-  });
-
-  test(`can navigate to card view`, async function(assert) {
-    await login();
-    await createCards({
-      [card1Id]: [['body', 'string', false, 'test body']],
-    });
-    await visit(`/cards/${card1Id}/schema`);
-    await click('[data-test-mode-switcher] .ember-power-select-trigger');
-    await click('[data-test-mode-switcher-mode="view"]');
-    await waitFor(`[data-test-card-view="${card1Id}"]`, { timeout });
-
-    assert.equal(currentURL(), `/cards/${card1Id}`);
-    assert.dom(`[data-test-mode-switcher] .ember-power-select-selected-item`).hasText('Layout View');
-    assert.dom(`[data-test-card-view="${card1Id}"]`).exists();
-  });
 });
