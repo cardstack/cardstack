9dc0db2cfc3201c172b20de446c428ffe80dc8a2
Jen Weber <j@jenweber.me>
2020-01-07 11:03:32 -0500

    Accessibility tests and small fixes (#1201)
    * install ember-a11y-testing
    
    * Add missing lang attribute
    
    * Add aria-label to embedded cards
    
    * Remove tabindex - not valid on divs
    
    * Fix color contrast issue on Remove button
    
    * disable browser errors for ember-a11y-testing
    
    * bugfix - no discernable text in button
    
    * Bugfix - low contrast text for a11y
    
    * Ensure that scrollable region has keyboard access
    
    * Test programatically for accessibility issues
    
    * Fix test
    
    * linting

---
+++ packages/cardhost/tests/acceptance/accessibility-test.js
@@ -0,0 +1,62 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, waitFor } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import Fixtures from '@cardstack/test-support/fixtures';
+import { createCards } from '@cardstack/test-support/card-ui-helpers';
+import { setupMockUser, login } from '../helpers/login';
+import a11yAudit from 'ember-a11y-testing/test-support/audit';
+
+const timeout = 20000;
+const card1Id = 'millenial-puppies';
+const qualifiedCard1Id = `local-hub::${card1Id}`;
+
+const scenario = new Fixtures({
+  create(factory) {
+    setupMockUser(factory);
+  },
+  destroy() {
+    return [{ type: 'cards', id: qualifiedCard1Id }];
+  },
+});
+
+module('Acceptance | accessibility', function(hooks) {
+  setupApplicationTest(hooks);
+  scenario.setupTest(hooks);
+  hooks.beforeEach(function() {
+    this.owner.lookup('service:data')._clearCache();
+  });
+
+  test('basic a11y tests for main routes', async function(assert) {
+    await login();
+    await createCards({
+      [card1Id]: [['body', 'string', false, 'test body']],
+    });
+
+    await visit(`/cards/${card1Id}/edit`);
+    await waitFor(`[data-test-card-edit="${card1Id}"]`, { timeout });
+    assert.equal(currentURL(), `/cards/${card1Id}/edit`);
+    await a11yAudit();
+    assert.ok(true, 'no a11y errors found for edit');
+
+    await visit(`/cards/${card1Id}/schema`);
+    await waitFor(`[data-test-card-schema="${card1Id}"]`, { timeout });
+    assert.equal(currentURL(), `/cards/${card1Id}/schema`);
+    await a11yAudit();
+    assert.ok(true, 'no a11y errors found for schema');
+
+    await visit(`/cards/${card1Id}`);
+    await waitFor(`[data-test-card-view="${card1Id}"]`, { timeout });
+    assert.equal(currentURL(), `/cards/${card1Id}`);
+    await a11yAudit();
+    assert.ok(true, 'no a11y errors found for layout');
+
+    await visit(`/cards/${card1Id}?editingCss=true`);
+    assert.equal(currentURL(), `/cards/${card1Id}?editingCss=true`);
+    await a11yAudit();
+    assert.ok(true, 'no a11y errors found for themer');
+
+    await visit('/');
+    assert.equal(currentURL(), `/`);
+    assert.ok(true, 'no a11y errors found for index');
+  });
+});
