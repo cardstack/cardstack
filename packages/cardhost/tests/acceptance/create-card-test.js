e6ccefb4d6b08ce2689235d738df54c1881e53fc
Burcu Noyan <burcunoyan@gmail.com>
2020-01-15 12:58:41 -0500

    Fix linting errors

--- packages/cardhost/tests/acceptance/create-card-test.js
+++ packages/cardhost/tests/acceptance/create-card-test.js
@@ -112,19 +112,25 @@ module('Acceptance | card create', function(hooks) {
     await click('[data-test-field="body"] [data-test-field-schema-renderer]');
     assert.dom('[data-test-isolated-card="millenial-puppies"] [data-test-field="body"]').hasClass('selected');
     assert.dom('[data-test-field="body"] [data-test-field-renderer-type]').hasText('body (Text)');
-    assert.dom('[data-test-field="body"] [data-test-field-renderer-type]').hasAttribute('style', 'background-image: url("/images/field-types/text-field-icon.svg")');
+    assert
+      .dom('[data-test-field="body"] [data-test-field-renderer-type]')
+      .hasAttribute('style', 'background-image: url("/images/field-types/text-field-icon.svg")');
     assert.dom('[data-test-right-edge] [data-test-schema-attr="embedded"] input').isNotChecked();
 
     await click('[data-test-field="author"] [data-test-field-schema-renderer]');
     assert.dom('[data-test-isolated-card="millenial-puppies"] [data-test-field="author"]').hasClass('selected');
     assert.dom('[data-test-field="author"] [data-test-field-renderer-type]').hasText('author (Single-select)');
-    assert.dom('[data-test-field="author"] [data-test-field-renderer-type]').hasAttribute('style', 'background-image: url("/images/field-types/dropdown-field-icon.svg")');
+    assert
+      .dom('[data-test-field="author"] [data-test-field-renderer-type]')
+      .hasAttribute('style', 'background-image: url("/images/field-types/dropdown-field-icon.svg")');
     assert.dom('[data-test-right-edge] [data-test-schema-attr="embedded"] input').isChecked();
 
     await click('[data-test-field="reviewers"] [data-test-field-schema-renderer]');
     assert.dom('[data-test-isolated-card="millenial-puppies"] [data-test-field="reviewers"]').hasClass('selected');
     assert.dom('[data-test-field="reviewers"] [data-test-field-renderer-type]').hasText('reviewers (Multi-select)');
-    assert.dom('[data-test-field="reviewers"] [data-test-field-renderer-type]').hasAttribute('style', 'background-image: url("/images/field-types/has-many-field-icon.svg")');
+    assert
+      .dom('[data-test-field="reviewers"] [data-test-field-renderer-type]')
+      .hasAttribute('style', 'background-image: url("/images/field-types/has-many-field-icon.svg")');
     assert.dom('[data-test-right-edge] [data-test-schema-attr="embedded"] input').isChecked();
 
     await focus('[data-test-card-renderer-isolated]');
