35f3d03052c51dfd859acae0d65a08def2859e86
Jen Weber <j@jenweber.me>
2020-01-17 15:27:54 -0500

    Builder app - update to Ember 3.15 Octane (#1244)
    * Builder app - update ember to 3.15 octane
    * disable default-async-observers

--- packages/cardhost/app/router.js
+++ packages/cardhost/app/router.js
@@ -1,10 +1,10 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL,
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
   this.route('cards', function() {
@@ -25,5 +25,3 @@ Router.map(function() {
   });
   this.route('ui-components');
 });
-
-export default Router;
