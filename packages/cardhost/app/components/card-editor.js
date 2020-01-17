794e9e1ac77ee91332147193fdd081d92238e8c5
Will Bagby <bagsbee@gmail.com>
2020-01-17 14:58:59 -0500

    WIP plumbing

--- packages/cardhost/app/components/card-editor.js
+++ packages/cardhost/app/components/card-editor.js
@@ -1,2 +1,20 @@
 import CardManipulator from './card-manipulator';
-export default class CardEditor extends CardManipulator {}
+import resize from 'ember-animated/motions/resize';
+import move from 'ember-animated/motions/move';
+import adjustCSS from 'ember-animated/motions/adjust-css';
+import { printSprites } from 'ember-animated';
+// import { easeInAndOut } from 'ember-animated/easings/cosine';
+// import { remove } from 'lodash';
+
+export default class CardEditor extends CardManipulator {
+  *cardTransition({ sentSprites, receivedSprites }) {
+    printSprites(arguments[0], 'card edit transition');
+    sentSprites.concat(receivedSprites).forEach(sprite => {
+      move(sprite);
+      resize(sprite);
+      adjustCSS('padding', sprite);
+      adjustCSS('border-radius', sprite);
+      sprite.applyStyles({ 'z-index': '2' });
+    });
+  }
+}
