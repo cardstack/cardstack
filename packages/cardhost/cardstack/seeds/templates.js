0eef2dea5e4c9fe2b3aeebed46ddbb45554eb502
Burcu Noyan <burcunoyan@gmail.com>
2020-01-22 17:19:34 -0500

    Block fields

--- packages/cardhost/cardstack/seeds/templates.js
+++ packages/cardhost/cardstack/seeds/templates.js
@@ -129,9 +129,9 @@ let eventCardTemplate = eventFactory.getDocumentFor(
         eventLocation: 'One World Trade Center',
         eventCity: 'New York, NY',
         eventAdmission: 'Free',
-        eventCta: '',
         eventDescription:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed scelerisque ex, sed elementum lorem. Phasellus sit amet ipsum in tellus vestibulum tincidunt. Etiam rhoncus, orci quis elementum pulvinar, leo lectus feugiat ligula, vel tincidunt massa elit eu augue. Nulla eget tortor non est ullamcorper egestas eu sit amet justo. Cras consectetur tempor dui, eget finibus orci vestibulum vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec faucibus mi sed turpis posuere euismod. Sed leo erat, ultricies non ligula eu, ornare consectetur justo. Donec non orci tellus. Aenean ac nibh imperdiet, sollicitudin risus eu, malesuada ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam commodo sed lorem posuere lobortis. Nam a condimentum nulla, nec tempor dolor. Fusce tincidunt, mi at viverra cursus, tellus metus consequat massa, nec interdum urna ante non libero.',
+        eventCta: '',
       })
     )
 );
