10be6ada8e1ac98658e696328fe50875e7a04cee
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2020-01-26 09:24:14 -0500

    using ESM to import modules for cardstack-hub bin

--- packages/hub/main.ts
+++ packages/hub/main.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-process-exit */
+
 import Koa from 'koa';
 import logger from '@cardstack/logger';
 import { Registry, Container } from './dependency-injection';
@@ -40,6 +42,67 @@ export async function makeServer(container?: Container) {
   app.use((await container.lookup('authentication-middleware')).middleware());
   app.use((await container.lookup('jsonapi-middleware')).middleware());
   return app;
+}
+
+export function bootEnvironment() {
+  if (process.env.EMBER_ENV === 'test') {
+    logger.configure({
+      defaultLevel: 'warn',
+    });
+  } else {
+    logger.configure({
+      defaultLevel: 'warn',
+      logLevels: [['cardstack/*', 'info']],
+    });
+  }
+
+  process.on('warning', (warning: Error) => {
+    if (warning.stack) {
+      process.stderr.write(warning.stack);
+    }
+  });
+
+  if (process.connected === false) {
+    // This happens if we were started by another node process with IPC
+    // and that parent has already died by the time we got here.
+    //
+    // (If we weren't started under IPC, `process.connected` is
+    // undefined, so this never happens.)
+    log.info(`Shutting down because connected parent process has already exited.`);
+    process.exit(0);
+  }
+  process.on('disconnect', () => {
+    log.info(`Hub shutting down because connected parent process exited.`);
+    process.exit(0);
+  });
+
+  runServer(startupConfig()).catch((err: Error) => {
+    log.error('Server failed to start cleanly: %s', err.stack || err);
+    process.exit(-1);
+  });
+}
+
+async function runServer(config: StartupConfig) {
+  let app = await makeServer();
+  app.listen(config.port);
+  log.info('server listening on %s', config.port);
+  if (process.connected) {
+    process.send!('hub hello');
+  }
+}
+
+interface StartupConfig {
+  port: number;
+}
+
+function startupConfig(): StartupConfig {
+  let config: StartupConfig = {
+    port: 3000,
+  };
+  if (process.env.PORT) {
+    config.port = parseInt(process.env.PORT, 10);
+  }
+  return config;
 }
 
 async function httpLogging(ctxt: Koa.Context, next: Koa.Next) {
