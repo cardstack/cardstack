332531d8d293c641ccb35f07ceaf05c627d6157a
Burcu Noyan <burcunoyan@gmail.com>
2020-01-29 14:31:27 -0500

    Add starter page

--- packages/cardhost/app/controllers/index.js
+++ packages/cardhost/app/controllers/index.js
@@ -4,9 +4,29 @@ import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class IndexController extends Controller {
+  @service cardstackSession;
   @service scroller;
 
   @tracked selectedSection = 'recent-cards';
+
+  featuredCards = [
+    {
+      title: 'Product Ranking',
+      preview: '/assets/images/cards/started-page/theme-product-ranking.png',
+    },
+    {
+      title: 'Wedding Invitation',
+      preview: '/assets/images/cards/started-page/theme-wedding-invitation.png',
+    },
+    {
+      title: 'Event Ticket',
+      preview: '/assets/images/cards/started-page/theme-event-ticket.png',
+    },
+    {
+      title: 'Photo Card',
+      preview: '/assets/images/cards/started-page/theme-photo-card.png',
+    },
+  ];
 
   @action
   scrollToSection(sectionId) {
