68dd4b74fcbed47cdee8aa6d19c731b37e747d6b
Jen Weber <j@jenweber.me>
2020-02-05 15:30:31 -0500

    Track all menu state on the parent

--- packages/cardhost/app/components/card-renderer-header.js
+++ packages/cardhost/app/components/card-renderer-header.js
@@ -1,27 +1,12 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class CardRendererHeaderComponent extends Component {
   @service cardstackSession;
-  @tracked contextMenuOpened = false;
 
   @action
   toggleMenu() {
-    this.contextMenuOpened = !this.contextMenuOpened;
-  }
-
-  // When the header is hidden behind the card, we need to close
-  // the menu and save the closed state. Otherwise, if the user
-  // hovers over the card again, the header would pop up with this
-  // menu stil open.
-  @action
-  resetMenu() {
-    this.contextMenuOpened = false;
-  }
-
-  get showContextMenu() {
-    return (this.contextMenuOpened && this.args.cardSelected) || (this.contextMenuOpened && this.args.alwaysShowHeader);
+    this.args.setContextMenu(!this.args.contextMenuOpen);
   }
 }
