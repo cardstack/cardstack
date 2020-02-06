bf86d7c596a2cfa04dd111b0a943d5725d629384
Burcu Noyan <burcunoyan@gmail.com>
2020-02-06 17:43:51 -0500

    Update edit-mode close button

--- packages/cardhost/app/controllers/cards/card/edit.js
+++ packages/cardhost/app/controllers/cards/card/edit.js
@@ -1,6 +1,12 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class EditCardController extends Controller {
   @service routeInfo;
+
+  @action
+  leaveEditMode() {
+    this.transitionToRoute('cards.card.view', this.model);
+  }
 }
