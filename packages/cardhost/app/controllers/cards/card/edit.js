36255ba398ff455a98e2eb8639f411bb19ee9b29
Burcu Noyan <burcunoyan@gmail.com>
2020-02-06 18:31:56 -0500

    Better for accessibility

--- packages/cardhost/app/controllers/cards/card/edit.js
+++ packages/cardhost/app/controllers/cards/card/edit.js
@@ -1,12 +1,6 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class EditCardController extends Controller {
   @service routeInfo;
-
-  @action
-  leaveEditMode() {
-    this.transitionToRoute('cards.card.view', this.model);
-  }
 }
