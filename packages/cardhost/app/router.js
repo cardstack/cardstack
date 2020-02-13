1e2fe3445a382b5b79557f828eceba9729ace70e
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2020-02-13 16:09:06 -0500

    Added card view route

--- packages/cardhost/app/router.js
+++ packages/cardhost/app/router.js
@@ -12,6 +12,7 @@ Router.map(function() {
     // using card-v2 to build out the app with the new data service. consolidate
     // back into the /cards/card route when ready
     this.route('card-v2', { path: '/v2/:id' }, function() {
+      this.route('view', { path: '' });
       this.route('edit', function() {
         this.route('layout', function() {
           this.route('themer');
