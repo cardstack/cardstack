ce30fe4fa2d34ee6370e48e596ae05a3d15c0f4a
Burcu Noyan <burcunoyan@gmail.com>
2020-02-17 15:55:42 -0500

    Update tests

--- packages/cardhost/tests/integration/components/themer-toolbar-test.js
+++ packages/cardhost/tests/integration/components/themer-toolbar-test.js
@@ -9,6 +9,17 @@ module('Integration | Component | themer-toolbar', function(hooks) {
   test('it renders', async function(assert) {
     this.set('model', {});
     await render(hbs`<ThemerToolbar @model={{this.model}}/>`);
-    assert.dom(this.element).hasTextContaining('Close');
+    assert.dom('[data-test-preview-link-btn]').exists();
+    assert.dom('[data-test-card-size-toggle]').exists();
+    assert.dom('[data-test-small-btn]').hasClass('selected');
+    assert.dom('[data-test-hide-editor-btn]').exists();
+    assert.dom('[data-test-hide-editor-btn]').hasClass('eye-on');
+    assert.dom('[data-test-dock-bottom]').exists();
+    assert.dom('[data-test-dock-right]').exists();
+    assert.dom('[data-test-mode-indicator]').exists();
+    assert.dom('[data-test-mode-indicator]').containsText('themer mode');
+    assert.dom('[data-test-mode-indicator-link="layout"]').exists();
+    assert.dom('[data-test-card-save-btn]').exists();
+    assert.dom('[data-test-edge-actions-btn]').exists();
   });
 });
