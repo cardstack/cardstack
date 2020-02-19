76d7c0ff21fd2914fd68f19aff790421e4156f06
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2020-02-19 06:30:25 -0500

    add an edge build target

--- packages/cardhost/config/targets.js
+++ packages/cardhost/config/targets.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
-
 module.exports = {
-  browsers,
+  browsers: ['last 1 edge versions', 'last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'],
 };
