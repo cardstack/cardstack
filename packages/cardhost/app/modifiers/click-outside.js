dc3790ffb0b10b433096ed524d7eb99a34627796
Jen Weber <j@jenweber.me>
2020-02-19 17:40:29 -0500

    Better docs

--- packages/cardhost/app/modifiers/click-outside.js
+++ packages/cardhost/app/modifiers/click-outside.js
@@ -1,18 +1,25 @@
 import { modifier } from 'ember-modifier';
 
 /*
+
+`<div {{click-outside this.someFunction ignore=".some-selector"}}> ... </div>`
+
 Use this modifier if you want to call an action whenever someone clicks or focuses
 on an element that is outside the one you've put the modifier on. In the example below,
 clicking on the "some-other-thing" div will call the closeTheMenu function passed in.
 
-It takes a positional arg that is the function to call, and an optional selector of an
-elememnt to ignore. For example, if a button is clicked to reveal a nav menu,
-you will want to ignore clicks to the button along with ignoring clicks to the nav.
+It takes a positional arg that is the function to call, and an optional selector string of an
+element to ignore (i.e. `.some-class` or `#some-id`). For example, if a button controls revealing a nav menu,
+you will want to ignore that button's clicks.
 
 ```html
-<div class="some-menu"  {{click-outside this.closeTheMenu ignore=".some-selector"}}>
-  ...
-</div>
+<button class="my-button" {{on "click" this.closeTheMenu}}>Open</button>
+
+{{#if this.showMenu}}
+  <div class="some-menu" {{click-outside this.closeTheMenu ignore=".my-button"}}>
+    ...
+  </div>
+{{/if}}
 
 <div class="some-other-thing">
   ...
