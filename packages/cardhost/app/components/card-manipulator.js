2839f6aed9f47e0128a297c7590373871e9f3f8b
Will Bagby <bagsbee@gmail.com>
2020-02-24 16:24:51 -0500

    Drop draggable into field (#1373)
    Drop draggable into field

--- packages/cardhost/app/components/card-manipulator.js
+++ packages/cardhost/app/components/card-manipulator.js
@@ -9,6 +9,9 @@ import ENV from '@cardstack/cardhost/config/environment';
 import { fieldTypeMappings, fieldComponents } from '../utils/mappings';
 import drag from '../motions/drag';
 import move from 'ember-animated/motions/move';
+import scaleBy from '../motions/scale';
+import { parallel } from 'ember-animated';
+import { fadeOut } from 'ember-animated/motions/opacity';
 
 const { environment } = ENV;
 
@@ -368,9 +371,19 @@ export default class CardManipulator extends Component {
       activeSprite.element.parentElement.appendChild(ghostElement);
       others.forEach(move);
     } else {
-      keptSprites.forEach(sprite => {
-        move(sprite, { duration: 300 });
-      });
+      let droppedSprite = keptSprites.find(sprite => sprite.owner.value.dropTo);
+      if (droppedSprite) {
+        let scaleTo = 0.1;
+        let dropZoneEl = droppedSprite.owner.value.dropTo;
+        let position = parseInt(dropZoneEl.dataset.dropZonePosition) + 1;
+        let targetField = document.querySelector(
+          `.isolated-card section:nth-of-type(${position}) .schema-field-renderer`
+        );
+        let { width, height } = targetField.getBoundingClientRect();
+        droppedSprite.endTranslatedBy(((1 - scaleTo) / 2) * width, ((1 - scaleTo) / 2) * height);
+        yield parallel(scaleBy(droppedSprite, { by: scaleTo }), move(droppedSprite), fadeOut(droppedSprite));
+        droppedSprite.owner.value.dropTo = null;
+      }
     }
   }
 }
