988bba5a52accb62608a7b1a053618ca6099257b
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2020-03-03 11:39:47 -0500

    added missing test for scoping css of multiple cards simultaneously

--- packages/cardhost/tests/integration/helpers/scope-css-test.js
+++ packages/cardhost/tests/integration/helpers/scope-css-test.js
@@ -3,16 +3,25 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-let scoped = `.my-card--isolated .a {}
+let scopedSingleCard = `.my-card--isolated .a {}
 .my-card--isolated .b {}`;
+let scopedMultipleCards = `.card1--embedded .a , .card2--embedded .a {}
+.card1--embedded .b , .card2--embedded .b {}`;
 
 module('Integration | Helper | scope-css', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders for a single card', async function(assert) {
     this.set('card', { canonicalURL: 'my-card' });
     await render(hbs`{{scope-css ".a {}
 .b {}" card "isolated"}}`);
-    assert.equal(this.element.textContent.trim(), scoped);
+    assert.equal(this.element.textContent.trim(), scopedSingleCard);
+  });
+
+  test('it renders for multiple cards', async function(assert) {
+    this.set('cards', [{ canonicalURL: 'card1' }, { canonicalURL: 'card2' }]);
+    await render(hbs`{{scope-css ".a {}
+.b {}" cards "embedded"}}`);
+    assert.equal(this.element.textContent.trim(), scopedMultipleCards);
   });
 });
