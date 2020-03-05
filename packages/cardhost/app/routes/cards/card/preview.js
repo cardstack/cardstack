7acd6901956d9e1bd77a8070ec20823b0c1c4274
Burcu Noyan <burcunoyan@gmail.com>
2020-03-05 17:47:32 -0500

    If it exists, transition to previous route from preview

---
+++ packages/cardhost/app/routes/cards/card/preview.js
@@ -0,0 +1,8 @@
+import CardModelRoute from './card-model-route';
+
+export default class PreviewCardRoute extends CardModelRoute {
+  beforeModel(transition) {
+    let cardsController = this.controllerFor('cards');
+    cardsController.set('previousTransition', transition.from);
+  }
+}
