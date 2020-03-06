a9e59a60a25bac167996dac53016100dabe6dd62
Burcu Noyan <burcunoyan@gmail.com>
2020-03-06 17:28:49 -0500

    Linting fix

--- packages/cardhost/tests/acceptance/card-preview-test.js
+++ packages/cardhost/tests/acceptance/card-preview-test.js
@@ -5,9 +5,7 @@ import Fixtures from '@cardstack/test-support/fixtures';
 import { createCards } from '@cardstack/test-support/card-ui-helpers';
 import { setupMockUser, login } from '../helpers/login';
 import { percySnapshot } from 'ember-percy';
-import { animationsSettled } from 'ember-animated/test-support';
 
-const timeout = 20000;
 const card1Id = 'millenial-puppies';
 const qualifiedCard1Id = `local-hub::${card1Id}`;
 const card2Id = 'van-gogh';
