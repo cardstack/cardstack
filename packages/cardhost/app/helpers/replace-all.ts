4ff4ffe1ab85091f2ae0108282f11912c3600761
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2020-03-09 16:10:01 -0400

    moving all TS helpers to JS (working around embroider issues in ember deploy)

--- packages/cardhost/app/helpers/replace-all.ts
+++
@@ -1,9 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export default helper(function([string, from, to = '']) {
-  if (!from || !string) {
-    return string;
-  }
-  let regex = new RegExp(from, 'g');
-  return string.replace(regex, to);
-});
