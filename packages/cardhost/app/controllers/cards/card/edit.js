788209d70ab3791495fdb04b63d9a3b15c6e77c3
Burcu Noyan <burcunoyan@gmail.com>
2020-03-11 13:20:01 -0400

    Combine right-edge toolbar actions for both edit modes

--- packages/cardhost/app/controllers/cards/card/edit.js
+++ packages/cardhost/app/controllers/cards/card/edit.js
@@ -1,6 +1,41 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class EditCardController extends Controller {
   @service routeInfo;
+  @service router;
+
+  get isDefault() {
+    let css = this.model.isolatedCss;
+
+    if (css) {
+      // Some cards have a null value for isolatedCss, so we have to typecheck
+      // For the comparison, first strip out whitespace and newlines so that extra whitespace doesn't register as a custom theme
+      css = css.replace(/\s*|[\r\n]/g, '');
+      return css === '' || css === '.cardstack_base-card-isolated{}';
+    } else {
+      return true;
+    }
+  }
+
+  @tracked
+  themerOptions = [{ name: 'Cardstack default' }, { name: 'Custom' }];
+
+  @action
+  createTheme() {
+    this.router.transitionTo('cards.card.edit.layout.themer', this.model);
+  }
+
+  get selectedTheme() {
+    return this.isDefault ? { name: 'Cardstack default' } : { name: 'Custom' };
+  }
+
+  @action
+  handleThemeChange(val) {
+    if (val.name === 'Cardstack default') {
+      this.model.setIsolatedCss('.cardstack_base-card-isolated {}');
+    }
+  }
 }
