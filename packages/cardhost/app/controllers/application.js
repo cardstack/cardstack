5b34ac25e0c2ecaca9343c9105863978e73db20d
Jen Weber <j@jenweber.me>
2020-03-11 10:31:34 -0400

    bugfix - loading page never resolved for new card (#1424)
    * bugfix - loading page never resolved for new card
    
    Whenever you created a new card, it would just
    hang with the loading icon spinning, even on
    a successful server response.
    
    * Remove animation from loading, use try-catch

--- packages/cardhost/app/controllers/application.js
+++ packages/cardhost/app/controllers/application.js
@@ -1,9 +1,10 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import fade from 'ember-animated/transitions/fade';
 
 export default class ApplicationController extends Controller {
   @service overlays;
 
-  fade = fade;
+  get showLoading() {
+    return this.overlays.showLoading;
+  }
 }
