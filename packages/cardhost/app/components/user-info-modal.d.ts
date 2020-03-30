22795f8dffee3a3085616cfbe417e3c03e301256
Burcu Noyan <burcunoyan@gmail.com>
2020-03-30 18:04:21 -0400

    Add new component generated files to .gitignore

--- packages/cardhost/app/components/user-info-modal.d.ts
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-export default class UserInfoModalComponent extends Component<{
-    title: string;
-    mode: string;
-    icon: string;
-    closeDialog: () => void;
-}> {
-    willDestroy(): void;
-}
--- packages/cardhost/app/components/user-info-modal.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-export default class UserInfoModalComponent extends Component {
-    willDestroy() {
-        if (this.args.closeDialog) {
-            this.args.closeDialog();
-        }
-    }
-}
-//# sourceMappingURL=user-info-modal.js.map
