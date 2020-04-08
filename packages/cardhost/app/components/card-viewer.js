2ac4042663cfb63bbd752ff721a0457fb5dea34d
Alex Speller <alex@alexspeller.com>
2020-04-08 14:24:01 +0100

    Improve how header and border show when hovering over a card
    Prevent header and border showing when hovering outside of the visible card
    boundary.
    
    Also clean up some monkey patching of the card api by making the property live
    on the component instead of the card and changing what the animated-value component
    watches

--- packages/cardhost/app/components/card-viewer.js
+++ packages/cardhost/app/components/card-viewer.js
@@ -3,21 +3,9 @@ import { tracked } from '@glimmer/tracking';
 import CardManipulator from './card-manipulator';
 
 export default class CardViewer extends CardManipulator {
-  @tracked isSelected = false;
   @tracked contextMenuOpen = false;
 
   resizeable = true;
-
-  @action
-  setSelected(bool) {
-    this.isSelected = bool;
-    if (!bool) {
-      // When the card header is closed, we need to set the context menu to closed as well.
-      // Otherwise, when you hover over the card again, you will see the context menu
-      // without clicking on the ... button
-      this.contextMenuOpen = false;
-    }
-  }
 
   @action
   setContextMenu(bool) {
