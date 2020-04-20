5a2c288d895d409d77dffc81ef373597c6e04fbc
Burcu Noyan <burcunoyan@gmail.com>
2020-04-20 18:11:28 -0400

    Add fallback for change view format action

--- addon/components/collection-isolated.js
+++ addon/components/collection-isolated.js
@@ -1,10 +1,22 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class CollectionIsolatedComponent extends Component {
-  @action close() {
+  @tracked format = this.args.format || 'grid';
+
+  @action
+  close() {
     if (this.args.close) {
       this.args.close();
     }
+  }
+
+  @action
+  changeFormat(val) {
+    if (this.args.changeFormat) {
+      this.args.changeFormat(val);
+    }
+    this.format = val;
   }
 }
