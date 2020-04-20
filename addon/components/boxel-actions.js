b9227ba86cd21f6d04375df86ff1bed65fe5497b
Burcu Noyan <burcunoyan@gmail.com>
2020-04-20 17:27:20 -0400

    Adds isolated coll action buttons; can change format

--- addon/components/boxel-actions.js
+++ addon/components/boxel-actions.js
@@ -1,14 +1,16 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default class BoxelActions extends Component {
   @service router;
+  @tracked format = this.args.format || 'grid';
 
   @action
   expand() {
     if (this.args.expandRoute && this.args.expandId) {
-      this.router.transitionTo(this.args.expandRoute, this.args.expandId);
+      this.router.transitionTo(this.args.expandRoute, this.args.expandId, { queryParams: { format: this.format }});
     }
   }
 }
--- tests/dummy/app/controllers/movie-registry/edit/collection-view.js
+++ tests/dummy/app/controllers/movie-registry/edit/collection-view.js
@@ -3,11 +3,18 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class MovieRegistryEditCollectionViewController extends Controller {
+  queryParams = ['format'];
+  @tracked format = 'grid';
   @tracked isModalOpen = true;
 
   @action
   closeModal() {
     this.isModalOpen = false;
     this.transitionToRoute('movie-registry.edit');
+  }
+
+  @action
+  changeFormat(format) {
+    this.format = format;
   }
 }
