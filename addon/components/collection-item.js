399e6714bc51e598dad7900e97f6fb5ab43e018e
Burcu Noyan <burcunoyan@gmail.com>
2020-04-21 12:20:10 -0400

    Can toggle between list and grid views

--- addon/components/collection-item.js
+++ addon/components/collection-item.js
@@ -1,7 +1,4 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 
 export default class CollectionItemComponent extends Component {
-  @tracked mode = this.args.mode || 'view';
-  @tracked format = this.args.format || 'list';
 }
