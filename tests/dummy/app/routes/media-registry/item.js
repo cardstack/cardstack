47bfe36c8249b820e86bb9558e3b74dd7fedd4b3
Burcu Noyan <burcunoyan@gmail.com>
2020-05-07 13:38:15 -0400

    Update media data and data display

--- tests/dummy/app/routes/media-registry/item.js
+++ tests/dummy/app/routes/media-registry/item.js
@@ -2,14 +2,16 @@ import Route from '@ember/routing/route';
 import fetch from 'fetch';
 import { dasherize } from '@ember/string';
 
+const MASTER_TRACKS = '/media-registry/api/bunny_records_tracks.json';
+
 export default class MediaRegistryItemRoute extends Route {
   async model({ itemId }) {
-    const data = await fetch('/data/full_catalog_bunny_records_table_1.json');
+    const data = await fetch(MASTER_TRACKS);
     const records = await data.json();
 
     const record = records.filter(item => {
       if (item.catalog) {
-        return dasherize(item.song_title) === itemId;
+        return dasherize(item.song_title.trim()) === itemId;
       }
     })[0];
 
