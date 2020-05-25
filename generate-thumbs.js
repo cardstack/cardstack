574368fea76da112c5fb74669b7078bb3cc61d15
Alex Speller <alex@alexspeller.com>
2020-05-25 14:33:34 +0100

    Add image thumbs
    After looking into imgix for static deployments, I figured out a way we could
    generate signed urls on build. It requires patches to both broccoli-asset-rev
    and broccoli-fingerprint.
    
    Instead, I made a quick script to generate thumbs with imagemagick for static
    deploy to solve the immediate performance issue.
    
    It also adds the urls into the JSON files automatically.
    
    I think imgix is a good choice for an app with a backend server that can
    sign stuff, but not for static site deployment.

---
+++ generate-thumbs.js
@@ -0,0 +1,65 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const { basename, dirname, join } = require('path');
+const { writeFileSync } = require('fs');
+
+const sizes = {
+  thumb: "200x200",
+  medium: "640x480",
+  large: "1280x1024",
+}
+
+const coverDir = "tests/dummy/public/media-registry/covers";
+
+const jsonToAddKeys = ["./tests/dummy/public/media-registry/api/custom_catalog_batch_f_table_1.json", "./tests/dummy/public/media-registry/api/bunny_records_tracks.json"];
+
+(async function() {
+
+  for (let sizeName of Object.keys(sizes)) {
+    await process(sizeName);
+  }
+
+  for (let file of jsonToAddKeys) {
+    let json = require(file);
+
+    for (let record of json) {
+
+      let currentPath = record.cover_art;
+
+
+      if (currentPath) {
+
+        let dirName = dirname(currentPath);
+        let baseName = basename(currentPath);
+
+        for (let sizeName of Object.keys(sizes)) {
+          let key = `cover_art_${sizeName}`;
+          record[key] = join(dirName, sizeName, baseName);
+        }
+      }
+
+
+      writeFileSync(file, JSON.stringify(json, null, 2));
+
+
+
+    }
+
+
+  }
+})();
+
+async function process(sizeName) {
+  let outDir = join(coverDir, sizeName)
+  let size = sizes[sizeName];
+
+  await runCmd(`rm -rf ${outDir}`)
+  await runCmd(`mkdir ${outDir}`)
+  await runCmd(`mogrify -verbose -path ${outDir} -thumbnail ${size}  ${join(coverDir, '*.jpg')}`)
+}
+
+
+async function runCmd(cmd) {
+  console.log("Running command:", cmd); // eslint-disable-line no-console
+  await exec(cmd);
+}
