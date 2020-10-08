5fbed2b5a2247ca819ae09b2331cc4b9351c8507
Burcu Noyan <burcunoyan@gmail.com>
2020-10-08 16:35:37 -0400

    Remove generated files

--- packages/cardhost/app/components/isolated-collection.d.ts
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-export default class IsolatedCollection extends Component {
-    format: string;
-    selectedCards: string[];
-    constructor(owner: unknown, args: any);
-    changeFormat(val: string): void;
-    toggleSelect(id: string): string[] | undefined;
-}
