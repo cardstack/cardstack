eb7171cb13bf1080767af37b0d1e9a9ab093404f
Burcu Noyan <burcunoyan@gmail.com>
2020-10-16 18:41:52 -0400

    More collections components and styles

--- packages/cardhost/app/routes/cards/collection.ts
+++ packages/cardhost/app/routes/cards/collection.ts
@@ -4,7 +4,13 @@ import { inject as service } from '@ember/service';
 //@ts-ignore
 import { task } from 'ember-concurrency';
 import DataService from '../../services/data';
-import { AddressableCard } from '@cardstack/hub';
+import CardLocalStorageService from '../../services/card-local-storage';
+import { getUserRealm } from '../../utils/scaffolding';
+import { AddressableCard, CARDSTACK_PUBLIC_REALM } from '@cardstack/hub';
+//@ts-ignore
+import ENV from '@cardstack/cardhost/config/environment';
+
+const { environment } = ENV;
 
 const verifiRealm = 'https://builder-hub.stack.cards/api/realms/verifi';
 const size = 100;
@@ -17,6 +23,7 @@ interface Model {
 }
 export default class CollectionRoute extends Route {
   @service data!: DataService;
+  @service cardLocalStorage!: CardLocalStorageService;
 
   @tracked collectionEntries: AddressableCard[] = [];
 
@@ -30,21 +37,41 @@ export default class CollectionRoute extends Route {
   }
 
   @task(function*(this: CollectionRoute) {
-    let collectionEntries = yield this.data.search(
-      {
-        filter: {
-          eq: {
-            csRealm: verifiRealm,
-            csTitle: collectionType,
+    let collectionEntries;
+
+    if (environment === 'development') {
+      collectionEntries = yield this.data.search(
+        {
+          filter: {
+            type: { csRealm: CARDSTACK_PUBLIC_REALM, csId: 'base' },
+            eq: {
+              csRealm: getUserRealm(),
+              csTitle: collectionType,
+            },
           },
+          sort: '-csCreated',
+          page: { size },
         },
-        sort: '-csCreated',
-        page: { size },
-      },
-      { includeFieldSet: 'embedded' }
-    );
+        { includeFieldSet: 'embedded' }
+      );
+    } else {
+      collectionEntries = yield this.data.search(
+        {
+          filter: {
+            eq: {
+              csRealm: verifiRealm,
+              csTitle: collectionType,
+            },
+          },
+          sort: '-csCreated',
+          page: { size },
+        },
+        { includeFieldSet: 'embedded' }
+      );
+    }
 
     this.collectionEntries = collectionEntries;
+    return;
   })
   load: any; //TS and EC don't play nice;
 }
