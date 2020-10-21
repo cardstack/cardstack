b48f7e7bf1a8c0c30e6abc43c9d0fb8e66297d4c
Burcu Noyan <burcunoyan@gmail.com>
2020-10-21 16:33:41 -0400

    Do not render the header and breadcrumbs everywhere

--- packages/cardhost/app/routes/index.ts
+++ packages/cardhost/app/routes/index.ts
@@ -1,9 +1,7 @@
 import Route from '@ember/routing/route';
-import Transition from '@ember/routing/-private/transition';
 
 export default class IndexRoute extends Route {
-  async beforeModel(transition: Transition) {
-    await super.beforeModel(transition);
+  async beforeModel() {
     this.transitionTo('cards');
   }
 }
