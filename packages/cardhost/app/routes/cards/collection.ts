2bfafd1998faece0f3b62ce3167fe99322dfda93
Burcu Noyan <burcunoyan@gmail.com>
2020-11-20 18:28:14 -0500

    Fix default collection fetching

--- packages/cardhost/app/routes/cards/collection.ts
+++ packages/cardhost/app/routes/cards/collection.ts
@@ -6,11 +6,13 @@ import { task } from 'ember-concurrency';
 import DataService from '../../services/data';
 import CardLocalStorageService from '../../services/card-local-storage';
 import { AddressableCard } from '@cardstack/hub';
+import { getUserRealm } from '../../utils/scaffolding';
 import { Org, USER_ORGS } from '../../services/cardstack-session';
 //@ts-ignore
 import ENV from '@cardstack/cardhost/config/environment';
 
-const { environment, hubURL } = ENV;
+const { environment, hubURL, devDir } = ENV;
+
 const realmOrigin = 'https://builder-hub.stack.cards';
 const size = 100;
 
@@ -31,13 +33,14 @@ export default class CollectionRoute extends Route {
   @tracked realmURL!: string;
 
   async model({ collection }: RouteParams): Promise<Model> {
-    let realm = collection ? collection : 'default';
     let currentOrg = USER_ORGS.find(el => el.id === collection);
 
-    if (environment === 'test') {
-      this.realmURL = `${hubURL}/api/realms/${realm}`;
+    if (environment === 'test' || (!devDir && collection === 'default')) {
+      this.realmURL = getUserRealm();
+    } else if (devDir) {
+      this.realmURL = `${hubURL}/api/realms/${collection}`;
     } else {
-      this.realmURL = `${realmOrigin}/api/realms/${realm}`;
+      this.realmURL = `${realmOrigin}/api/realms/${collection}`;
     }
 
     await this.load.perform();
