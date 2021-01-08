c7373c9df3f6d6e4d182368b41173a5108100293
Edward Faulkner <ef@alum.mit.edu>
2021-01-08 11:06:19 -0500

    generate fresh ember app at ember-cli 3.24.0

---
+++ cardhost/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'cardhost/app';
+import config from 'cardhost/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
