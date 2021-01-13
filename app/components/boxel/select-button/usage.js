cc88003e53ce5d0e67f1570c822c5c62f74bd152
Luke Melia <luke@lukemelia.com>
2021-01-13 19:16:52 +0800

    Extract Button, SelectionControlGroup, DropdownButton components
    - Introduce ember-class-names-helper
    - Add css-vars helper
    - Boxel::DropdownButton replaces MediaCollection::Button
    - Boxel::Button accepts a boolean @collectionStyle arg to apply styles for buttons in the collection view
    - Boxel::Button yields a hash making it easy to add a "..." more button with dropdown
    - SelectBtn renamed to Boxel::SelectButton

---
+++ app/components/boxel/select-button/usage.js
@@ -0,0 +1,19 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class extends Component {
+  @tracked isPartial = false;
+  @tracked isSelected = false;
+
+  @action onClick() {
+    if (this.isSelected) {
+      this.isSelected = false;
+    } else if (this.isPartial) {
+      this.isPartial = false;
+    } else {
+      this.isPartial = true;
+      this.isSelected = true;
+    }
+  }
+}
