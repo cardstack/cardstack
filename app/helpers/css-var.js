002581c852ac85c52ed8fa985e72456988e45115
Matt McManus <matt@mcmanus.io>
2021-01-14 13:55:10 -0500

    Switch to helper from element modifier for css-variable approach

---
+++ app/helpers/css-var.js
@@ -0,0 +1,26 @@
+import { helper } from '@ember/component/helper';
+import { htmlSafe } from '@ember/template';
+
+function formatValue(value) {
+  if (typeof value === 'function') {
+    value = value();
+  }
+  if (typeof value === 'string') {
+    value = `'${value}'`
+  }
+
+  return value;
+}
+
+export function cssVar(values) {
+  let vars = [];
+  Object.keys(values).forEach(name => {
+    vars.push(`--${name}: ${formatValue(values[name])}`);
+  });
+
+  return vars.join('; ')
+}
+
+export default helper(function(params, hash) {
+  return htmlSafe(cssVar(hash));
+});
---
+++ tests/unit/helpers/css-var-test.js
@@ -0,0 +1,28 @@
+import { cssVar } from '@cardstack/boxel/helpers/css-var';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Helper | css-var', function(hooks) {
+  setupTest(hooks);
+
+  test('takes an object and converts it to css variables', function(assert) {
+    assert.equal(cssVar({ foo: 'bar' }), "--foo: 'bar'");
+    assert.equal(cssVar({
+      foo: 'bar',
+      baz: 1
+    }), "--foo: 'bar'; --baz: 1");
+
+    assert.equal(cssVar({
+      'foo-bar': 'baz',
+    }), "--foo-bar: 'baz'");
+
+    assert.equal(cssVar({
+      'fooBar': 'baz',
+    }), "--fooBar: 'baz'");
+
+    let fun = function() { return 'baz'};
+    assert.equal(cssVar({
+      'fooBar': fun
+    }), "--fooBar: 'baz'");
+  });
+});
