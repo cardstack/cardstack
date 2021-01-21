cardstack/boxel-archive-2021

Use webpack to process stylesheets

app/routes/about.js
@@ -1,0 +1,5 @@
+import Route from '@ember/routing/route';
+import '@cardstack/boxel/assets/templates/about.css';
+
+export default class AboutRoute extends Route {
+}

app/routes/application.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 
+import '@cardstack/boxel/assets/boxel.js';
 export default class ApplicationRoute extends Route {
   title(tokens) {
     if (tokens) {

app/routes/docs.js
@@ -1,0 +1,5 @@
+import Route from '@ember/routing/route';
+import '@cardstack/boxel/assets/templates/docs.css';
+
+export default class DocsRoute extends Route {
+}

app/routes/media-registry.js
@@ -1,6 +1,17 @@
 import Route from '@ember/routing/route';
 import ORGS from '@cardstack/boxel/data/organizations';
 
+import '@cardstack/boxel/assets/templates/media-registry.css';
+import '@cardstack/boxel/assets/templates/media-registry/master-collection.css';
+import '@cardstack/boxel/assets/templates/media-registry/collection.css';
+import '@cardstack/boxel/assets/templates/media-registry/item.css';
+import '@cardstack/boxel/assets/templates/media-registry/item-edit.css';
+import '@cardstack/boxel/assets/templates/media-registry/agreements.css';
+import '@cardstack/boxel/assets/templates/media-registry/breadcrumbs.css';
+import '@cardstack/boxel/assets/templates/media-registry/embedded-collection-table.css';
+import '@cardstack/boxel/assets/templates/media-registry/discrepancies-index.css';
+import '@cardstack/boxel/assets/templates/media-registry/discrepancy.css';
+
 export default class MediaRegistryRoute extends Route {
   defaultGroup = [ 'bunny_records', 'crd_records' ];
   verifiGroup = [ 'warner-music-group', 'allegro-music-publishing', 'warner-chappell-music', 'global-music-rights', 'deezer'];

app/routes/workflow.js
@@ -1,8 +1,8 @@
 import Route from '@ember/routing/route';
-
 // NOTE: This import should eventually be replaced by fetch to
 // mirage data
 import dbWorkflow from '@cardstack/boxel/data/db-workflow'
+import '@cardstack/boxel/assets/templates/workflow.css';
 
 const USER_ID = 'haley-oconnell';
 

cardstack/cardstack

app/routes/about.js
@@ -1,0 +1,5 @@
+import Route from '@ember/routing/route';
+import '@cardstack/boxel/css/templates/about.css';
+
+export default class AboutRoute extends Route {
+}

app/routes/application.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 
+import '@cardstack/boxel/css/boxel.js';
 export default class ApplicationRoute extends Route {
   title(tokens) {
     if (tokens) {

app/routes/docs.js
@@ -1,0 +1,5 @@
+import Route from '@ember/routing/route';
+import '@cardstack/boxel/css/templates/docs.css';
+
+export default class DocsRoute extends Route {
+}

app/routes/media-registry.js
@@ -1,6 +1,16 @@
 import Route from '@ember/routing/route';
 import ORGS from '@cardstack/boxel/data/organizations';
 
+import '@cardstack/boxel/css/templates/media-registry.css';
+import '@cardstack/boxel/css/templates/media-registry/master-collection.css';
+import '@cardstack/boxel/css/templates/media-registry/collection.css';
+import '@cardstack/boxel/css/templates/media-registry/item.css';
+import '@cardstack/boxel/css/templates/media-registry/item-edit.css';
+import '@cardstack/boxel/css/templates/media-registry/agreements.css';
+import '@cardstack/boxel/css/templates/media-registry/embedded-collection-table.css';
+import '@cardstack/boxel/css/templates/media-registry/discrepancies-index.css';
+import '@cardstack/boxel/css/templates/media-registry/discrepancy.css';
+
 export default class MediaRegistryRoute extends Route {
   defaultGroup = [ 'bunny_records', 'crd_records' ];
   verifiGroup = [ 'warner-music-group', 'allegro-music-publishing', 'warner-chappell-music', 'global-music-rights', 'deezer'];

app/routes/workflow.js
@@ -1,8 +1,8 @@
 import Route from '@ember/routing/route';
-
 // NOTE: This import should eventually be replaced by fetch to
 // mirage data
 import dbWorkflow from '@cardstack/boxel/data/db-workflow'
+import '@cardstack/boxel/css/templates/workflow.css';
 
 const USER_ID = 'haley-oconnell';
 