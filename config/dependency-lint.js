cc374c67d401202dc595506f739ed917571aa0de
Luke Melia <luke@lukemelia.com>
2021-01-29 00:30:14 +0800

    Adjust dependencies and dependency-lint config to address linting issues

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -3,5 +3,7 @@
 module.exports = {
   allowedVersions: {
     'ember-truth-helpers': '2.1.0 || 3.x',
+    'ember-test-selectors': '2.1.0 || 5.x',
+    'ember-concurrency-decorators': '1.0.0 || 2.x',
   },
 };
