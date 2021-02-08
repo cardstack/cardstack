f74a43e747d846245d7ef5f85b04c257bb55c054
Luke Melia <luke@lukemelia.com>
2021-02-09 01:13:29 +0800

    Give names to some magic numbers.

--- app/components/boxel/sort-menu/index.js
+++ app/components/boxel/sort-menu/index.js
@@ -1,5 +1,9 @@
 import Component from '@glimmer/component';
 import { htmlSafe } from '@ember/template';
+
+const MINIMUM_USEFUL_MAX_HEIGHT_PX = 150;
+const VERTICAL_BUFFER_BELOW_MENU_PX = 40;
+const FALLBACK_MAX_HEIGHT = '40vh';
 export default class SortMenuComponent extends Component {
   get styleAttribute() {
     return htmlSafe(`max-height: ${this.maxHeight}`);
@@ -7,9 +11,9 @@ export default class SortMenuComponent extends Component {
 
   get maxHeight() {
     let maxHeightArg = this.args.maxHeight;
-    if (maxHeightArg && maxHeightArg > 150) {
-      return `${maxHeightArg - 40}px`;
+    if (maxHeightArg && maxHeightArg > MINIMUM_USEFUL_MAX_HEIGHT_PX) {
+      return `${maxHeightArg - VERTICAL_BUFFER_BELOW_MENU_PX}px`;
     }
-    return '40vh';
+    return FALLBACK_MAX_HEIGHT;
   }
 }
