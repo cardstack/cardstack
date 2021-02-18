96282d8b46baf3d805c6c4d1be8171b860f19a23
Luke Melia <luke@lukemelia.com>
2021-02-19 02:13:45 +0800

    Update environment.js of dummy app

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -9,7 +9,12 @@ module.exports = function (environment) {
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
-        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
+        EMBER_NATIVE_DECORATOR_SUPPORT: true,
+        EMBER_METAL_TRACKED_PROPERTIES: true,
+        EMBER_GLIMMER_ANGLE_BRACKET_NESTED_LOOKUP: true,
+        EMBER_GLIMMER_ANGLE_BRACKET_BUILT_INS: true,
+        EMBER_GLIMMER_FN_HELPER: true,
+        EMBER_GLIMMER_ON_MODIFIER: true,
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
@@ -45,6 +50,8 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.rootURL = '/boxel/';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
