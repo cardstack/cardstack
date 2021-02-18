0741fc33d8b5c9322759e5918d9eb25be82df776
Matt McManus <matt@mcmanus.io>
2021-02-18 15:26:07 -0500

    Fix flac URLs so they work in production or dev

--- addon/components/boxel/wave-player/usage.js
+++ addon/components/boxel/wave-player/usage.js
@@ -1,7 +1,8 @@
 import Component from '@glimmer/component';
 
 import { tracked } from '@glimmer/tracking';
+import demoFlac from '../../../public/@cardstack/boxel/assets/demo_flac.flac';
 
 export default class BoxelWaveplayerUsageComponent extends Component {
-  @tracked url = '/@cardstack/boxel/assets/demo_flac.flac';
+  @tracked url = demoFlac;
 }
---
+++ tests/dummy/app/components/cards/audio.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import demoFlac from '../../../../public/@cardstack/boxel/assets/demo_flac.flac';
+
+export default class MusicalWorkEmbedded extends Component {
+  audio = demoFlac;
+}
