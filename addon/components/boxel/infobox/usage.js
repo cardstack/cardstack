1e1639b6f5480145cf5b5a5c4a01f78610aa2a8c
Aierie <39579264+Aierie@users.noreply.github.com>
2021-03-16 11:51:46 +0800

    infobox prototypes (#304)
    * infobox prototypes
    
    * fix linting issues
    
    * move infobox image to usage-support
    
    * infobox initial version

---
+++ addon/components/boxel/infobox/usage.js
@@ -0,0 +1,16 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import rightInfoboxImage from '@cardstack/boxel/usage-support/images/cardspace/infobox/right-infobox-image.svg';
+
+export default class InfoboxUsage extends Component {
+  @tracked image = rightInfoboxImage;
+  @tracked imageSize = 'auto 100%';
+  @tracked imagePosition = 'right -2.25rem bottom -2.25rem';
+  @tracked title = 'Add cards to your space';
+  @tracked description =
+    'You can drag and drop different types of cards from the Card Catalog into your space. Once you’ve added a card, you can configure its fields and edit the content.';
+
+  get imageUrl() {
+    return `url(${this.image})`;
+  }
+}
