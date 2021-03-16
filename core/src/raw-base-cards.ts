698dc170645b6061ad0bbfbad9887b52cda2e272
Matt McManus <matt@mcmanus.io>
2021-03-16 18:11:59 -0400

    WIP card-loader

--- core/src/raw-base-cards.ts
+++ core/src/raw-base-cards.ts
@@ -1,41 +1,13 @@
-import StringSchemaJS from '!raw-loader!@cardstack/base-cards/string/schema.js';
-import StringEmbedded from '!raw-loader!@cardstack/base-cards/string/embedded.js';
-import DateSchemaJS from '!raw-loader!@cardstack/base-cards/date/schema.js';
-import DateEmbedded from '!raw-loader!@cardstack/base-cards/date/embedded.js';
-import TagSchemaJS from '!raw-loader!@cardstack/base-cards/tag/schema.js';
-import TagEmbedded from '!raw-loader!@cardstack/base-cards/tag/embedded.js';
-import CommentSchemaJS from '!raw-loader!@cardstack/base-cards/comment/schema.js';
-import CommentEmbedded from '!raw-loader!@cardstack/base-cards/comment/embedded.js';
+import StringCard from '!@cardstack/card-loader!@cardstack/base-cards/string/card.json';
+import DateCard from '!@cardstack/card-loader!@cardstack/base-cards/date/card.json';
+import TagCard from '!@cardstack/card-loader!@cardstack/base-cards/tag/card.json';
+import CommentCard from '!@cardstack/card-loader!@cardstack/base-cards/comment/card.json';
 
 import { RawCard } from './interfaces';
 
 export const RAW_BASE_CARDS: RawCard[] = [
-  {
-    url: 'https://cardstack.com/base/models/string',
-    files: {
-      'schema.js': StringSchemaJS,
-      'embedded.js': StringEmbedded,
-    },
-  },
-  {
-    url: 'https://cardstack.com/base/models/date',
-    files: {
-      'schema.js': DateSchemaJS,
-      'embedded.js': DateEmbedded,
-    },
-  },
-  {
-    url: 'https://cardstack.com/base/models/tag',
-    files: {
-      'schema.js': TagSchemaJS,
-      'embedded.js': TagEmbedded,
-    },
-  },
-  {
-    url: 'https://cardstack.com/base/models/comment',
-    files: {
-      'schema.js': CommentSchemaJS,
-      'embedded.js': CommentEmbedded,
-    },
-  },
+  StringCard,
+  DateCard,
+  TagCard,
+  CommentCard,
 ];
