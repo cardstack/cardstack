7e2b2d34511f6adaed5896ab6c6c47d1cd1b2839
Luke Melia <luke@lukemelia.com>
2021-03-24 23:53:35 +0800

    Build with embroider
    - Separate build steps
    - No IE 11 support needed

--- packages/web-client/config/targets.js
+++ packages/web-client/config/targets.js
@@ -6,12 +6,8 @@ const browsers = [
   'last 1 Safari versions',
 ];
 
-const isCI = Boolean(process.env.CI);
-const isProduction = process.env.EMBER_ENV === 'production';
-
-if (isCI || isProduction) {
-  browsers.push('ie 11');
-}
+// const isCI = Boolean(process.env.CI);
+// const isProduction = process.env.EMBER_ENV === 'production';
 
 module.exports = {
   browsers,
