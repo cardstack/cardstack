c95ebbb01680956e636c721e8219952e7757bdf0
Aierie <39579264+Aierie@users.noreply.github.com>
2021-03-30 17:56:03 +0800

    Fix deployment comment (#319)
    * use latest @actions/github
    
    * update action to find pr number
    - vs directly commenting
    
    * update workflow
    - only comment if deployed and there is no comment with identifier
    
    * tidy up
    
    * should not trigger comment
    
    * add names to steps
    
    * change comment message
    
    * add sparkles for flavour

---
+++ .github/actions/find-pr-number/index.js
@@ -0,0 +1,36 @@
+const core = require('@actions/core'); // eslint-disable-line node/no-unpublished-require
+const github = require('@actions/github'); // eslint-disable-line node/no-unpublished-require
+
+async function run() {
+  const branch = core.getInput('branch');
+
+  if (!branch)
+    throw new Error('The branch was not provided to the find-pr-number action');
+
+  if (!process.env.GITHUB_TOKEN)
+    throw new Error('Required Github access token');
+
+  const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+
+  const { data: pullRequests } = await octokit.pulls.list({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+  });
+
+  const prAssociatedWithBranch = pullRequests.find(({ head, base }) => {
+    console.log(head, base);
+    return base.ref === 'main' && head.ref === branch;
+  });
+
+  core.setOutput(
+    'pr-number',
+    prAssociatedWithBranch && prAssociatedWithBranch.number
+  );
+}
+
+try {
+  run();
+} catch (e) {
+  core.setFailed(e.message);
+  throw e;
+}
