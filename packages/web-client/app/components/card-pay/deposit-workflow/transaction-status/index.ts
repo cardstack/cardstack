d4fe745563ed84eaa7f1f0b1bcf02d7f0862fb3d
Luke Melia <luke@lukemelia.com>
2021-04-05 21:01:35 +0800

    Disable lint error - temporary

--- packages/web-client/app/components/card-pay/deposit-workflow/transaction-status/index.ts
+++ packages/web-client/app/components/card-pay/deposit-workflow/transaction-status/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable ember/no-empty-glimmer-component-classes */
 import Component from '@glimmer/component';
 
 class CardPayDepositWorkflowTransactionStatusComponent extends Component {}
