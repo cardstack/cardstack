c4b585123b514fb6ff7bb41a45a921dc3e75a834
Aierie <michael.khor@cardstack.com>
2021-04-06 19:16:17 +0800

    FIX! landing page without auth

--- packages/web-client/app/controllers/index.ts
+++ packages/web-client/app/controllers/index.ts
@@ -3,10 +3,18 @@ import { inject as service } from '@ember/service';
 import { Registry as Services } from '@ember/service';
 import { action } from '@ember/object';
 
+import EdgesService from '../services/edges';
+
 class IndexController extends Controller {
   @service router!: Services['router'];
+  @service declare edges: EdgesService;
+
   @action transitionTo(routeName: string) {
     this.router.transitionTo(routeName);
+  }
+
+  @action updateEdges() {
+    this.edges.updateDisplayLeftEdge(true);
   }
 }
 
