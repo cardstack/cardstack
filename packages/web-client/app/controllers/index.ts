8213be7caddbb2025fbf6f5d67284dbadf03c90b
Burcu Noyan <burieberry@users.noreply.github.com>
2021-04-07 18:10:37 -0400

    Remove unused deps, use latest boxel release (#1556)

--- packages/web-client/app/controllers/index.ts
+++ packages/web-client/app/controllers/index.ts
@@ -1,17 +1,11 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { Registry as Services } from '@ember/service';
 import { action } from '@ember/object';
 
 import Layer2Network from '@cardstack/web-client/services/layer2-network';
 
 class IndexController extends Controller {
-  @service router!: Services['router'];
   @service declare layer2Network: Layer2Network;
-
-  @action transitionTo(routeName: string) {
-    this.router.transitionTo(routeName);
-  }
 
   @action connectToWallet() {
     console.log('Connect to wallet here');
