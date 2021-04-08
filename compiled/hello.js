ce62bd9e8732730bed735101bf0b9860ea09b1a8
Matt McManus <matt@mcmanus.io>
2021-04-08 17:05:38 -0400

    Ignore cards in compiled cards

--- compiled/hello.js
+++
@@ -1,4 +0,0 @@
-import { setComponentTemplate } from "@ember/component";
-import templateOnlyComponent from "@ember/component/template-only";
-import template from "./hello.hbs";
-export default setComponentTemplate(template, templateOnlyComponent());
