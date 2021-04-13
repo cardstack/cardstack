b1e8fa844009aae73012b920450f976776ab0598
Aierie <michael.khor@cardstack.com>
2021-04-13 17:37:17 +0800

    make usage interactive

--- addon/components/boxel/cta-block/usage.ts
+++ addon/components/boxel/cta-block/usage.ts
@@ -1,31 +1,19 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class CtaBlockUsage extends Component {
   @tracked stepNumber = 1;
   @tracked canCancel = true;
   @tracked canEdit = true;
-  @tracked state = 'inProgress';
-  @tracked atRestArgs = {
-    text: 'Do it!',
-    action: (): void => {
-      console.log('doing it');
-    },
-  };
-  @tracked disabledArgs = {
-    text: 'I am disabled',
-  };
-  @tracked inProgressArgs = {
-    text: 'Doing it right now',
-    cancelText: 'Cancel me',
-    cancelAction: (): void => {
-      console.log('cancelled');
-    },
-  };
-  @tracked doneArgs = {
-    text: 'Done.',
-    action: (): void => {
-      console.log('Done but going again');
-    },
-  };
+  @tracked state = 'atRest';
+  @action changeState(str: string) {
+    console.log(str);
+    this.state = str;
+    if (str === 'inProgress') {
+      setTimeout(() => {
+        this.changeState('done');
+      }, 1500);
+    }
+  }
 }
