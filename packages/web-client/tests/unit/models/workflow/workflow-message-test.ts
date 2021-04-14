cdcc0085bb7687b06bc169dc03ffac15e9ae3441
Luke Melia <luke@lukemelia.com>
2021-04-15 01:33:55 +0800

    Add unit tests for WorkflowMessag and WorkflowCard

---
+++ packages/web-client/tests/unit/models/workflow/workflow-message-test.ts
@@ -0,0 +1,21 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import { Participant } from '../../../../app/models/workflow/workflow-postable';
+import { WorkflowMessage } from '@cardstack/web-client/models/workflow/workflow-message';
+
+module('Unit | WorkflowMessage model', function (hooks) {
+  setupTest(hooks);
+
+  let participant: Participant;
+  hooks.beforeEach(function () {
+    participant = { name: 'cardbot' };
+  });
+
+  test('isComplete starts off as true', function (assert) {
+    let subject = new WorkflowMessage({
+      author: participant,
+      message: 'Hello, world',
+    });
+    assert.equal(subject.isComplete, true);
+  });
+});
