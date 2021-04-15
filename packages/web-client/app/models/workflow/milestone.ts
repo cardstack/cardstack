3e1cda65c0ef15370eccefff3f7c896b4d2bfac7
Luke Melia <luke@lukemelia.com>
2021-04-15 19:34:28 +0800

    Render post-workflow postables once workflow is complete - extract PostableCollection to support Milestone and use for Workflow#epilogue as well

--- packages/web-client/app/models/workflow/milestone.ts
+++ packages/web-client/app/models/workflow/milestone.ts
@@ -1,4 +1,5 @@
 import { Workflow } from '../workflow';
+import PostableCollection from './postable-collection';
 import { WorkflowPostable } from './workflow-postable';
 
 interface MilestoneOptions {
@@ -8,64 +9,30 @@ interface MilestoneOptions {
 }
 export class Milestone {
   title: string;
-  postables: WorkflowPostable[] = [];
-  #includedPostables = new Set<WorkflowPostable>();
-  #excludedPostables = new Set<WorkflowPostable>();
-  workflow: Workflow | undefined;
+  #postableCollection: PostableCollection = new PostableCollection();
   setWorkflow(wf: Workflow) {
-    this.workflow = wf;
-    this.postables.invoke('setWorkflow', wf);
+    this.#postableCollection.setWorkflow(wf);
   }
+  get workflow() {
+    return this.#postableCollection.workflow;
+  }
+  get visiblePostables() {
+    return this.#postableCollection.visiblePostables;
+  }
+
   get isComplete() {
-    return this.visiblePostables.isEvery('isComplete', true);
+    return this.#postableCollection.isComplete;
   }
+
   completedDetail;
 
   constructor(opts: MilestoneOptions) {
     this.title = opts.title;
-    this.postables = opts.postables;
+    this.#postableCollection.postables = opts.postables;
     this.completedDetail = opts.completedDetail;
   }
 
-  // return visible postables that should be visible -- all completed posts up to
-  // and including the first incomplete post. Computation intentionally has some side effects:
-  //   * calculate whether a post should be visible once and only once
-  //   * set the timestamp on the post when we determine it should be visible
-  // Designed to be called from the template.
-  get visiblePostables() {
-    let postablesArr = [];
-
-    for (let i = 0; i < this.postables.length; i++) {
-      let post = this.postables[i];
-      if (this.#excludedPostables.has(post)) {
-        continue;
-      }
-      if (
-        post.includeIf &&
-        !this.#includedPostables.has(post) &&
-        post.includeIf() == false
-      ) {
-        this.#excludedPostables.add(post);
-        continue;
-      } else {
-        this.#includedPostables.add(post);
-        if (!post.timestamp) {
-          post.timestamp = new Date();
-        }
-        postablesArr.push(post);
-      }
-
-      if (!post.isComplete) {
-        break;
-      }
-    }
-
-    return postablesArr;
-  }
-
-  // return visible postables with zero side effects
   peekAtVisiblePostables() {
-    let includedPostables = this.#includedPostables;
-    return this.postables.filter((p) => includedPostables.has(p));
+    return this.#postableCollection.peekAtVisiblePostables();
   }
 }
--- packages/web-client/tests/integration/components/workflow-thread-test.ts
+++ packages/web-client/tests/integration/components/workflow-thread-test.ts
@@ -1,10 +1,12 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, waitFor } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { Workflow } from '@cardstack/web-client/models/workflow';
 import { Milestone } from '@cardstack/web-client/models/workflow/milestone';
 import { WorkflowMessage } from '@cardstack/web-client/models/workflow/workflow-message';
+import { WorkflowPostable } from '@cardstack/web-client/models/workflow/workflow-postable';
+import PostableCollection from '@cardstack/web-client/models/workflow/postable-collection';
 
 module('Integration | Component | workflow-thread', function (hooks) {
   setupRenderingTest(hooks);
@@ -26,46 +28,74 @@ module('Integration | Component | workflow-thread', function (hooks) {
 
   test('it renders date divider before first post and when the date changes', async function (assert) {
     let workflow = new ConcreteWorkflow(this.owner);
+    let postable1 = new WorkflowMessage({
+      author: { name: 'cardbot' },
+      message: 'Hello world',
+    });
+    let postable2 = new WorkflowMessage({
+      author: { name: 'cardbot' },
+      message: 'How are you today?',
+    });
+    let postable3 = new WorkflowMessage({
+      author: { name: 'joe' },
+      message: 'Fine thank you.',
+    });
+    let postable4 = new WorkflowMessage({
+      author: { name: 'steve' },
+      message: 'One more thing...',
+    });
+
     workflow.milestones = [
       new Milestone({
         title: 'First milestone',
-        postables: [
-          new WorkflowMessage({
-            author: { name: 'cardbot' },
-            message: 'Hello world',
-          }),
-        ],
+        postables: [postable1],
         completedDetail: 'You are number one!',
       }),
       new Milestone({
         title: 'Second milestone',
-        postables: [
-          new WorkflowMessage({
-            author: { name: 'cardbot' },
-            message: 'How are you today?',
-          }),
-          new WorkflowMessage({
-            author: { name: 'joe' },
-            message: 'Fine thank you.',
-          }),
-        ],
+        postables: [postable2, postable3],
         completedDetail: 'Had to do number two!',
       }),
     ];
-    workflow.milestones[0].postables[0].timestamp = new Date(
-      Date.parse('2021-05-20 00:00:00')
-    );
-    workflow.milestones[1].postables[0].timestamp = new Date(
-      Date.parse('2021-05-20 00:00:00')
-    );
-    workflow.milestones[1].postables[1].timestamp = new Date(
-      Date.parse('2021-05-21 00:00:00')
-    );
+    workflow.epilogue.postables = [postable4];
+    postable1.timestamp = new Date(Date.parse('2021-05-20 00:00:00'));
+    postable2.timestamp = new Date(Date.parse('2021-05-20 00:00:00'));
+    postable3.timestamp = new Date(Date.parse('2021-05-21 00:00:00'));
+    postable4.timestamp = new Date(Date.parse('2021-05-21 00:00:00'));
     this.set('workflow', workflow);
     workflow.attachWorkflow();
     await render(hbs`
       <WorkflowThread @workflow={{this.workflow}} />
     `);
     assert.dom('[data-test-date-divider]').exists({ count: 2 });
+  });
+
+  test('it renders epilogue posts after the workflow is complete', async function (assert) {
+    let workflow = new ConcreteWorkflow(this.owner);
+    let postable1 = new WorkflowPostable({ name: 'cardbot' });
+    workflow.milestones = [
+      new Milestone({
+        title: 'First milestone',
+        postables: [postable1],
+        completedDetail: 'You are number one!',
+      }),
+    ];
+    workflow.epilogue = new PostableCollection([
+      new WorkflowMessage({
+        author: { name: 'cardbot' },
+        message: 'This is the epilogue',
+      }),
+    ]);
+    this.set('workflow', workflow);
+    workflow.attachWorkflow();
+    await render(hbs`
+      <WorkflowThread @workflow={{this.workflow}} />
+    `);
+    assert.dom('[data-test-epilogue][data-test-postable]').doesNotExist();
+    postable1.isComplete = true;
+    await waitFor('[data-test-epilogue][data-test-postable]');
+    assert
+      .dom('[data-test-epilogue][data-test-postable]')
+      .hasTextContaining('This is the epilogue');
   });
 });
