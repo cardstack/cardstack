432e401f43a92d4b7f0daa3de13ca71700e99f84
Aierie <39579264+Aierie@users.noreply.github.com>
2021-04-16 18:00:09 +0800

    Feature/custom radio (#339)
    * initial setup for custom radio
    
    * custom radio item changes
    - handle change in radio button
    - focus on the input adds a class to the label
    - put the input outside in case we want to do aria-labelledby (not sure if it's ok inside?)
    - add @focusedClass argument
    
    * move legend out of the way for fieldset
    
    * use html input rather than Ember Input
    
    * full custom radio component
    
    * initial usage
    
    * add checkedClass
    
    * add example of checkedClass and focusedClass
    
    * update todo
    
    * update selected -> checked
    
    * add splattributes to container/main component
    
    * special case fix for chrome layout
    
    * disabled argument for fieldset
    
    * add disabled argument to freestyle
    
    * lint
    
    * use ember input component

---
+++ addon/components/boxel/custom-radio/usage.ts
@@ -0,0 +1,29 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { A } from '@ember/array';
+import { action } from '@ember/object';
+
+export default class CustomRadioUsage extends Component {
+  @tracked items = A([
+    {
+      id: 'eggs',
+      text: 'eggs',
+    },
+    {
+      id: 'tofu',
+      text: 'tofu',
+    },
+    {
+      id: 'strawberry',
+      text: 'strawberry',
+    },
+  ]);
+  @tracked groupDescription =
+    'Select one of these options for breakfast sandwiches';
+  @tracked checkedId = 'strawberry';
+  @tracked disabled = false;
+
+  @action onChange(e: InputEvent): void {
+    this.checkedId = e.target.id;
+  }
+}
