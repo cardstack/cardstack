33121250037f76bf7de535fab6e625d55a0499b2
Luke Melia <luke@lukemelia.com>
2021-04-22 00:05:42 +0800

    Add assertions on rendered token balances to deposit acceptance test

--- packages/web-client/app/components/card-pay/deposit-workflow/transaction-setup/index.ts
+++ packages/web-client/app/components/card-pay/deposit-workflow/transaction-setup/index.ts
@@ -1,7 +1,10 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import Layer1Network from '@cardstack/web-client/services/layer1-network';
 
 class CardPayDepositWorkflowTransactionSetupComponent extends Component {
+  @service declare layer1Network: Layer1Network;
   @tracked isShowingLayer1SourceOptions = false;
   @tracked isShowingLayer2TargetOptions = false;
 }
