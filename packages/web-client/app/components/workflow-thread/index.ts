92a5a5b9f30e753ad74758a1e04fc06e10bfad23
Luke Melia <luke@lukemelia.com>
2021-04-30 00:13:11 +0800

    Scroll when milestone clicked in right sidebar
    - When completed milestone clicked, scroll that milestone end to the bottom
    - When current or future milestone clicked, scroll end of thread to bottom

--- packages/web-client/app/components/workflow-thread/index.ts
+++ packages/web-client/app/components/workflow-thread/index.ts
@@ -2,7 +2,18 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class WorkflowThread extends Component {
-  @action focus(element: any) {
+  threadEl: HTMLElement | undefined;
+  @action focus(element: HTMLElement) {
+    this.threadEl = element;
     element.focus();
+  }
+  @action scrollMilestoneIntoView(milestoneIndex: number, ev: Event) {
+    ev.preventDefault();
+    let milestoneEl = this.threadEl?.querySelector(
+      `[data-milestone="${milestoneIndex}"]`
+    );
+    let targetEl =
+      milestoneEl || this.threadEl?.querySelector('[data-thread-end]');
+    targetEl?.scrollIntoView({ block: 'end', behavior: 'smooth' });
   }
 }
