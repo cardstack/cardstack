e9e193e3134eb4423f5197e11e879920c56ffd04
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-04-30 19:34:46 -0400

    clean up linting errors

--- packages/cardpay-cli/entrypoint.ts
+++ packages/cardpay-cli/entrypoint.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-/* eslint no-process-exit: "off", node/shebang: "off" */
+/* eslint no-process-exit: "off", node/shebang: "off",  @typescript-eslint/no-var-requires: "off", @typescript-eslint/no-require-imports: "off", */
 //@ts-ignore not actually redefining block-scoped var
 const esmRequire = require('esm')(module, { cjs: true });
 module.exports = esmRequire('./index');
