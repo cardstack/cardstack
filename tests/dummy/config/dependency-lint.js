af50b568931411be91640b78a91d17073a2a91f5
Luke Melia <luke@lukemelia.com>
2021-05-03 22:51:22 +0800

    Update ember-animated to 0.11.0 and ember-basic-dropdown to 3.0.17 - uses newer embroider-compatible ember-element-helper

--- tests/dummy/config/dependency-lint.js
+++ tests/dummy/config/dependency-lint.js
@@ -2,7 +2,8 @@
 
 module.exports = {
   allowedVersions: {
-    'ember-element-helper': '0.2.0 || 0.3.x',
+    '@embroider/util': '0.39.1 || 0.40.x',
+    'ember-element-helper': '0.2.0 || 0.5.x',
     'ember-fetch': '6.x || 8.x',
     'ember-test-selectors': '2.1.0 || 5.x',
   },
