18b99ac48379e7d0a785f46df93bea601f14193a
Burcu Noyan <burcunoyan@gmail.com>
2021-05-03 17:12:12 -0400

    Transaction setup / Deposit card

--- packages/web-client/app/components/card-pay/deposit-workflow/transaction-setup/index.ts
+++ packages/web-client/app/components/card-pay/deposit-workflow/transaction-setup/index.ts
@@ -1,21 +1,67 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { equal, and } from 'macro-decorators';
 import { inject as service } from '@ember/service';
 import Layer1Network from '@cardstack/web-client/services/layer1-network';
+import Layer2Network from '@cardstack/web-client/services/layer2-network';
 import WorkflowSession from '../../../../models/workflow/workflow-session';
+
 interface CardPayDepositWorkflowTransactionSetupComponentArgs {
   workflowSession: WorkflowSession;
   onComplete: (() => void) | undefined;
   onIncomplete: (() => void) | undefined;
 }
+interface token {
+  symbol: string;
+  description: string;
+  icon: string;
+}
+const TOKENS: token[] = [
+  {
+    symbol: 'DAI',
+    description: 'USD-based stablecoin',
+    icon: 'dai-token',
+  },
+  {
+    symbol: 'CARD',
+    description: 'ERC-20 Cardstack token',
+    icon: 'card-token',
+  },
+];
+
 class CardPayDepositWorkflowTransactionSetupComponent extends Component<CardPayDepositWorkflowTransactionSetupComponentArgs> {
+  tokens = TOKENS;
   @service declare layer1Network: Layer1Network;
-  @tracked isShowingLayer1SourceOptions = false;
-  @tracked isShowingLayer2TargetOptions = false;
+  @service declare layer2Network: Layer2Network;
+
+  @equal('args.workflowSession.state.depositSourceToken', 'CARD')
+  cardSelected: Boolean | undefined;
+  @equal('args.workflowSession.state.depositSourceToken', 'DAI')
+  daiSelected: Boolean | undefined;
+  @and('cardSelected', 'layer1Network.cardBalance')
+  hasCardBalance: Boolean | undefined;
+  @and('daiSelected', 'layer1Network.daiBalance')
+  hasDaiBalance: Boolean | undefined;
+
+  @tracked isComplete: boolean | undefined;
 
   @action chooseSource(tokenSymbol: string) {
     this.args.workflowSession.update('depositSourceToken', tokenSymbol);
+  }
+
+  @action onComplete() {
+    if (this.hasCardBalance || this.hasDaiBalance) {
+      this.isComplete = !this.isComplete;
+      if (this.args.onComplete) {
+        this.args.onComplete();
+      }
+    } else {
+      this.isComplete = false;
+      if (this.args.onIncomplete) {
+        this.args.onIncomplete();
+      }
+    }
   }
 }
 
