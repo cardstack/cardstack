21c9a4e6dc857104e61e7577ccc5cf46eb66bdec
Aierie <michael.khor@cardstack.com>
2021-05-03 20:50:43 +0800

    initial styled components for deposit amount

--- packages/web-client/app/components/card-pay/deposit-workflow/transaction-amount/index.ts
+++ packages/web-client/app/components/card-pay/deposit-workflow/transaction-amount/index.ts
@@ -7,28 +7,114 @@ import { inject as service } from '@ember/service';
 import { taskFor } from 'ember-concurrency-ts';
 import WorkflowSession from '@cardstack/web-client/models/workflow/workflow-session';
 import { TransactionReceipt } from 'web3-core';
+import { BigNumber } from '@ethersproject/bignumber';
 
 interface CardPayDepositWorkflowTransactionAmountComponentArgs {
   workflowSession: WorkflowSession;
   onComplete: () => void;
 }
 
+interface TokenDisplayDetails {
+  name: string;
+  description: string;
+  symbol: string;
+  icon: string;
+}
+
+const tokenDetails: {
+  [tokenId: string]: TokenDisplayDetails;
+} = {
+  CARD: {
+    name: 'Card',
+    symbol: 'CARD',
+    description: 'ERC-20 Cardstack token',
+    icon: 'card-token',
+  },
+  DAI: {
+    name: 'Dai',
+    symbol: 'DAI',
+    description: 'USD-based stablecoin',
+    icon: 'dai-token',
+  },
+};
+
 class CardPayDepositWorkflowTransactionAmountComponent extends Component<CardPayDepositWorkflowTransactionAmountComponentArgs> {
   @tracked amount = 0;
   @tracked isUnlocked = false;
   @tracked isUnlocking = false;
-  @tracked isDepositing = false;
   @tracked unlockTxnReceipt: TransactionReceipt | undefined;
   @tracked relayTokensTxnReceipt: TransactionReceipt | undefined;
+  @tracked depositTxnReceipt: TransactionReceipt | undefined;
+  @tracked isDepositing = false;
+  @tracked hasDeposited = false;
   @service declare layer1Network: Layer1Network;
   @service declare layer2Network: Layer2Network;
 
-  get isUnlockButtonDisabled() {
-    return this.amount <= 0 || this.isUnlocked;
+  get currentTokenSymbol(): string | undefined {
+    return this.args.workflowSession.state.depositSourceToken;
   }
 
-  get isDepositButtonDisabled() {
-    return !this.isUnlocked;
+  get currentTokenDetails(): TokenDisplayDetails | undefined {
+    if (tokenDetails[this.currentTokenSymbol!]) {
+      return tokenDetails[this.currentTokenSymbol!];
+    } else {
+      return undefined;
+    }
+  }
+
+  get currentTokenBalance(): BigNumber {
+    let balance;
+    if (this.currentTokenSymbol === 'DAI') {
+      balance = this.layer1Network.daiBalance;
+    } else if (this.currentTokenSymbol === 'CARD') {
+      balance = this.layer1Network.cardBalance;
+    }
+    return balance || BigNumber.from(0);
+  }
+
+  get currentTokenBalanceInUsd(): string {
+    // TODO: figure out how to get this
+    return 'TODO';
+  }
+
+  get unlockCtaState() {
+    if (this.isUnlocked) {
+      return 'memorialized';
+    } else if (this.isUnlocking) {
+      return 'in-progress';
+    } else if (this.isValidAmount(this.amount)) {
+      return 'default';
+    } else {
+      return 'disabled';
+    }
+  }
+
+  get depositCtaState() {
+    if (
+      !this.isUnlocked ||
+      (!this.hasDeposited && !this.isValidAmount(this.amount))
+    ) {
+      return 'disabled';
+    } else if (this.isDepositing) {
+      return 'in-progress';
+    } else if (this.hasDeposited) {
+      return 'memorialized';
+    } else {
+      return 'default';
+    }
+  }
+
+  get amountInUsd() {
+    // TODO:
+    return this.amount;
+  }
+
+  isValidAmount(amount: number) {
+    return (
+      !isNaN(amount) &&
+      amount > 0 &&
+      BigNumber.from(amount).lte(this.currentTokenBalance)
+    );
   }
 
   get unlockTxnViewerUrl() {
@@ -74,6 +160,7 @@ class CardPayDepositWorkflowTransactionAmountComponent extends Component<CardPay
           transactionReceipt
         );
         this.args.onComplete();
+        this.hasDeposited = true;
       })
       .finally(() => {
         this.isDepositing = false;
