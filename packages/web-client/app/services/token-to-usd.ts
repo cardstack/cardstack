f46cb5661ada61c4a65d25f3aa6dacd6946f08c6
Aierie <michael.khor@cardstack.com>
2021-05-04 22:09:35 +0800

    convert to mock

--- packages/web-client/app/services/token-to-usd.ts
+++ packages/web-client/app/services/token-to-usd.ts
@@ -2,41 +2,19 @@ import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { BigNumber } from '@ethersproject/bignumber';
 import { fromWei } from 'web3-utils';
-
-const query =
-  'https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,CARD,DAI&tsyms=USD&api_key=';
-
-const API_KEY = '';
-
-interface ExchangeRateResponse {
-  DAI: { USD: number };
-  ETH: { USD: number };
-  CARD: { USD: number };
-}
-
 class ExchangeRates {
   @tracked lastUpdated: Date | undefined;
-  @tracked DAI: number = 0;
-  @tracked ETH: number = 0;
-  @tracked CARD: number = 0;
+  @tracked DAI: number = 1.001;
+  @tracked ETH: number = 3482.5;
+  @tracked CARD: number = 0.007543;
 
   constructor() {
     this.update();
   }
 
   // update all rates
-  async update() {
-    try {
-      const res: ExchangeRateResponse = await (
-        await fetch(query + API_KEY)
-      ).json();
-      this.DAI = res.DAI.USD;
-      this.CARD = res.CARD.USD;
-      this.ETH = res.ETH.USD;
-      this.lastUpdated = new Date();
-    } catch (e) {
-      console.error(e);
-    }
+  update() {
+    // TODO:
   }
 
   getExchangeRate(symbol: 'DAI' | 'ETH' | 'CARD') {
