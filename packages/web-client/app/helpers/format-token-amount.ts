527708f9ea13c60913932b1323882b8e9db3a344
Aierie <michael.khor@cardstack.com>
2021-05-04 18:36:12 +0800

    remove unused

--- packages/web-client/app/helpers/format-token-amount.ts
+++ packages/web-client/app/helpers/format-token-amount.ts
@@ -1,13 +1,10 @@
 import Helper from '@ember/component/helper';
-import { inject as service } from '@ember/service';
-import TextFormattingService from '../services/text-formatting';
 import { BigNumber } from '@ethersproject/bignumber';
 import { formatUnits } from '@ethersproject/units';
 
 type FormatTokenAmountHelperParams = [BigNumber];
 
 class FormatTokenAmountHelper extends Helper {
-  @service declare textFormatting: TextFormattingService;
   compute([amountInSmallestUnit]: FormatTokenAmountHelperParams /*, hash*/) {
     if (amountInSmallestUnit == null) {
       return null;
