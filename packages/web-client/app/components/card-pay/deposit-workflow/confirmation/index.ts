c29a3535a81649a7c1021c0c3c61d3ce4d15d7d6
Aierie <michael.khor@cardstack.com>
2021-05-08 03:22:49 +0800

    Use data from session in the confirmation card

--- packages/web-client/app/components/card-pay/deposit-workflow/confirmation/index.ts
+++ packages/web-client/app/components/card-pay/deposit-workflow/confirmation/index.ts
@@ -1,6 +1,76 @@
-/* eslint-disable ember/no-empty-glimmer-component-classes */
 import Component from '@glimmer/component';
+import { reads } from 'macro-decorators';
+import { inject as service } from '@ember/service';
+import Layer1Network from '@cardstack/web-client/services/layer1-network';
+import Layer2Network from '@cardstack/web-client/services/layer2-network';
+import WorkflowSession from '../../../../models/workflow/workflow-session';
+import { toBN } from 'web3-utils';
+import BN from 'bn.js';
 
-class CardPayDepositWorkflowConfirmationComponent extends Component {}
+interface CardPayDepositWorkflowConfirmationComponentArgs {
+  workflowSession: WorkflowSession;
+}
+
+interface token {
+  symbol: string;
+  description: string;
+  icon: string;
+}
+
+const DAI_TOKEN = {
+  symbol: 'DAI',
+  description: 'USD-based stablecoin',
+  icon: 'dai-token',
+};
+const CARD_TOKEN = {
+  symbol: 'CARD',
+  description: 'ERC-20 Cardstack token',
+  icon: 'card-token',
+};
+
+const TOKENS: { [symbol: string]: token } = {
+  DAI: DAI_TOKEN,
+  CARD: CARD_TOKEN,
+};
+class CardPayDepositWorkflowConfirmationComponent extends Component<CardPayDepositWorkflowConfirmationComponentArgs> {
+  @service declare layer1Network: Layer1Network;
+  @service declare layer2Network: Layer2Network;
+  @reads('args.workflowSession.state.depositSourceToken')
+  declare selectedTokenSymbol: string;
+
+  get depositedToken(): token {
+    return TOKENS[this.selectedTokenSymbol] as token;
+  }
+
+  get depositedAmount(): BN {
+    return toBN(this.args.workflowSession.state.depositedAmount);
+  }
+
+  get depositTxnViewerUrl(): string | undefined {
+    return this.layer1Network.blockExplorerUrl(
+      this.args.workflowSession.state.relayTokensTxnReceipt.transactionHash
+    );
+  }
+
+  get bridgeExplorerUrl(): string | undefined {
+    return this.layer1Network.bridgeExplorerUrl(
+      this.args.workflowSession.state.relayTokensTxnReceipt.transactionHash
+    );
+  }
+
+  get blockscoutUrl(): string {
+    return (
+      this.args.workflowSession.state.completedLayer2TransactionReceipt &&
+      this.layer2Network.blockExplorerUrl(
+        this.args.workflowSession.state.completedLayer2TransactionReceipt
+          .transactionHash
+      )
+    );
+  }
+
+  get depotAddress(): string {
+    return this.args.workflowSession.state.depotAddress;
+  }
+}
 
 export default CardPayDepositWorkflowConfirmationComponent;
