fa0075e426d904a8cc04ea45a61ad90655211609
Dominic Sherman <dominic.sherman98@gmail.com>
2021-05-10 08:40:47 -0700

    Allow user address to be passed to the asset functions (#1613)

--- packages/cardpay-sdk/sdk/assets.ts
+++ packages/cardpay-sdk/sdk/assets.ts
@@ -5,14 +5,14 @@ import Web3 from 'web3';
 export default class Assets {
   constructor(private layer2Web3: Web3) {}
 
-  async getNativeTokenBalance(): Promise<string> {
-    let address = (await this.layer2Web3.eth.getAccounts())[0];
+  async getNativeTokenBalance(userAddress?: string): Promise<string> {
+    let address = userAddress || (await this.layer2Web3.eth.getAccounts())[0];
 
     return this.layer2Web3.eth.getBalance(address);
   }
 
-  async getBalanceForToken(tokenAddress: string): Promise<string> {
-    let address = (await this.layer2Web3.eth.getAccounts())[0];
+  async getBalanceForToken(tokenAddress: string, userAddress?: string): Promise<string> {
+    let address = userAddress || (await this.layer2Web3.eth.getAccounts())[0];
     const tokenContract = new this.layer2Web3.eth.Contract(ERC20ABI as AbiItem[], tokenAddress);
 
     return tokenContract.methods.balanceOf(address).call();
