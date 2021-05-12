b0d3c91e747c8b6fc623e5255288e27a23ca5384
Aierie <michael.khor@cardstack.com>
2021-05-13 01:28:45 +0800

    static next steps component

--- packages/web-client/app/components/card-pay/deposit-workflow/next-steps/index.ts
+++ packages/web-client/app/components/card-pay/deposit-workflow/next-steps/index.ts
@@ -1,6 +1,15 @@
 /* eslint-disable ember/no-empty-glimmer-component-classes */
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-class CardPayDepositWorkflowNextStepsComponent extends Component {}
+class CardPayDepositWorkflowNextStepsComponent extends Component {
+  @action openNewDepositWorkflow() {
+    console.log('Open new deposit workflow');
+  }
+
+  @action returnToDashboard() {
+    console.log('Return to dashboard');
+  }
+}
 
 export default CardPayDepositWorkflowNextStepsComponent;
