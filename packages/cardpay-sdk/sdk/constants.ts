934d7c9b4e749bd0fd1006403c1c2a82a3bbfc36
Dominic Sherman <dominic.sherman98@gmail.com>
2021-05-13 05:45:28 -0700

    Add name and balance checker contract address

--- packages/cardpay-sdk/sdk/constants.ts
+++ packages/cardpay-sdk/sdk/constants.ts
@@ -12,8 +12,11 @@ export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 const SOKOL = {
   apiBaseUrl: 'https://blockscout.com/poa/sokol/api/eth-rpc',
+  /** deployed instance of this contract: https://github.com/wbobeirne/eth-balance-checker */
+  balanceCheckerContractAddress: '0xaeDFe60b0732924249866E3FeC71835EFb1fc9fF',
   blockExplorer: 'https://blockscout.com/poa/sokol',
   nativeCoin: 'SPOA',
+  name: 'Sokol',
   rpcNode: 'https://sokol.stack.cards',
   rpcWssNode: 'https://sokol-wss.stack.cards',
   relayServiceURL: 'https://relay-staging.stack.cards/api',
@@ -21,24 +24,33 @@ const SOKOL = {
 };
 const KOVAN = {
   apiBaseUrl: 'https://api-kovan.etherscan.io/api',
+  /** deployed instance of this contract: https://github.com/wbobeirne/eth-balance-checker */
+  balanceCheckerContractAddress: '0xf3352813b612a2d198e437691557069316b84ebe',
   blockExplorer: 'https://kovan.etherscan.io',
   bridgeExplorer: 'https://alm-test-amb.herokuapp.com/42',
   nativeCoin: 'KETH',
+  name: 'Kovan',
   rpcNode: `${KOVAN_INFURA_URL}/${INFURA_PROJECT_ID}`,
   rpcWssNode: `${KOVAN_INFURA_WSS_URL}/${INFURA_PROJECT_ID}`,
 };
 const MAINNET = {
   apiBaseUrl: 'https://api.etherscan.io/api',
+  /** deployed instance of this contract: https://github.com/wbobeirne/eth-balance-checker */
+  balanceCheckerContractAddress: '0x4dcf4562268dd384fe814c00fad239f06c2a0c2b',
   blockExplorer: 'https://etherscan.io',
   bridgeExplorer: 'https://alm-xdai.herokuapp.com',
   nativeCoin: 'ETH',
+  name: 'Ethereum Mainnet',
   rpcNode: `${MAINNET_INFURA_URL}/${INFURA_PROJECT_ID}`,
   rpcWssNode: `${MAINNET_INFURA_WSS_URL}/${INFURA_PROJECT_ID}`,
 };
 const XDAI = {
   apiBaseUrl: 'https://blockscout.com/xdai/mainnet/api',
+  /** deployed instance of this contract: https://github.com/wbobeirne/eth-balance-checker */
+  balanceCheckerContractAddress: '0x6B78C121bBd10D8ef0dd3623CC1abB077b186F65',
   blockExplorer: 'https://blockscout.com/xdai/mainnet',
   nativeCoin: 'DAI',
+  name: 'xDai Chain',
   rpcNode: 'https://rpc.xdaichain.com',
   rpcWssNode: 'wss://rpc.xdaichain.com/wss',
   relayServiceURL: 'https://relay.cardstack.com/api',
@@ -46,6 +58,7 @@ const XDAI = {
 };
 
 type ConstantKeys = keyof typeof SOKOL | keyof typeof KOVAN | keyof typeof MAINNET | keyof typeof XDAI;
+type Network = 'sokol' | 'kovan' | 'mainnet' | 'xdai';
 
 const constants: {
   [network: string]: {
@@ -66,7 +79,7 @@ export const networks: { [networkId: number]: string } = Object.freeze({
 });
 export const networkIds = (Object.freeze(invert({ ...networks })) as unknown) as { [networkName: string]: number };
 
-export function getConstantByNetwork(name: ConstantKeys, network: string): string {
+export function getConstantByNetwork(name: ConstantKeys, network: Network): string {
   let value = constants[network][name];
   if (!value) {
     throw new Error(`Don't know about the constant '${name}' for network ${network}`);
@@ -74,9 +87,9 @@ export function getConstantByNetwork(name: ConstantKeys, network: string): strin
   return value;
 }
 
-export async function getConstant(name: ConstantKeys, network: string): Promise<string>;
+export async function getConstant(name: ConstantKeys, network: Network): Promise<string>;
 export async function getConstant(name: ConstantKeys, web3: Web3): Promise<string>;
-export async function getConstant(name: ConstantKeys, web3OrNetwork: Web3 | string): Promise<string> {
+export async function getConstant(name: ConstantKeys, web3OrNetwork: Web3 | Network): Promise<string> {
   let network: string;
   if (typeof web3OrNetwork === 'string') {
     network = web3OrNetwork;
