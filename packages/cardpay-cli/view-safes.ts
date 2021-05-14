a64c35d040a2dc2bd9caf1a7002d025bd3025ff4
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-05-14 18:08:53 -0400

    Fixes #CS-802 include safe types in API

--- packages/cardpay-cli/view-safes.ts
+++ packages/cardpay-cli/view-safes.ts
@@ -5,19 +5,22 @@ import { getWeb3 } from './utils';
 export default async function (network: string, mnemonic: string, address?: string): Promise<void> {
   let web3 = await getWeb3(network, mnemonic);
 
-  let safes = new Safes(web3);
+  let safesApi = new Safes(web3);
   console.log('Getting safes');
-  let safeDetails = await safes.view(address);
+  let safes = await safesApi.view(address);
   console.log('\n\n');
-  safeDetails.forEach((detail) => {
-    let { address, isPrepaidCard, tokens } = detail;
-    console.log(`${address} -- ${isPrepaidCard ? 'prepaid card' : 'depot'}`);
+  safes.forEach((safe) => {
+    let { address, type, tokens } = safe;
+    if (type === 'external') {
+      return;
+    }
+    console.log(`${address} -- ${type}`);
     console.log('-------------------------');
-    if (detail.isPrepaidCard) {
-      console.log(`Face value: §${detail.spendFaceValue} SPEND`);
+    if (safe.type === 'prepaid-card') {
+      console.log(`Face value: §${safe.spendFaceValue} SPEND`);
     }
     tokens.forEach((item) => {
-      let isIssuingToken = detail.isPrepaidCard && detail.issuingToken === item.tokenAddress;
+      let isIssuingToken = safe.type === 'prepaid-card' && safe.issuingToken === item.tokenAddress;
       console.log(
         `${item.token.name} - ${Web3.utils.fromWei(item.balance)} ${item.token.symbol} ${
           isIssuingToken ? '(issuing token)' : ''
--- packages/cardpay-sdk/sdk/safes.ts
+++ packages/cardpay-sdk/sdk/safes.ts
@@ -2,22 +2,29 @@
 
 import Web3 from 'web3';
 import PrepaidCardManagerABI from '../contracts/abi/prepaid-card-manager';
+import RevenuePoolABI from '../contracts/abi/revenue-pool';
+import BridgeUtilsABI from '../contracts/abi/bridge-utils';
 import { AbiItem } from 'web3-utils';
 import { getAddress } from '../contracts/addresses';
 import { getConstant, ZERO_ADDRESS } from './constants';
 import ExchangeRate from './exchange-rate';
 
-type SafeInfo = DepotInfo | PrepaidCardInfo;
-interface DepotInfo {
-  isPrepaidCard: false;
+type Safe = DepotSafe | PrepaidCardSafe | MerchantSafe | ExternalSafe;
+interface BaseSafe {
   address: string;
   tokens: TokenInfo[];
 }
-
-interface PrepaidCardInfo {
-  isPrepaidCard: true;
-  address: string;
-  tokens: TokenInfo[];
+interface DepotSafe extends BaseSafe {
+  type: 'depot';
+}
+interface MerchantSafe extends BaseSafe {
+  type: 'merchant';
+}
+interface ExternalSafe extends BaseSafe {
+  type: 'external';
+}
+interface PrepaidCardSafe extends BaseSafe {
+  type: 'prepaid-card';
   issuingToken: string;
   spendFaceValue: number;
   issuer: string;
@@ -36,7 +43,7 @@ interface TokenInfo {
 export default class Safes {
   constructor(private layer2Web3: Web3) {}
 
-  async view(owner?: string): Promise<SafeInfo[]> {
+  async view(owner?: string): Promise<Safe[]> {
     owner = owner ?? (await this.layer2Web3.eth.getAccounts())[0];
     let transactionServiceURL = await getConstant('transactionServiceURL', this.layer2Web3);
     let response = await fetch(`${transactionServiceURL}/v1/owners/${owner}/`);
@@ -45,6 +52,15 @@ export default class Safes {
       PrepaidCardManagerABI as AbiItem[],
       await getAddress('prepaidCardManager', this.layer2Web3)
     );
+    let revenuePool = new this.layer2Web3.eth.Contract(
+      RevenuePoolABI as AbiItem[],
+      await getAddress('revenuePool', this.layer2Web3)
+    );
+    let bridgeUtils = new this.layer2Web3.eth.Contract(
+      BridgeUtilsABI as AbiItem[],
+      await getAddress('bridgeUtils', this.layer2Web3)
+    );
+
     let exchangeRate = new ExchangeRate(this.layer2Web3);
 
     return await Promise.all(
@@ -53,29 +69,41 @@ export default class Safes {
         if (!balanceResponse?.ok) {
           throw new Error(await balanceResponse.text());
         }
-        let { issuer, issueToken: issuingToken } = await prepaidCardManager.methods.cardDetails(safeAddress).call();
         let balances: TokenInfo[] = await balanceResponse.json();
         let tokens = balances.filter((balanceItem) => balanceItem.tokenAddress);
-        let issuingTokenBalance =
-          tokens.find((t) => t.tokenAddress.toLowerCase() === issuingToken.toLowerCase())?.balance ?? '0';
+        let safeInfo = { address: safeAddress, tokens };
+        let { issuer, issueToken: issuingToken } = await prepaidCardManager.methods.cardDetails(safeAddress).call();
 
-        let isPrepaidCard = issuer !== ZERO_ADDRESS;
-        if (!isPrepaidCard) {
+        // prepaid card safe
+        if (issuer !== ZERO_ADDRESS) {
+          let issuingTokenBalance =
+            tokens.find((t) => t.tokenAddress.toLowerCase() === issuingToken.toLowerCase())?.balance ?? '0';
           return {
-            isPrepaidCard: false,
-            address: safeAddress,
-            tokens,
-          } as DepotInfo;
-        } else {
-          return {
-            isPrepaidCard: true,
-            address: safeAddress,
-            tokens,
+            ...safeInfo,
+            type: 'prepaid-card' as 'prepaid-card',
             issuer,
             issuingToken,
             spendFaceValue: await exchangeRate.convertToSpend(issuingToken, issuingTokenBalance),
-          } as PrepaidCardInfo;
+          };
         }
+        let supplier = await bridgeUtils.methods.safes(safeAddress).call();
+        if (supplier !== ZERO_ADDRESS) {
+          return {
+            ...safeInfo,
+            type: 'depot' as 'depot',
+          };
+        }
+        let isMerchantSafe = await revenuePool.methods.isMerchantSafe(safeAddress).call();
+        if (isMerchantSafe) {
+          return {
+            ...safeInfo,
+            type: 'merchant' as 'merchant',
+          };
+        }
+        return {
+          ...safeInfo,
+          type: 'external' as 'external',
+        };
       })
     );
   }
