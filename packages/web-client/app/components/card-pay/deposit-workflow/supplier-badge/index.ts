31146bca73b9b441edd1bf573e980feefc768b98
Luke Melia <luke@lukemelia.com>
2021-05-17 18:06:47 +0800

    Remove unused component

--- packages/web-client/app/components/card-pay/deposit-workflow/supplier-badge/index.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable ember/no-empty-glimmer-component-classes */
-import Component from '@glimmer/component';
-
-class CardPayDepositWorkflowSupplierBadgeComponent extends Component {}
-
-export default CardPayDepositWorkflowSupplierBadgeComponent;
