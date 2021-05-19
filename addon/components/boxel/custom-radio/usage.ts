f3bd0d7d1df6cbb3ee441fd9fe559ea80fefa6ff
Aierie <39579264+Aierie@users.noreply.github.com>
2021-05-19 19:23:20 +0800

    fix customradio example event listener (#367)

--- addon/components/boxel/custom-radio/usage.ts
+++ addon/components/boxel/custom-radio/usage.ts
@@ -23,7 +23,7 @@ export default class CustomRadioUsage extends Component {
   @tracked checkedId = 'strawberry';
   @tracked disabled = false;
 
-  @action onChange(event: Event): void {
-    this.checkedId = (event.target as HTMLInputElement).id;
+  @action onChange(id: string): void {
+    this.checkedId = id;
   }
 }
