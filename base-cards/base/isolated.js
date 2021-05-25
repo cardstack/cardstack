d87efec2a48671f14257c75a3a27341c1a3e9337
Matt McManus <matt@mcmanus.io>
2021-05-25 15:32:55 -0400

    Add comments in base card templates that make it easier to debug

--- base-cards/base/isolated.js
+++ base-cards/base/isolated.js
@@ -2,8 +2,11 @@ import { setComponentTemplate } from '@ember/component';
 import { precompileTemplate } from '@ember/template-compilation';
 import templateOnlyComponent from '@ember/component/template-only';
 export default setComponentTemplate(
-  precompileTemplate('', {
-    strictMode: true,
-  }),
+  precompileTemplate(
+    '<!-- Inherited from base card isolated view. Did your card forget to specify its isolated component? -->',
+    {
+      strictMode: true,
+    }
+  ),
   templateOnlyComponent()
 );
