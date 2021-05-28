8f72b15729772049289987e75b5cf94455ed2a5d
Aierie <michael.khor@cardstack.com>
2021-05-28 20:59:52 +0800

    workflow canceled component

---
+++ packages/web-client/app/components/card-pay/deposit-workflow/workflow-canceled-cta/index.ts
@@ -0,0 +1,19 @@
+/* eslint-disable ember/no-empty-glimmer-component-classes */
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import RouterService from '@ember/routing/router-service';
+import { next } from '@ember/runloop';
+
+class CardPayDepositWorkflowCanceledComponent extends Component {
+  @service declare router: RouterService;
+
+  @action async openNewDepositWorkflow() {
+    await this.router.transitionTo({ queryParams: { flow: null } });
+    next(this, () => {
+      this.router.transitionTo({ queryParams: { flow: 'deposit' } });
+    });
+  }
+}
+
+export default CardPayDepositWorkflowCanceledComponent;
