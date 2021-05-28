44c3624ea6004ac00e828e73bbab575f02b4efb2
Burcu Noyan <burieberry@users.noreply.github.com>
2021-05-28 16:22:25 -0400

    Make NextStepsBox title dynamic (#370)
    * Make NextStepsBox title dynamic
    
    * Update dependency allowed version

--- tests/dummy/config/dependency-lint.js
+++ tests/dummy/config/dependency-lint.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   allowedVersions: {
-    '@embroider/util': '0.39.1 || 0.40.x',
+    '@embroider/util': '0.39.1 || 0.40.x || 0.41.x',
     'ember-fetch': '6.x || 8.x',
     'ember-test-selectors': '2.1.0 || 5.x',
   },
