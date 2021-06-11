5655cfb3dd9ffa02437864365f197b03be621485
Buck Doyle <buck.doyle@cardstack.com>
2021-06-11 15:02:16 -0500

    Add prepaid card issued confirmation card (#1681)
    * Add preliminary display of confirmed prepaid card
    
    This only shows the header background and image for now.
    
    * Add issuer name to confirmation card
    
    * Add placeholders for other card values
    
    This is duplicated in …LayoutCustomization::DisplayView and
    …::FormView but it’s probably fine to use here until the face
    value-choosing step is implemented.
    
    * Add card container wrapper
    
    This isn’t an action so maybe this is the incorrect wrapper,
    but other things I found lacked a `::Section` 🤔
    
    * Add confetti background
    
    This includes changing the background of CardPay::PrepaidCard
    to white, otherwise a transparent-background card header shows
    the confetti. It also adds the :where pseudo-class to
    .prepaid-card to allow its box shadow to be overwritten,
    thanks to @Aierie for introducing me to that.
    
    * Add test selector to assert on card issuer name
    
    * Change assertion to use parent selector
    
    This seems preferable to asserting that the card exists twice.

--- packages/web-client/tests/acceptance/issue-prepaid-card-test.ts
+++ packages/web-client/tests/acceptance/issue-prepaid-card-test.ts
@@ -262,6 +262,14 @@ module('Acceptance | issue prepaid card', function (hooks) {
       .containsText('Congratulations, you have created a prepaid card!');
 
     assert.dom(epiloguePostableSel(1)).containsText('Prepaid card issued');
+    assert
+      .dom(`${epiloguePostableSel(1)} [data-test-prepaid-card-issuer-name]`)
+      .containsText('JJ');
+    assert.dom(
+      `${epiloguePostableSel(
+        1
+      )} [data-test-prepaid-card-background="${backgroundChoice}"][data-test-prepaid-card-theme="${themeChoice}"]`
+    );
 
     await waitFor(epiloguePostableSel(2));
 
