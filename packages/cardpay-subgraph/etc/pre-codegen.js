994696fd69afd3a41985c85100206a5c5eb5dc9a
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-06-11 20:24:00 -0400

    using direct module access for ABI's

--- packages/cardpay-subgraph/etc/pre-codegen.js
+++ packages/cardpay-subgraph/etc/pre-codegen.js
@@ -2,10 +2,10 @@ import { writeJSONSync, writeFileSync, readFileSync, removeSync } from 'fs-extra
 import { join, resolve } from 'path';
 import { addFilePreamble } from './pre-tsc-build-entrypoint';
 
-import PrepaidCardManagerABI from '@cardstack/cardpay-sdk/contracts/abi/latest/prepaid-card-manager';
-import BridgeUtilsABI from '@cardstack/cardpay-sdk/contracts/abi/latest/bridge-utils';
-import RevenuePoolABI from '@cardstack/cardpay-sdk/contracts/abi/latest/revenue-pool';
-import SpendABI from '@cardstack/cardpay-sdk/contracts/abi/latest/spend';
+import PrepaidCardManagerABI from '../../cardpay-sdk/contracts/abi/latest/prepaid-card-manager';
+import BridgeUtilsABI from '../../cardpay-sdk/contracts/abi/latest/bridge-utils';
+import RevenuePoolABI from '../../cardpay-sdk/contracts/abi/latest/revenue-pool';
+import SpendABI from '../../cardpay-sdk/contracts/abi/latest/spend';
 import { getAddressByNetwork } from '@cardstack/cardpay-sdk/contracts/addresses';
 
 const abiDir = resolve(join(__dirname, '..', 'abis'));
