27d49c8c9cfa234be3681e20a41cd32c3024c3d8
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-06-11 20:09:43 -0400

    Updating our auto generation to create ABI's and addresses from the SDK

---
+++ packages/cardpay-subgraph/etc/pre-tsc-build.js
@@ -0,0 +1,26 @@
+import { writeFileSync, readFileSync } from 'fs-extra';
+import { join, resolve } from 'path';
+import { sync as glob } from 'glob';
+
+const generatedDir = resolve(join(__dirname, 'generated'));
+const graphProtocolNodeModules = resolve(join(__dirname, 'node_modules', '@graphprotocol'));
+const protofireNodeModules = resolve(join(__dirname, 'node_modules', '@protofire'));
+let generatedFiles = glob(`${generatedDir}/**/*.ts`)
+  .concat(glob(`${graphProtocolNodeModules}/**/*.ts`))
+  .concat(glob(`${protofireNodeModules}/**/*.ts`));
+const noCheck = `// @ts-nocheck`;
+
+for (let filePath of generatedFiles) {
+  addFilePreamble(filePath, noCheck);
+}
+
+export function addFilePreamble(filePath, value) {
+  let file = readFileSync(filePath, { encoding: 'utf8' });
+  if (!file.startsWith(value)) {
+    writeFileSync(
+      filePath,
+      `${value}
+${file}`
+    );
+  }
+}
