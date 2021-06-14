f70670e90f171b9bea3b8bdafdc8ae7a4300d16e
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-06-14 15:39:21 -0400

    typo

--- packages/cardpay-sdk/sdk/exchange-rate/index.ts
+++ packages/cardpay-sdk/sdk/exchange-rate/index.ts
@@ -1,7 +1,7 @@
 /* eslint @typescript-eslint/naming-convention: "off" */
 
 import { ContractMeta } from '../version-resolver';
-import v0_5_3 from './v0.5.0';
+import v0_5_3 from './v0.5.3';
 
 // add more versions as we go, but also please do drop version that we don't
 // want to maintain simultaneously
