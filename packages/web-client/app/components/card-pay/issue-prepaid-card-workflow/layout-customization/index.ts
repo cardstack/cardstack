00fbc51c668f6e36a19aae96ba2deea13ba0b6d9
Aierie <michael.khor@cardstack.com>
2021-06-16 19:34:26 +0800

    integration of customization service with ui

--- packages/web-client/app/components/card-pay/issue-prepaid-card-workflow/layout-customization/index.ts
+++ packages/web-client/app/components/card-pay/issue-prepaid-card-workflow/layout-customization/index.ts
@@ -1,11 +1,18 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import MockTheme1 from '@cardstack/web-client/images/backgrounds/mock-theme-1.svg';
-import MockTheme2 from '@cardstack/web-client/images/backgrounds/mock-theme-2.svg';
-import BB from '@cardstack/web-client/images/backgrounds/bridge-background.svg';
-import blankChoicePlaceholder from '@cardstack/web-client/images/backgrounds/background-selection-placeholder.svg';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency-decorators';
+import { taskFor } from 'ember-concurrency-ts';
+import CardCustomizationOptionsService from '@cardstack/web-client/services/card-customization-options';
 import WorkflowSession from '@cardstack/web-client/models/workflow/workflow-session';
+import { reads } from 'macro-decorators';
+import {
+  ColorCustomizationOption,
+  PatternCustomizationOption,
+} from '@cardstack/web-client/utils/web3-strategies/types';
+import { waitForProperty } from 'ember-concurrency';
+
 interface LayoutCustomizationCardArgs {
   workflowSession: WorkflowSession;
   isComplete: boolean;
@@ -13,51 +20,33 @@ interface LayoutCustomizationCardArgs {
   onIncomplete: () => void;
 }
 
-// data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw== is a blank image
-// see https://stackoverflow.com/questions/9126105/blank-image-encoded-as-data-uri
-let blankImage =
-  'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
-
 export default class LayoutCustomizationCard extends Component<LayoutCustomizationCardArgs> {
-  colorOptions = [
-    '#00ebe5',
-    '#37eb77',
-    '#ac00ff',
-    '#efefef',
-    '#393642',
-    '#c3fc33',
-    // gradients generated in https://codepen.io/aierie/pen/qBryBoy?editors=0010
-    // the codepen assumes that you are only generating gradients with two colors and is hardcoded
-    // if gradient style changes, this should be modified to make sure that angle and starting point is correct
-    // to use this, copy the svg code for a swatch's gradient and paste it in the codepen's input
-    'linear-gradient(139.27deg, #ff5050 16%, #ac00ff 100%)',
-    'linear-gradient(139.27deg, #03c4bf 16%, #ac00ff 100%)',
-    'linear-gradient(139.27deg, #c3fc33 16%, #0069f9 100%)',
-    'linear-gradient(139.27deg, #ac00ff 16%, #ffd800 100%)',
-    'transparent',
-  ].map((color) =>
-    color === 'transparent'
-      ? { image: blankChoicePlaceholder, background: null, id: color }
-      : { image: null, background: color, id: color }
-  );
-  themeOptions = [MockTheme1, MockTheme2, BB, blankImage].map((url) =>
-    url === blankImage
-      ? {
-          image: blankChoicePlaceholder,
-          background: null,
-          id: url,
-        }
-      : {
-          image: url,
-          background: 'black',
-          id: url,
-        }
-  );
-  @tracked headerBackground = this.colorOptions[0];
-  @tracked headerTheme = this.themeOptions[0];
+  @service('card-customization-options')
+  declare cardCustomizationOptionsService: CardCustomizationOptionsService;
+  @reads('cardCustomizationOptionsService.colorOptions')
+  declare colorOptions: ColorCustomizationOption[];
+  @reads('cardCustomizationOptionsService.patternOptions')
+  declare themeOptions: PatternCustomizationOption[];
+  @tracked headerBackground: ColorCustomizationOption | undefined;
+  @tracked headerTheme: PatternCustomizationOption | undefined;
   @tracked issuerName = '';
   @tracked nameFieldErrorMessage = '';
   @tracked isNameInvalid = false;
+
+  constructor(owner: unknown, args: any) {
+    super(owner, args);
+    taskFor(this.setInitialCustomizationOptions).perform();
+  }
+
+  @task *setInitialCustomizationOptions() {
+    yield waitForProperty(
+      this,
+      'cardCustomizationOptionsService.loaded',
+      (v: boolean) => v
+    );
+    this.headerBackground = this.colorOptions[0];
+    this.headerTheme = this.themeOptions[0];
+  }
 
   get ctaState() {
     if (this.args.isComplete) {
