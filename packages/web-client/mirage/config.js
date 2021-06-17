c128e8d72ac10352dfee5d666e50faf4a3df9959
Aierie <michael.khor@cardstack.com>
2021-06-18 01:12:25 +0800

    mirage handles api requests in tests

--- packages/web-client/mirage/config.js
+++ packages/web-client/mirage/config.js
@@ -1,26 +1,15 @@
-export default function() {
+export default function () {
+  this.namespace = 'api';
 
-  // These comments are here to help you get started. Feel free to delete them.
+  this.get('/prepaid-card-color-schemes', (schema) => {
+    return schema.prepaidCardColorSchemes.all();
+  });
 
-  /*
-    Config (with defaults).
+  this.get('/prepaid-card-patterns', (schema) => {
+    return schema.prepaidCardPatterns.all();
+  });
 
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
-  */
+  this.passthrough((request) => {
+    return !request.url.includes('/api/');
+  });
 }
---
+++ packages/web-client/mirage/fixture-data/prepaid-card-color-schemes.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    background: '#FFD800',
+    textColor: 'black',
+    patternColor: 'white',
+  },
+  {
+    background: '#37EB77',
+    textColor: 'black',
+    patternColor: 'white',
+  },
+  {
+    background: '#C3FC33',
+    textColor: 'black',
+    patternColor: 'white',
+  },
+  {
+    background: '#00EBE5',
+    textColor: 'black',
+    patternColor: 'white',
+  },
+  {
+    background: '#F5F5F5',
+    textColor: 'black',
+    patternColor: 'white',
+  },
+  {
+    background: '#FFEDDB',
+    textColor: 'black',
+    patternColor: 'white',
+  },
+  {
+    background: '#FFDBE5',
+    textColor: 'black',
+    patternColor: 'white',
+  },
+  {
+    background: '#E9DBFF',
+    textColor: 'black',
+    patternColor: 'white',
+  },
+  {
+    background: '#AC00FF',
+    textColor: 'white',
+    patternColor: 'black',
+  },
+  {
+    background: '#393642',
+    textColor: 'white',
+    patternColor: 'black',
+  },
+  {
+    background: '#0069F9',
+    textColor: 'white',
+    patternColor: 'black',
+  },
+  {
+    background: '#FF5050',
+    textColor: 'white',
+    patternColor: 'black',
+  },
+  {
+    background: 'linear-gradient(139.27deg, #00EBE5 16%, #C3FC33 100%)',
+    textColor: 'black',
+    patternColor: 'white',
+  },
+  {
+    background: 'linear-gradient(139.27deg, #FC8C8C 16%, #FFF5A7 100%)',
+    textColor: 'black',
+    patternColor: 'white',
+  },
+  {
+    background: 'linear-gradient(139.27deg, #FF88D1 16%, #A3FFFF 100%)',
+    textColor: 'black',
+    patternColor: 'white',
+  },
+  {
+    background: 'linear-gradient(139.27deg, #FFFFAA 16%, #B7FFFC 100%)',
+    textColor: 'black',
+    patternColor: 'white',
+  },
+  {
+    background: 'linear-gradient(139.27deg, #004DB7 16%, #00C18D 100%)',
+    textColor: 'white',
+    patternColor: 'black',
+  },
+  {
+    background: 'linear-gradient(139.27deg, #9300FF 16%, #FF0058 100%)',
+    textColor: 'white',
+    patternColor: 'black',
+  },
+];
