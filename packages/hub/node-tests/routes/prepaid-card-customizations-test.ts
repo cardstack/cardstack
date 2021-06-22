2c7c46ff88d00285c66627bbf88cd29167a1ace0
Luke Melia <luke@lukemelia.com>
2021-06-22 19:16:12 +0800

    Unskip /api/prepaid-card-customizations test

--- packages/hub/node-tests/routes/prepaid-card-customizations-test.ts
+++ packages/hub/node-tests/routes/prepaid-card-customizations-test.ts
@@ -32,7 +32,7 @@ function handleValidateAuthToken(encryptedString: string) {
   return stubUserAddress;
 }
 
-describe.skip('POST /api/prepaid-card-customizations', function () {
+describe('POST /api/prepaid-card-customizations', function () {
   let server: Server;
   let db: Client;
   let request: supertest.SuperTest<Test>;
