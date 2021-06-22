295776c04a5e9ec85fd96dc3107e1e907efd5018
Aierie <michael.khor@cardstack.com>
2021-06-22 23:27:57 +0800

    workflow-canceled-cta -> disconnection-cta - to account for different cancelation reasons

--- packages/web-client/tests/acceptance/issue-prepaid-card-test.ts
+++ packages/web-client/tests/acceptance/issue-prepaid-card-test.ts
@@ -412,7 +412,7 @@ module('Acceptance | issue prepaid card', function (hooks) {
     assert.dom(cancelationPostableSel(1)).containsText('Workflow canceled');
 
     assert
-      .dom('[data-test-issue-prepaid-card-workflow-canceled-restart]')
+      .dom('[data-test-issue-prepaid-card-workflow-disconnection-restart]')
       .exists();
   });
 
@@ -505,7 +505,7 @@ module('Acceptance | issue prepaid card', function (hooks) {
 
     layer2Service.test__simulateDisconnectFromWallet();
 
-    await waitFor('[data-test-issue-prepaid-card-workflow-canceled-cta]');
+    await waitFor('[data-test-issue-prepaid-card-workflow-disconnection-cta]');
     // test that all cta buttons are disabled
     let milestoneCtaButtonCount = Array.from(
       document.querySelectorAll(
@@ -527,7 +527,7 @@ module('Acceptance | issue prepaid card', function (hooks) {
       );
     assert.dom(cancelationPostableSel(1)).containsText('Workflow canceled');
     assert
-      .dom('[data-test-issue-prepaid-card-workflow-canceled-restart]')
+      .dom('[data-test-issue-prepaid-card-workflow-disconnection-restart]')
       .exists();
   });
 });
