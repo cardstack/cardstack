93d046505bb5b2109ba5cc1a2d66f0f760b456de
Aierie <michael.khor@cardstack.com>
2021-06-24 20:55:33 +0800

    fix test waiters

--- packages/web-client/app/components/card-pay/issue-prepaid-card-workflow/wrapped-layer-two-connect-card/index.ts
+++ packages/web-client/app/components/card-pay/issue-prepaid-card-workflow/wrapped-layer-two-connect-card/index.ts
@@ -3,27 +3,29 @@ import { inject as service } from '@ember/service';
 import { toWei } from 'web3-utils';
 import { BN } from 'bn.js';
 import Layer2Network from '@cardstack/web-client/services/layer2-network';
+import { action } from '@ember/object';
 
 interface WrappedLayerTwoConnectCardComponentArgs {
   cancelWorkflow: (reason: string) => void;
+  onComplete: () => void;
 }
 
-// This exists because
+// We want to always have the cancelation show after the wallet details are shown.
 export default class WrappedLayerTwoConnectCardComponent extends Component<WrappedLayerTwoConnectCardComponentArgs> {
   @service declare layer2Network: Layer2Network;
 
-  constructor(owner: unknown, args: any) {
-    super(owner, args);
-    this.layer2Network.waitForAccount.then(() => {
-      // TODO: get real min dai amount
-      let MIN_DAI_AMOUNT = new BN(toWei('50'));
+  // hijack onComplete here, and cancel instead if balance is not sufficient
+  // this assumes that layer 2 connection includes fetching of balances
+  @action onComplete() {
+    let MIN_DAI_AMOUNT = new BN(toWei('50'));
 
-      if (
-        !this.layer2Network.defaultTokenBalance ||
-        this.layer2Network.defaultTokenBalance?.lt(MIN_DAI_AMOUNT)
-      ) {
-        this.args.cancelWorkflow('INSUFFICIENT_FUNDS');
-      }
-    });
+    if (
+      !this.layer2Network.defaultTokenBalance ||
+      this.layer2Network.defaultTokenBalance?.lt(MIN_DAI_AMOUNT)
+    ) {
+      this.args.cancelWorkflow('INSUFFICIENT_FUNDS');
+    } else {
+      this.args.onComplete();
+    }
   }
 }
