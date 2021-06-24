1177b899079d01a76019905a8edf2be3ba5ef089
Burcu Noyan <burcunoyan@gmail.com>
2021-06-24 16:18:47 -0400

    Create spendToUsd helper

--- packages/web-client/app/components/card-pay/issue-prepaid-card-workflow/face-value/index.ts
+++ packages/web-client/app/components/card-pay/issue-prepaid-card-workflow/face-value/index.ts
@@ -7,7 +7,6 @@ import Layer2Network from '@cardstack/web-client/services/layer2-network';
 import { fromWei, toBN, toWei } from 'web3-utils';
 import WorkflowSession from '@cardstack/web-client/models/workflow/workflow-session';
 import { faceValueOptions, spendToUsdRate, Token } from '../workflow-config';
-import BN from 'web3-core/node_modules/@types/bn.js';
 
 interface FaceValueCardArgs {
   workflowSession: WorkflowSession;
