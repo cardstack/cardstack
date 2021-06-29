b8907c3daa29856551ee71a9529804ad1a05a251
Buck Doyle <buck.doyle@cardstack.com>
2021-06-29 16:51:14 -0500

    Change memorialised address to be truncated

--- packages/web-client/tests/acceptance/issue-prepaid-card-test.ts
+++ packages/web-client/tests/acceptance/issue-prepaid-card-test.ts
@@ -352,7 +352,7 @@ module('Acceptance | issue prepaid card', function (hooks) {
       .dom(
         `${postableSel(3, 1)} [data-test-prepaid-card-address-labeled-value]`
       )
-      .containsText('0xaeFbA62A2B3e90FD131209CC94480E722704E1F8');
+      .containsText('0xaeFb...E1F8');
 
     assert
       .dom(epiloguePostableSel(0))
