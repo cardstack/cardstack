0bf10116476d3ab5f24f3e4f2ab1e58d09a5393f
Buck Doyle <buck.doyle@cardstack.com>
2021-06-29 14:06:08 -0500

    Add in-progress state for approval action chin

--- packages/web-client/app/utils/web3-strategies/test-layer2.ts
+++ packages/web-client/app/utils/web3-strategies/test-layer2.ts
@@ -27,6 +27,7 @@ export default class TestLayer2Web3Strategy implements Layer2Web3Strategy {
   @tracked defaultTokenBalance: BN | undefined;
   @tracked cardBalance: BN | undefined;
   @tracked depotSafe: DepotSafe | null = null;
+  issuePrepaidCardDeferred = defer();
 
   // property to test whether the refreshBalances method is called
   // to test if balances are refreshed after relaying tokens
@@ -95,8 +96,8 @@ export default class TestLayer2Web3Strategy implements Layer2Web3Strategy {
     return await this.test__simulateConvertFromSpend(symbol, amount);
   }
 
-  async issuePrepaidCard(): Promise<string> {
-    return Promise.resolve('2423');
+  async issuePrepaidCard(): Promise<any> {
+    return this.issuePrepaidCardDeferred.promise;
   }
 
   test__lastSymbolsToUpdate: ConvertibleSymbol[] = [];
@@ -143,5 +144,9 @@ export default class TestLayer2Web3Strategy implements Layer2Web3Strategy {
     } else {
       return '0';
     }
+  }
+
+  test__simulateIssuePrepaidCard(walletAddress: string) {
+    return this.issuePrepaidCardDeferred.resolve(walletAddress);
   }
 }
