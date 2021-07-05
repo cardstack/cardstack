b17ef31244741761413bcaf0956a904de39b69be
Buck Doyle <buck.doyle@cardstack.com>
2021-07-05 10:16:20 -0500

    Extract helper for placeholder addresses

---
+++ packages/web-client/app/helpers/placeholder-address.ts
@@ -0,0 +1,7 @@
+import { helper } from '@ember/component/helper';
+
+export function placeholderAddress() {
+  return '0x123400000000000000000000000000000000abcd';
+}
+
+export default helper(placeholderAddress);
