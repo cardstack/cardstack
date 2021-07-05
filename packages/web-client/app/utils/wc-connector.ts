4e3d5da3aae4c6a00ac9b71ab07b528d624b22f0
Aierie <michael.khor@cardstack.com>
2021-07-05 22:54:57 +0800

    utility to remove stored walletConnect session

--- packages/web-client/app/utils/wc-connector.ts
+++ packages/web-client/app/utils/wc-connector.ts
@@ -7,6 +7,13 @@ import * as cryptoLib from '@walletconnect/iso-crypto';
 import Connector from '@walletconnect/core';
 import SessionStorage from '@walletconnect/core/dist/cjs/storage';
 
+const GET_STORAGE_ID = (chainId: string | number) =>
+  `wallet-connect-chain-${chainId}`;
+
+export function clearWalletConnectStorage(chainId: string | number) {
+  window.localStorage.removeItem(GET_STORAGE_ID(chainId));
+}
+
 // based on https://github.com/WalletConnect/walletconnect-monorepo/blob/1d2828fe63c97e4c0a72eea0150e2f65b819152d/packages/clients/client/src/index.ts
 export default class CustomStorageWalletConnect extends Connector {
   constructor(
@@ -20,7 +27,7 @@ export default class CustomStorageWalletConnect extends Connector {
       );
     }
     const storage = new SessionStorage();
-    storage.storageId = `wallet-connect-chain-${chainId}`;
+    storage.storageId = GET_STORAGE_ID(chainId);
     super({
       cryptoLib,
       connectorOpts,
