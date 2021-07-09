75b5549e68ba744416b01ba3467a9ebe5ce77701
Aierie <michael.khor@cardstack.com>
2021-07-10 01:38:25 +0800

    test disconnection of layer 2 clears auth token

--- packages/web-client/tests/unit/services/layer2-network-test.ts
+++ packages/web-client/tests/unit/services/layer2-network-test.ts
@@ -9,4 +9,13 @@ module('Unit | Service | Layer2Network', function (hooks) {
     let service = this.owner.lookup('service:layer2-network');
     assert.ok(service);
   });
+
+  test('it clears the auth token when onDisconnect is called', function (assert) {
+    let hubAuthentication = this.owner.lookup('service:hub-authentication');
+    hubAuthentication.authToken = 'something';
+    assert.equal(hubAuthentication.authToken, 'something');
+    let layer2Network = this.owner.lookup('service:layer2-network');
+    layer2Network.onDisconnect();
+    assert.equal(hubAuthentication.authToken, null);
+  });
 });
