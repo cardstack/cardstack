c4d10fbef7c6e18324d3b3f58c9bc35ce52daaa5
Aierie <michael.khor@cardstack.com>
2021-07-19 20:24:18 +0800

    Add layers to Boxel's modal - add two different z-indices to boxel's variables - use in boxel's modal as a "layer" argument

--- packages/boxel/addon/components/boxel/modal/usage.ts
+++ packages/boxel/addon/components/boxel/modal/usage.ts
@@ -9,9 +9,28 @@ export default class ModalUsage extends Component {
   @tracked offsetLeft = '0px';
   @tracked offsetTop = 'none';
   @tracked maxWidth = '60%';
+  @tracked layer = 'default';
+  @tracked isDefaultOpen = false;
+  @tracked isUrgentOpen = false;
 
   @action
   onClose(): void {
     this.isOpen = false;
+  }
+
+  @action openDefault(): void {
+    this.isDefaultOpen = true;
+  }
+
+  @action closeDefault(): void {
+    this.isDefaultOpen = false;
+  }
+
+  @action openUrgent(): void {
+    this.isUrgentOpen = true;
+  }
+
+  @action closeUrgent(): void {
+    this.isUrgentOpen = false;
   }
 }
