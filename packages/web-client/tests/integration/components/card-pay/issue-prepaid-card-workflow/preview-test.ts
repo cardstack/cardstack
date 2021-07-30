d4242d1ab523cab66ab344e22940532753be6b39
Aierie <michael.khor@cardstack.com>
2021-07-30 21:17:12 +0800

    Update default error message for prepaid card preview

--- packages/web-client/tests/integration/components/card-pay/issue-prepaid-card-workflow/preview-test.ts
+++ packages/web-client/tests/integration/components/card-pay/issue-prepaid-card-workflow/preview-test.ts
@@ -19,7 +19,7 @@ const TIMEOUT_ERROR_MESSAGE =
   'There was a problem creating your prepaid card. Please contact Cardstack support to find out the status of your transaction.';
 const INSUFFICIENT_FUNDS_ERROR_MESSAGE = `Looks like there's no balance in your ${c.layer2.fullName} wallet to fund your selected prepaid card. Before you can continue, please add funds to your ${c.layer2.fullName} wallet by bridging some tokens from your ${c.layer1.fullName} wallet.`;
 const DEFAULT_ERROR_MESSAGE =
-  'There was a problem creating your prepaid card. Please try again if you want to continue with this workflow, or contact Cardstack support.';
+  'There was a problem creating your prepaid card. This may be due to a network issue, or perhaps you canceled the request in your wallet. Please try again if you want to continue with this workflow, or contact Cardstack support.';
 
 interface Context extends MirageTestContext {}
 
