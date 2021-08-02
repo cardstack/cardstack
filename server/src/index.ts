98e5ec8dc263aa6c3e79b75f3e281bb484eba5d8
Matt McManus <matt@mcmanus.io>
2021-08-02 17:08:13 -0400

    Refactor server command to allow just priming of cache

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,61 @@
 import { Server } from './server';
 import { join } from 'path';
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
 
-async function run() {
-  const cardCacheDir = join(__dirname, '..', '..', 'compiled');
+const cardCacheDir = join(__dirname, '..', '..', 'compiled');
 
-  const realms = [
-    {
-      url: 'https://cardstack.com/base/',
-      directory: join(__dirname, '..', '..', 'base-cards'),
-    },
-    {
-      url: 'https://demo.com/',
-      directory: join(__dirname, '..', '..', 'demo-cards'),
-    },
-  ];
+const realms = [
+  {
+    url: 'https://cardstack.com/base/',
+    directory: join(__dirname, '..', '..', 'base-cards'),
+  },
+  {
+    url: 'https://demo.com/',
+    directory: join(__dirname, '..', '..', 'demo-cards'),
+  },
+];
+
+async function serve(args: any) {
   let server = await Server.create({
     realms,
     cardCacheDir,
-    routeCard: 'https://demo.com/routes',
+    routeCard: args.routeCard,
   });
 
   await server.startWatching();
-  server.app.listen(3000);
+  server.app.listen(args.port);
 }
 
-run();
+async function prime(args: any) {
+  let server = await Server.create({
+    realms,
+    cardCacheDir,
+  });
+
+  await server.primeCache();
+}
+
+yargs(hideBin(process.argv))
+  .command(
+    'serve',
+    'start the server',
+    (yargs) => {
+      return yargs
+        .option('port', {
+          alias: 'p',
+          type: 'number',
+          description: 'Port to bind on',
+          default: 3000,
+        })
+        .option('routeCard', {
+          type: 'string',
+          description: 'URL for servers route card',
+          default: 'https://demo.com/routes',
+        });
+    },
+    serve
+  )
+  .command('prime', 'prime the server cache', prime)
+  .demandCommand()
+  .help().argv;
