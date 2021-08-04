3e82855d4726ca7becaea0b5c738158456a7bb65
pcjun97 <44688367+pcjun97@users.noreply.github.com>
2021-08-05 00:15:19 +0800

    ci: Standardize variable naming

--- scripts/purge-services.sh
+++ scripts/purge-services.sh
@@ -3,7 +3,7 @@ set -e
 
 CLUSTER=$1
 TASK_DEFINITION=${2:-waypoint-hub}
-LIMIT=${3:-2}
+COUNT=${3:-2}
 
 if [ -z "$CLUSTER" ] || [ -z "$TASK_DEFINITION" ]; then
   echo "usage: $0 CLUSTER [TASK_DEFINITION] [COUNT]"
@@ -13,7 +13,7 @@ fi
 SERVICE_LIST="$(aws ecs list-services --cluster $CLUSTER | jq -r '.serviceArns | .[]')"
 if [ "$(echo $SERVICE_LIST | wc -w)" -gt 1 ]; then
   SORTED=($(aws ecs describe-services --cluster $CLUSTER --services $SERVICE_LIST | jq -r ".services | map(select(.taskDefinition | contains(\"/$TASK_DEFINITION:\"))) | sort_by(.createdAt) | .[].serviceName"))
-  for SERVICE in ${SORTED[@]:$LIMIT}; do
+  for SERVICE in ${SORTED[@]:$COUNT}; do
     aws ecs delete-service --cluster $CLUSTER --service $SERVICE --force
   done
 fi
