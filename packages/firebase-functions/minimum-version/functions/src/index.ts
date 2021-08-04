41cf0af4506cdf8ca1f77ca81a183b5b6f7e06bc
danibonilha <dani.bonilha@live.com>
2021-08-04 16:00:02 -0300

    [CS-1510] - Add mininum-version fb function

---
+++ packages/firebase-functions/minimum-version/functions/src/index.ts
@@ -0,0 +1,7 @@
+import { https } from 'firebase-functions';
+
+const MIN_VERSION = '1.0.2';
+
+exports.minimumVersion = https.onRequest(async (_, res) => {
+  res.status(200).send(JSON.stringify({ minVersion: MIN_VERSION }));
+});
