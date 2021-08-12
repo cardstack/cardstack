178a8a79ad6d5317989cd7e12c9cf6f6071f09f7
Chi Jun <pcjun97@gmail.com>
2021-08-12 23:00:09 +0800

    feat: Echo purge commmand before execution

--- scripts/purge-services.sh
+++ scripts/purge-services.sh
@@ -14,6 +14,8 @@ SERVICE_LIST="$(aws ecs list-services --cluster $CLUSTER | jq -r '.serviceArns |
 if [ "$(echo $SERVICE_LIST | wc -w)" -gt 1 ]; then
   SORTED=($(aws ecs describe-services --cluster $CLUSTER --services $SERVICE_LIST | jq -r ".services | map(select(.taskDefinition | contains(\"/$TASK_DEFINITION:\"))) | sort_by(.createdAt) | .[].serviceName"))
   for SERVICE in ${SORTED[@]:$COUNT}; do
-    aws ecs delete-service --cluster $CLUSTER --service $SERVICE --force
+    CMD="aws ecs delete-service --cluster $CLUSTER --service $SERVICE --force"
+    echo "$CMD"
+    $CMD
   done
 fi
