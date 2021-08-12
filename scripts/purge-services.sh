0ec6e4c00063fa3b6cc33a027bdb0a2bef8b054a
Chi Jun <pcjun97@gmail.com>
2021-08-12 23:27:47 +0800

    Fix conditional check

--- scripts/purge-services.sh
+++ scripts/purge-services.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+echo "$0 $@"
 
 CLUSTER=$1
 TASK_DEFINITION=${2:-waypoint-hub}
@@ -11,7 +12,8 @@ if [ -z "$CLUSTER" ] || [ -z "$TASK_DEFINITION" ]; then
 fi
 
 SERVICE_LIST="$(aws ecs list-services --cluster $CLUSTER | jq -r '.serviceArns | .[]')"
-if [ "$(echo $SERVICE_LIST | wc -w)" -gt 1 ]; then
+
+if [ "$(echo $SERVICE_LIST | wc -w)" -gt "$COUNT" ]; then
   SORTED=($(aws ecs describe-services --cluster $CLUSTER --services $SERVICE_LIST | jq -r ".services | map(select(.taskDefinition | contains(\"/$TASK_DEFINITION:\"))) | sort_by(.createdAt) | .[].serviceName"))
   for SERVICE in ${SORTED[@]:$COUNT}; do
     CMD="aws ecs delete-service --cluster $CLUSTER --service $SERVICE --force"
