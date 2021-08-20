31b8cb951778f860874fa81539dd798001b2881e
Aierie <michael.khor@cardstack.com>
2021-08-21 02:17:05 +0800

    updating first-char helper to handle multiple code units

--- packages/web-client/app/helpers/first-char.ts
+++ packages/web-client/app/helpers/first-char.ts
@@ -4,7 +4,12 @@ export function firstChar([str]: [string] /*, hash*/) {
   if (!str || typeof str !== 'string') {
     return;
   }
-  return str.trim()[0];
+
+  // The string iterator (and by extension methods that use it, including Array.from)
+  // can iterate over astral code points (code points that are actually more than one code unit combined)
+  // This is necessary to handle emoji
+  // Detailed explanation: https://mathiasbynens.be/notes/javascript-unicode
+  return Array.from(str.trim())[0];
 }
 
 export default helper(firstChar);
