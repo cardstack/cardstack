535ec5de83ea911950b47761b97eb275be368993
Matic Jurglic <matic@jurglic.si>
2021-08-27 15:05:48 +0200

    Appease the linter which complained dotenv should be published

--- packages/hub/.eslintrc.ts
+++ packages/hub/.eslintrc.ts
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@cardstack/eslint-config',
+  rules: {
+    'node/no-unpublished-require': [
+      'error',
+      {
+        allowModules: ['dotenv'],
+      },
+    ],
+  },
 };
