47ff01657eeea395828d531a5ece54fccffd3bf7
Buck Doyle <buck.doyle@cardstack.com>
2021-08-30 09:16:40 -0500

    boxel: Update dependency lint version
    This version obviates the need for #2005.

--- packages/boxel/tests/dummy/config/dependency-lint.js
+++ packages/boxel/tests/dummy/config/dependency-lint.js
@@ -5,6 +5,5 @@ module.exports = {
     '@embroider/util': '0.39.1 || 0.40.x || 0.41.x',
     'ember-fetch': '6.x || 8.x',
     'ember-test-selectors': '2.1.0 || 5.x',
-    '@ember/test-waiters': '*',
   },
 };
