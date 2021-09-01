3d267ad5202b36a0eaacea63761d6c9438301694
Matic Jurglic <matic@jurglic.si>
2021-09-01 10:46:13 +0200

    Use flow-id param and short uuid

--- packages/web-client/app/controllers/card-pay/balances.ts
+++ packages/web-client/app/controllers/card-pay/balances.ts
@@ -3,16 +3,22 @@ import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import Layer2Network from '@cardstack/web-client/services/layer2-network';
 import { action } from '@ember/object';
+import * as short from 'short-uuid';
 
 class CardPayBalancesController extends Controller {
   @service declare layer2Network: Layer2Network;
 
-  queryParams = ['flow', 'workflowPersistenceId'];
+  queryParams = ['flow', { workflowPersistenceId: 'flow-id' }];
   @tracked flow: string | null = null;
   @tracked workflowPersistenceId: string | null = null;
 
   get prepaidCards() {
     return this.layer2Network.safes.value?.filterBy('type', 'prepaid-card');
+  }
+
+  @action setFlow(flow: string) {
+    this.flow = flow;
+    this.workflowPersistenceId = short.generate();
   }
 
   @action resetQueryParams() {
