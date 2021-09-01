45ed893ed7f535ee1a6bcafad6667410f48581fd
Matic Jurglic <matic@jurglic.si>
2021-09-01 11:41:58 +0200

    Add workflow name to the persisted state so that it's easier to know which flow was persisted

--- packages/web-client/app/services/workflow-persistence.ts
+++ packages/web-client/app/services/workflow-persistence.ts
@@ -1,17 +1,25 @@
 import { default as Service } from '@ember/service';
 
+interface WorkflowPersistencePersistedData {
+  name: string;
+  state: any;
+}
+
 export default class WorkflowPersistence extends Service {
-  getPersistedState(workflowPersistenceId: string) {
+  getPersistedData(workflowPersistenceId: string) {
     return JSON.parse(
       localStorage.getItem(`workflowPersistence:${workflowPersistenceId}`) ||
         '{}'
     );
   }
 
-  persistState(workflowPersistenceId: string, state: any) {
+  persistData(
+    workflowPersistenceId: string,
+    data: WorkflowPersistencePersistedData
+  ) {
     return localStorage.setItem(
       `workflowPersistence:${workflowPersistenceId}`,
-      JSON.stringify(state)
+      JSON.stringify(data)
     );
   }
 }
