4b2a2df45dae18369fa7bf91ef959435a13f6b84
Aierie <michael.khor@cardstack.com>
2021-09-03 23:26:52 +0800

    convert static component data to arguments

--- packages/web-client/app/components/card-pay/merchant-payment-request-card/index.ts
+++ packages/web-client/app/components/card-pay/merchant-payment-request-card/index.ts
@@ -3,16 +3,6 @@ import config from '@cardstack/web-client/config/environment';
 import CardstackLogoForQR from '../../../images/icons/cardstack-logo-opaque-bg.svg';
 
 export default class CardPayMerchantPaymentRequestCard extends Component {
-  canDeepLink = true;
   cardstackLogoForQR = CardstackLogoForQR;
   testFlightLink = config.urls.testFlightLink;
-  paymentURL =
-    'https://pay.cardstack.com/merchat-asdnsadkasd?id=0x1238urfds&amount=73298587423545';
-  merchant = {
-    name: 'Happii Creations',
-    logoBackground: 'cornflowerblue',
-    logoTextColor: 'black',
-  };
-  amount = '300';
-  usdAmount = '3';
 }
