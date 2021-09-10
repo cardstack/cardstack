fffc2d0b2251ebdd904b7d96a435f6c78c5ef230
Matic Jurglic <matic@jurglic.si>
2021-09-10 11:26:01 +0200

    Add the ability to persist and restore the session from persisted state

--- packages/web-client/app/models/workflow/workflow-session.ts
+++ packages/web-client/app/models/workflow/workflow-session.ts
@@ -1,14 +1,37 @@
+import WorkflowPersistence from '@cardstack/web-client/services/workflow-persistence';
 import { tracked } from '@glimmer/tracking';
 
 export interface ArbitraryDictionary {
   [key: string]: any;
 }
+
 export default class WorkflowSession {
+  workflow: any;
+  workflowPersistenceStorage: WorkflowPersistence | undefined;
+
+  constructor(workflow?: any) {
+    this.workflow = workflow;
+
+    // Allow WorkflowSession to be instantiated as a POJO (for unit tests)
+    if (this.workflow && typeof this.workflow.owner.lookup === 'function') {
+      this.workflowPersistenceStorage = this.workflow.owner.lookup(
+        'service:workflow-persistence'
+      );
+    }
+  }
+
   @tracked state: ArbitraryDictionary = {};
+
+  get hasPersistence() {
+    return !!this.workflow?.workflowPersistenceId;
+  }
+
   update(key: string, val: any) {
     this.state[key] = val;
     // eslint-disable-next-line no-self-assign
     this.state = this.state; // for reactivity
+
+    this.persistToStorage();
   }
 
   updateMany(hash: Record<string, any>) {
@@ -17,11 +40,40 @@ export default class WorkflowSession {
     }
     // eslint-disable-next-line no-self-assign
     this.state = this.state; // for reactivity
+
+    this.persistToStorage();
   }
 
   delete(key: string) {
     delete this.state[key];
     // eslint-disable-next-line no-self-assign
     this.state = this.state; // for reactivity
+
+    this.persistToStorage();
+  }
+
+  restoreFromStorage(): void {
+    if (!this.hasPersistence) return;
+
+    const persistedData = this.getPersistedData();
+
+    if (persistedData?.state) {
+      this.state = persistedData.state;
+    }
+  }
+
+  getPersistedData(): any {
+    return this.workflowPersistenceStorage?.getPersistedData(
+      this.workflow.workflowPersistenceId
+    );
+  }
+
+  private persistToStorage(): void {
+    if (!this.hasPersistence) return;
+
+    this.workflowPersistenceStorage?.persistData(
+      this.workflow.workflowPersistenceId,
+      { name: this.workflow.name, state: this.state }
+    );
   }
 }
