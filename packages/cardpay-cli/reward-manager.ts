b829395d1c31eda4d1881a1a5c4e18e255d1beec
Justin <justinthong93@gmail.com>
2021-09-14 09:57:15 +0800

    fix log

--- packages/cardpay-cli/reward-manager.ts
+++ packages/cardpay-cli/reward-manager.ts
@@ -10,10 +10,9 @@ export async function registerRewardProgram(
   let web3 = await getWeb3(network, mnemonic);
   let prepaidCardAPI = await getSDK('PrepaidCard', web3);
   let blockExplorer = await getConstant('blockExplorer', web3);
-  console.log(`Registering reward prorgram`);
+  console.log(`Registering reward program`);
   await prepaidCardAPI.registerRewardProgram(prepaidCard, admin, {
     onTxnHash: (txnHash) => console.log(`Transaction hash: ${blockExplorer}/tx/${txnHash}/token-transfers`),
   });
   console.log('done');
 }
-
