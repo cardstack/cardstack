7442596c159f01e2a02dfbae089b25bc2a4184b0
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-09-24 12:01:55 -0400

    fixing headers for relay server

--- packages/hub/services/relay.ts
+++ packages/hub/services/relay.ts
@@ -21,8 +21,8 @@ export default class RelayService {
     let response = await fetch(`${relayUrl}/v1/prepaid-card/provision/${sku}/`, {
       method: 'POST',
       headers: {
-        contentType: 'application/json',
-        authorization: provisionerSecret,
+        'Content-Type': 'application/json',
+        Authorization: provisionerSecret,
       },
       body: JSON.stringify({
         owner: toChecksumAddress(userAddress),
