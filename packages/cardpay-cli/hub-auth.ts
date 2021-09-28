d904cd6d3beb5a33a1ce7e44054d42bdb00837f0
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-09-28 11:59:38 -0400

    log hubRootURL in CLI to get auth token (#2166)

--- packages/cardpay-cli/hub-auth.ts
+++ packages/cardpay-cli/hub-auth.ts
@@ -4,5 +4,5 @@ import { getSDK } from '@cardstack/cardpay-sdk';
 export const hubAuth = async (hubRootUrl: string, network: string, mnemonic?: string): Promise<void> => {
   let web3 = await getWeb3(network, mnemonic);
   let authToken = await (await getSDK('HubAuth', web3, hubRootUrl)).authenticate();
-  console.log(`Authentication token: ${authToken}`);
+  console.log(`Authentication token for ${hubRootUrl}: ${authToken}`);
 };
