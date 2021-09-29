b5fd314c52a9e239de63a21c090d2849bdb6706a
Buck Doyle <buck.doyle@cardstack.com>
2021-09-29 16:58:35 -0500

    Remove unused getters

--- packages/web-client/app/components/card-pay/header/workflow-tracker/dropdown-list/index.ts
+++ packages/web-client/app/components/card-pay/header/workflow-tracker/dropdown-list/index.ts
@@ -9,14 +9,6 @@ interface CardPayHeaderWorkflowTrackerDropdownListArgs {}
 export default class CardPayHeaderWorkflowTrackerDropdownList extends Component<CardPayHeaderWorkflowTrackerDropdownListArgs> {
   @service declare workflowPersistence: WorkflowPersistence;
 
-  get activeWorkflows() {
-    return this.workflowPersistence.activeWorkflows;
-  }
-
-  get completedWorkflows() {
-    return this.workflowPersistence.completedWorkflows;
-  }
-
   @action clearCompletedWorkflows() {
     this.workflowPersistence.completedWorkflows.forEach((workflowAndId) => {
       this.workflowPersistence.clearWorkflowWithId(workflowAndId.id);
