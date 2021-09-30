20b318d68d97eca1226e63e4973cad8fd4b6197b
Burcu Noyan <burcunoyan@gmail.com>
2021-09-30 19:29:48 -0400

    Lock page scroll during interaction with modal

--- packages/web-client/app/components/card-pay/header/workflow-tracker/dropdown-list/index.ts
+++ packages/web-client/app/components/card-pay/header/workflow-tracker/dropdown-list/index.ts
@@ -16,18 +16,37 @@ export default class CardPayHeaderWorkflowTrackerDropdownList extends Component<
   @action getScrollPosition(element: HTMLElement) {
     this.element = element;
 
-    let item = document.getElementById('active-list')?.firstElementChild;
+    let item =
+      document.getElementById('workflow-tracker-active-list')
+        ?.firstElementChild ??
+      document.getElementById('workflow-tracker-completed-list')
+        ?.firstElementChild;
     this.initialPosition = item?.getBoundingClientRect().top;
 
-    element?.addEventListener('scroll', () => {
+    element.addEventListener('scroll', () => {
       this.currentPosition = item?.getBoundingClientRect().top;
     });
+
+    element.onfocus = this.lockBodyScroll;
+    element.onblur = this.unlockBodyScroll;
+    element.onmouseover = this.lockBodyScroll;
+    element.onmouseout = this.unlockBodyScroll;
   }
 
   get isScrolled() {
     return Boolean(
       this.currentPosition && this.currentPosition !== this.initialPosition
     );
+  }
+
+  lockBodyScroll() {
+    let body = document.getElementsByTagName('body')[0];
+    body.classList.add('has-modal');
+  }
+
+  unlockBodyScroll() {
+    let body = document.getElementsByTagName('body')[0];
+    body.classList.remove('has-modal');
   }
 
   @action clearCompletedWorkflows() {
