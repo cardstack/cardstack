d1c7352d45ebfeae29febb2e541146092749790d
Burcu Noyan <burcunoyan@gmail.com>
2021-09-30 18:33:54 -0400

    Add header shadow on wf history list scroll

--- packages/web-client/app/components/card-pay/header/workflow-tracker/dropdown-list/index.ts
+++ packages/web-client/app/components/card-pay/header/workflow-tracker/dropdown-list/index.ts
@@ -3,15 +3,43 @@ import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 import WorkflowPersistence from '@cardstack/web-client/services/workflow-persistence';
+import { tracked } from '@glimmer/tracking';
 
 interface CardPayHeaderWorkflowTrackerDropdownListArgs {}
 
 export default class CardPayHeaderWorkflowTrackerDropdownList extends Component<CardPayHeaderWorkflowTrackerDropdownListArgs> {
   @service declare workflowPersistence: WorkflowPersistence;
+  element!: HTMLElement;
+  initialPosition?: number;
+  @tracked currentPosition?: number;
+
+  @action getScrollPosition(element: HTMLElement) {
+    this.element = element;
+
+    let item = document.getElementById('active-list')?.firstElementChild;
+    this.initialPosition = item?.getBoundingClientRect().top;
+
+    element?.addEventListener('scroll', () => {
+      this.currentPosition = item?.getBoundingClientRect().top;
+    });
+  }
+
+  get isScrolled() {
+    return Boolean(
+      this.currentPosition && this.currentPosition !== this.initialPosition
+    );
+  }
 
   @action clearCompletedWorkflows() {
     this.workflowPersistence.completedWorkflows.forEach((workflowAndId) => {
       this.workflowPersistence.clearWorkflowWithId(workflowAndId.id);
     });
+  }
+
+  willDestroy() {
+    super.willDestroy();
+    this.initialPosition = undefined;
+    this.currentPosition = undefined;
+    this.element?.removeEventListener('scroll', () => {});
   }
 }
