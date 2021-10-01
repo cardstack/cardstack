6c1462ef8d12a01d2d6f5da2acd0ae5abc8b3a98
Burcu Noyan <burcunoyan@gmail.com>
2021-10-01 17:37:57 -0400

    Initial workflow

---
+++ packages/web-client/app/components/card-space/setup-workflow/setup/index.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import { WorkflowCardComponentArgs } from '@cardstack/web-client/models/workflow';
+
+class CardSpaceSetupWorkflowSetupComponent extends Component<WorkflowCardComponentArgs> {}
+
+export default CardSpaceSetupWorkflowSetupComponent;
---
+++ packages/web-client/app/components/card-space/setup-workflow/user-profile/index.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import { WorkflowCardComponentArgs } from '@cardstack/web-client/models/workflow';
+
+class CardSpaceSetupWorkflowUserProfileComponent extends Component<WorkflowCardComponentArgs> {}
+
+export default CardSpaceSetupWorkflowUserProfileComponent;
