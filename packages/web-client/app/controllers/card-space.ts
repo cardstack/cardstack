154f8eba7eb3122c196a5c5a6a25239d512a7a65
Burcu Noyan <burcunoyan@gmail.com>
2021-10-04 15:39:47 -0400

    Dashboard basic css

--- packages/web-client/app/controllers/card-space.ts
+++ packages/web-client/app/controllers/card-space.ts
@@ -1,15 +1,22 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import * as short from 'short-uuid';
+import '../css/card-space.css';
 
 export default class CardSpaceController extends Controller {
   queryParams = ['flow', { workflowPersistenceId: 'flow-id' }];
+
   @tracked flow: string | null = null;
   @tracked workflowPersistenceId: string | null = null;
 
+  @action transitionHome() {
+    this.transitionToRoute('index');
+  }
+
   @action transitionToWorkflow(flow: string) {
     this.transitionToRoute('card-space', {
-      queryParams: { flow },
+      queryParams: { flow, workflowPersistenceId: short.generate() },
     });
   }
 
