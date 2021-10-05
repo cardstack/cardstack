b6ca2af198a0e1861aa1f379efcb79d1ef92aabf
Buck Doyle <buck.doyle@cardstack.com>
2021-10-05 17:21:44 -0500

    Update issuance to allow merchant safe source
    This works somewhat for the funding source card but the
    chosen values aren’t picked up by the following cards.

--- packages/web-client/app/components/card-pay/issue-prepaid-card-workflow/funding-source/index.ts
+++ packages/web-client/app/components/card-pay/issue-prepaid-card-workflow/funding-source/index.ts
@@ -4,25 +4,37 @@ import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import BN from 'bn.js';
 
+import { Safe } from '@cardstack/cardpay-sdk/sdk/safes';
 import Layer2Network from '@cardstack/web-client/services/layer2-network';
 import {
   TokenBalance,
   BridgedTokenSymbol,
+  getUnbridgedSymbol,
 } from '@cardstack/web-client/utils/token';
 import { WorkflowCardComponentArgs } from '@cardstack/web-client/models/workflow';
 
 class FundingSourceCard extends Component<WorkflowCardComponentArgs> {
+  compatibleSafeTypes = ['depot', 'merchant'];
+
   defaultTokenSymbol: BridgedTokenSymbol = 'DAI.CPXD';
   tokenOptions = [this.defaultTokenSymbol];
   @service declare layer2Network: Layer2Network;
-  @tracked selectedTokenSymbol: BridgedTokenSymbol =
-    this.args.workflowSession.getValue('prepaidFundingToken') ??
-    this.defaultTokenSymbol;
 
-  get selectedToken(): TokenBalance<BridgedTokenSymbol> {
+  @tracked selectedSafe: Safe | undefined;
+  @tracked selectedTokenSymbol: BridgedTokenSymbol;
+
+  constructor(owner: unknown, args: WorkflowCardComponentArgs) {
+    super(owner, args);
+    this.selectedSafe = this.layer2Network.depotSafe;
+    this.selectedTokenSymbol =
+      this.tokens.find((t) => t.symbol === this.prepaidFundingToken)
+        ?.tokenDisplayInfo.symbol ?? this.tokens[0].tokenDisplayInfo.symbol;
+  }
+
+  get prepaidFundingToken(): BridgedTokenSymbol {
     return (
-      this.tokens.find((t) => t.symbol === this.selectedTokenSymbol) ??
-      this.tokens[0]
+      this.args.workflowSession.getValue('prepaidFundingToken') ??
+      this.defaultTokenSymbol
     );
   }
 
@@ -30,30 +42,43 @@ class FundingSourceCard extends Component<WorkflowCardComponentArgs> {
     return this.layer2Network.depotSafe?.address || undefined;
   }
 
+  get compatibleSafes() {
+    return this.layer2Network.safes.value.filter((safe) =>
+      this.compatibleSafeTypes.includes(safe.type)
+    );
+  }
+
   get tokens() {
     return this.tokenOptions.map((symbol) => {
-      let balance = this.getTokenBalance(symbol);
+      let unbridgedSymbol = getUnbridgedSymbol(symbol);
+      let selectedSafeToken = (this.selectedSafe?.tokens || []).find(
+        (token) => token.token.symbol === unbridgedSymbol
+      );
+      let balance = new BN(selectedSafeToken?.balance || '0');
       return new TokenBalance(symbol, balance);
     });
   }
 
-  getTokenBalance(symbol: BridgedTokenSymbol) {
-    if (symbol === this.defaultTokenSymbol) {
-      return this.layer2Network.defaultTokenBalance ?? new BN('0');
-    }
-    return new BN('0');
+  get selectedToken() {
+    return this.tokens.find(
+      (token) => token.symbol === this.selectedTokenSymbol
+    );
   }
 
   get isDisabled() {
     return (
       !this.depotAddress ||
       !this.tokens.length ||
-      !this.selectedToken.balance ||
-      this.selectedToken.balance.isZero()
+      !this.selectedToken?.balance ||
+      this.selectedToken?.balance.isZero()
     );
   }
 
-  @action chooseSource(token: TokenBalance<BridgedTokenSymbol>) {
+  @action chooseSafe(safe: Safe) {
+    this.selectedSafe = safe;
+  }
+
+  @action chooseBalance(token: TokenBalance<BridgedTokenSymbol>) {
     this.selectedTokenSymbol = token.symbol;
   }
 
@@ -62,10 +87,10 @@ class FundingSourceCard extends Component<WorkflowCardComponentArgs> {
       return;
     }
     if (this.selectedTokenSymbol) {
-      this.args.workflowSession.setValue(
-        'prepaidFundingToken',
-        this.selectedTokenSymbol
-      );
+      this.args.workflowSession.setValue({
+        prepaidFundingToken: this.selectedTokenSymbol,
+        prepaidFundingSafe: this.selectedSafe,
+      });
     }
     this.args.onComplete?.();
   }
--- packages/web-client/tests/acceptance/issue-prepaid-card-test.ts
+++ packages/web-client/tests/acceptance/issue-prepaid-card-test.ts
@@ -32,6 +32,7 @@ import {
 } from '@cardstack/web-client/models/workflow/workflow-session';
 import {
   createDepotSafe,
+  createMerchantSafe,
   createPrepaidCardSafe,
   createSafeToken,
   defaultCreatedPrepaidCardDID,
@@ -120,6 +121,16 @@ module('Acceptance | issue prepaid card', function (hooks) {
     let layer2AccountAddress = '0x182619c6Ea074C053eF3f1e1eF81Ec8De6Eb6E44';
     layer2Service.test__simulateAccountsChanged([layer2AccountAddress]);
     let depotAddress = '0xB236ca8DbAB0644ffCD32518eBF4924ba8666666';
+    let merchantSafe = createMerchantSafe({
+      address: '0xmerchantbAB0644ffCD32518eBF4924ba8666666',
+      merchant: '0xprepaidDbAB0644ffCD32518eBF4924ba8666666',
+      tokens: [
+        createSafeToken('DAI', '125000000000000000000'),
+        createSafeToken('CARD', '450000000000000000000'),
+      ],
+      accumulatedSpendValue: 100,
+    });
+
     layer2Service.test__simulateAccountSafes(layer2AccountAddress, [
       createDepotSafe({
         address: depotAddress,
@@ -136,6 +147,7 @@ module('Acceptance | issue prepaid card', function (hooks) {
         prepaidCardOwner: layer2AccountAddress,
         issuer: layer2AccountAddress,
       }),
+      merchantSafe,
     ]);
 
     await waitUntil(
@@ -261,16 +273,24 @@ module('Acceptance | issue prepaid card', function (hooks) {
     post = postableSel(2, 2);
     // // funding-source card
     assert
-      .dom('[data-test-account-outer] [data-test-account-address]')
-      .hasText('0x1826...6E44');
-    assert
-      .dom(
-        `${post} [data-test-account-depot-outer] [data-test-account-address]`
-      )
-      .hasText(depotAddress);
+      .dom(`${post} [data-test-choose-balance-from-safe]`)
+      .hasText(`DEPOT ${depotAddress}`);
+
     assert
       .dom(`${post} [data-test-balance-chooser-dropdown="DAI.CPXD"]`)
       .containsText(`${SLIGHTLY_LESS_THAN_MAX_VALUE_IN_ETHER.toFixed(2)} DAI`);
+
+    await click(
+      '[data-test-safe-chooser-dropdown] .ember-power-select-trigger'
+    );
+    assert
+      .dom('[data-test-safe-chooser-dropdown] li:nth-child(1)')
+      .containsText(depotAddress);
+    assert
+      .dom('[data-test-safe-chooser-dropdown] li:nth-child(2)')
+      .containsText(merchantSafe.address);
+
+    await click('[data-test-safe-chooser-dropdown] li:nth-child(2)');
     await click(
       `${post} [data-test-boxel-action-chin] [data-test-boxel-button]`
     );
@@ -278,16 +298,14 @@ module('Acceptance | issue prepaid card', function (hooks) {
       .dom(`${post} [data-test-balance-chooser-dropdown="DAI.CPXD"]`)
       .doesNotExist();
     assert
-      .dom(`${post} [data-test-account-balance]`)
-      .containsText(`${SLIGHTLY_LESS_THAN_MAX_VALUE_IN_ETHER.toFixed(2)} DAI`);
+      .dom(`${post} [data-test-balance-display-amount]`)
+      .containsText('125.00 DAI');
 
     assert
       .dom(postableSel(2, 3))
       .containsText('choose the face value of your prepaid card');
     // // face-value card
-    assert
-      .dom('[data-test-balance-view-summary]')
-      .containsText(`${SLIGHTLY_LESS_THAN_MAX_VALUE_IN_ETHER.toFixed(2)} DAI`);
+    assert.dom('[data-test-balance-view-summary]').containsText('125.00 DAI');
     await click('[data-test-balance-view-summary]');
     assert
       .dom('[data-test-balance-view-account-address]')
@@ -566,6 +584,7 @@ module('Acceptance | issue prepaid card', function (hooks) {
       },
       prepaidCardAddress: '0xaeFbA62A2B3e90FD131209CC94480E722704E1F8',
       prepaidCardSafe,
+      prepaidFundingSafe: merchantSafe,
       prepaidFundingToken: 'DAI.CPXD',
       reloadable: false,
       spendFaceValue: 10000,
