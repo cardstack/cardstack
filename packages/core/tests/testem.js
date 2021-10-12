06b21b6ae629a85ba54022056e778aca630c300f
Matt McManus <matt@mcmanus.io>
2021-10-12 11:11:27 -0400

    Remove unneeded files in core

--- packages/core/tests/testem.js
+++
@@ -1,3 +0,0 @@
-// when running `yarn start` to view tests in browser, there is no testem
-// controlling things so you would see an error from the testem.js script tag.
-// This just prevents that error.
