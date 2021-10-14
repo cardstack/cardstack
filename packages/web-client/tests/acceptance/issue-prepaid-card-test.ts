6ee1bf3638a2f125e51f6d378b68f894480178cc
Buck Doyle <buck.doyle@cardstack.com>
2021-10-14 14:44:48 -0500

    Rename test selector to not assume depot type

--- packages/web-client/tests/acceptance/issue-prepaid-card-test.ts
+++ packages/web-client/tests/acceptance/issue-prepaid-card-test.ts
@@ -329,7 +329,7 @@ module('Acceptance | issue prepaid card', function (hooks) {
       .dom('[data-test-balance-view-account-address]')
       .containsText(layer2AccountAddress);
     assert
-      .dom('[data-test-balance-view-depot-address]')
+      .dom('[data-test-balance-view-safe-address]')
       .containsText(merchantSafe.address);
     assert
       .dom('[data-test-balance-view-token-amount]')
