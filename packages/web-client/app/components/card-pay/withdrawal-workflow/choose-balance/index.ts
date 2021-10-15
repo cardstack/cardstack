4aebad79d0bbacf1222ab5913eb61f4dc7d548a6
Buck Doyle <buck.doyle@cardstack.com>
2021-10-15 10:39:29 -0500

    web: Fix display of restored withdrawal source
    The default value for the selected safe was being used
    even when was already persisted in the workflow.

--- packages/web-client/app/components/card-pay/withdrawal-workflow/choose-balance/index.ts
+++ packages/web-client/app/components/card-pay/withdrawal-workflow/choose-balance/index.ts
@@ -27,7 +27,11 @@ class CardPayWithdrawalWorkflowChooseBalanceComponent extends Component<Workflow
 
   constructor(owner: unknown, args: WorkflowCardComponentArgs) {
     super(owner, args);
-    this.selectedSafe = this.layer2Network.depotSafe;
+    let withdrawalSafeAddress =
+      this.args.workflowSession.getValue<string>('withdrawalSafe');
+    this.selectedSafe = withdrawalSafeAddress
+      ? this.layer2Network.safes.getByAddress(withdrawalSafeAddress)
+      : this.layer2Network.depotSafe;
     this.selectedTokenSymbol =
       this.tokens.find((t) => t.symbol === this.withdrawalToken)
         ?.tokenDisplayInfo.symbol ?? this.tokens[0].tokenDisplayInfo.symbol;
