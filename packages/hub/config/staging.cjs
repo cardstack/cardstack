236d368c6609e27f74a98556e6253a6895f03923
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-10-18 17:34:14 -0400

    using staging cjs config

---
+++ packages/hub/config/staging.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  discord: {
+    botId: '898890288762875925',
+    allowedGuilds: '584043165066199050',
+    allowedChannels: '898941895525425172',
+  },
+  wyre: {
+    callbackUrl: 'https://hub-staging.stack.cards/callbacks/wyre',
+  },
+};
