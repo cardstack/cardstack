a199f577d1bd39f7eeed941ce7bae93b233f1ea3
Matt McManus <matt@mcmanus.io>
2021-10-18 15:16:51 -0400

    Remove unnecessary helper

--- packages/base-cards/string/edit.js
+++ packages/base-cards/string/edit.js
@@ -1,23 +1,12 @@
 import { setComponentTemplate } from '@ember/component';
 import { precompileTemplate } from '@ember/template-compilation';
 import templateOnlyComponent from '@ember/component/template-only';
-import { on as realOn } from '@ember/modifier';
-import { helper } from '@ember/component/helper';
 import BoxelInput from '@cardstack/boxel/components/boxel/input';
 
-// https://github.com/ember-cli/babel-plugin-htmlbars-inline-precompile/issues/379
-let on = realOn;
-
-const getTargetValue = helper(([action] /*, hash*/) => {
-  return function (value) {
-    action(value);
-  };
-});
-
 export default setComponentTemplate(
-  precompileTemplate('<BoxelInput type="text" @value="{{@model}}" @onInput={{getTargetValue @set}} ...attributes />', {
+  precompileTemplate('<BoxelInput type="text" @value="{{@model}}" @onInput={{@set}} ...attributes />', {
     strictMode: true,
-    scope: () => ({ on, getTargetValue, BoxelInput }),
+    scope: () => ({ BoxelInput }),
   }),
   templateOnlyComponent()
 );
