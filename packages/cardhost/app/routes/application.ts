af262817cb8a5dfe6bdeadb28c2a83b64cf9ffd3
Aierie <michael.khor@cardstack.com>
2021-10-26 15:55:27 +0800

    make cardhost responsible for importing boxel css it wants to use

--- packages/cardhost/app/routes/application.ts
+++ packages/cardhost/app/routes/application.ts
@@ -1,5 +1,18 @@
 import Route from '@ember/routing/route';
 
-import '@cardstack/boxel/styles/boxel';
+import '@cardstack/boxel/styles/variables.css';
+import '@cardstack/boxel/styles/global.css';
+import '@cardstack/boxel/components/boxel/action-chin/index.css';
+import '@cardstack/boxel/components/boxel/action-chin/action-status-area/index.css';
+import '@cardstack/boxel/components/boxel/action-container/index.css';
+import '@cardstack/boxel/components/boxel/button/index.css';
+import '@cardstack/boxel/components/boxel/button/kind-variants.css';
+import '@cardstack/boxel/components/boxel/button/size-variants.css';
+import '@cardstack/boxel/components/boxel/card-container/index.css';
+import '@cardstack/boxel/components/boxel/field/view/index.css';
+import '@cardstack/boxel/components/boxel/field/edit/index.css';
+import '@cardstack/boxel/components/boxel/header/index.css';
+import '@cardstack/boxel/components/boxel/input/index.css';
+import '@cardstack/boxel/components/boxel/select-button/index.css';
 
 export default class ApplicationRoute extends Route {}
