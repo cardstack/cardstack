8e7e178017f254de2d30ffc52d4b8a04c005fc58
Edward Faulkner <ef@alum.mit.edu>
2021-10-28 16:37:00 -0400

    updates

--- packages/hub/rollup.config.js
+++ packages/hub/rollup.config.js
@@ -5,8 +5,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
 import replace from '@rollup/plugin-replace';
-//import graph from 'rollup-plugin-graph';
+import graph from 'rollup-plugin-graph';
 import nativePlugin from 'rollup-plugin-natives';
+import path from 'path';
 
 export default {
   input: 'bin/hub.cjs',
@@ -16,26 +17,46 @@ export default {
     sourcemap: true,
     exports: 'default',
   },
-  external: ['config', 'pg-native'],
+  external: ['config', 'readable-stream', 'parse-asn1', 'discord.js', 'glob'],
+  onwarn(warning, rollupWarn) {
+    console.log(warning);
+  },
   plugins: [
     replace({
       // the dependency "formidable" includes code for hijacking require that it
       // doesn't need that generates build warnings. This silences them.
       'require = GENTLY.hijack(require)': 'true',
+
+      // these are optional dependencies.
+      "require('electron')": "(function(){ var e = new Error(); e.code='MODULE_NOT_FOUND'; throw e })",
+      "require('ffmpeg-static')": "(function(){ var e = new Error(); e.code='MODULE_NOT_FOUND'; throw e })",
+
+      // it's hard to actually use the delimters option correctly due to
+      // https://github.com/rollup/plugins/issues/904 we don't want the default
+      // because our patterns don't end with word characters, so the trailing
+      // /\b/ won't match. But we can't actually keep the default leading
+      // delimter due to the bug.
+      delimiters: ['', ''],
+    }),
+    replace({
+      "require('./native')": "(function(){ var e = new Error(); e.code='MODULE_NOT_FOUND'; throw e })",
+      delimiters: ['', ''],
+      include: [path.relative(process.cwd(), require.resolve('pg/lib/index.js'))],
+    }),
+    replace({
       delimiters: ['', ''],
     }),
     //customPatches(),
     nodeResolve({ preferBuiltins: true, extensions: ['.mjs', '.js', '.json', '.node', '.ts'] }),
     commonjs({
-      ignore: ['pg-native', 'electron', 'ffmpeg-static'],
-      dynamicRequireTargets: ['./config/*.cjs', '../../node_modules/split2/node_modules/readable-stream/lib/*.js'],
+      //dynamicRequireTargets: ['./config/*.cjs', '../../node_modules/split2/node_modules/readable-stream/lib/*.js'],
     }),
     babel({
       plugins: ['@babel/plugin-transform-typescript', ['@babel/plugin-proposal-decorators', { legacy: true }]],
       babelHelpers: 'bundled',
       extensions: ['.ts'],
     }),
-    //graph(),
+    // graph(),
     json(),
 
     // TODO: this is unused so far, if it remains so we should remove it
@@ -62,13 +83,20 @@ function customPatches() {
   return {
     name: 'custom-patches',
     resolveId(importee, requester) {
-      if (requester?.endsWith('node_modules/pg/lib/index.js') && /\0?\.\/native/.test(importee)) {
+      if (/pg\/lib\/index/.test(requester)) {
+        console.log(
+          `${requester} ${requester?.endsWith('node_modules/pg/lib/index.js')} ${importee} ${/^\0?\.\/native/.test(
+            importee
+          )}`
+        );
+      }
+      if (requester?.endsWith('node_modules/pg/lib/index.js') && /^\0?\.\/native/.test(importee)) {
         return runtimeFailure(importee);
       }
 
-      if (['pg-native', 'electron', 'ffmpeg-static'].includes(importee)) {
-        return runtimeFailure(importee);
-      }
+      // if (['pg-native', 'electron', 'ffmpeg-static'].includes(importee)) {
+      //   return runtimeFailure(importee);
+      // }
       return null;
     },
     load(id) {
