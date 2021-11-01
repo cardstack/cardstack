878e99f9ac11e35b23ce4d4ed4962636322be32b
Buck Doyle <buck.doyle@cardstack.com>
2021-11-01 13:36:48 -0500

    Update Percy configuration for full-size snapshots

--- packages/web-client/.percy.js
+++ packages/web-client/.percy.js
@@ -1,10 +1,27 @@
 module.exports = {
   version: 2,
   snapshot: {
-    minHeight: 2000,
+    widths: [1280],
     percyCSS: `
+    body.has-modal {
+      overflow: visible;
+    }
+
+    .boxel-modal-overlay {
+      position: static;
+    }
+
+    .boxel-modal {
+      position: static;
+      height: auto;
+    }
+
     .boxel-date-divider__date, .boxel-thread-message__time {
       visibility: hidden;
+    }
+
+    .boxel-thread {
+      height: auto;
     }
   `,
   },
