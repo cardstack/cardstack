8ef88553c994055a421ebe253280a8c2574dfd88
Buck Doyle <buck.doyle@cardstack.com>
2021-11-01 15:32:02 -0500

    Add version display to hidden elements

--- packages/web-client/.percy.js
+++ packages/web-client/.percy.js
@@ -16,7 +16,7 @@ module.exports = {
       height: auto;
     }
 
-    .boxel-date-divider__date, .boxel-thread-message__time {
+    .boxel-date-divider__date, .boxel-thread-message__time, .workflow-thread__app-version {
       visibility: hidden;
     }
 
