61f8b1aedf17c378a9ff98ee7c4105f57767cd1f
Matic Jurglic <matic@jurglic.si>
2021-11-02 09:30:18 +0100

    Make transform publicly available on the service

--- packages/hub/node-tests/services/reserved-words-test.ts
+++ packages/hub/node-tests/services/reserved-words-test.ts
@@ -1,7 +1,5 @@
 import ReservedWordsService from '../../services/reserved-words';
 
-const lowercaseAlphanumeric = (str: string) => str.toLowerCase().replace(/[^a-z0-9]/g, '');
-
 let reservedWordsService = new ReservedWordsService();
 describe('ReservedWordsService', function () {
   it('can detect a reserved word', async function () {
@@ -9,7 +7,9 @@ describe('ReservedWordsService', function () {
   });
 
   it('can detect a reserved word with transforms', async function () {
-    expect(reservedWordsService.isReserved('christse', lowercaseAlphanumeric)).equal(true);
+    expect(reservedWordsService.isReserved('christse', reservedWordsService.lowerCaseAlphaNumericTransform)).equal(
+      true
+    );
   });
 
   it('will not match a non-profane, non-exact reserved word', async function () {
@@ -21,7 +21,9 @@ describe('ReservedWordsService', function () {
   });
 
   it('can detect a profane word with transforms', async function () {
-    expect(reservedWordsService.isProfane('leatherrestraint', lowercaseAlphanumeric)).equal(true);
+    expect(
+      reservedWordsService.isProfane('leatherrestraint', reservedWordsService.lowerCaseAlphaNumericTransform)
+    ).equal(true);
   });
 
   it('can detect a profane word in the midst of other words', async function () {
