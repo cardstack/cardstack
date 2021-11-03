7619289cc34b374615287e8d53ec9e426aff49f2
Burcu Noyan <burcunoyan@gmail.com>
2021-11-03 15:02:12 -0400

    More dep conflicts resolve

--- packages/boxel/tests/dummy/config/dependency-lint.js
+++ packages/boxel/tests/dummy/config/dependency-lint.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   allowedVersions: {
-    '@embroider/util': '0.41.x || 0.47.0',
+    '@embroider/util': '0.41.0 || 0.47.x',
     'ember-test-selectors': '2.1.0 || 5.x',
   },
 };
