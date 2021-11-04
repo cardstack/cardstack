09419efa13e50396c817b3d8639c19c7b3daa05b
Buck Doyle <buck.doyle@cardstack.com>
2021-11-04 10:55:57 -0500

    Add CSS explanations

--- packages/web-client/.percy.js
+++ packages/web-client/.percy.js
@@ -3,6 +3,7 @@ module.exports = {
   snapshot: {
     widths: [1280],
     percyCSS: `
+    /* Break workflow threads out of modal/overflow containers */
     body.has-modal {
       overflow: visible;
     }
@@ -16,12 +17,13 @@ module.exports = {
       height: auto;
     }
 
-    .boxel-date-divider__date, .boxel-thread-message__time, .workflow-thread__app-version {
-      visibility: hidden;
-    }
-
     .boxel-thread {
       height: auto;
+    }
+
+    /* Hide frequently-changing elements: dates, times, and the sidebar application version */
+    .boxel-date-divider__date, .boxel-thread-message__time, .workflow-thread__app-version {
+      visibility: hidden;
     }
   `,
   },
