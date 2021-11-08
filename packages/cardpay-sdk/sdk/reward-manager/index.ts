0e76e47c41920d3e2dad1e74134b3e2e6afa636b
Justin <justinthong93@gmail.com>
2021-11-08 20:44:58 +0800

    update resolver imports

--- packages/cardpay-sdk/sdk/reward-manager/index.ts
+++ packages/cardpay-sdk/sdk/reward-manager/index.ts
@@ -2,12 +2,13 @@ import { ContractMeta } from '../version-resolver';
 
 import v0_8_0 from './v0.8.0';
 import v0_8_4 from './v0.8.4';
+import v0_8_5 from './v0.8.5';
 
 // add more versions as we go, but also please do drop version that we don't
 // want to maintain simultaneously
-export type RewardManager = v0_8_4;
+export type RewardManager = v0_8_5;
 
 export const rewardManagerMeta = {
-  apiVersions: { v0_8_0, v0_8_4 },
+  apiVersions: { v0_8_0, v0_8_4, v0_8_5 },
   contractName: 'rewardManager',
 } as ContractMeta;
