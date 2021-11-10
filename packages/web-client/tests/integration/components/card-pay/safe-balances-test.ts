462caf968c9855cdc9f6e96a38abc776b3183636
Buck Doyle <buck.doyle@cardstack.com>
2021-11-10 12:22:41 -0600

    Remove unnecessary shared setup
    As suggested by @Aierie:
    https://github.com/cardstack/cardstack/pull/2349#discussion_r746323121

--- packages/web-client/tests/integration/components/card-pay/safe-balances-test.ts
+++ packages/web-client/tests/integration/components/card-pay/safe-balances-test.ts
@@ -7,7 +7,6 @@ import { setupMirage } from 'ember-cli-mirage/test-support';
 import { MirageTestContext } from 'ember-cli-mirage/test-support';
 import Layer2TestWeb3Strategy from '@cardstack/web-client/utils/web3-strategies/test-layer2';
 
-import { Safe } from '@cardstack/cardpay-sdk';
 import {
   createDepotSafe,
   createMerchantSafe,
@@ -32,8 +31,6 @@ module('Integration | Component | card-pay/safe-balances', function (hooks) {
   let merchantAddress = '0xmerchantbAB0644ffCD32518eBF4924ba8666666';
   let prepaidCardAddress = '0xprepaidDbAB0644ffCD32518eBF4924ba8666666';
 
-  let depotSafe: Safe, merchantSafe: Safe, prepaidCardSafe: Safe;
-
   hooks.beforeEach(async function (this: Context) {
     this.server.create('merchant-info', {
       id: await getFilenameFromDid(EXAMPLE_DID),
@@ -47,41 +44,20 @@ module('Integration | Component | card-pay/safe-balances', function (hooks) {
 
     layer2Service = this.owner.lookup('service:layer2-network')
       .strategy as Layer2TestWeb3Strategy;
-    layer2Service.test__simulateRemoteAccountSafes(layer2AccountAddress, [
-      (depotSafe = createDepotSafe({
+    await layer2Service.test__simulateAccountsChanged([layer2AccountAddress]);
+  });
+
+  test('it renders a depot safe', async function (this: Context, assert) {
+    this.set(
+      'safe',
+      createDepotSafe({
         address: depotAddress,
         tokens: [
           createSafeToken('DAI.CPXD', '250111100000000000000'),
           createSafeToken('CARD.CPXD', '500000000000000000000'),
         ],
-      })),
-      (merchantSafe = createMerchantSafe({
-        address: merchantAddress,
-        merchant: '0xprepaidDbAB0644ffCD32518eBF4924ba8666666',
-        tokens: [createSafeToken('DAI.CPXD', '125000000000000000000')],
-        accumulatedSpendValue: 100,
-        infoDID: EXAMPLE_DID,
-      })),
-      (prepaidCardSafe = createPrepaidCardSafe({
-        address: prepaidCardAddress,
-        owners: [layer2AccountAddress],
-        tokens: [
-          createSafeToken('DAI.CPXD', '225000000000000000000'),
-          createSafeToken('CARD.CPXD', '0'),
-        ],
-        spendFaceValue: 2324,
-        prepaidCardOwner: layer2AccountAddress,
-        issuer: layer2AccountAddress,
-        transferrable: false,
-      })),
-    ]);
-
-    // Ensure safes have been loaded, as in a workflow context
-    await layer2Service.test__simulateAccountsChanged([layer2AccountAddress]);
-  });
-
-  test('it renders a depot safe', async function (this: Context, assert) {
-    this.set('safe', depotSafe);
+      })
+    );
     await render(hbs`
       <CardPay::SafeBalances
         @safe={{this.safe}}
@@ -114,7 +90,16 @@ module('Integration | Component | card-pay/safe-balances', function (hooks) {
   });
 
   test('it renders a merchant safe', async function (this: Context, assert) {
-    this.set('safe', merchantSafe);
+    this.set(
+      'safe',
+      createMerchantSafe({
+        address: merchantAddress,
+        merchant: '0xprepaidDbAB0644ffCD32518eBF4924ba8666666',
+        tokens: [createSafeToken('DAI.CPXD', '125000000000000000000')],
+        accumulatedSpendValue: 100,
+        infoDID: EXAMPLE_DID,
+      })
+    );
     await render(hbs`
       <CardPay::SafeBalances
         @safe={{this.safe}}
@@ -151,7 +136,21 @@ module('Integration | Component | card-pay/safe-balances', function (hooks) {
         'CardPay::SafeBalances does not support a safe type of prepaid-card'
       );
     });
-    this.set('safe', prepaidCardSafe);
+    this.set(
+      'safe',
+      createPrepaidCardSafe({
+        address: prepaidCardAddress,
+        owners: [layer2AccountAddress],
+        tokens: [
+          createSafeToken('DAI.CPXD', '225000000000000000000'),
+          createSafeToken('CARD.CPXD', '0'),
+        ],
+        spendFaceValue: 2324,
+        prepaidCardOwner: layer2AccountAddress,
+        issuer: layer2AccountAddress,
+        transferrable: false,
+      })
+    );
     await render(hbs`
       <CardPay::SafeBalances
         @safe={{this.safe}}
