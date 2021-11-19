298f199899aba023b82c76882642be952d338dbb
Burcu Noyan <burcunoyan@gmail.com>
2021-11-19 10:45:21 -0500

    Move CardContainers to template in isolated.js

--- packages/demo-cards/links/embedded.js
+++ packages/demo-cards/links/embedded.js
@@ -1,24 +1,19 @@
 import { setComponentTemplate } from '@ember/component';
 import { precompileTemplate } from '@ember/template-compilation';
 import templateOnlyComponent from '@ember/component/template-only';
-import BoxelCardContainer from '@cardstack/boxel/components/boxel/card-container';
-import BoxelHeader from '@cardstack/boxel/components/boxel/header';
 import './embedded.css';
 
 export default setComponentTemplate(
   precompileTemplate(
-    `<BoxelCardContainer @displayBoundaries={{true}}>
-      <BoxelHeader>Links</BoxelHeader>
-      <section class="links">
-        <header class="links__header">My Links</header>
-        <ul class="links__list">
-          <@fields.links/>
-        </ul>
-      </section>
-    </BoxelCardContainer>`,
+    `<section class="links">
+      <header class="links__header">My Links</header>
+      <ul class="links__list">
+        <@fields.links/>
+      </ul>
+    </section>`,
     {
       strictMode: true,
-      scope: () => ({ BoxelCardContainer, BoxelHeader }),
+      scope: () => ({}),
     }
   ),
   templateOnlyComponent()
--- packages/demo-cards/profile/embedded.js
+++ packages/demo-cards/profile/embedded.js
@@ -1,27 +1,22 @@
 import { setComponentTemplate } from '@ember/component';
 import { precompileTemplate } from '@ember/template-compilation';
 import templateOnlyComponent from '@ember/component/template-only';
-import BoxelCardContainer from '@cardstack/boxel/components/boxel/card-container';
-import BoxelHeader from '@cardstack/boxel/components/boxel/header';
 import './embedded.css';
 
 export default setComponentTemplate(
   precompileTemplate(
-    `<BoxelCardContainer @displayBoundaries={{true}}>
-      <BoxelHeader>Profile</BoxelHeader>
-      <section class="profile">
-        <header class="profile__header">
-          <@fields.profilePicture/>
-          <h1 class="profile__title"><@fields.name/></h1>
-          <div class="profile__url"><@fields.url/></div>
-          <span class="profile__label"><@fields.category/></span>
-        </header>
-        <p><@fields.description/></p>
-      </section>
-    </BoxelCardContainer>`,
+    `<section class="profile">
+      <header class="profile__header">
+        <@fields.profilePicture/>
+        <h1 class="profile__title"><@fields.name/></h1>
+        <div class="profile__url"><@fields.url/></div>
+        <span class="profile__label"><@fields.category/></span>
+      </header>
+      <p><@fields.description/></p>
+    </section>`,
     {
       strictMode: true,
-      scope: () => ({ BoxelCardContainer, BoxelHeader }),
+      scope: () => ({}),
     }
   ),
   templateOnlyComponent()
