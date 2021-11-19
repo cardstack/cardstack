9da5e7e29b25f8b1353d9cf15e82ec964a57f919
Aierie <39579264+Aierie@users.noreply.github.com>
2021-11-19 23:30:36 +0800

    web: Change cancel to resend for deposit workflow (#2393)
    This modifies the deposit workflow to use a retry UI that allows us to
    keep track of every deposit signing request sent until one returns with
    a transaction hash, and then we continue following only that one. The
    reason for this is that we don't reliably have a way to cancel
    transactions via nonce.

--- packages/web-client/tests/integration/components/card-pay/deposit-workflow/transaction-amount-test.ts
+++ packages/web-client/tests/integration/components/card-pay/deposit-workflow/transaction-amount-test.ts
@@ -7,6 +7,7 @@ import {
   click,
   settled,
   waitFor,
+  find,
 } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import Layer1TestWeb3Strategy from '@cardstack/web-client/utils/web3-strategies/test-layer1';
@@ -15,6 +16,9 @@ import { WorkflowSession } from '@cardstack/web-client/models/workflow';
 import BN from 'bn.js';
 import { toWei } from 'web3-utils';
 import sinon from 'sinon';
+import { TransactionReceipt } from 'web3-core';
+import { RelayTokensOptions } from '@cardstack/web-client/utils/web3-strategies/types';
+import RSVP, { defer } from 'rsvp';
 
 let layer1Service: Layer1TestWeb3Strategy;
 let session: WorkflowSession;
@@ -104,10 +108,12 @@ module(
       assert.ok(completed);
     });
 
-    test('its deposit step can be canceled if there is no deposit transaction hash', async function (assert) {
+    test('its deposit step can be retried if there is no deposit transaction hash', async function (assert) {
       session.setValue('unlockTxnHash', 'unlockTokensTxnHash');
       session.setValue('depositSourceToken', 'DAI');
       session.setValue('depositedAmount', new BN(toWei('5')));
+
+      let relayTokensSpy = sinon.spy(layer1Service, 'relayTokens');
 
       await render(hbs`
       <CardPay::DepositWorkflow::TransactionAmount
@@ -120,19 +126,29 @@ module(
       await layer1Service.test__simulateUnlock();
 
       await click('[data-test-deposit-button]');
+
       assert.dom('[data-test-deposit-button]').containsText('Depositing');
+      assert.ok(relayTokensSpy.calledOnce);
 
       await new Promise((resolve) => setTimeout(() => resolve(true), 500));
-      assert.dom('[data-test-deposit-cancel-button]').isVisible();
-      assert.dom('[data-test-deposit-cancel-button]').isEnabled();
 
-      await click('[data-test-deposit-cancel-button]');
+      assert.dom('[data-test-deposit-retry-button]').isVisible();
+      assert
+        .dom('[data-test-deposit-retry-button]')
+        .containsText('Resend Signing Request');
+      assert.dom('[data-test-deposit-retry-button]').isEnabled();
+      assert
+        .dom('[data-test-deposit-error-message]')
+        .containsText(
+          `If you haven't received a signing request, make sure to check your connected L1 test chain wallet. You can also try to click on "Resend Signing Request" below, or contact Cardstack Support.`
+        );
 
-      assert.dom('[data-test-deposit-button]').containsText('Deposit');
-      assert.dom('[data-test-deposit-button]').isEnabled();
+      await click('[data-test-deposit-retry-button]');
+
+      assert.ok(relayTokensSpy.calledTwice);
     });
 
-    test('its deposit step can not be canceled if there is a deposit transaction hash', async function (assert) {
+    test('its deposit step can not be retried if there is a deposit transaction hash', async function (assert) {
       session.setValue('unlockTxnHash', 'unlockTokensTxnHash');
       session.setValue('depositSourceToken', 'DAI');
       session.setValue('depositedAmount', new BN(toWei('5')));
@@ -152,8 +168,7 @@ module(
       await click('[data-test-deposit-button]');
 
       await new Promise((resolve) => setTimeout(() => resolve(true), 500));
-      assert.dom('[data-test-deposit-button]').containsText('Depositing');
-      assert.dom('[data-test-deposit-cancel-button]').doesNotExist();
+      assert.dom('[data-test-deposit-retry-button]').doesNotExist();
     });
 
     test('It disables the unlock button when amount entered is more than balance (18-decimal floating point)', async function (assert) {
@@ -282,6 +297,125 @@ module(
       assert
         .dom('[data-test-boxel-input-error-message]')
         .containsText('Amount must be above 0.00 DAI');
+    });
+
+    module('it can resolve retries correctly', function (hooks) {
+      let attempt1: RSVP.Deferred<void>;
+      let attempt2: RSVP.Deferred<void>;
+      let isComplete = false;
+      let onComplete: Function;
+
+      hooks.beforeEach(async function () {
+        session.setValue('unlockTxnHash', 'unlockTokensTxnHash');
+        session.setValue('depositSourceToken', 'DAI');
+        session.setValue('depositedAmount', new BN(toWei('5')));
+
+        isComplete = false;
+        onComplete = () => (isComplete = true);
+        this.set('onComplete', onComplete);
+
+        let relayTokensStub = sinon.stub(layer1Service, 'relayTokens');
+
+        let attempts = 0;
+        // resolving attempt1 will cause the transaction hash to be resolved with
+        // "attempt 1 hash", and attempt2 with "attempt 2 hash"
+        attempt1 = defer();
+        attempt2 = defer();
+
+        relayTokensStub.callsFake(async function (
+          _token: string,
+          _destinationAddress: string,
+          _amountInWei: BN,
+          options: RelayTokensOptions
+        ) {
+          attempts += 1;
+          let currentAttempt = attempts;
+          let hash: string;
+          if (currentAttempt === 1) {
+            hash = 'attempt 1 hash';
+            await attempt1.promise;
+          } else if (currentAttempt === 2) {
+            hash = 'attempt 2 hash';
+            await attempt2.promise;
+          }
+
+          options.onTxnHash?.(hash!);
+
+          return {
+            blockNumber: 0,
+          } as TransactionReceipt;
+        });
+
+        await render(hbs`
+          <CardPay::DepositWorkflow::TransactionAmount
+            @workflowSession={{this.session}}
+            @onComplete={{this.onComplete}}
+            @onIncomplete={{noop}}
+          />
+        `);
+
+        await layer1Service.test__simulateUnlock();
+
+        await click('[data-test-deposit-button]');
+        await waitFor('[data-test-deposit-retry-button]');
+        await click('[data-test-deposit-retry-button]');
+      });
+
+      test('it resets if we reject both attempts', async function (assert) {
+        attempt1.reject();
+        attempt2.reject();
+
+        await settled();
+
+        assert.notOk(isComplete, 'onComplete was not called');
+        assert
+          .dom('[data-test-deposit-error-message]')
+          .containsText(
+            'There was a problem initiating the bridging of your tokens to the L2 test chain. This may be due to a network issue, or perhaps you canceled the request in your wallet. Please try again if you want to continue with this workflow, or contact Cardstack support.'
+          );
+      });
+
+      test('it completes successfully if we reject the first attempt and resolve the second', async function (assert) {
+        attempt1.reject();
+        attempt2.resolve();
+
+        await settled();
+
+        assert.ok(isComplete, 'onComplete was called');
+        assert.ok(
+          find('[data-test-deposit-etherscan-button]')
+            ?.getAttribute('href')
+            ?.includes('attempt 2 hash')
+        );
+      });
+
+      test('it completes successfully if we resolve the first attempt and reject the second', async function (assert) {
+        attempt1.resolve();
+        attempt2.reject();
+
+        await settled();
+
+        assert.ok(isComplete, 'onComplete was called');
+        assert.ok(
+          find('[data-test-deposit-etherscan-button]')
+            ?.getAttribute('href')
+            ?.includes('attempt 1 hash')
+        );
+      });
+
+      test('it completes successfully if we resolve both attempts', async function (assert) {
+        attempt1.resolve();
+        attempt2.resolve();
+
+        await settled();
+
+        assert.ok(isComplete, 'onComplete was called');
+        assert.ok(
+          find('[data-test-deposit-etherscan-button]')
+            ?.getAttribute('href')
+            ?.includes('attempt 1 hash')
+        );
+      });
     });
   }
 );
