ed38f2c0dd473ed97e4c4e83e7362ef9a3d61578
Matt McManus <matt@mcmanus.io>
2021-11-22 12:04:14 -0500

    Remove beta, canary try configs, hardcode 3.24 scenario

--- packages/boxel/config/ember-try.js
+++ packages/boxel/config/ember-try.js
@@ -2,12 +2,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const getChannelURL = require('ember-source-channel-url');
-const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
     useYarn: true,
     scenarios: [
+      {
+        name: 'ember-lts-3.24',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.24.3',
+          },
+        },
+      },
       {
         name: 'ember-release',
         allowedToFail: true, // Allowed to fail until we handle ember-4 related deprecations
@@ -26,40 +33,6 @@ module.exports = async function () {
           },
         },
       },
-      {
-        name: 'ember-canary',
-        allowedToFail: true,
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary'),
-          },
-        },
-      },
-      // The default `.travis.yml` runs this scenario via `npm test`,
-      // not via `ember try`. It's still included here so that running
-      // `ember try:each` manually or from a customized CI config will run it
-      // along with all the other scenarios.
-      {
-        name: 'ember-default',
-        npm: {
-          devDependencies: {},
-        },
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-          },
-        },
-      },
-      embroiderSafe(),
-      embroiderOptimized(),
     ],
   };
 };
