2184e3646495df098c9f952eb51ab12f5c039834
Aierie <michael.khor@cardstack.com>
2021-11-23 21:36:16 +0800

    improve image editor cropping - requires width and height arguments rather than aspect ratio - change api to return a file directly, along with the preview

--- packages/web-client/app/components/image-editor/index.ts
+++ packages/web-client/app/components/image-editor/index.ts
@@ -5,50 +5,54 @@ import Cropper from 'cropperjs';
 import 'cropperjs/dist/cropper.css';
 
 interface ImageEditorComponentArguments {
-  aspectRatio: number; // in px
-  image: string;
+  width: number;
+  height: number;
+  image: string; // data url or url
+  fileType: string;
   rounded: false;
-  saveImageEditData(data: { image: string }): void;
+  saveImageEditData(data: { preview: string; file: Blob }): void;
   onClose(): void;
 }
 
 export default class ImageEditorComponent extends Component<ImageEditorComponentArguments> {
   cropper: Cropper | undefined;
+  canvas: HTMLCanvasElement | undefined;
   @tracked loading = true;
-  @tracked output: string | undefined;
+  @tracked preview: string | undefined;
 
   @action initializeCropper(image: HTMLImageElement) {
     let component = this;
     this.cropper = new Cropper(image, {
       autoCrop: true,
-      aspectRatio: this.args.aspectRatio,
+      aspectRatio: this.args.width / this.args.height,
       autoCropArea: 0.9,
       viewMode: 1,
       cropBoxMovable: false,
       cropBoxResizable: false,
       dragMode: 'move',
       toggleDragModeOnDblclick: false,
-      ready: function (this: HTMLImageElement & { cropper: Cropper }) {
-        const dataURL = this.cropper.getCroppedCanvas().toDataURL();
-        component.output = dataURL;
+      ready: async function (this: HTMLImageElement & { cropper: Cropper }) {
+        component.updatePreview();
         component.loading = false;
       },
-      cropend(this: HTMLImageElement & { cropper: Cropper }) {
-        const dataURL = this.cropper.getCroppedCanvas().toDataURL();
-        component.output = dataURL;
+      cropend: async function (this: HTMLImageElement & { cropper: Cropper }) {
+        component.updatePreview();
       },
-      zoom(this: HTMLImageElement & { cropper: Cropper }) {
-        const dataURL = this.cropper.getCroppedCanvas().toDataURL();
-        component.output = dataURL;
+      zoom: async function (this: HTMLImageElement & { cropper: Cropper }) {
+        component.updatePreview();
       },
     });
   }
 
-  @action rotate(amountInDegrees: number) {
+  @action async rotate(amountInDegrees: number) {
     if (this.loading) return;
-    this.cropper?.rotate(amountInDegrees);
-    const dataURL = this.cropper?.getCroppedCanvas().toDataURL();
-    this.output = dataURL;
+    this.cropper!.rotate(amountInDegrees);
+    this.updatePreview();
+  }
+
+  updatePreview() {
+    this.canvas = this.getCroppedCanvas();
+    this.preview = this.getCroppedPreviewImage();
   }
 
   @action close() {
@@ -56,14 +60,57 @@ export default class ImageEditorComponent extends Component<ImageEditorComponent
     this.loading = true;
   }
 
-  @action save() {
-    if (this.loading || !this.output) return;
+  @action async save() {
+    if (this.loading || !this.preview || !this.canvas) return;
 
     this.args.saveImageEditData({
-      image: this.output!,
+      file: await this.getCroppedImageFile(),
+      preview: this.preview,
     });
 
     this.args.onClose();
     this.loading = true;
+  }
+
+  async getCroppedImageFile(): Promise<Blob> {
+    return await new Promise((resolve, reject) => {
+      try {
+        this.canvas?.toBlob(
+          (blob) =>
+            blob
+              ? resolve(blob)
+              : reject(
+                  new Error('Failed to construct blob from cropped image')
+                ),
+          this.args.fileType ?? 'png',
+          1
+        );
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+
+  getCroppedPreviewImage() {
+    return this.canvas!.toDataURL();
+  }
+
+  getCroppedCanvas() {
+    const canvas = this.cropper!.getCroppedCanvas({
+      width: this.args.width,
+      height: this.args.height,
+      minWidth: this.args.width,
+      minHeight: this.args.height,
+      // be forgiving with this so we can still get high res images
+      // but cap it because there are limits to canvas size
+      // this is used to render the image onto a canvas (before we get the cropped version)
+      maxWidth: Math.min(this.args.width * 5, 20000),
+      maxHeight: Math.min(this.args.height * 5, 20000),
+      fillColor: this.args.fileType === 'image/png' ? undefined : '#fff',
+      imageSmoothingEnabled: false,
+      imageSmoothingQuality: 'high',
+    });
+
+    return canvas;
   }
 }
