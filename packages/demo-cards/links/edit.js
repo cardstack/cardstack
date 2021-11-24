d837bee1447444810c819ad79a1d75786386726c
Matt McManus <matt@mcmanus.io>
2021-11-24 12:11:19 -0500

    WIP ContainsMany Idea

--- packages/demo-cards/links/edit.js
+++ packages/demo-cards/links/edit.js
@@ -3,20 +3,29 @@ import { precompileTemplate } from '@ember/template-compilation';
 import templateOnlyComponent from '@ember/component/template-only';
 import BoxelField from '@cardstack/boxel/components/boxel/field';
 import './edit.css';
+import HttpsDemoComLinkField from '@cardstack/compiled/https-demo.com-link/edit-35fd91449f6aa8f0194a81ef8eea5c34.js';
+
+import { helper } from '@ember/component/helper';
+
+const log = helper(function (params) {
+  console.log(...params);
+});
 
 export default setComponentTemplate(
   precompileTemplate(
     `<section class="links-edit">
       <header class="links-edit__header">Links</header>
       <ul class="links-edit__list">
-        {{#each @fields.links as |Field|}}
-          <li><Field /></li>
-        {{/each}}
+        <ContainsManyManager
+          @component={{HttpsDemoComLinkField}}
+          @model={{@model.links}}
+          @set={{@set.setters.links}}
+        />
       </ul>
     </section>`,
     {
       strictMode: true,
-      scope: () => ({ BoxelField }),
+      scope: () => ({ BoxelField, HttpsDemoComLinkField, log }),
     }
   ),
   templateOnlyComponent()
