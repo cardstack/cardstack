6493bff28711b68e08143766e7d1df03be3413c6
JT <justinthong93@gmail.com>
2021-11-29 15:22:07 +0800

    Update abi v0.8.6 (#2422)
    * add v0.8.6 abis
    
    * fix symlink
    
    * update imports
    
    * add exports for diff
    
    * fix typo

--- packages/cardpay-sdk/sdk/layer-two-oracle/index.ts
+++ packages/cardpay-sdk/sdk/layer-two-oracle/index.ts
@@ -1,11 +1,12 @@
 import { ContractMeta } from '../version-resolver';
 import v0_8_5 from './v0.8.5';
+import v0_8_6 from './v0.8.6';
 
 // add more versions as we go, but also please do drop version that we don't
 // want to maintain simultaneously
-export type LayerTwoOracle = v0_8_5;
+export type LayerTwoOracle = v0_8_6;
 
 export const layerTwoOracleMeta = {
-  apiVersions: { v0_8_5 },
+  apiVersions: { v0_8_5, v0_8_6 },
   contractName: 'revenuePool',
 } as ContractMeta;
--- packages/cardpay-sdk/sdk/prepaid-card-market/index.ts
+++ packages/cardpay-sdk/sdk/prepaid-card-market/index.ts
@@ -1,12 +1,13 @@
 import { ContractMeta } from '../version-resolver';
 
 import v0_8_5 from './v0.8.5';
+import v0_8_6 from './v0.8.6';
 
 // add more versions as we go, but also please do drop version that we don't
 // want to maintain simultaneously
-export type PrepaidCardMarket = v0_8_5;
+export type PrepaidCardMarket = v0_8_6;
 
 export const prepaidCardMarketMeta = {
-  apiVersions: { v0_8_5 },
+  apiVersions: { v0_8_5, v0_8_6 },
   contractName: 'prepaidCardMarket',
 } as ContractMeta;
