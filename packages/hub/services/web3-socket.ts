ab8f31af1f5856e25529d15e98d525454c4e47ec
Aierie <michael.khor@cardstack.com>
2021-12-01 00:10:37 +0800

    simplify web3 initialization

--- packages/hub/services/web3-socket.ts
+++ packages/hub/services/web3-socket.ts
@@ -8,37 +8,38 @@ interface Web3Config {
 }
 
 const { network } = config.get('web3') as Web3Config;
+let rpcURL = getConstantByNetwork('rpcWssNode', network);
+let log = Logger('service:web3-socket');
 
 export default class Web3SocketService {
   private web3: Web3 | undefined;
 
   getInstance() {
     if (!this.web3) {
-      let rpcURL = getConstantByNetwork('rpcWssNode', network);
-      this.web3 = new Web3(rpcURL);
-      let provider = this.createWebsocketProvider(rpcURL, {
-        timeout: 30000,
-        reconnect: {
-          auto: true,
-          delay: 1000,
-          onTimeout: true,
-          maxAttempts: 10,
-        },
-        clientConfig: {
-          keepalive: true,
-          keepaliveInterval: 60000,
-          maxReceivedFrameSize: 100000000,
-          maxReceivedMessageSize: 100000000,
-        },
-      });
-      this.web3.setProvider(provider);
+      try {
+        this.web3 = new Web3();
+        let provider = new Web3.providers.WebsocketProvider(rpcURL, {
+          timeout: 30000,
+          reconnect: {
+            auto: true,
+            delay: 1000,
+            onTimeout: true,
+            maxAttempts: 10,
+          },
+          clientConfig: {
+            keepalive: true,
+            keepaliveInterval: 60000,
+            maxReceivedFrameSize: 100000000,
+            maxReceivedMessageSize: 100000000,
+          },
+        });
+        this.web3.setProvider(provider);
+      } catch (e) {
+        log.error(`Error encountered while trying to connect to rpc node with url ${rpcURL}`, e);
+        throw e;
+      }
     }
     return this.web3;
-  }
-
-  createWebsocketProvider(...args: ConstructorParameters<typeof Web3['providers']['WebsocketProvider']>) {
-    let [host, options] = args;
-    return new Web3.providers.WebsocketProvider(host, options);
   }
 }
 
