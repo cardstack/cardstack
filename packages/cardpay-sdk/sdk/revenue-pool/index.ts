064107de29ff633d86a6cba3d06ddd42394ce338
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-12-01 14:39:34 -0500

    updated SDK for 0.8.7 of protocol

--- packages/cardpay-sdk/sdk/revenue-pool/index.ts
+++ packages/cardpay-sdk/sdk/revenue-pool/index.ts
@@ -2,12 +2,13 @@ import { ContractMeta } from '../version-resolver';
 
 import v0_8_5 from './v0.8.5';
 import v0_8_6 from './v0.8.6';
+import v0_8_7 from './v0.8.7';
 
 // add more versions as we go, but also please do drop version that we don't
 // want to maintain simultaneously
-export type RevenuePool = v0_8_6;
+export type RevenuePool = v0_8_7;
 
 export const revenuePoolMeta = {
-  apiVersions: { v0_8_5, v0_8_6 },
+  apiVersions: { v0_8_5, v0_8_6, v0_8_7 },
   contractName: 'revenuePool',
 } as ContractMeta;
--- packages/cardpay-sdk/sdk/reward-pool/index.ts
+++ packages/cardpay-sdk/sdk/reward-pool/index.ts
@@ -2,14 +2,15 @@ import { ContractMeta } from '../version-resolver';
 
 import v0_8_5 from './v0.8.5';
 import v0_8_6 from './v0.8.6';
+import v0_8_7 from './v0.8.7';
 
 export { Proof, RewardTokenBalance } from './base';
 
 // add more versions as we go, but also please do drop version that we don't
 // want to maintain simultaneously
-export type RewardPool = v0_8_6;
+export type RewardPool = v0_8_7;
 
 export const rewardPoolMeta = {
-  apiVersions: { v0_8_5, v0_8_6 },
+  apiVersions: { v0_8_5, v0_8_6, v0_8_7 },
   contractName: 'rewardPool',
 } as ContractMeta;
