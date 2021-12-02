d72948b4afde2e4729f6895f8714f5bd3347cbab
Matt McManus <matt@mcmanus.io>
2021-12-02 14:52:20 -0500

    Fix missed script name

--- packages/hub/cli/db/index.ts
+++ packages/hub/cli/db/index.ts
@@ -1,4 +1,4 @@
 import * as dump from './dump';
-import * as initTest from './init-test';
+import * as initTest from './init';
 import * as seed from './seed';
 export const commands: any = [dump, initTest, seed];
