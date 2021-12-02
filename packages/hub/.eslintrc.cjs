2cfddf1218dae521a6958f7ade7eae5804f8b447
Edward Faulkner <edward@eaf4.com>
2021-12-02 12:10:59 -0500

    tell eslint about hub's separate tsconfig

--- packages/hub/.eslintrc.cjs
+++ packages/hub/.eslintrc.cjs
@@ -1,12 +1,7 @@
 module.exports = {
   root: true,
   extends: '@cardstack/eslint-config',
-  rules: {
-    'node/no-unpublished-require': [
-      'error',
-      {
-        allowModules: ['dotenv'],
-      },
-    ],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
   },
 };
