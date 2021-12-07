2293d63f89aa5219ab7b2e724dca3f351e898cf8
Aierie <michael.khor@cardstack.com>
2021-12-08 01:32:13 +0800

    send-notifications task checks if notification has been sent, and records successful sending

--- packages/hub/tasks/send-notifications.ts
+++ packages/hub/tasks/send-notifications.ts
@@ -1,7 +1,23 @@
+import { inject } from '@cardstack/di';
 import { Helpers } from 'graphile-worker';
 
 export default class SendNotificationsTask {
+  sentPushNotificationsQueries = inject('sent-push-notifications-queries', { as: 'sentPushNotificationsQueries' });
+
   async perform(payload: any, helpers: Helpers) {
+    let index = {
+      transactionHash: payload.transactionHash,
+      pushClientId: payload.pushClientId,
+      ownerAddress: payload.ownerAddress,
+    };
+
+    let notificationHasBeenSent = await this.sentPushNotificationsQueries.exists(index);
+    if (notificationHasBeenSent) {
+      helpers.logger.info('Not sending notification because it has already been sent');
+      return;
+    }
+
     helpers.logger.info('Notification to send:', payload);
+    this.sentPushNotificationsQueries.insert(payload);
   }
 }
