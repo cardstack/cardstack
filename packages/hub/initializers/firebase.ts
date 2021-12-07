cf4b07b82ce73deb2fe28acbde3ea32c7d15c6f7
Matic Jurglič <matic@jurglic.si>
2021-12-07 14:44:33 +0100

    Don't try to initialize Firebase when ENV variables aren't set

--- packages/hub/initializers/firebase.ts
+++ packages/hub/initializers/firebase.ts
@@ -3,12 +3,14 @@ import admin from 'firebase-admin';
 import config from 'config';
 
 export default function initFirebase() {
-  initializeApp({
-    credential: admin.credential.cert({
-      projectId: config.get('firebase.projectId'),
-      clientEmail: config.get('firebase.clientEmail'),
-      privateKey: config.get('firebase.privateKey'),
-    }),
-    databaseURL: config.get('firebase.databaseURL'),
-  });
+  if (config.get('firebase.projectId')) {
+    initializeApp({
+      credential: admin.credential.cert({
+        projectId: config.get('firebase.projectId'),
+        clientEmail: config.get('firebase.clientEmail'),
+        privateKey: config.get('firebase.privateKey'),
+      }),
+      databaseURL: config.get('firebase.databaseURL'),
+    });
+  }
 }
