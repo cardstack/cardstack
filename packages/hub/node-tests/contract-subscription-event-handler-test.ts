aa7299e3aaa50ccec3b8785c557e71bdc779bd7b
Buck Doyle <buck.doyle@cardstack.com>
2021-12-07 09:41:14 -0600

    Add error-handling assertion

--- packages/hub/node-tests/contract-subscription-event-handler-test.ts
+++ packages/hub/node-tests/contract-subscription-event-handler-test.ts
@@ -1,15 +1,15 @@
 import { ContractSubscriptionEventHandler } from '../contract-subscription-event-handler';
 import { Job, TaskSpec } from 'graphile-worker';
 import { expect } from 'chai';
-// import sentryTestkit from 'sentry-testkit';
-// import * as Sentry from '@sentry/node';
-// import waitFor from './utils/wait-for';
+import sentryTestkit from 'sentry-testkit';
+import * as Sentry from '@sentry/node';
+import waitFor from './utils/wait-for';
 import Web3SocketService from '../services/web3-socket';
 import WorkerClient from '../services/worker-client';
 import { Contracts } from '../services/contracts';
 import Web3 from 'web3';
 
-// const { testkit, sentryTransport } = sentryTestkit();
+const { testkit, sentryTransport } = sentryTestkit();
 
 class StubContracts {
   handlers: Record<any, any> = {};
@@ -76,5 +76,22 @@ describe('ContractSubscriptionEventHandler', function () {
     contracts.handlers.CustomerPayment(null, { transactionHash: '0x123' });
 
     expect(workerClient.jobs).to.deep.equal([['notify-merchant-claim', '0x123']]);
+  });
+
+  it('logs an error when receiving a CustomerPayment error', async function () {
+    Sentry.init({
+      dsn: 'https://acacaeaccacacacabcaacdacdacadaca@sentry.io/000001',
+      release: 'test',
+      tracesSampleRate: 1,
+      transport: sentryTransport,
+    });
+
+    contracts.handlers.CustomerPayment(new Error('Mock CustomerPayment error'));
+
+    await waitFor(() => testkit.reports().length > 0);
+
+    expect(testkit.reports()[0].tags).to.deep.equal({
+      action: 'contract-subscription-event-handler',
+    });
   });
 });
