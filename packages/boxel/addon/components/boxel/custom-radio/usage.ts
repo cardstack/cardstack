cfdc25df54658190dfddd57b18cb9a684a665359
Burcu Noyan <burcunoyan@gmail.com>
2021-12-13 17:20:24 -0500

    Boxel radio input upstream styling

--- packages/boxel/addon/components/boxel/custom-radio/usage.ts
+++ packages/boxel/addon/components/boxel/custom-radio/usage.ts
@@ -22,9 +22,15 @@ export default class CustomRadioUsage extends Component {
   @tracked groupDescription =
     'Select one of these options for breakfast sandwiches';
   @tracked checkedId = 'strawberry';
+  @tracked checkedId2 = '';
   @tracked disabled = false;
+  @tracked hiddenInput = false;
 
   @action onChange(id: string): void {
     this.checkedId = id;
+  }
+
+  @action onChange2(id: string): void {
+    this.checkedId2 = id;
   }
 }
