3fd8c3806cb15667c8f506da605b2d7227237567
Justin <justinthong93@gmail.com>
2021-12-16 18:55:55 +0800

    add reward progrma info export to sdk

--- packages/cardpay-sdk/index.ts
+++ packages/cardpay-sdk/index.ts
@@ -3,7 +3,7 @@ export { ITokenBridgeHomeSide, BridgeValidationResult } from './sdk/token-bridge
 export { PrepaidCard } from './sdk/prepaid-card';
 export { PrepaidCardMarket } from './sdk/prepaid-card-market';
 export { RevenuePool } from './sdk/revenue-pool';
-export { RewardManager } from './sdk/reward-manager';
+export { RewardManager, RewardProgramInfo } from './sdk/reward-manager';
 export { RewardPool, Proof, RewardTokenBalance } from './sdk/reward-pool';
 export {
   ISafes,
