9aa7b2b5f4f1c60f5dcf48d272d777082902472f
Matic Jurglič <matic@jurglic.si>
2021-12-20 10:50:09 +0100

    Look up ContractSubscriptionEventHandler from the container instead of creating a new container at this level

--- packages/hub/cli/event-listener.ts
+++ packages/hub/cli/event-listener.ts
@@ -1,8 +1,25 @@
-import { HubEventListenerController } from '../process-controllers/hub-event-listener-controller';
+/* eslint-disable no-process-exit */
+
+import { createContainer, runInitializers } from '../main';
+import { eventListenerLog } from '../utils/logger';
+import * as Sentry from '@sentry/node';
 
 export const command = 'event-listener';
 export const describe = 'Boot the contract events listener (used for push notifications)';
 export const builder = {};
+
 export async function handler() {
-  await HubEventListenerController.create();
+  let container = createContainer();
+  runInitializers();
+
+  try {
+    let eventListener = await container.lookup('contract-subscription-event-handler');
+    eventListener.setupContractEventSubscriptions();
+  } catch (e: any) {
+    eventListenerLog.error(`Unexpected error while running contracts event listener: ${e.message}`, e);
+    Sentry.withScope(function () {
+      Sentry.captureException(e);
+    });
+    process.exit(-1);
+  }
 }
