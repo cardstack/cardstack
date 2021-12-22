4e5f51a3da1298004124c2d9baf655e602e8ccc0
Burcu Noyan <burcunoyan@gmail.com>
2021-12-22 10:53:33 -0500

    Remove artifacts from usage file

--- packages/boxel/addon/components/boxel/radio-input/usage.ts
+++ packages/boxel/addon/components/boxel/radio-input/usage.ts
@@ -21,7 +21,6 @@ export default class RadioInputUsage extends Component {
   @tracked groupDescription =
     'Select one of these options for breakfast sandwiches';
   @tracked checkedId = 'strawberry';
-  @tracked checkedId2 = '';
   @tracked disabled = false;
   @tracked hiddenInput = false;
   @tracked hiddenBorder = false;
@@ -32,9 +31,5 @@ export default class RadioInputUsage extends Component {
 
   @action onChange(id: string): void {
     this.checkedId = id;
-  }
-
-  @action onChange2(id: string): void {
-    this.checkedId2 = id;
   }
 }
