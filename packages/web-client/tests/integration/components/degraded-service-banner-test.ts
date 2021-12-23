b46a190f56f4512899f762d25efcbf1911db2f06
Matic Jurglič <matic@jurglic.si>
2021-12-23 09:35:17 +0100

    Put status page url in a variable

--- packages/web-client/tests/integration/components/degraded-service-banner-test.ts
+++ packages/web-client/tests/integration/components/degraded-service-banner-test.ts
@@ -5,26 +5,26 @@ import hbs from 'htmlbars-inline-precompile';
 import { Response as MirageResponse } from 'ember-cli-mirage';
 import { MirageTestContext } from 'ember-cli-mirage/test-support';
 import { setupMirage } from 'ember-cli-mirage/test-support';
+import config from '@cardstack/web-client/config/environment';
 
 interface Context extends MirageTestContext {}
+
+let statusPageUrl = `${config.urls.statusPageUrl}/api/v2/incidents/unresolved.json`;
 
 module('Integration | Component | degraded-service-banner', function (hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
 
   test('It doesn not display a banner when there are no incidents', async function (this: Context, assert) {
-    this.server.get(
-      'https://status.cardstack.com/api/v2/incidents/unresolved.json',
-      function () {
-        return new MirageResponse(
-          200,
-          {},
-          {
-            incidents: [],
-          }
-        );
-      }
-    );
+    this.server.get(statusPageUrl, function () {
+      return new MirageResponse(
+        200,
+        {},
+        {
+          incidents: [],
+        }
+      );
+    });
 
     await render(hbs`<Common::DegradedServiceBanner />`);
 
@@ -32,23 +32,20 @@ module('Integration | Component | degraded-service-banner', function (hooks) {
   });
 
   test('It shows a critical impact incident as severe', async function (this: Context, assert) {
-    this.server.get(
-      'https://status.cardstack.com/api/v2/incidents/unresolved.json',
-      function () {
-        return new MirageResponse(
-          200,
-          {},
-          {
-            incidents: [
-              {
-                name: 'All systems down.',
-                impact: 'critical',
-              },
-            ],
-          }
-        );
-      }
-    );
+    this.server.get(statusPageUrl, function () {
+      return new MirageResponse(
+        200,
+        {},
+        {
+          incidents: [
+            {
+              name: 'All systems down.',
+              impact: 'critical',
+            },
+          ],
+        }
+      );
+    });
 
     await render(hbs`<Common::DegradedServiceBanner />`);
     await waitFor('[data-test-degraded-service-banner]');
@@ -63,23 +60,20 @@ module('Integration | Component | degraded-service-banner', function (hooks) {
   });
 
   test('It shows a major impact incident as severe', async function (this: Context, assert) {
-    this.server.get(
-      'https://status.cardstack.com/api/v2/incidents/unresolved.json',
-      function () {
-        return new MirageResponse(
-          200,
-          {},
-          {
-            incidents: [
-              {
-                name: 'All systems down.',
-                impact: 'major',
-              },
-            ],
-          }
-        );
-      }
-    );
+    this.server.get(statusPageUrl, function () {
+      return new MirageResponse(
+        200,
+        {},
+        {
+          incidents: [
+            {
+              name: 'All systems down.',
+              impact: 'major',
+            },
+          ],
+        }
+      );
+    });
 
     await render(hbs`<Common::DegradedServiceBanner />`);
     await waitFor('[data-test-degraded-service-banner]');
@@ -89,23 +83,20 @@ module('Integration | Component | degraded-service-banner', function (hooks) {
   });
 
   test('It shows a minor impact incident as default', async function (this: Context, assert) {
-    this.server.get(
-      'https://status.cardstack.com/api/v2/incidents/unresolved.json',
-      function () {
-        return new MirageResponse(
-          200,
-          {},
-          {
-            incidents: [
-              {
-                name: 'One small system down.',
-                impact: 'minor',
-              },
-            ],
-          }
-        );
-      }
-    );
+    this.server.get(statusPageUrl, function () {
+      return new MirageResponse(
+        200,
+        {},
+        {
+          incidents: [
+            {
+              name: 'One small system down.',
+              impact: 'minor',
+            },
+          ],
+        }
+      );
+    });
 
     await render(hbs`<Common::DegradedServiceBanner />`);
     await waitFor('[data-test-degraded-service-banner]');
@@ -120,39 +111,36 @@ module('Integration | Component | degraded-service-banner', function (hooks) {
   });
 
   test('It shows only the most recent and most impactful incident', async function (this: Context, assert) {
-    this.server.get(
-      'https://status.cardstack.com/api/v2/incidents/unresolved.json',
-      function () {
-        return new MirageResponse(
-          200,
-          {},
-          {
-            incidents: [
-              {
-                name: 'All systems down.',
-                impact: 'major',
-                started_at: '2021-10-10T10:10:00.000Z',
-              },
-              {
-                name: 'One small system down.',
-                impact: 'minor',
-                started_at: '2021-10-10T10:10:00.003Z',
-              },
-              {
-                name: 'World down.',
-                impact: 'critical',
-                started_at: '2021-10-10T10:10:00.002Z',
-              },
-              {
-                name: 'Internet down.',
-                impact: 'critical',
-                started_at: '2021-10-10T10:10:00.001Z',
-              },
-            ],
-          }
-        );
-      }
-    );
+    this.server.get(statusPageUrl, function () {
+      return new MirageResponse(
+        200,
+        {},
+        {
+          incidents: [
+            {
+              name: 'All systems down.',
+              impact: 'major',
+              started_at: '2021-10-10T10:10:00.000Z',
+            },
+            {
+              name: 'One small system down.',
+              impact: 'minor',
+              started_at: '2021-10-10T10:10:00.003Z',
+            },
+            {
+              name: 'World down.',
+              impact: 'critical',
+              started_at: '2021-10-10T10:10:00.002Z',
+            },
+            {
+              name: 'Internet down.',
+              impact: 'critical',
+              started_at: '2021-10-10T10:10:00.001Z',
+            },
+          ],
+        }
+      );
+    });
 
     await render(hbs`<Common::DegradedServiceBanner />`);
     await waitFor('[data-test-degraded-service-banner]');
@@ -165,23 +153,20 @@ module('Integration | Component | degraded-service-banner', function (hooks) {
   });
 
   test('It adds punctuation if missing', async function (this: Context, assert) {
-    this.server.get(
-      'https://status.cardstack.com/api/v2/incidents/unresolved.json',
-      function () {
-        return new MirageResponse(
-          200,
-          {},
-          {
-            incidents: [
-              {
-                name: 'We are experiencing issues',
-                impact: 'major',
-              },
-            ],
-          }
-        );
-      }
-    );
+    this.server.get(statusPageUrl, function () {
+      return new MirageResponse(
+        200,
+        {},
+        {
+          incidents: [
+            {
+              name: 'We are experiencing issues',
+              impact: 'major',
+            },
+          ],
+        }
+      );
+    });
 
     await render(hbs`<Common::DegradedServiceBanner />`);
     await waitFor('[data-test-degraded-service-banner]');
