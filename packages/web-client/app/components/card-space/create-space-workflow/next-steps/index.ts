7f69ab611b9bdd1954e337d238c70ed4ec97a5f5
Buck Doyle <buck.doyle@cardstack.com>
2022-01-10 09:32:21 -0600

    web: Update Card Space next step link to userspace (#2559)
    This is a narrowly-focused PR to wire up the Next Steps button
    after the Create Card Space workflow to forward to the newly-
    created space.
    
    Some arbitrary decisions that are lightweight enough to change
    easily if/when needed:
    * the workflow property name is username
    * the route is /card-space/username
    * the template is a placeholder that shows the username

--- packages/web-client/app/components/card-space/create-space-workflow/next-steps/index.ts
+++ packages/web-client/app/components/card-space/create-space-workflow/next-steps/index.ts
@@ -2,13 +2,23 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import RouterService from '@ember/routing/router-service';
+import { next } from '@ember/runloop';
 import { WorkflowCardComponentArgs } from '@cardstack/web-client/models/workflow';
 
 class CreateSpaceWorkflowNextStepsComponent extends Component<WorkflowCardComponentArgs> {
   @service declare router: RouterService;
 
   @action async visitSpace() {
-    // TODO
+    let username = this.args.workflowSession.getValue('username');
+
+    await this.router.transitionTo({
+      queryParams: { flow: null, 'flow-id': null },
+    });
+
+    next(this, () => {
+      this.router.transitionTo('card-space.userspace', { username });
+    });
+
     return;
   }
 }
