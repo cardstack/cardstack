7762c14446f4d8eddd1e542bb09858fa780915d6
Burcu Noyan <burcunoyan@gmail.com>
2022-01-11 17:38:21 -0500

    Move ValidationState input component under input

--- packages/boxel/addon/components/boxel/input/index.ts
+++ packages/boxel/addon/components/boxel/input/index.ts
@@ -2,5 +2,6 @@ import templateOnlyComponent from '@ember/component/template-only';
 import '@cardstack/boxel/styles/global.css';
 import './index.css';
 import './token-amount/index.css';
+import './validation-state/index.css';
 
 export default templateOnlyComponent();
