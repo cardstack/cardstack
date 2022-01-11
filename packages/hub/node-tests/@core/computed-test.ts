1405d697126c742cd802c31429d934f38ffd2f89
Edward Faulkner <edward@eaf4.com>
2022-01-11 11:46:09 -0500

    first test for computeds

---
+++ packages/hub/node-tests/@core/computed-test.ts
@@ -0,0 +1,51 @@
+import { expect } from 'chai';
+import { configureHubWithCompiler } from '../helpers/cards';
+
+if (process.env.COMPILER) {
+  describe.skip('computed', function () {
+    let { realmURL: realm, cards } = configureHubWithCompiler(this);
+
+    this.beforeEach(async function () {
+      await cards.create({
+        realm,
+        id: 'person',
+        schema: 'schema.js',
+        files: {
+          'schema.js': `
+            import { contains } from "@cardstack/types";
+            import string from "https://cardstack.com/base/string";
+            export default class Person {
+              @contains(string) firstName;
+              @contains(string) lastName;
+
+              @contains(string)
+              async fullName() {
+                return (await this.firstName()) + " " + (await this.lastName());
+              }
+
+              @contains(string)
+              async summary() {
+                  return (await this.fullName()) + " is a person";
+              }
+            }
+          `,
+        },
+      });
+
+      await cards.create({
+        realm,
+        id: 'arthur',
+        adoptsFrom: '../person',
+        data: {
+          firstName: 'Arthur',
+          lastName: 'Faulkner',
+        },
+      });
+    });
+
+    it(`can access a one-level-deep computed field`, async function () {
+      let card = await cards.load(`${realm}arthur`);
+      expect(card.content.data.fullName).to.equal('Arthur Faulkner');
+    });
+  });
+}
