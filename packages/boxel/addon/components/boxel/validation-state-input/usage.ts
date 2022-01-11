51c45a5f9b1b4564e9a144ab9ec514e7a5632906
Burcu Noyan <burcunoyan@gmail.com>
2022-01-11 14:27:33 -0500

    Resolve accessibility error

--- packages/boxel/addon/components/boxel/validation-state-input/usage.ts
+++ packages/boxel/addon/components/boxel/validation-state-input/usage.ts
@@ -3,6 +3,7 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ValidationStateInputUsageComponent extends Component {
+  @tracked id = 'validation-input-usage-id';
   @tracked value = '';
   @tracked state = 'default';
   @tracked helperText = 'Please enter a value';
