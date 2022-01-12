553803397782a244d42b4a6aee35bfd54bef93ef
Aierie <michael.khor@cardstack.com>
2022-01-12 20:16:57 +0800

    add cleanup

--- packages/hub/node-tests/services/web3-socket-test.ts
+++ packages/hub/node-tests/services/web3-socket-test.ts
@@ -7,25 +7,40 @@ const { testkit, sentryTransport } = sentryTestkit();
 const DUMMY_DSN = 'https://acacaeaccacacacabcaacdacdacadaca@sentry.io/000001';
 
 describe('Web3Socket', function () {
-  this.beforeEach(function () {
+  let subject: Web3SocketService;
+  this.beforeAll(function () {
     Sentry.init({
       dsn: DUMMY_DSN,
       release: 'test',
       tracesSampleRate: 1,
       transport: sentryTransport,
     });
-
+  });
+  this.beforeEach(function () {
+    subject = new Web3SocketService();
     testkit.reset();
+  });
+  this.afterEach(async function () {
+    let connection = (subject.web3?.currentProvider as unknown as any)?.connection;
+    if (connection) {
+      if (connection.readyState === connection.OPEN) connection.close(1000, 'Closing because test is done');
+      // hack to make sure cleanup is properly done after tests
+      // https://github.com/theturtle32/WebSocket-Node/issues/426
+      connection._client.on('httpResponse', (r: any) => {
+        r.destroy();
+      });
+      connection._client.on('connect', (c: any) => {
+        c.close();
+      });
+    }
   });
 
   it('reports to sentry when web3 initialization fails', async function () {
-    let web3Socket = new Web3SocketService();
-
-    web3Socket.initializeWeb3 = () => {
+    subject.initializeWeb3 = () => {
       throw new Error('mock web3 initialization failure');
     };
 
-    expect(() => web3Socket.getInstance()).to.throw('mock web3 initialization failure');
+    expect(() => subject.getInstance()).to.throw('mock web3 initialization failure');
 
     await waitFor(() => testkit.reports().length > 0);
 
@@ -35,8 +50,7 @@ describe('Web3Socket', function () {
   });
 
   it('reports error events', async function () {
-    let web3Socket = new Web3SocketService();
-    let web3 = web3Socket.getInstance();
+    let web3 = subject.getInstance();
 
     let provider: any = web3.currentProvider;
     provider.emit(provider.ERROR, new Error('A test error'));
@@ -50,8 +64,7 @@ describe('Web3Socket', function () {
   });
 
   it('reports close events', async function () {
-    let web3Socket = new Web3SocketService();
-    let web3 = web3Socket.getInstance();
+    let web3 = subject.getInstance();
 
     let provider: any = web3.currentProvider;
     provider.emit(provider.CLOSE, {
