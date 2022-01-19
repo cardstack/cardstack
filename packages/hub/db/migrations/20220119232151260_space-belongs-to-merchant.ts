ab782391fa06e81094e7a2811586050a63b4a72b
Buck Doyle <buck.doyle@cardstack.com>
2022-01-19 17:57:49 -0600

    Add note about uniqueness for merchant_id

--- packages/hub/db/migrations/20220119232151260_space-belongs-to-merchant.ts
+++ packages/hub/db/migrations/20220119232151260_space-belongs-to-merchant.ts
@@ -7,6 +7,7 @@ export const shorthands: ColumnDefinitions | undefined = undefined;
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.dropColumns(CARD_SPACES, ['owner_address', 'url']);
   pgm.alterColumn(CARD_SPACES, 'merchant_id', { notNull: true });
+  // FIXME this should actually be unique also, to enforce 1:1 vs 1:n… obvs answer is to have it all in the same table but meh
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
