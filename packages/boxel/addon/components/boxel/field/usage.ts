0f952a00cdc03fef4008e62134579dcaf5944ad3
Burcu Noyan <burcunoyan@gmail.com>
2022-01-25 17:30:23 -0500

    Add @icon, @address, and @value params

--- packages/boxel/addon/components/boxel/field/usage.ts
+++ packages/boxel/addon/components/boxel/field/usage.ts
@@ -4,9 +4,7 @@ import { tracked } from '@glimmer/tracking';
 export default class extends Component {
   @tracked label = 'Full Name of the Issuer';
   @tracked value = 'Gary Walker';
-  @tracked mode = 'edit';
   @tracked id = 'sample-field';
-  @tracked labelClass = 'custom-classname';
   @tracked vertical = false;
   @tracked smallLabel = false;
 }
