66c0363d2c30f174e5f75d96f98df1070d4b4a97
Aierie <michael.khor@cardstack.com>
2022-01-25 23:30:33 +0800

    document scale-down-to-width

--- packages/web-client/app/modifiers/scale-down-to-width.ts
+++ packages/web-client/app/modifiers/scale-down-to-width.ts
@@ -1,7 +1,25 @@
 import { modifier } from 'ember-modifier';
 
 /**
- * Scale an element's child down to fit a given width
+ * This modifier is kind of awkward. Layouts behave as if the scaled element
+ * is still the same width as before, so we need to add a negative margin for height.
+ * It allows insertion of arbitrary content for previews - we need this for workflows
+ * to follow the layout size while displaying previews using the same components (except scaled down);
+ * and for the image editor to do the same, if necessary. I'm imagining the image editor to
+ * allow clicking on a scaled-down component and seeing a blown up version; though that is more
+ * of a possible future improvement.
+ *
+ * Possible improvements to this modifier:
+ * - make a component that uses scale-down-to-width internally/ applies similar logic and wraps a containing div around.
+ *   - to avoid the margin bottom from not being overwritable.
+ *   - to have the scale in state, as a number that can be displayed optionally / provided
+ * - window resize handling
+ */
+
+/**
+ * Sets an element's width to a given width,
+ * and scale its child down to fit that width.
+ * Only supports one HTML Element child at the moment.
  *
  * @param element The container element
  * @param params Array of length 1 that contains a single string for the width of the element
@@ -16,7 +34,9 @@ function scaleDownToWidth(element: HTMLElement, params: string[]) {
   }
 
   if (children.length > 1) {
-    throw new Error('scale-to-fit cannot support multiple children');
+    throw new Error(
+      'scale-down-to-width cannot support more than one child node'
+    );
   }
 
   let child = children[0] as HTMLElement;
