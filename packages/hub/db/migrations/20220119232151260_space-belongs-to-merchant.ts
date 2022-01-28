804ca737a612949ab15f98427676e0d4dbb0fded
Buck Doyle <buck.doyle@cardstack.com>
2022-01-28 10:18:56 -0600

    Add index for foreign key uniqueness

--- packages/hub/db/migrations/20220119232151260_space-belongs-to-merchant.ts
+++ packages/hub/db/migrations/20220119232151260_space-belongs-to-merchant.ts
@@ -7,10 +7,11 @@ export const shorthands: ColumnDefinitions | undefined = undefined;
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.dropColumns(CARD_SPACES, ['owner_address', 'url']);
   pgm.alterColumn(CARD_SPACES, 'merchant_id', { notNull: true });
-  // FIXME this should actually be unique also, to enforce 1:1 vs 1:n… obvs answer is to have it all in the same table but meh
+  pgm.createIndex(CARD_SPACES, 'merchant_id', { unique: true });
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropIndex(CARD_SPACES, 'merchant_id');
   pgm.alterColumn(CARD_SPACES, 'merchant_id', { notNull: false });
 
   pgm.addColumns(CARD_SPACES, {
