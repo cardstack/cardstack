ccfb8df78c3199280d88a9c6634c6c5e894c5cc0
Aierie <michael.khor@cardstack.com>
2022-01-30 20:05:37 +0800

    some styling for the donations card

--- packages/web-client/app/components/card-space/user-page/donations-card/index.ts
+++ packages/web-client/app/components/card-space/user-page/donations-card/index.ts
@@ -3,6 +3,7 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import CardSpaceUserData from '@cardstack/web-client/services/card-space-user-data';
+import Layer2Network from '@cardstack/web-client/services/layer2-network';
 
 const CardStates = {
   DEFAULT: 'default',
@@ -10,16 +11,39 @@ const CardStates = {
   SUBMITTING: 'submitting',
 } as const;
 
+// for the payments to work, this needs to be able to get the network (needs extending of layer 2 strategy type with `networkSymbol: string`)
+// it also needs a way to open a popup (maybe in-element, with the element at the top of the user-page component)
+// and also a way to get a merchant's address using their ID
+// the merchant payment URLs can be generated with the generateMerchantPaymentUrl utility from the SDK
 export default class UserPageDonationsCardComponent extends Component {
   @service declare cardSpaceUserData: CardSpaceUserData;
+  @service declare layer2Network: Layer2Network;
+
   @tracked state: typeof CardStates[keyof typeof CardStates] =
     CardStates.DEFAULT;
 
   get showInViewMode() {
-    const { donationDescription, donationTitle } =
-      this.cardSpaceUserData.currentUserData;
+    const {
+      donationDescription,
+      donationTitle,
+      donationSuggestionAmount1,
+      donationSuggestionAmount2,
+      donationSuggestionAmount3,
+      donationSuggestionAmount4,
+    } = this.cardSpaceUserData.currentUserData;
 
-    return donationDescription || donationTitle;
+    return (
+      donationDescription ||
+      donationTitle ||
+      donationSuggestionAmount1 ||
+      donationSuggestionAmount2 ||
+      donationSuggestionAmount3 ||
+      donationSuggestionAmount4
+    );
+  }
+
+  get displayName() {
+    return this.cardSpaceUserData.currentUserData.profileName;
   }
 
   get donationDescription() {
