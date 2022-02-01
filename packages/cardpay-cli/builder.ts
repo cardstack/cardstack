770045142a2051c445f2b65ff61a9a3ccbabfad3
Luke Melia <luke@lukemelia.com>
2022-02-01 13:03:25 -0500

    Extract builder module from CLI index

---
+++ packages/cardpay-cli/builder.ts
@@ -0,0 +1,44 @@
+import yargs from 'yargs';
+import { commands } from './commands';
+
+export function buildYargs(args: string[]) {
+  return yargs(args)
+    .scriptName('cardpay')
+    .demandCommand(1, 'Please specify a command')
+    .usage('Usage: $0 <command> [options]')
+    .help()
+    .command(commands)
+    .options({
+      network: {
+        alias: 'n',
+        type: 'string',
+        description: "The Layer 1 network to run this script in ('kovan' or 'mainnet')",
+      },
+      mnemonic: {
+        alias: 'm',
+        default: process.env.MNEMONIC_PHRASE,
+        type: 'string',
+        description: 'Phrase for mnemonic wallet. Also can be pulled from env using MNEMONIC_PHRASE',
+      },
+      walletConnect: {
+        alias: 'w',
+        type: 'boolean',
+        description: 'A flag to indicate that wallet connect should be used for the wallet',
+      },
+    })
+    .check((argv) => {
+      if (process.env.BUILDING_README) {
+        return true;
+      }
+
+      if (!argv.mnemonic && !argv.walletConnect) {
+        return 'Wallet is not specified. Either specify that wallet connect should be used for the wallet, or specify the mnemonic as a positional arg, or pass the mnemonic in using the MNEMONIC_PHRASE env var';
+      }
+      // is this the right way to do this?
+      if (argv.walletConnect) {
+        argv.mnemonic = undefined;
+      }
+      return true;
+    })
+    .demandOption(['network'], `'network' must be specified.`);
+}
--- packages/cardpay-cli/index.ts
+++ packages/cardpay-cli/index.ts
@@ -1,49 +1,13 @@
 /* eslint no-process-exit: "off" */
-import yargs from 'yargs';
 import fetch from 'node-fetch';
+import { buildYargs } from './builder';
 import { hideBin } from 'yargs/helpers';
-import { commands } from './commands';
 
 //@ts-ignore polyfilling fetch
 global.fetch = fetch;
 
 (async () => {
-  await yargs(hideBin(process.argv))
-    .scriptName('cardpay')
-    .demandCommand(1, 'Please specify a command')
-    .usage('Usage: $0 <command> [options]')
-    .help()
-    .command(commands)
-    .options({
-      network: {
-        alias: 'n',
-        type: 'string',
-        description: "The Layer 1 network to run this script in ('kovan' or 'mainnet')",
-      },
-      mnemonic: {
-        alias: 'm',
-        default: process.env.MNEMONIC_PHRASE,
-        type: 'string',
-        description: 'Phrase for mnemonic wallet. Also can be pulled from env using MNEMONIC_PHRASE',
-      },
-      walletConnect: {
-        alias: 'w',
-        type: 'boolean',
-        description: 'A flag to indicate that wallet connect should be used for the wallet',
-      },
-    })
-    .check((argv) => {
-      if (!argv.mnemonic && !argv.walletConnect) {
-        return 'Wallet is not specified. Either specify that wallet connect should be used for the wallet, or specify the mnemonic as a positional arg, or pass the mnemonic in using the MNEMONIC_PHRASE env var';
-      }
-      // is this the right way to do this?
-      if (argv.walletConnect) {
-        argv.mnemonic = undefined;
-      }
-      return true;
-    })
-    .demandOption(['network'], `'network' must be specified.`)
-    .parse();
+  await buildYargs(hideBin(process.argv)).parse();
   process.exit(0);
 })().catch((e) => {
   console.error(e);
