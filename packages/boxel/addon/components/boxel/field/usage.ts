8ad7f0f74e6d776281b71fe17c09ea431f6eaf9e
Burcu Noyan <burcunoyan@gmail.com>
2022-02-01 15:07:57 -0500

    Clean up

--- packages/boxel/addon/components/boxel/field/usage.ts
+++ packages/boxel/addon/components/boxel/field/usage.ts
@@ -2,12 +2,11 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class extends Component {
-  @tracked label = 'Account';
+  @tracked label = 'Full Name of the Account Holder';
   @tracked value = 'Gary Walker';
   @tracked id = 'sample-field';
   @tracked vertical = false;
-  @tracked smallLabel = false;
-  @tracked address = '0x...1234';
+  @tracked horizontalLabelSize = 'default';
   @tracked icon = 'card';
   @tracked tag = '';
 }
