b739378a432f2b6a3d19f84022e268dd6a8a5587
Buck Doyle <buck.doyle@cardstack.com>
2022-02-03 11:44:42 -0600

    Add attribution

--- packages/web-client/app/helpers/config.ts
+++ packages/web-client/app/helpers/config.ts
@@ -1,3 +1,4 @@
+// Adapted from https://github.com/nvdk/ember-config-helper/blob/6a9891afce1ef68c1a42fb5e5fc54ce02f5ae811/addon/helpers/config.js
 import { getOwner } from '@ember/application';
 import Helper from '@ember/component/helper';
 import { get } from '@ember/object';
