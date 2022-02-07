f385d7302f37baa6f0d2e154a802b6835ff81024
Buck Doyle <buck.doyle@cardstack.com>
2022-02-07 11:53:57 -0600

    Update after merge
    tragique!

--- packages/web-client/app/components/card-pay/error-message/index.ts
+++
@@ -1,6 +0,0 @@
-import Component from '@glimmer/component';
-import config from '@cardstack/web-client/config/environment';
-
-export default class CardPayErrorMessageComponent extends Component {
-  mailToSupportUrl = config.urls.mailToSupportUrl;
-}
