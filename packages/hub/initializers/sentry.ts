8a0326e63191685a44053c48542c83102ec2812e
Luke Melia <luke@lukemelia.com>
2022-02-10 16:53:00 -0500

    Do not send user transaction signing rejection errors to Sentry.

--- packages/hub/initializers/sentry.ts
+++ packages/hub/initializers/sentry.ts
@@ -9,6 +9,12 @@ export default function initSentry() {
       enabled: config.get('sentry.enabled'),
       environment: config.get('sentry.environment'),
       release: 'hub@' + packageJson.version,
+      beforeSend(event, hint) {
+        if (hint?.originalException?.toString() === 'Error: USER_REJECTION') {
+          return null;
+        }
+        return event;
+      },
     });
   }
 }
