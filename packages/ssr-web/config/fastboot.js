3603521a68735291b08518153948df7c8cac6d2f
Buck Doyle <buck.doyle@cardstack.com>
2022-02-15 13:33:00 -0600

    Add btoa for Fastboot

---
+++ packages/ssr-web/config/fastboot.js
@@ -0,0 +1,11 @@
+module.exports = function (/* environment */) {
+  return {
+    buildSandboxGlobals(defaultGlobals) {
+      return Object.assign({}, defaultGlobals, {
+        btoa: function (str) {
+          return Buffer.from(str).toString('base64');
+        },
+      });
+    },
+  };
+};
