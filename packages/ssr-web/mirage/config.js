1dc5286b1ea828d6b21690418a09ac5505eec368
Buck Doyle <buck.doyle@cardstack.com>
2022-02-16 12:38:21 -0600

    Remove unused Mirage code

--- packages/ssr-web/mirage/config.js
+++ packages/ssr-web/mirage/config.js
@@ -1,7 +1,3 @@
-import {
-  getFilenameFromDid,
-  defaultCreatedPrepaidCardDID,
-} from '@cardstack/ssr-web/utils/test-factories';
 import { nativeCurrencies } from '@cardstack/cardpay-sdk';
 import config from '@cardstack/ssr-web/config/environment';
 
@@ -21,71 +17,6 @@ export default function () {
       },
     };
   });
-
-  this.post('/merchant-infos');
-  this.get('/merchant-infos', function (schema) {
-    return schema.merchantInfos.all();
-  });
-
-  this.get(
-    '/merchant-infos/validate-slug/:slug',
-    function ({ merchantInfos }, { params: { slug } }) {
-      let merchantBySlug = merchantInfos.findBy({ slug });
-
-      if (slug === 'www') {
-        return {
-          slugAvailable: false,
-          detail: 'This Business ID is not allowed',
-        };
-      } else if (merchantBySlug) {
-        return {
-          slugAvailable: false,
-          detail:
-            'This Business ID is already taken. Please choose another one',
-        };
-      } else {
-        return {
-          slugAvailable: true,
-          detail: 'Business slug is available',
-        };
-      }
-    }
-  );
-
-  this.get('/prepaid-card-color-schemes', (schema) => {
-    return schema.prepaidCardColorSchemes.all();
-  });
-
-  this.get('/prepaid-card-patterns', (schema) => {
-    return schema.prepaidCardPatterns.all();
-  });
-
-  /**
-   * This is hardcoded to create a fixed id.
-   */
-  this.post('/prepaid-card-customizations', async function (schema) {
-    let customization = schema.create('prepaid-card-customization', {
-      id: await getFilenameFromDid(defaultCreatedPrepaidCardDID),
-      did: defaultCreatedPrepaidCardDID,
-      ...this.normalizedRequestAttrs(),
-    });
-
-    return customization;
-  });
-
-  this.post('/card-spaces/validate-profile-name', function () {
-    return {
-      errors: [],
-    };
-  });
-
-  this.get(
-    'https://storage.cardstack.com/prepaid-card-customization/:idWithExtension',
-    function (schema, { params: { idWithExtension } }) {
-      let [id] = idWithExtension.split('.');
-      return schema.prepaidCardCustomizations.find(id);
-    }
-  );
 
   this.get(
     'https://storage.cardstack.com/merchant-info/:idWithExtension',
--- packages/ssr-web/mirage/models/prepaid-card-customization.js
+++
@@ -1,6 +0,0 @@
-import { Model, belongsTo } from 'ember-cli-mirage';
-
-export default Model.extend({
-  colorScheme: belongsTo('prepaid-card-color-scheme'),
-  pattern: belongsTo('prepaid-card-pattern'),
-});
