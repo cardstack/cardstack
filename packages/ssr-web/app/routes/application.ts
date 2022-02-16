69fdb6dfe0b39f169766184f7efaa1f587b7bf07
Buck Doyle <buck.doyle@cardstack.com>
2022-02-16 13:25:33 -0600

    Remove unused files

--- packages/ssr-web/app/routes/application.ts
+++ packages/ssr-web/app/routes/application.ts
@@ -1,7 +1,6 @@
 import Route from '@ember/routing/route';
 import RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
-import '@cardstack/ssr-web/css/variables.css';
 
 export default class ApplicationRoute extends Route {
   @service declare router: RouterService;
