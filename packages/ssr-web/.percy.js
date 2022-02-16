fc13472c684c182939e1ef1b09d76de1664dfa6d
Buck Doyle <buck.doyle@cardstack.com>
2022-02-16 12:56:06 -0600

    Remove web-client Percy overrides

--- packages/ssr-web/.percy.js
+++ packages/ssr-web/.percy.js
@@ -3,28 +3,7 @@ module.exports = {
   snapshot: {
     widths: [1280],
     percyCSS: `
-    /* Break workflow threads out of modal/overflow containers */
-    body.has-modal {
-      overflow: visible !important;
-    }
-
-    .boxel-modal-overlay {
-      position: static !important;
-    }
-
-    .boxel-modal {
-      position: static !important;
-      height: auto !important;
-    }
-
-    .boxel-thread {
-      height: auto !important;
-    }
-
-    /* Hide frequently-changing elements: dates, times, and the sidebar application version */
-    .boxel-date-divider__date,
-    .boxel-thread-message__time,
-    .workflow-thread__app-version,
+    /* Hide frequently-changing element */
     .styled-qr-code {
       visibility: hidden !important;
     }
