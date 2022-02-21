dfa4f9fa5e3fc8ec95b3e26b2d2925a7c2aaf4e5
Aierie <michael.khor@cardstack.com>
2022-02-21 23:17:59 +0800

    tippy modifier

---
+++ packages/web-client/app/modifiers/tippy.ts
@@ -0,0 +1,82 @@
+/**
+ * Vendored from https://github.com/nag5000/ember-tippy/blob/master/addon/modifiers/tippy.js
+ * So that we can apply modifications globally, easily
+ */
+import Modifier from 'ember-modifier';
+import { isHTMLSafe } from '@ember/template';
+
+import tippy, { Instance as TippyInstance } from 'tippy.js';
+import type { Props as TippyOptions } from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/light-border.css';
+
+const baseTippyOptions = {
+  theme: 'light-border',
+  placement: 'bottom',
+  arrow: false,
+};
+
+type ModifierOptions = Partial<TippyOptions>;
+
+export default class TippyModifier extends Modifier<{
+  positional: [string] | [];
+  named: any;
+}> {
+  _instances: TippyInstance[] = [];
+
+  get defaultTarget() {
+    return this.element;
+  }
+
+  get options() {
+    const optionsHash: ModifierOptions =
+      this.args.named.options || this.args.named;
+    return {
+      content: this.args.positional?.[0],
+      ...baseTippyOptions,
+      ...optionsHash,
+    };
+  }
+
+  parseOptions(options: any) {
+    if (options.content instanceof HTMLElement && options.content.hidden) {
+      options.content.hidden = false;
+    }
+
+    if (options.allowHTML == null && isHTMLSafe(options.content)) {
+      options.allowHTML = true;
+    }
+
+    const {
+      // the rest are tippy options (tippy warns about unknown options)
+      ...tippyOptions
+    } = options;
+
+    return {
+      tippyTargets: this.defaultTarget,
+      tippyOptions,
+    };
+  }
+
+  didInstall() {
+    const options = this.parseOptions(this.options);
+    const { tippyTargets, tippyOptions } = options;
+
+    // NOTE: tippy() returns a single instance or an array of instances,
+    // depending on the type of targets argument.
+    // https://atomiks.github.io/tippyjs/v6/tippy-instance/#accessing-an-instance
+    const instances = tippy(tippyTargets, tippyOptions);
+    this._instances = ([] as TippyInstance[]).concat(instances);
+  }
+
+  didUpdateArguments() {
+    const options = this.parseOptions(this.options);
+
+    this._instances.forEach((x) => x.setProps(options.tippyOptions));
+  }
+
+  willDestroy() {
+    this._instances.forEach((x) => x.destroy());
+    this._instances = [];
+  }
+}
---
+++ packages/web-client/tests/integration/modifiers/tippy-test.ts
@@ -0,0 +1,103 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find, focus, triggerEvent, tap } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+import { htmlSafe } from '@ember/string';
+
+module('Integration | Modifier | tippy', function (hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(async function () {
+    this.set('content', 'Tooltip');
+    // seems like appendTo=parent is necessary
+    // to make it possible to detect the tooltip in tests
+    await render(hbs`
+      <div id="trigger" tabindex="0"
+        {{tippy content=this.content appendTo="parent"}}
+      >Trigger</div>
+    `);
+  });
+
+  test('expected html attrs and structure', async function (assert) {
+    const trigger = find('#trigger')!;
+    await focus(trigger);
+
+    const tippyId = trigger.getAttribute('aria-describedby') as string;
+    assert.ok(tippyId, 'trigger has an attached tippy');
+
+    const tippy = find('#' + tippyId)!;
+    assert.ok(tippy, 'attached tippy found');
+
+    assert.equal(trigger.parentElement!.children.namedItem(tippyId), tippy);
+    assert.equal(trigger.innerHTML, 'Trigger');
+    assert.equal(tippy.textContent, 'Tooltip');
+
+    // @ts-ignore property patched onto HTML element
+    const tippyInstance = trigger._tippy;
+    assert.ok(tippyInstance, 'tippyInstance');
+    assert.equal(tippyInstance.popper, tippy, 'tippyInstance.popper');
+    assert.equal(tippyInstance.reference, trigger, 'tippyInstance.reference');
+
+    assert.dom('[data-tippy-root]').hasAttribute('id', tippyId);
+    let tippyBoxData = (find('[data-tippy-root] .tippy-box') as HTMLElement)
+      .dataset;
+    assert.equal(tippyBoxData.state, 'visible');
+    assert.equal(tippyBoxData.placement, 'bottom');
+    assert.equal(
+      (find('[data-tippy-root] .tippy-box .tippy-content') as HTMLElement)
+        .dataset.state,
+      'visible'
+    );
+    assert
+      .dom('[data-tippy-root] .tippy-box .tippy-content')
+      .containsText('Tooltip');
+    assert.dom('[data-tippy-root] .tippy-box .tippy-arrow').doesNotExist();
+  });
+
+  test('it shows on hover', async function (assert) {
+    const trigger = find('#trigger')!;
+    await triggerEvent(trigger, 'mouseenter');
+
+    assert.deepEqual(
+      (find('[data-tippy-root] .tippy-box') as HTMLElement).dataset.state,
+      'visible'
+    );
+  });
+
+  test('it shows on focus', async function (assert) {
+    const trigger = find('#trigger')!;
+    await focus(trigger);
+
+    assert.deepEqual(
+      (find('[data-tippy-root] .tippy-box') as HTMLElement).dataset.state,
+      'visible'
+    );
+  });
+
+  test('it shows on touch', async function (assert) {
+    const trigger = find('#trigger')!;
+    await tap(trigger);
+
+    assert.deepEqual(
+      (find('[data-tippy-root] .tippy-box') as HTMLElement).dataset.state,
+      'visible'
+    );
+  });
+
+  test('it can render htmlSafe content', async function (assert) {
+    this.set('content', htmlSafe('Tool<b id="b">tip</b>'));
+
+    const trigger = find('#trigger')!;
+    await focus(trigger);
+
+    const tippyId = trigger.getAttribute('aria-describedby');
+    assert.ok(tippyId, 'trigger has an attached tippy');
+
+    const tippy = find('#' + tippyId)!;
+    assert.ok(tippy, 'attached tippy found');
+
+    assert.equal(tippy.textContent, 'Tooltip');
+    assert.equal(find('#b')!.textContent, 'tip');
+  });
+});
