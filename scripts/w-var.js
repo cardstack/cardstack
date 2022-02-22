4163e55c5d117d5cf8232814db3020b9c1f0d9bb
Aierie <michael.khor@cardstack.com>
2022-02-22 21:53:48 +0800

    w-var script to get waypoint variables

---
+++ scripts/w-var.js
@@ -0,0 +1,41 @@
+/* eslint-disable node/no-unpublished-require */
+const exec = require('child_process').exec;
+const Table = require('cli-table3');
+const variablesToGet = process.argv.slice(2);
+
+/**
+ * Get waypoint variables and print them neatly.
+ * Has very rough fuzzy search.
+ */
+exec('waypoint config get -json', (error, stdout, stderr) => {
+  if (stderr) console.error(stderr);
+  if (!stdout || error) {
+    console.error('Could not retrieve waypoint config');
+    console.error(error);
+    return;
+  }
+  const data = JSON.parse(stdout);
+  if (variablesToGet.length) {
+    const result = {};
+    for (let key in data) {
+      if (variablesToGet.some((str) => key.includes(str))) {
+        result[key] = data[key];
+      }
+    }
+    console.log(result);
+  } else {
+    const table = new Table({
+      head: ['Key', 'Value'],
+      colWidths: [40, 100],
+    });
+
+    for (let key in data) {
+      table.push([key, data[key]]);
+    }
+
+    console.log(table.toString());
+    console.log(
+      `\nThis table is for easy scanning of the variables so text is truncated where necessary. Get the specific variables by specifying them in the command for precise values.`
+    );
+  }
+});
