e9dea0bb514a38d036973344ff8281396e26d393
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2022-02-22 11:39:44 -0500

    DRY

---
+++ packages/cardpay-subgraph/src/mappings/gnosis-proxy-factory.ts
@@ -0,0 +1,31 @@
+import { GnosisSafe } from '../../generated/Gnosis_v1_3/GnosisSafe';
+import { GnosisSafe as GnosisSafeTemplate } from '../../generated/templates';
+import { Safe, SafeOwner } from '../../generated/schema';
+import { toChecksumAddress, makeAccount } from '../utils';
+import { Address, ethereum, log } from '@graphprotocol/graph-ts';
+
+export function processGnosisProxyEvent(proxyAddress: Address, event: ethereum.Event, gnosisVer: string): void {
+  let safeAddress = toChecksumAddress(proxyAddress);
+  let safeEntity = new Safe(safeAddress);
+  safeEntity.createdAt = event.block.timestamp;
+
+  let safe = GnosisSafe.bind(proxyAddress);
+  let owners = safe.getOwners();
+
+  for (let i = 0; i < owners.length; i++) {
+    let ownerAddress = toChecksumAddress(owners[i]);
+    makeAccount(ownerAddress);
+
+    let safeOwnerId = safeAddress + '-' + ownerAddress;
+    let safeOwnerEntity = new SafeOwner(safeOwnerId);
+    safeOwnerEntity.owner = ownerAddress;
+    safeOwnerEntity.safe = safeAddress;
+    safeOwnerEntity.createdAt = event.block.timestamp;
+    safeOwnerEntity.ownershipChangedAt = event.block.timestamp;
+    safeOwnerEntity.save();
+  }
+  safeEntity.save();
+  log.debug('created gnosis v{} safe entity {}', [gnosisVer, safeAddress]);
+
+  GnosisSafeTemplate.create(proxyAddress);
+}
--- packages/cardpay-subgraph/src/mappings/gnosis-proxy-factory_v1_2.ts
+++ packages/cardpay-subgraph/src/mappings/gnosis-proxy-factory_v1_2.ts
@@ -1,32 +1,6 @@
 import { ProxyCreation } from '../../generated/Gnosis_v1_2/GnosisProxyFactory_v1_2';
-import { GnosisSafe } from '../../generated/Gnosis_v1_2/GnosisSafe';
-import { GnosisSafe as GnosisSafeTemplate } from '../../generated/templates';
-import { Safe, SafeOwner } from '../../generated/schema';
-import { toChecksumAddress, makeAccount } from '../utils';
-import { log } from '@graphprotocol/graph-ts';
+import { processGnosisProxyEvent } from './gnosis-proxy-factory';
 
 export function handleProxyCreation(event: ProxyCreation): void {
-  let safeAddress = toChecksumAddress(event.params.proxy);
-  let safeEntity = new Safe(safeAddress);
-  safeEntity.createdAt = event.block.timestamp;
-
-  let safe = GnosisSafe.bind(event.params.proxy);
-  let owners = safe.getOwners();
-
-  for (let i = 0; i < owners.length; i++) {
-    let ownerAddress = toChecksumAddress(owners[i]);
-    makeAccount(ownerAddress);
-
-    let safeOwnerId = safeAddress + '-' + ownerAddress;
-    let safeOwnerEntity = new SafeOwner(safeOwnerId);
-    safeOwnerEntity.owner = ownerAddress;
-    safeOwnerEntity.safe = safeAddress;
-    safeOwnerEntity.createdAt = event.block.timestamp;
-    safeOwnerEntity.ownershipChangedAt = event.block.timestamp;
-    safeOwnerEntity.save();
-  }
-  safeEntity.save();
-  log.debug('created gnosis 1.2 safe entity {}', [safeAddress]);
-
-  GnosisSafeTemplate.create(event.params.proxy);
+  processGnosisProxyEvent(event.params.proxy, event, '1.2');
 }
