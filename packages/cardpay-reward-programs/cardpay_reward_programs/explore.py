6455ebd6579584fc22c1746c27e97af347c1adda
tintinthong <justinthong93@gmail.com>
2022-02-23 11:07:46 +0800

    fix explore

--- packages/cardpay-reward-programs/cardpay_reward_programs/explore.py
+++ packages/cardpay-reward-programs/cardpay_reward_programs/explore.py
@@ -1,3 +1,4 @@
+import numpy as np
 import programs.usage.view
 import streamlit as st
 
@@ -10,12 +11,16 @@ reward_program = st.sidebar.selectbox(
             "name": "CardPay Usage",
             "view_functions": [
                 {
-                    "name": "Historical",
-                    "view": programs.usage.view.view_historical,
+                    "name": "Query",
+                    "view": programs.usage.view.view_query,
                 },
                 {
                     "name": "Range",
                     "view": programs.usage.view.view_range,
+                },
+                {
+                    "name": "Historical",
+                    "view": programs.usage.view.view_historical,
                 },
             ],
         }
@@ -28,8 +33,14 @@ reward_ui = st.sidebar.selectbox(
     reward_program["view_functions"],
     format_func=lambda x: x["name"],
 )
-default_config_root = "s3://cardpay-staging-partitioned-graph-data/data/staging_rewards/0.0.1/"
-config_location = st.sidebar.text_input("Config root", default_config_root)
 
+root_prefix = "s3://cardpay-staging-partitioned-graph-data/data/"
+config_roots = tuple(["staging_rewards/0.0.1/", "prepaid_card_payments/0.0.2/", "rewards/0.0.1/"])
+
+config_choice = st.sidebar.selectbox("Config root", config_roots)
+config_location = f"{root_prefix}{config_choice}"
+
+
+st.write("Current config location", config_location)
 if config_location:
     reward_ui["view"](config_location)
