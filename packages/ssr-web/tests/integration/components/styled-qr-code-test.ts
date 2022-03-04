ec8bbec1ea509dab1299de619ebea5cb1fea76e3
Aierie <michael.khor@cardstack.com>
2022-03-04 17:51:24 +0800

    assertion on styled qr code loading

---
+++ packages/ssr-web/tests/integration/components/styled-qr-code-test.ts
@@ -0,0 +1,31 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, waitFor } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+const QR = '[data-test-styled-qr-code]';
+const QR_LOADING_INDICATOR = '[data-test-styled-qr-code-loading-indicator]';
+
+const link =
+  'https://pay.cardstack.com/merchat-asdnsadkasd?id=0x1238urfds&amount=73298587423545';
+
+module('Integration | Component | styled-qr-code', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it can render a visual loading state and moves out of it after', async function (assert) {
+    await render(hbs`
+      <StyledQrCode @data={{this.data}} />
+    `);
+
+    assert.dom(`${QR} canvas`).doesNotExist();
+    assert.dom(QR_LOADING_INDICATOR).isVisible();
+
+    this.setProperties({
+      data: link,
+    });
+
+    await waitFor(`${QR} canvas`, { timeout: 1000, count: 1 });
+
+    assert.dom(QR_LOADING_INDICATOR).doesNotExist();
+  });
+});
