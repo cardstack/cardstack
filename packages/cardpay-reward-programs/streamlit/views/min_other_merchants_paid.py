7904f6977e38963fb47f8291ea73760b5413efe3
tintinthong <justinthong93@gmail.com>
2022-03-08 19:34:28 +0800

    fix views

--- packages/cardpay-reward-programs/streamlit/views/min_other_merchants_paid.py
+++ packages/cardpay-reward-programs/streamlit/views/min_other_merchants_paid.py
@@ -17,7 +17,7 @@ def min_other_merchants_paid(core_parameters):
         "min_other_merchants": min_other_merchants,
         "token": "0x999999cf1046e68e36E1aA2E0E07105eDDD1f08E",
         "subgraph_config_location": {
-            "prepaid_card_payment": "s3://cardpay-staging-partitioned-graph-data/data/staging_rewards/0.0.1/"
+            "prepaid_card_payment": "s3://cardpay-staging-partitioned-graph-data//data/prepaid_card_payments/0.0.3/"
         },
         "duration": 43200,
     }
--- packages/cardpay-reward-programs/streamlit/views/weighted_usage.py
+++ packages/cardpay-reward-programs/streamlit/views/weighted_usage.py
@@ -22,7 +22,7 @@ def weighted_usage(core_parameters):
         "spend_factor": spend_factor,
         "token": "0x999999cf1046e68e36E1aA2E0E07105eDDD1f08E",
         "subgraph_config_location": {
-            "prepaid_card_payment": "s3://cardpay-staging-partitioned-graph-data/data/staging_rewards/0.0.1/"
+            "prepaid_card_payment": "s3://cardpay-staging-partitioned-graph-data//data/prepaid_card_payments/0.0.3/"
         },
         "duration": 43200,
     }
