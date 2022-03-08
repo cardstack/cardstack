bfa8cd8736ff0e930b5e19810e57b18571fe35cc
Edward Faulkner <edward@eaf4.com>
2022-03-08 18:56:33 -0500

    following main branch

--- packages/core/src/compiler.ts
+++ packages/core/src/compiler.ts
@@ -493,7 +493,7 @@ export class Compiler<Identity extends Saved | Unsaved = Saved> {
 
   private getSchemaModuleRef(parentCard: CompiledCard | undefined, schemaPath: string | undefined): ModuleRef {
     if (isBaseCard(this.cardSource)) {
-      return { global: 'todo' };
+      return { global: `${BASE_CARD_URL}/schema` };
     } else if (schemaPath) {
       return { local: schemaPath };
     } else if (parentCard?.schemaModule) {
