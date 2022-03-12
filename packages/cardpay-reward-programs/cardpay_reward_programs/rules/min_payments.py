c7f9149c752ff3610a7cbf0ed35c536825d73164
Ian Calvert <ianjcalvert@gmail.com>
2022-03-12 20:05:25 +0000

    Remove unspecified rule

--- packages/cardpay-reward-programs/cardpay_reward_programs/rules/min_payments.py
+++
@@ -1,9 +0,0 @@
-from cardpay_reward_programs.rule import Rule
-
-
-class MinPayments(Rule):
-    def __init__(self, **kwargs):
-        super(MinPayments, self).__init__(**kwargs)
-
-    def sql(self):
-        return
