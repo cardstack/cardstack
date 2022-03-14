f21eb7490d6b706a76bfe1abad392ebd11739325
Buck Doyle <buck.doyle@cardstack.com>
2022-03-14 09:57:33 -0500

    Add explicit health check route

--- packages/ssr-web/app/router.ts
+++ packages/ssr-web/app/router.ts
@@ -13,8 +13,11 @@ Router.map(function () {
   this.route('pay-missing-route', {
     path: '/pay/:*',
   });
+
   this.route('boom');
   this.route('boom-client');
+
+  this.route('health');
 
   this.route('not-found', { path: '/*path' });
 });
