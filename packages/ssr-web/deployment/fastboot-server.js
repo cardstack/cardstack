380c70310db76bf12b62c7759b473b66c2b787a8
Aierie <michael.khor@cardstack.com>
2022-03-15 21:08:19 +0800

    oops, sentry needs to be first

--- packages/ssr-web/deployment/fastboot-server.js
+++ packages/ssr-web/deployment/fastboot-server.js
@@ -41,8 +41,8 @@ let server = new FastBootAppServer({
   chunkedResponse: false, // Optional - Opt-in to chunked transfer encoding, transferring the head, body and potential shoeboxes in separate chunks. Chunked transfer encoding should have a positive effect in particular when the app transfers a lot of data in the shoebox.
 
   beforeMiddleware: function (app) {
-    app.use(healthCheckMiddleware);
     app.use(Sentry.Handlers.requestHandler());
+    app.use(healthCheckMiddleware);
   },
 
   afterMiddleware: function (app) {
