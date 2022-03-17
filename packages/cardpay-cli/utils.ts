51869be789acbbe8c4d08d9794ea0283e750d13d
tintinthong <justinthong93@gmail.com>
2022-03-17 16:08:11 +0800

    add trezor to getWeb3

--- packages/cardpay-cli/utils.ts
+++ packages/cardpay-cli/utils.ts
@@ -4,11 +4,18 @@ import { AbstractProvider } from 'web3-core';
 import { HttpProvider, getConstant, networkIds, getConstantByNetwork } from '@cardstack/cardpay-sdk';
 import WalletConnectProvider from '@walletconnect/web3-provider';
 import { Options } from 'yargs';
+const TrezorWalletProvider = require('trezor-cli-wallet-provider');
 
 const BRIDGE = 'https://safe-walletconnect.gnosis.io/';
 
-export async function getWeb3(network: string, mnemonic?: string): Promise<Web3> {
-  if (mnemonic) {
+export async function getWeb3(network: string, mnemonic?: string, trezor?: boolean): Promise<Web3> {
+  if (trezor) {
+    return new Web3(
+      new TrezorWalletProvider(await getConstant('rpcNode', network), {
+        chainId: networkIds[network],
+      })
+    );
+  } else if (mnemonic) {
     return new Web3(
       new HDWalletProvider({
         chainId: networkIds[network],
