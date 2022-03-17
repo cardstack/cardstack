33b35e70fdf9d9847e721a9168b7abd56a5e941f
tintinthong <justinthong93@gmail.com>
2022-03-17 11:29:18 +0800

    get hardcoded reward tokens

--- packages/cardpay-sdk/sdk/reward-pool/base.ts
+++ packages/cardpay-sdk/sdk/reward-pool/base.ts
@@ -523,7 +523,7 @@ but the balance is the reward pool is ${fromWei(rewardPoolBalanceForRewardProgra
   }
 
   async balances(rewardProgramId: string): Promise<WithSymbol<RewardTokenBalance>[]> {
-    const tokensAvailable = await this.rewardTokensAvailable(rewardProgramId);
+    const tokensAvailable = await this.get_reward_tokens();
     let promises = tokensAvailable.map((tokenAddress) => {
       return this.balance(rewardProgramId, tokenAddress);
     });
@@ -544,6 +544,11 @@ but the balance is the reward pool is ${fromWei(rewardPoolBalanceForRewardProgra
 
   async address(): Promise<string> {
     return await getAddress('rewardPool', this.layer2Web3);
+  }
+
+  async get_reward_tokens(): Promise<string[]> {
+    let card_token_address = await getAddress('cardCpxd', this.layer2Web3)
+    return [card_token_address]
   }
 
   async addTokenSymbol<T extends Proof | RewardTokenBalance>(arrWithTokenAddress: T[]): Promise<WithSymbol<T>[]> {
