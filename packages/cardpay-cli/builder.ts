8448221e67387cb50eac9bff93430fb414af40e8
tintinthong <justinthong93@gmail.com>
2022-03-20 16:29:13 +0800

    update builder and utils to support web3Opts

--- packages/cardpay-cli/builder.ts
+++ packages/cardpay-cli/builder.ts
@@ -13,37 +13,30 @@ export function buildYargs(args: string[]) {
         alias: 'm',
         default: process.env.MNEMONIC_PHRASE,
         type: 'string',
-        description: 'Phrase for mnemonic wallet. Also can be pulled from env using MNEMONIC_PHRASE',
-      },
-      walletConnect: {
-        alias: 'w',
-        type: 'boolean',
-        description: 'A flag to indicate that wallet connect should be used for the wallet',
-      },
-      trezor: {
-        alias: 't',
-        type: 'boolean',
-        description: 'A flag to indicate that trezor should be used for the wallet',
+        description: 'Phrase for mnemonic wallet',
       },
     })
     .check((argv) => {
       if (process.env.BUILDING_README) {
         return true;
       }
-
-      if (!argv.trezor && !argv.mnemonic && !argv.walletConnect) {
-        return 'Wallet is not specified. Either specify that wallet connect should be used for the wallet, or specify the mnemonic as a positional arg, or pass the mnemonic in using the MNEMONIC_PHRASE env var';
+      switch (argv.connectionType) {
+        case 'mnemonic':
+          if (!argv.mnemonic) {
+            return `mnemonic not specified with connectionType ${argv.connectionType}`;
+          } else {
+            argv.web3Opts = {
+              mnemonic: argv.mnemonic,
+            };
+            return true;
+          }
+        case 'wallet-connect':
+          return true;
+        case 'trezor':
+          return true;
+        default:
+          return 'You need connectionType';
       }
-
-      if (argv.trezor) {
-        argv.mnemonic = undefined;
-        argv.walletConnect = undefined;
-      }
-      if (argv.walletConnect && !argv.trezor) {
-        argv.mnemonic = undefined;
-        argv.trezor = undefined;
-      }
-      return true;
     })
     .demandOption(['network'], `'network' must be specified.`);
 }
--- packages/cardpay-cli/utils.ts
+++ packages/cardpay-cli/utils.ts
@@ -3,47 +3,89 @@ import Web3 from 'web3';
 import { AbstractProvider } from 'web3-core';
 import { HttpProvider, getConstant, networkIds, getConstantByNetwork } from '@cardstack/cardpay-sdk';
 import WalletConnectProvider from '@walletconnect/web3-provider';
-import { Options } from 'yargs';
+import { Options, Arguments } from 'yargs';
 /* eslint-disable @typescript-eslint/no-require-imports,@typescript-eslint/no-var-requires */
 const TrezorWalletProvider = require('trezor-cli-wallet-provider');
 
 const BRIDGE = 'https://safe-walletconnect.gnosis.io/';
 
-export async function getWeb3(network: string, mnemonic?: string, trezor?: boolean): Promise<Web3> {
-  if (trezor) {
-    return new Web3(
-      new TrezorWalletProvider(await getConstant('rpcNode', network), {
-        chainId: networkIds[network],
-      })
-    );
-  } else if (mnemonic) {
-    return new Web3(
-      new HDWalletProvider({
-        chainId: networkIds[network],
-        mnemonic: {
-          phrase: mnemonic,
+export type ConnectionType = 'mnemonic' | 'trezor' | 'wallet-connect';
+
+interface Web3OptsDefault {}
+
+interface Web3OptsMnemonic extends Web3OptsDefault {
+  connectionType: 'mnemonic';
+  mnemonic: string;
+}
+
+interface Web3OptsTrezor extends Web3OptsDefault {
+  connectionType: 'trezor';
+}
+
+interface Web3OptsWalletConnect extends Web3OptsDefault {
+  connectionType: 'wallet-connect';
+}
+
+export type Web3Opts = Web3OptsMnemonic | Web3OptsTrezor | Web3OptsWalletConnect;
+
+export function getWeb3Opts(args: Arguments): Web3Opts {
+  let o: Web3OptsDefault = {};
+  switch (args.connectionType as ConnectionType) {
+    case 'wallet-connect':
+      return {
+        ...o,
+        connectionType: 'wallet-connect',
+      } as Web3OptsWalletConnect;
+    case 'trezor':
+      return {
+        ...o,
+        connectionType: 'trezor',
+      } as Web3OptsTrezor;
+    case 'mnemonic':
+      return {
+        ...o,
+        connectionType: 'mnemonic',
+        mnemonic: args.mnemonic,
+      } as Web3OptsMnemonic;
+  }
+}
+
+export async function getWeb3(network: string, opts: Web3Opts): Promise<Web3> {
+  switch (opts.connectionType) {
+    case 'wallet-connect':
+      let provider = new WalletConnectProvider({
+        clientMeta: {
+          description: '',
+          url: 'https://app.cardstack.com',
+          icons: [],
+          name: 'Cardstack - Cardpay CLI',
         },
-        providerOrUrl: new HttpProvider(await getConstant('rpcNode', network)),
-      })
-    );
-  } else {
-    let provider = new WalletConnectProvider({
-      clientMeta: {
-        description: '',
-        url: 'https://app.cardstack.com',
-        icons: [],
-        name: 'Cardstack - Cardpay CLI',
-      },
-      rpc: {
-        // we can't use OpenEthereum nodes as it was issues with modern web3
-        // providers (specifically the xdai archive node that POA hosts falls
-        // into this category)
-        [networkIds[network]]: getConstantByNetwork('rpcNode', network),
-      },
-      bridge: BRIDGE,
-    });
-    await provider.enable();
-    return new Web3(provider as unknown as AbstractProvider);
+        rpc: {
+          // we can't use OpenEthereum nodes as it was issues with modern web3
+          // providers (specifically the xdai archive node that POA hosts falls
+          // into this category)
+          [networkIds[network]]: getConstantByNetwork('rpcNode', network),
+        },
+        bridge: BRIDGE,
+      });
+      await provider.enable();
+      return new Web3(provider as unknown as AbstractProvider);
+    case 'mnemonic':
+      return new Web3(
+        new HDWalletProvider({
+          chainId: networkIds[network],
+          mnemonic: {
+            phrase: opts.mnemonic,
+          },
+          providerOrUrl: new HttpProvider(await getConstant('rpcNode', network)),
+        })
+      );
+    case 'trezor':
+      return new Web3(
+        new TrezorWalletProvider(await getConstant('rpcNode', network), {
+          chainId: networkIds[network],
+        })
+      );
   }
 }
 
