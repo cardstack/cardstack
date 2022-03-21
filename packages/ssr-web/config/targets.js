28f161472aa2c858e876b0df438b7122645928dd
Aierie <michael.khor@cardstack.com>
2022-03-21 16:24:07 +0800

    remove opera mini from targets

--- packages/ssr-web/config/targets.js
+++ packages/ssr-web/config/targets.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const browsers = ['defaults', 'not IE 11'];
+const browsers = ['defaults', 'not ie 11', 'not op_mini all'];
 
 // const isCI = Boolean(process.env.CI);
 // const isProduction = process.env.EMBER_ENV === 'production';
