1c382dd2beacd6d5d415d27db6f4cdf1f56c81f5
Buck Doyle <buck.doyle@cardstack.com>
2022-03-21 12:27:43 -0500

    Add escaping to hide link previews

--- packages/hub/services/discord-bots/hub-bot/commands/guild/card-drop.ts
+++ packages/hub/services/discord-bots/hub-bot/commands/guild/card-drop.ts
@@ -68,7 +68,7 @@ export const run: Command['run'] = async (bot: Client, message: Message) => {
 
 To claim a prepaid card you’ll need Card Wallet app to receive your prepaid card.
 
-If you do not have the Card Wallet app, download Card Wallet at https://cardstack.com/ios or https://cardstack.com/android
+If you do not have the Card Wallet app, download Card Wallet at <https://cardstack.com/ios> or <https://cardstack.com/android>
 
 Type "ok" if you are ready to continue.`
   );
