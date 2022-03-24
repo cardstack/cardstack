29db102beb7e9800db5bd456b6d65ae05d365aac
Burcu Noyan <burcunoyan@gmail.com>
2022-03-24 14:01:24 -0400

    Todo: Fix hardcoded

--- packages/core/src/compiler.ts
+++ packages/core/src/compiler.ts
@@ -514,7 +514,7 @@ export class Compiler<Identity extends Saved | Unsaved = Saved> {
 
   private getSchemaModuleRef(parentCard: CompiledCard | undefined, schemaPath: string | undefined): ModuleRef {
     if (isBaseCard(this.cardSource)) {
-      return { global: `${BASE_CARD_URL}/schema` };
+      return { global: `@cardstack/compiled/https-cardstack.com-base-base/schema.js` };
     } else if (schemaPath) {
       return { local: schemaPath };
     } else if (parentCard?.schemaModule) {
