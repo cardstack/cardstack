ebfee3e8eadf698e6f112795a81aa3746240cdf8
Buck Doyle <buck.doyle@cardstack.com>
2022-03-25 14:46:15 -0500

    Add preliminary preview deployment setup

---
+++ packages/ssr-web/config/deploy.js
@@ -0,0 +1,110 @@
+/* eslint-env node */
+
+// FIXME copypaste from web-client
+
+module.exports = function (deployTarget) {
+  // these get less aggressive caching because they exist at stable URLs
+  let s3PagePattern = '**/*.{html,zip,pdf,css}'; // note that fingerprinting has not been implemented in embroider yet, using less agressive caching for css for now
+  let s3WellKnownPattern = '.well-known/*';
+
+  // these get more aggressive caching because they are sub-resources with fingerprinted URLs
+  let s3AssetPattern =
+    '**/*.{js,png,gif,webp,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,ttc,otf}';
+
+  let ENV = {
+    build: {},
+    pipeline: {
+      activateOnDeploy: true,
+      disabled: {},
+      alias: {
+        s3: {
+          as: ['s3Pages', 's3WellKnown', 's3Assets'],
+        },
+        compress: {
+          as: ['compressOwnAssets', 'compressPrepaidCardPatterns'],
+        },
+      },
+    },
+    'revision-data': {
+      scm: null,
+    },
+    s3Assets: {
+      filePattern: s3AssetPattern,
+      allowOverwrite: true,
+    },
+    s3Pages: {
+      filePattern: s3PagePattern,
+      allowOverwrite: true,
+      cacheControl: 'max-age=600, public',
+    },
+    s3WellKnown: {
+      filePattern: s3WellKnownPattern,
+      allowOverwrite: true,
+      cacheControl: 'max-age=600, public',
+    },
+    compressOwnAssets: {
+      filePattern: '**/*.{js,css,json,ico,map,xml,txt,svg,eot,ttf,woff,woff2}',
+      ignorePattern: 'images/prepaid-card-customizations/**/*.svg',
+      compression: ['best'],
+    },
+    compressPrepaidCardPatterns: {
+      filePattern: 'images/prepaid-card-customizations/**/*.svg',
+      compression: ['gzip'],
+    },
+    cloudfront: {
+      objectPaths: [
+        '/',
+        '/*',
+        '/*/*',
+        '/index.html',
+        '/assets/*',
+        '/assets/@cardstack/*',
+      ],
+    },
+  };
+
+  process.env.DEPLOY_TARGET = deployTarget;
+
+  if (
+    deployTarget === 's3-preview-staging' ||
+    deployTarget === 's3-preview-production'
+  ) {
+    ENV.pipeline = {
+      activateOnDeploy: true,
+    };
+    ENV.s3 = {
+      accessKeyId: process.env.PREVIEW_DEPLOY_AWS_ACCESS_KEY,
+      secretAccessKey: process.env.PREVIEW_DEPLOY_AWS_ACCESS_SECRET,
+      bucket: process.env.S3_PREVIEW_BUCKET_NAME,
+      region: process.env.S3_PREVIEW_REGION,
+      prefix: process.env.PR_BRANCH_NAME,
+      filePattern:
+        '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,otf,wasm,json,flac,webp}',
+    };
+    ENV.manifest = {
+      filePattern:
+        '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,otf,wasm,json,flac,webp}',
+    };
+    ENV['s3-index'] = {
+      accessKeyId: process.env.PREVIEW_DEPLOY_AWS_ACCESS_KEY,
+      secretAccessKey: process.env.PREVIEW_DEPLOY_AWS_ACCESS_SECRET,
+      bucket: process.env.S3_PREVIEW_BUCKET_NAME,
+      region: process.env.S3_PREVIEW_REGION,
+      allowOverwrite: true,
+      prefix: process.env.PR_BRANCH_NAME,
+    };
+    ENV.plugins = [
+      'build',
+      'compress',
+      'display-revisions',
+      'revision-data',
+      's3',
+      's3-index',
+    ];
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
