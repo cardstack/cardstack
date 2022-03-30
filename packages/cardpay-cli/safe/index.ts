1854d2302a038cb4e0ebae91b948c5884cc5cd52
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2022-03-30 15:08:05 -0400

    Add debug util for signing typed data

--- packages/cardpay-cli/safe/index.ts
+++ packages/cardpay-cli/safe/index.ts
@@ -5,9 +5,10 @@ import list from './list';
 import view from './view';
 import transferTokens from './transfer-tokens';
 import transferTokensGasEstimate from './transfer-tokens-gas-estimate';
+import debugSignTypedData from './debug-sign-typed-data';
 
 export const builder = function (yargs: Argv) {
-  return yargs.command([list, transferTokens, transferTokensGasEstimate, view] as any);
+  return yargs.command([list, transferTokens, transferTokensGasEstimate, view, debugSignTypedData] as any);
 };
 
 export function handler(/* argv: Argv */) {
