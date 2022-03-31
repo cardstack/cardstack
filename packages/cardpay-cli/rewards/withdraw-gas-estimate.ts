7be674e4c0f8e1e1179af69b4ba3b9919a4ba260
tintinthong <justinthong93@gmail.com>
2022-03-31 17:00:16 +0800

    add withdraw gas estimate cli

---
+++ packages/cardpay-cli/rewards/withdraw-gas-estimate.ts
@@ -0,0 +1,51 @@
+import { Argv } from 'yargs';
+import { getWeb3, NETWORK_OPTION_LAYER_2, getWeb3Opts } from '../utils';
+import { Arguments, CommandModule } from 'yargs';
+import { getSDK } from '@cardstack/cardpay-sdk';
+import Web3 from 'web3';
+const { fromWei } = Web3.utils;
+
+export default {
+  command: 'withdraw-from-safe <rewardSafe> <recipient> <tokenAddress> <amount>',
+  describe: 'Withdraw from reward safe',
+  builder(yargs: Argv) {
+    return yargs
+      .positional('rewardSafe', {
+        type: 'string',
+        description: 'The address of the rewardSafe that already contains rewards',
+      })
+      .positional('recipient', {
+        type: 'string',
+        description: "The token recipient's address",
+      })
+      .positional('tokenAddress', {
+        type: 'string',
+        description: 'The address of the tokens that are being transferred from reward safe',
+      })
+      .positional('amount', {
+        type: 'string',
+        description: 'The amount of tokens to transfer (not in units of wei, but in eth)',
+      })
+      .option('network', NETWORK_OPTION_LAYER_2);
+  },
+  async handler(args: Arguments) {
+    let { network, rewardSafe, recipient, tokenAddress, amount } = args as unknown as {
+      network: string;
+      rewardSafe: string;
+      recipient: string;
+      tokenAddress: string;
+      amount: string;
+    };
+    let web3 = await getWeb3(network, getWeb3Opts(args));
+    let rewardManagerAPI = await getSDK('RewardManager', web3);
+    let assets = await getSDK('Assets', web3);
+    let { gasToken, amount: estimate } = await rewardManagerAPI.withdrawGasEstimate(
+      rewardSafe,
+      recipient,
+      tokenAddress,
+      amount
+    );
+    let { symbol } = await assets.getTokenInfo(gasToken);
+    console.log(`The gas estimate for withdrawing from reward safe ${rewardSafe} is ${fromWei(estimate)} ${symbol}`);
+  },
+} as CommandModule;
