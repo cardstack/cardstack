83a94d9ec8139b6b6c37571860ff650056891333
Buck Doyle <buck.doyle@cardstack.com>
2022-04-06 14:13:37 -0400

    Add self-exclusion for job-listing query

--- packages/hub/tasks/print-queued-jobs.ts
+++ packages/hub/tasks/print-queued-jobs.ts
@@ -1,6 +1,10 @@
 import { Helpers } from 'graphile-worker';
 import { inject } from '@cardstack/di';
 
+const LIST_OTHER_JOBS_QUERY =
+  "SELECT * FROM graphile_worker.jobs WHERE attempts < max_attempts AND task_identifier != 'print-queued-jobs';";
+const COUNT_OTHER_JOBS_QUERY = LIST_OTHER_JOBS_QUERY.replace('*', 'COUNT(*)');
+
 /**
  * Meant to be used as a cron job. Logs the Graphile Worker job queue
  */
@@ -12,15 +16,11 @@ export default class PrintQueuedJobs {
 
       const {
         rows: [{ count }],
-      } = await db.query(`
-        SELECT COUNT(*) FROM graphile_worker.jobs WHERE attempts < max_attempts;
-      `);
+      } = await db.query(COUNT_OTHER_JOBS_QUERY);
 
       helpers.logger.info(`Queued jobs count: ${count}`);
 
-      const { rows } = await db.query(`
-        SELECT * FROM graphile_worker.jobs WHERE attempts < max_attempts;
-      `);
+      const { rows } = await db.query(LIST_OTHER_JOBS_QUERY);
 
       helpers.logger.info(`Queued jobs: ${JSON.stringify(rows, null, 2)}`);
     } catch (e) {
