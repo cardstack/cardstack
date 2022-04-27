c13101bd618de9b80210301a63128860ada1bc24
tintinthong <justinthong93@gmail.com>
2022-04-27 16:26:39 +0800

    remove unwanted models

--- packages/cardpay-reward-api/cardpay_reward_api/models.py
+++ packages/cardpay-reward-api/cardpay_reward_api/models.py
@@ -13,14 +13,11 @@ class Proof(Base):
     id = Column(Integer, primary_key=True, index=True)
     reward_program_id = Column(String)
     root_hash = Column(String)
-    # block_number = Column(Integer)
     leaf = Column(String, unique=True)
-    # proof_array = Column(Array(String))
     payment_cycle = Column(Integer)
     payee = Column(String)
     token = Column(String)
     proof_array = Column(MutableList.as_mutable(PickleType), default=[])
-    # timestamp = Column(Time)
 
 
 class Root(Base):
@@ -31,6 +28,3 @@ class Root(Base):
     payment_cycle = Column(Integer)
     block_number = Column(Integer)
     timestamp = Column(Time)
-
-    # def __repr__(self):
-    #     return "hi"
