226e5e74ea5dc33bffb9289c0e6c389468809f7a
Buck Doyle <buck.doyle@cardstack.com>
2022-04-29 13:03:07 -0500

    Remove unused header components

--- packages/web-client/app/components/card-drop-page/header/index.ts
+++ packages/web-client/app/components/card-drop-page/header/index.ts
@@ -1,8 +1,6 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import CardPayLogo from '@cardstack/web-client/images/icons/card-pay-logo.svg';
 
 export default class extends Component {
-  @tracked hamburgerMenuOpen = false;
   cardPayLogo = CardPayLogo;
 }
