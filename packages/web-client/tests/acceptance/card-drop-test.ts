63a93ca2da9280bff7201ec88dc7dd056a823380
Buck Doyle <buck.doyle@cardstack.com>
2022-04-29 12:46:33 -0500

    Add Percy snapshot for success route

---
+++ packages/web-client/tests/acceptance/card-drop-test.ts
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import percySnapshot from '@percy/ember';
+
+module('Acceptance | card-drop', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('it shows a success message', async function (assert) {
+    assert.expect(0);
+    await visit('/card-drop/success');
+    await percySnapshot(assert);
+  });
+});
