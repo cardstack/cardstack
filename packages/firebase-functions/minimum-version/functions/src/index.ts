724b7af463a162805fec646bb58056c7db539502
Luke Melia <luke@lukemelia.com>
2022-04-29 20:01:39 -0400

    Update minimum mobile app version

--- packages/firebase-functions/minimum-version/functions/src/index.ts
+++ packages/firebase-functions/minimum-version/functions/src/index.ts
@@ -1,6 +1,6 @@
 import { https } from 'firebase-functions';
 
-const MIN_VERSION = '1.1.0';
+const MIN_VERSION = '1.1.1';
 
 exports.minimumVersion = https.onRequest(async (_, res) => {
   res.status(200).send(JSON.stringify({ minVersion: MIN_VERSION }));
