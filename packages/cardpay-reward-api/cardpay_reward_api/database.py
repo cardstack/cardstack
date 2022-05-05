a0343e482954993905b9a1c23159801cdccba7cf
tintinthong <justinthong93@gmail.com>
2022-05-05 12:14:17 +0800

    placeholder  sql url

--- packages/cardpay-reward-api/cardpay_reward_api/database.py
+++ packages/cardpay-reward-api/cardpay_reward_api/database.py
@@ -4,7 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres@localhost/postgres"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://postgres:mysecretpassword@postgres:5432/postgres"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
