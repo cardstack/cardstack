ea50e5a475f634a6ce60ffabf8f732ede481dfab
Aierie <michael.khor@cardstack.com>
2022-05-10 19:52:36 +0800

    use mailchimp in subscribe-email task

--- packages/hub/tasks/subscribe-email.ts
+++ packages/hub/tasks/subscribe-email.ts
@@ -1,7 +1,22 @@
+import { inject } from '@cardstack/di';
 import { Helpers } from 'graphile-worker';
+import * as Sentry from '@sentry/node';
 
 export default class SubscribeEmail {
-  async perform(_payload: any, helpers: Helpers) {
-    helpers.logger.info(`Placeholder for SubscribeEmail`);
+  mailchimp = inject('mailchimp');
+
+  async perform(payload: { email: string }, helpers: Helpers) {
+    try {
+      await this.mailchimp.subscribe(payload.email);
+    } catch (e) {
+      helpers.logger.error('Failed to subscribe user to newsletter');
+      helpers.logger.error(JSON.stringify(e, null, 2));
+      Sentry.captureException(e, {
+        tags: {
+          event: 'newsletter-subscription',
+        },
+      });
+      throw e;
+    }
   }
 }
