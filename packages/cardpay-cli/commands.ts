a5d9ac7b8a3e0ff27b70b0f2cc2fd80fc4ee9ba8
Matic Jurglič <matic@jurglic.si>
2022-05-13 16:20:41 +0200

    Add addTokens, addSKU, getQuantity

--- packages/cardpay-cli/commands.ts
+++ packages/cardpay-cli/commands.ts
@@ -5,8 +5,21 @@ import * as hub from './hub';
 import * as merchant from './merchant';
 import * as prepaidCard from './prepaid-card';
 import * as prepaidCardMarket from './prepaid-card-market';
+import * as prepaidCardMarketV2 from './prepaid-card-market-v-2';
 import * as price from './price';
 import * as rewards from './rewards';
 import * as safe from './safe';
 
-export const commands: any = [assets, bridge, did, hub, merchant, prepaidCard, prepaidCardMarket, price, rewards, safe];
+export const commands: any = [
+  assets,
+  bridge,
+  did,
+  hub,
+  merchant,
+  prepaidCard,
+  prepaidCardMarket,
+  prepaidCardMarketV2,
+  price,
+  rewards,
+  safe,
+];
