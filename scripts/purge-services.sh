2be87be61387f2e990a5d3fa95021837bae3335c
Chi Jun <pcjun97@gmail.com>
2022-05-20 21:48:43 +0800

    Add missing semicolon

--- scripts/purge-services.sh
+++ scripts/purge-services.sh
@@ -17,6 +17,6 @@ if [ "$(echo $SERVICE_LIST | wc -w)" -gt "$COUNT" ]; then
   for SERVICE in ${SORTED[@]:$COUNT}; do
     CMD="aws ecs delete-service --cluster $CLUSTER --service $SERVICE --force"
     echo "$CMD"
-    $CMD &> /tmp/purge-services.log || { echo "Failed to delete service $CLUSTER/$SERVICE"; cat /tmp/purge-service.log }
+    $CMD &> /tmp/purge-services.log || { echo "Failed to delete service $CLUSTER/$SERVICE"; cat /tmp/purge-service.log; }
   done
 fi
