cfd8aac847cc4e5f6603f366b8d57928b9f15fdb
Matic Jurglič <matic@jurglic.si>
2022-05-24 10:13:21 +0200

    Add getSKU

--- packages/cardpay-cli/prepaid-card-market-v-2/index.ts
+++ packages/cardpay-cli/prepaid-card-market-v-2/index.ts
@@ -2,12 +2,15 @@ import type { Argv } from 'yargs';
 import addTokens from './add-tokens';
 import addSku from './add-sku';
 import getQuantity from './get-quantity';
+import getSKU from './get-sku';
 
 export const command = 'prepaid-card-market-v2 <command>';
 export const desc = 'Commands to interact with prepaid card market';
 
 export const builder = function (yargs: Argv) {
-  return yargs.command([addTokens, addSku, getQuantity] as any).demandCommand(1, 'You must specify a valid subcommand');
+  return yargs
+    .command([addTokens, addSku, getQuantity, getSKU] as any)
+    .demandCommand(1, 'You must specify a valid subcommand');
 };
 
 export function handler(/* argv: Argv */) {
