1b6373e5bc8506b7a8fe57009faf08279ef13957
Ian Calvert <ianjcalvert@gmail.com>
2022-05-24 14:47:39 +0200

    Rename package

--- packages/cardpay-reward-scheduler/reward_scheduler/utils.py
+++ packages/cardpay-reward-scheduler/reward_scheduler/utils.py
@@ -8,7 +8,6 @@ from cloudpathlib import AnyPath, CloudPath
 from cachetools import cached, TTLCache
 
 
-
 def get_local_file(file_location):
     if isinstance(file_location, PosixPath):
         return file_location.as_posix()
@@ -23,10 +22,12 @@ def get_local_file(file_location):
     else:
         raise Exception("Unsupported path type")
 
+
 @cached(TTLCache(maxsize=1000, ttl=60))
 def get_latest_details(config_location):
     with open(AnyPath(config_location) / "latest.yaml", "r") as stream:
         return yaml.safe_load(stream)
+
 
 def get_partition_iterator(min_partition, max_partition, partition_sizes):
     for partition_size in sorted(partition_sizes, reverse=True):
@@ -40,6 +41,7 @@ def get_partition_iterator(min_partition, max_partition, partition_sizes):
             yield partition_size, start_partition, start_partition + partition_size
         if last_max_partition is not None:
             min_partition = last_max_partition
+
 
 def get_partition_files(config_location, table, min_partition=None, max_partition=None):
     # Get config
@@ -59,44 +61,44 @@ def get_partition_files(config_location, table, min_partition=None, max_partitio
     )
     files = []
     for partition_size, start_partition, end_partition in get_partition_iterator(
-        min_partition, latest_block, partition_sizes):
+        min_partition, latest_block, partition_sizes
+    ):
         if start_partition < max_partition:
-            files.append(table_dir.joinpath(
-                f"partition_size={partition_size}",
-                f"start_partition={start_partition}",
-                f"end_partition={end_partition}",
-                "data.parquet",
-            ))
+            files.append(
+                table_dir.joinpath(
+                    f"partition_size={partition_size}",
+                    f"start_partition={start_partition}",
+                    f"end_partition={end_partition}",
+                    "data.parquet",
+                )
+            )
     return files
 
 
 def get_files(config_location, table, min_partition=None, max_partition=None):
-    file_list = get_partition_files(AnyPath(config_location), table, min_partition, max_partition)
+    file_list = get_partition_files(
+        AnyPath(config_location), table, min_partition, max_partition
+    )
     return list(map(get_local_file, file_list))
 
 
 def get_job_definition_for_image(image_name):
-    client = boto3.client('batch')
-    job_definitions = client.describe_job_definitions(
-        maxResults=100,
-        status='ACTIVE'
-    )
-    for job_definition in job_definitions['jobDefinitions']:
-        if job_definition['tags'].get('reward_rule_status') != 'approved':
+    client = boto3.client("batch")
+    job_definitions = client.describe_job_definitions(maxResults=100, status="ACTIVE")
+    for job_definition in job_definitions["jobDefinitions"]:
+        if job_definition["tags"].get("reward_rule_status") != "approved":
             continue
-        if job_definition['containerProperties']['image'] == image_name:
+        if job_definition["containerProperties"]["image"] == image_name:
             return job_definition
 
+
 def run_job(job_definition, parameters_location, output_location):
-    client = boto3.client('batch')
+    client = boto3.client("batch")
     return client.submit_job(
-        jobName='reward_programs_test',
-        jobQueue='reward_programs_batch_job_queue',
-        jobDefinition=job_definition['jobDefinitionArn'],
+        jobName="reward_programs_test",
+        jobQueue="reward_programs_batch_job_queue",
+        jobDefinition=job_definition["jobDefinitionArn"],
         containerOverrides={
-            'command': [
-                parameters_location,
-                output_location
-            ],
-        }
+            "command": [parameters_location, output_location],
+        },
     )
