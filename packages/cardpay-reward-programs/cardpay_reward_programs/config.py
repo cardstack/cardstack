400f3285c11cbf76b7482fa30bbcaa4c00d681a1
tintinthong <justinthong93@gmail.com>
2022-05-24 19:20:19 +0800

    add flat drop

--- packages/cardpay-reward-programs/cardpay_reward_programs/config.py
+++ packages/cardpay-reward-programs/cardpay_reward_programs/config.py
@@ -17,11 +17,15 @@ config = {
     "staging": {
         "tokens": {
             "card": "0xB236ca8DbAB0644ffCD32518eBF4924ba866f7Ee",
-        }
+        },
+        "reward_program": "0x5E4E148baae93424B969a0Ea67FF54c315248BbA",
+        "rewards_bucket": "s3://tally-staging-reward-programs",
     },
     "production": {
         "tokens": {
             "card": "0x52031d287Bb58E26A379A7Fec2c84acB54f54fe3",
-        }
+        },
+        "reward_program": "0x979C9F171fb6e9BC501Aa7eEd71ca8dC27cF1185",
+        "rewards_bucket": "s3://tally-production-reward-programs",
     },
 }
