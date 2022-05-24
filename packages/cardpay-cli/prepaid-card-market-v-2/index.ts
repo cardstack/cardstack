ffa416777de538baf0c2b129861c619e86caa05b
Matic Jurglič <matic@jurglic.si>
2022-05-24 11:15:00 +0200

    Add SetAsk

--- packages/cardpay-cli/prepaid-card-market-v-2/index.ts
+++ packages/cardpay-cli/prepaid-card-market-v-2/index.ts
@@ -3,13 +3,14 @@ import addTokens from './add-tokens';
 import addSku from './add-sku';
 import getQuantity from './get-quantity';
 import getSKU from './get-sku';
+import setAsk from './set-ask';
 
 export const command = 'prepaid-card-market-v2 <command>';
 export const desc = 'Commands to interact with prepaid card market';
 
 export const builder = function (yargs: Argv) {
   return yargs
-    .command([addTokens, addSku, getQuantity, getSKU] as any)
+    .command([addTokens, addSku, getQuantity, getSKU, setAsk] as any)
     .demandCommand(1, 'You must specify a valid subcommand');
 };
 
