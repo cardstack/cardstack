8e7e3244705073f7ac1a0cb7ea62756ca1503ecb
tintinthong <justinthong93@gmail.com>
2022-05-31 15:45:38 +0800

    cleanup flat_drop

--- packages/cardpay-reward-programs/scripts/flat_drop.py
+++ packages/cardpay-reward-programs/scripts/flat_drop.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+import json
 import os
+from enum import Enum
+from typing import Optional
 
+import boto3
 import pyarrow.parquet as pq
+import typer
 from boto3.session import Session
 from cardpay_reward_programs.config import config
 from cardpay_reward_programs.payment_tree import PaymentTree
@@ -14,57 +19,75 @@ from cloudpathlib import AnyPath, S3Client
 from dotenv import load_dotenv
 from web3 import Web3
 
-# ATTENTION!
-# Please ensure EVM_FULL_NODE_URL corresponds to the ENVIRONMENT
 
-load_dotenv()
-cached_client = S3Client(
-    local_cache_dir=".cache",
-    boto3_session=Session(),
-)
-cached_client.set_as_default_client()
-
-reward_amount = 10_000_000_000_000_000_000  # 10 eth
-
-for expected_env in ["EVM_FULL_NODE_URL", "ENVIRONMENT"]:
-    if expected_env not in os.environ:
-        raise ValueError(f"Missing environment variable {expected_env}")
-
-# Make sure evm node corresponds to the environment
-w3 = Web3(Web3.HTTPProvider(os.getenv("EVM_FULL_NODE_URL")))
-env = os.getenv("ENVIRONMENT")
+class Environment(str, Enum):
+    staging = "staging"
+    production = "production"
 
 
-def flat_drop():
+def flat_drop(
+    env: Environment = typer.Argument(
+        default=Environment.staging, help="staging or production"
+    ),
+    parameters_file_path: Optional[str] = typer.Argument(
+        None, help="file path to parameters file"
+    ),
+):
+    reward_amount = 10_000_000_000_000_000_000  # 10 eth
+    default_accounts = ["0x159ADe032073d930E85f95AbBAB9995110c43C71"]
 
-    core_parameters = {
-        "payment_cycle_length": 32768,
-        "start_block": 24000000,
-        "end_block": 26000000,
-        "subgraph_config_locations": {},
-    }  # all core parameters not needed
-    user_defined_parameters = {
-        "reward_per_user": reward_amount,
-        "token": config[env]["tokens"]["card"],
-        "duration": 777600,
-        "accounts": ["0x159ADe032073d930E85f95AbBAB9995110c43C71"],
-    }
-    reward_program_id = config[env]["reward_program"]
-    payment_cycle = w3.eth.get_block_number()  # current block number
-    run = {"reward_program_id": reward_program_id, "payment_cycle": payment_cycle}
-    o = {"core": core_parameters, "user_defined": user_defined_parameters, "run": run}
-    rule = FlatPayment(core_parameters, user_defined_parameters)
-    payment_list = rule.run(payment_cycle, reward_program_id)
+    secrets_client = boto3.client("secretsmanager")
+    cached_client = S3Client(
+        local_cache_dir=".cache",
+        boto3_session=Session(),
+    )
+    cached_client.set_as_default_client()
 
-    rewards_bucket = config[env]["rewards_bucket"]
-    output_location = f"{rewards_bucket}/rewardProgramID={reward_program_id}/paymentCycle={payment_cycle}"
+    evm_full_node_url = secrets_client.get_secret_value(
+        SecretId=f"{env}_evm_full_node_url"
+    )["SecretString"]
+    # Make sure evm node corresponds to the environment
+    w3 = Web3(Web3.HTTPProvider(evm_full_node_url))
 
-    print(
-        f"Writing flat drop of {len(user_defined_parameters['accounts'])} accounts to {output_location}"
+    if parameters_file_path is None:
+
+        params = {
+            "core": {
+                "payment_cycle_length": 32768,
+                "start_block": 24000000,
+                "end_block": 26000000,
+                "subgraph_config_locations": {},
+            },
+            "user_defined": {
+                "reward_per_user": reward_amount,
+                "token": config[env]["tokens"]["card"],
+                "duration": 777600,
+                "accounts": default_accounts,
+            },
+            "run": {
+                "reward_program_id": config[env]["reward_program"],
+                "payment_cycle": w3.eth.get_block_number(),
+            },
+        }
+
+    else:
+        with open(parameters_file_path) as f:
+            params = json.load(f)
+
+    rule = FlatPayment(params["core"], params["user_defined"])
+    payment_list = rule.run(
+        params["run"]["payment_cycle"], params["run"]["payment_cycle"]
     )
     tree = PaymentTree(payment_list.to_dict("records"))
     table = tree.as_arrow()
+
+    rewards_bucket = config[env]["rewards_bucket"]
+    output_location = f"{rewards_bucket}/rewardProgramID={reward_program_id}/paymentCycle={payment_cycle}"
     output_path = AnyPath(output_location)
+
+    print(
+        f"Writing flat drop of {len(params['user_defined']['accounts'])} accounts to {output_location}"
+    )
     if output_path.exists():
         raise Exception(f"{output_location} already exists")
     else:
@@ -74,4 +97,4 @@ def flat_drop():
 
 
 if __name__ == "__main__":
-    flat_drop()
+    typer.run(flat_drop)
