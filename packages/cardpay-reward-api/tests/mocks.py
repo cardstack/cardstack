473d8aa1ff0680c851998a8d34d29a162935a944
tintinthong <justinthong93@gmail.com>
2022-06-02 09:37:20 +0800

    wip

---
+++ packages/cardpay-reward-api/tests/mocks.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+merkle_roots = [
+    {
+        "id": "0x68c1355a6f7b96aaa03ae6085d37f23d04e753b07b8cf4d18e546c0d1565bb63",
+        "blockNumber": "26777606",
+        "paymentCycle": "26777601",
+        "rewardProgram": {"id": "0x5E4E148baae93424B969a0Ea67FF54c315248BbA"},
+        "timestamp": "1653905565",
+    },
+    {
+        "id": "0x7acb8082f540d8e7a3dc283534f09bd50e9ad37eea940441a87b43db3266e026",
+        "blockNumber": "26778059",
+        "paymentCycle": "26778053",
+        "rewardProgram": {"id": "0x5E4E148baae93424B969a0Ea67FF54c315248BbA"},
+        "timestamp": "1653912015",
+    },
+    {
+        "id": "0x5cb1bb27201e79b9dc6a95c86f7ebc6bac9fdd0974daf3c74829e0218dc64e0b",
+        "blockNumber": "26779083",
+        "paymentCycle": "26779078",
+        "rewardProgram": {"id": "0x5E4E148baae93424B969a0Ea67FF54c315248BbA"},
+        "timestamp": "1653927315",
+    },
+]
+
+reward_programs = [
+    {"id": "0x0A4c62c8616342Fc3C5390c4e55aD26DeE694b0F"},
+    {"id": "0x2F57D4cf81c87A92dd5f0686fEc6e02887662d07"},
+    {"id": "0x5E4E148baae93424B969a0Ea67FF54c315248BbA"},
+    {"id": "0x64D65d17B26312c0a2532E3cfB0a681A92eEf89d"},
+    {"id": "0x73F92405b438D85ee46539a8FaDbBCa04C155F81"},
+    {"id": "0x954A9C27bC398B4A8f6F543b13eea833d0e73308"},
+    {"id": "0xd40c4b61D0B08548Dd1E2b79c1E8Ad98f15c65d8"},
+]
