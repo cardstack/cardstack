5d017732480b798e01fa9a4b2275fbbaad73bc29
Buck Doyle <buck.doyle@cardstack.com>
2022-06-06 13:47:01 -0500

    ssr/web: Change application global to be exported (#3000)
    This overrides exportApplicationGlobal to always be true vs the default
    which disables it in production builds. This will facilitate debugging
    in deployed builds. It also adds a warning displayed in the developer
    tools to counterbalance the easier access to sensitive data this change
    facilitates.

--- packages/web-client/config/environment.js
+++ packages/web-client/config/environment.js
@@ -37,6 +37,7 @@ module.exports = function (environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+    exportApplicationGlobal: true,
     hubURL: process.env.HUB_URL,
     universalLinkDomain:
       universalLinkHostnamesByTarget[deployTargetClass] ??
