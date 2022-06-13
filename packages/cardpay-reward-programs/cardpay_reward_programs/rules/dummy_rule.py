34675f16c0a3bd4c552b45f2eb8dcc4be2e067bf
Paulin Alcoser <50881828+pac469@users.noreply.github.com>
2022-06-13 09:00:40 -0400

    final modification for dummy rule

--- packages/cardpay-reward-programs/cardpay_reward_programs/rules/dummy_rule.py
+++ packages/cardpay-reward-programs/cardpay_reward_programs/rules/dummy_rule.py
@@ -22,28 +22,36 @@ class DummyRule(Rule):
         self.base_reward = base_reward
         self.duration = duration
         
-    def sql(self, table_query):
+    def sql(self, table_query): #THIS FUNCTION IS TO COMPLY WITH THE ABSTRACT METHODS OF rule.py BUT IT IS TEMPORAL
         return f"""
             select merchant_safe as payee from {table_query} limit 10;
         """
 
-    # def sql_2(self, table_query, table_query2):
-    #     return f"""
-    #         select merchant_safe as payee, _block_number
-	#              from {table_query}
-	#              where _block_number > $1::integer
-	#              and _block_number < $2::integer
-	#             and historic_spend_balance_uint64 > 100000
-    #     """
-    
-    def sql_2(self, table_query, table_query2):  #table_query = safe_owner, table_query2 = spend_accumulation
-        #Refactor this query select and check join vs in sql 
-        # how do I refactor sgd1 
-        # Why coma instead of join
+    def sql_2(self, tables_names): 
+        
+        spend_accumulation_table = self._get_table_query(
+            tables_names[0], 
+            tables_names[0], 
+            self.start_block,
+            self.end_block
+        )
+
+        safe_owner_table = self._get_table_query(
+            tables_names[1], 
+            tables_names[1],
+            self.start_block,
+            self.end_block
+        )
+
+        #If any of the tables is empty return a none string
+        if safe_owner_table == "parquet_scan([])" or spend_accumulation_table == "parquet_scan([])":
+            return None
+        
+     
         return f""" 
             with merchant_safes as (
  	            select merchant_safe, _block_number
-	            from {table_query}
+	            from {spend_accumulation_table}
 	            where _block_number > $1::integer
 	            and _block_number < $2::integer
 	            and historic_spend_balance_uint64 > 100000
@@ -54,7 +62,7 @@ class DummyRule(Rule):
                 from merchant_safes
                 group by 1      
             )
-            select safe.owner as payee from {table_query2} as safe, 
+            select safe.owner as payee from {safe_owner_table} as safe, 
                 final_safes as f_s
                 where f_s.merchant_safe = safe.safe;
         """
@@ -63,34 +71,27 @@ class DummyRule(Rule):
         vars = [
             self.start_block,
             self.end_block
-            # 17265697,
-            # 21986688
         ]
 
-        spend_accumulation_table = self._get_table_query(
-            "spend_accumulation", 
-            "spend_accumulation", 
-            self.start_block,
-            self.end_block
-        )
+        # spend_accumulation_table = self._get_table_query(
+        #     "spend_accumulation", 
+        #     "spend_accumulation", 
+        #     self.start_block,
+        #     self.end_block
+        # )
         
-        safe_owner_table = self._get_table_query(
-            "safe_owner", 
-            "safe_owner",
-            self.start_block,
-            self.end_block
-        )
+        # safe_owner_table = self._get_table_query(
+        #     "safe_owner", 
+        #     "safe_owner",
+        #     self.start_block,
+        #     self.end_block
+        # )
+
+        tables_names = ["spend_accumulation", "safe_owner"]
         
-        if safe_owner_table == "parquet_scan([])" or spend_accumulation_table == "parquet_scan([])":
-            df = pd.DataFrame(columns=[])
-            print("file empty!")
-        else:
-            # df = self.run_query(table_query, vars)
-            con = duckdb.connect(database=":memory:", read_only=False) 
-            con.execute(self.sql_2(spend_accumulation_table, safe_owner_table), vars)
-            df = con.fetchdf()
-
-
+        
+        df = self.run_query(tables_names, vars)
+           
         df["rewardProgramID"] = reward_program_id
         df["paymentCycle"] = payment_cycle
         df["validFrom"] = payment_cycle
@@ -98,6 +99,15 @@ class DummyRule(Rule):
         df["token"] = self.token
         df["amount"] = self.base_reward
 
-        print(df)
 
+        return df
+
+    def run_query(self,tables_names, vars):
+        query_string = self.sql_2(tables_names)
+        if query_string == None:
+            df = pd.DataFrame(columns=[])
+        else:
+            con = duckdb.connect(database=":memory:", read_only=False) 
+            con.execute(query_string, vars)
+            df = con.fetchdf()
         return df
