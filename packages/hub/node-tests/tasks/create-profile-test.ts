322d5b2964a9046dd153c59a734a471279921758
Buck Doyle <buck.doyle@cardstack.com>
2022-06-14 12:31:47 -0600

    Add preliminary call to relay server

--- packages/hub/node-tests/tasks/create-profile-test.ts
+++ packages/hub/node-tests/tasks/create-profile-test.ts
@@ -17,7 +17,28 @@ class StubWorkerClient {
 describe('CreateProfileTask', function () {
   let subject: CreateProfile;
 
+  let registeredAddress = '0x123';
+  let registeredDid = 'sku';
+  let mockTxnHash = '0x456';
+  let registerProfileCalls = 0;
+  let registeringShouldError = false;
+
+  class StubRelayService {
+    async registerProfile(userAddress: string, did: string) {
+      registerProfileCalls++;
+
+      if (registeringShouldError) {
+        throw new Error('provisioning should error');
+      }
+
+      registeredAddress = userAddress;
+      registeredDid = did;
+      return Promise.resolve(mockTxnHash);
+    }
+  }
+
   this.beforeEach(function () {
+    registry(this).register('relay', StubRelayService, { type: 'service' });
     registry(this).register('worker-client', StubWorkerClient);
   });
 
@@ -32,8 +53,16 @@ describe('CreateProfileTask', function () {
     subject = (await getContainer().lookup('create-profile')) as CreateProfile;
   });
 
-  it('queues persist-off-chain-merchant-info', async function () {
-    await subject.perform({});
+  it('calls the relay server endpoint to register a profile and queues persist-off-chain-merchant-info', async function () {
+    await subject.perform({
+      'merchant-infos': {
+        'owner-address': '0x000',
+      },
+    });
+
+    expect(registerProfileCalls).to.equal(1);
+    expect(registeredAddress).to.equal('0x000');
+    expect(registeredDid).to.equal('fixme');
 
     expect(addedJobIdentifiers[0]).to.equal('persist-off-chain-merchant-info');
     expect(addedJobPayloads[0]).to.deep.equal({ 'merchant-safe-id': 'fixme' });
--- packages/hub/tasks/create-profile.ts
+++ packages/hub/tasks/create-profile.ts
@@ -1,9 +1,17 @@
 import { inject } from '@cardstack/di';
+import { service } from '@cardstack/hub/services';
 
 export default class CreateProfile {
+  relay = service('relay');
   workerClient = inject('worker-client', { as: 'workerClient' });
 
-  async perform(_payload: any) {
+  async perform({
+    'merchant-infos': { 'owner-address': ownerAddress },
+  }: {
+    'merchant-infos': { 'owner-address': string };
+  }) {
+    await this.relay.registerProfile(ownerAddress, 'fixme');
+
     this.workerClient.addJob('persist-off-chain-merchant-info', { 'merchant-safe-id': 'fixme' });
   }
 }
