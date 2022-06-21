2220deb4f9b6d548cc7b6b8e6d2d4fa1ece5ad79
pcjun97 <44688367+pcjun97@users.noreply.github.com>
2022-06-21 21:45:29 +0800

    ci: Include inline policies for waypoint secrets access verification (#3033)
    * ci: Inclide inline policies for waypoint secrets access verification
    
    * Format waypoint.hcl to trigger workflow
    
    * Fix task role checking
    
    * Fix typo
    
    * Fix unintended change

--- scripts/verify-waypoint-secrets.ts
+++ scripts/verify-waypoint-secrets.ts
@@ -20,29 +20,47 @@ for (const appSlug of appSlugs) {
   if (appConfig.secretArns.length === 0) {
     continue;
   }
-  let allowedSecretArns = queryPolicyForAllowedSecretAccess(appConfig);
-  for (const neededSecretArn of appConfig.secretArns) {
-    if (!allowedSecretArns.includes(neededSecretArn)) {
+
+  let valid = true;
+  let allowedSecretArns = [];
+
+  try {
+    allowedSecretArns = [
+      ...queryAttachedPoliciesForAllowedSecretAccess(appConfig),
+      ...queryRolePoliciesForAllowedSecretAccess(appConfig),
+    ];
+  } catch (err) {
+    errors.push({ app: appSlug, error: err.message });
+    valid = false;
+  }
+
+  if (valid) {
+    let missingSecretArns = [];
+    for (const neededSecretArn of appConfig.secretArns) {
+      if (!allowedSecretArns.includes(neededSecretArn)) {
+        missingSecretArns.push(neededSecretArn);
+      }
+    }
+
+    if (missingSecretArns.length > 0) {
+      valid = false;
       errors.push({
         app: appSlug,
-        missingSecretArn: neededSecretArn,
+        error: `The role '${
+          appConfig.executionRoleName
+        }' is missing access to the following secrets:\n${missingSecretArns.join('\n')}\n`,
       });
     }
   }
+
+  console.log(`${valid ? '✓' : '✗'} ${appSlug}`);
 }
 
 if (errors.length > 0) {
-  let errorMessage = 'Task execution roles do not have permission to read required secrets:\n\n';
-  errorMessage += errors.map(
-    (e) => `
-  app: ${e.app}
-  secretArn: ${e.missingSecretArn}
-
-`
-  );
-  console.log(errorMessage);
-  throw new Error(errorMessage);
+  errors.forEach((error) => console.error(`app: ${error.app}\nerror: ${error.error}`));
+  throw new Error(errors.join('\n'));
 }
+
 console.log(`Task policy access to secrets configured in ${waypointConfigFile} has been confirmed.`);
 
 function parseWaypointConfig(waypointConfigFile): RelevantWaypointConfigByApp {
@@ -62,20 +80,45 @@ function parseWaypointConfig(waypointConfigFile): RelevantWaypointConfigByApp {
 }
 
 function execute(command, options) {
-  // console.log(`executing: ${command}`);
   return execSync(command, options ?? {})
     .toString()
     .trim();
 }
 
-function queryPolicyForAllowedSecretAccess(appConfig: RelevantWaypointConfig) {
+function queryAttachedPoliciesForAllowedSecretAccess(appConfig: RelevantWaypointConfig) {
   const policiesCmd = `aws iam list-attached-role-policies --role-name ${appConfig.executionRoleName} | grep PolicyArn | grep secrets | awk '{ print $2 }' | sed 's/[",]//g'`;
   let secretsPolicyArn = execute(policiesCmd, { env: { ...process.env, PAGER: '' } });
+
+  if (secretsPolicyArn == '') return [];
 
   const policyVersionsCmd = `aws iam list-policy-versions --policy-arn ${secretsPolicyArn} --query 'Versions[?IsDefaultVersion].VersionId' | grep v | awk '{ print $1 }' | sed 's/[",]//g'`;
   let defaultVersion = execute(policyVersionsCmd, { env: { ...process.env, PAGER: '' } });
 
   const policyDocumentCmd = `aws iam get-policy-version --policy-arn ${secretsPolicyArn} --version-id ${defaultVersion} --query 'PolicyVersion.Document.Statement[0].Resource'`;
   let allowedSecretArns = execute(policyDocumentCmd, { env: { ...process.env, PAGER: '' } });
+
+  return JSON.parse(allowedSecretArns);
+}
+
+function queryRolePoliciesForAllowedSecretAccess(appConfig: RelevantWaypointConfig) {
+  let allowedSecretArns = [];
+
+  const policiesCmd = `aws iam list-role-policies --role-name ${appConfig.executionRoleName} --query 'PolicyNames'`;
+  const policiesNames = JSON.parse(execute(policiesCmd, { env: { ...process.env, PAGER: '' } }));
+
+  for (const policyName of policiesNames) {
+    const statementsCmd = `aws iam get-role-policy --role-name ${appConfig.executionRoleName} --policy-name ${policyName} --query 'PolicyDocument.Statement'`;
+    const statements = JSON.parse(execute(statementsCmd, { env: { ...process.env, PAGER: '' } }));
+    for (const statement of statements) {
+      if (
+        statement.Action.includes('ssm:GetParameters') &&
+        statement.Action.includes('secretsmanager:GetSecretValue')
+      ) {
+        const arnRegex = new RegExp('arn:aws:secretsmanager:[^:]+:[^:]+:secret:[^:]+');
+        allowedSecretArns = allowedSecretArns.concat(statement.Resource.filter((resource) => arnRegex.test(resource)));
+      }
+    }
+  }
+
   return allowedSecretArns;
 }
