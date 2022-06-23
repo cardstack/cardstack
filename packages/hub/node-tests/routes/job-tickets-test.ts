9dbc2d582fe5168e4d484cfd80f51fcd81e9d477
Buck Doyle <buck.doyle@cardstack.com>
2022-06-23 10:44:43 -0500

    hub: Add retry endpoint for job tickets (#3045)
    POST /api/job-tickets/:id/retry queues a duplicate of the retried job
    and returns the id and attributes of the new job ticket for that job.
    
    The CreateProfile job is intended to be queued with spec maxAttempts: 1,
    so this adds a job_tickets.spec column to preserve the spec a job was
    started with so it can be used again when retrying.

--- packages/hub/node-tests/routes/job-tickets-test.ts
+++ packages/hub/node-tests/routes/job-tickets-test.ts
@@ -1,5 +1,7 @@
 import { registry, setupHub } from '../helpers/server';
 import shortUUID from 'short-uuid';
+import JobTicketsQueries from '../../queries/job-tickets';
+import { setupStubWorkerClient } from '../helpers/stub-worker-client';
 
 class StubAuthenticationUtils {
   validateAuthToken(encryptedAuthToken: string) {
@@ -99,5 +101,147 @@ describe('GET /api/job-tickets/:id', function () {
         ],
       })
       .expect('Content-Type', 'application/vnd.api+json');
+  });
+});
+
+describe('POST /api/job-tickets/:id/retry', function () {
+  let { getJobIdentifiers, getJobPayloads, getJobSpecs } = setupStubWorkerClient(this);
+
+  this.beforeEach(function () {
+    registry(this).register('authentication-utils', StubAuthenticationUtils);
+  });
+
+  let { request, getContainer } = setupHub(this);
+  let jobTicketsQueries: JobTicketsQueries,
+    jobTicketId: string,
+    otherOwnerJobTicketId: string,
+    notFailedJobTicketId: string;
+
+  this.beforeEach(async function () {
+    jobTicketsQueries = await getContainer().lookup('job-tickets', { type: 'query' });
+
+    jobTicketId = shortUUID.uuid();
+    await jobTicketsQueries.insert({
+      id: jobTicketId,
+      jobType: 'a-job',
+      ownerAddress: stubUserAddress,
+      payload: { 'a-payload': 'yes' },
+      spec: { 'a-spec': 'yes' },
+    });
+    await jobTicketsQueries.update(jobTicketId, { 'a-result': 'yes' }, 'failed');
+
+    otherOwnerJobTicketId = shortUUID.uuid();
+    await jobTicketsQueries.insert({ id: otherOwnerJobTicketId, jobType: 'a-job', ownerAddress: '0x111' });
+
+    notFailedJobTicketId = shortUUID.uuid();
+    await jobTicketsQueries.insert({ id: notFailedJobTicketId, jobType: 'a-job', ownerAddress: stubUserAddress });
+  });
+
+  it('adds a new job, adds a job ticket for it, and returns its details', async function () {
+    let newJobId: string | undefined;
+
+    await request()
+      .post(`/api/job-tickets/${jobTicketId}/retry`)
+      .set('Authorization', 'Bearer abc123--def456--ghi789')
+      .set('Content-Type', 'application/vnd.api+json')
+      .expect(201)
+      .expect(function (res) {
+        newJobId = res.body.data.id;
+
+        expect(res.body.data.id).to.not.equal(jobTicketId);
+        expect(res.body.data.attributes).to.deep.equal({
+          state: 'pending',
+        });
+      })
+      .expect('Content-Type', 'application/vnd.api+json');
+
+    expect(getJobIdentifiers()).to.deep.equal(['a-job']);
+    expect(getJobPayloads()).to.deep.equal([{ 'a-payload': 'yes' }]);
+    expect(getJobSpecs()).to.deep.equal([{ 'a-spec': 'yes' }]);
+
+    let newTicket = await jobTicketsQueries.find(newJobId!);
+
+    expect(newTicket?.ownerAddress).to.equal(stubUserAddress);
+    expect(newTicket?.jobType).to.equal('a-job');
+    expect(newTicket?.payload).to.deep.equal({ 'a-payload': 'yes' });
+    expect(newTicket?.spec).to.deep.equal({ 'a-spec': 'yes' });
+  });
+
+  it('returns 401 without bearer token', async function () {
+    await request()
+      .post(`/api/job-tickets/${jobTicketId}/retry`)
+      .set('Content-Type', 'application/vnd.api+json')
+      .expect(401)
+      .expect({
+        errors: [
+          {
+            status: '401',
+            title: 'No valid auth token',
+          },
+        ],
+      })
+      .expect('Content-Type', 'application/vnd.api+json');
+
+    expect(getJobIdentifiers()).to.be.empty;
+  });
+
+  it('returns 401 when the job ticket requested is for a different owner', async function () {
+    await request()
+      .post(`/api/job-tickets/${otherOwnerJobTicketId}/retry`)
+      .set('Authorization', 'Bearer abc123--def456--ghi789')
+      .set('Content-Type', 'application/vnd.api+json')
+      .expect(401)
+      .expect({
+        errors: [
+          {
+            status: '401',
+            title: 'No valid auth token',
+          },
+        ],
+      })
+      .expect('Content-Type', 'application/vnd.api+json');
+
+    expect(getJobIdentifiers()).to.be.empty;
+  });
+
+  it('returns 422 when the job ticket state is not failed', async function () {
+    await request()
+      .post(`/api/job-tickets/${notFailedJobTicketId}/retry`)
+      .set('Authorization', 'Bearer abc123--def456--ghi789')
+      .set('Content-Type', 'application/vnd.api+json')
+      .expect(422)
+      .expect({
+        errors: [
+          {
+            status: '422',
+            title: 'Can only retry a job with failed state (current state: pending)',
+          },
+        ],
+      })
+      .expect('Content-Type', 'application/vnd.api+json');
+
+    expect(getJobIdentifiers()).to.be.empty;
+  });
+
+  it('returns 404 for an unknown job', async function () {
+    let randomId = shortUUID.uuid();
+
+    await request()
+      .post(`/api/job-tickets/${randomId}/retry`)
+      .set('Authorization', 'Bearer abc123--def456--ghi789')
+      .set('Content-Type', 'application/vnd.api+json')
+      .expect(404)
+      .expect({
+        errors: [
+          {
+            status: '404',
+            title: 'Job ticket not found',
+            detail: `Could not find the job ticket ${randomId}`,
+          },
+        ],
+      })
+      .expect('Content-Type', 'application/vnd.api+json');
+
+    expect(getJobIdentifiers()).to.be.empty;
   });
 });
