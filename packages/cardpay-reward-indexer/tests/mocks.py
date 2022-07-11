b584effdb0c74286dcece6b6bfafce1304288293
JT <justinthong93@gmail.com>
2022-07-11 13:46:22 +0100

    Add payee root to subgraph and indexer (#3055)
    * add rootHash
    
    * add rootHash to indexer (not transactionHash)
    
    * add root hashes
    
    * fix typo
    
    * add root hash
    
    * remove fixture file

--- packages/cardpay-reward-indexer/tests/mocks.py
+++ packages/cardpay-reward-indexer/tests/mocks.py
@@ -7,6 +7,7 @@ merkle_roots = [
         "paymentCycle": "26777325",
         "rewardProgram": {"id": "0x5E4E148baae93424B969a0Ea67FF54c315248BbA"},
         "timestamp": "1653901895",
+         "rootHash": "0x9f880d8e63c4dca5f33fce867a36e988afbf41ed9f1ab0e08387b1bc76921173",
     },
     {
         "id": "0x68c1355a6f7b96aaa03ae6085d37f23d04e753b07b8cf4d18e546c0d1565bb63",
@@ -14,6 +15,7 @@ merkle_roots = [
         "paymentCycle": "26777601",
         "rewardProgram": {"id": "0x5E4E148baae93424B969a0Ea67FF54c315248BbA"},
         "timestamp": "1653905565",
+        "rootHash": "0x3b4096bd5a350c9469089b781e7ee0c7e57a08749b267c770604a14e80f835b2"
     },
     {
         "id": "0x7acb8082f540d8e7a3dc283534f09bd50e9ad37eea940441a87b43db3266e026",
@@ -21,6 +23,7 @@ merkle_roots = [
         "paymentCycle": "26778053",
         "rewardProgram": {"id": "0x5E4E148baae93424B969a0Ea67FF54c315248BbA"},
         "timestamp": "1653912015",
+        "rootHash": "0xee9c501af460fa65d059d64f415969cfa954413158af14c9e6a6e2f4e97d7d45"
     },
 ]
 
@@ -31,6 +34,7 @@ extra_one_merkle_roots = [
         "paymentCycle": "26779078",
         "rewardProgram": {"id": "0x5E4E148baae93424B969a0Ea67FF54c315248BbA"},
         "timestamp": "1653927315",
+        "rootHash": "0x7454609cd9e953122ea8c0dbeafb345d7b7bfb72121636341aa1e25387a9ae02",
     },
     {
         "id": "0x58c510692db536c594237b69c9f3826df7de51b2162cd85c5ce241be2a874a7b",
@@ -38,6 +42,7 @@ extra_one_merkle_roots = [
         "paymentCycle": "26779104",
         "rewardProgram": {"id": "0x5E4E148baae93424B969a0Ea67FF54c315248BbA"},
         "timestamp": "1653927740",
+        "rootHash": "0xd02399618b7a2bed6290a506605f51659c0de1889f2f271ff66fdcab3e048531",
     },
 ]
 
@@ -48,16 +53,18 @@ extra_one_merkle_roots_without_s3 = [
         "paymentCycle": "27000000",
         "rewardProgram": {"id": "0x5E4E148baae93424B969a0Ea67FF54c315248BbA"},
         "timestamp": "1663927740",
+        "rootHash": "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
     },
 ]
 
 extra_one_merkle_roots_old_file = [
     {
         "id": "0x8c7ce5f0740ab9003ed71008ba516421ff337590ca2805e4275897b4674ee598",
-        "blockNumber": "26779206",
-        "paymentCycle": "26779200",
+        "blockNumber": "27000001",
+        "paymentCycle": "27000000",
         "rewardProgram": {"id": "0x5E4E148baae93424B969a0Ea67FF54c315248BbA"},
         "timestamp": "1663927740",
+        "rootHash": "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
     },
 ]
 
@@ -68,6 +75,7 @@ other_merkle_roots = [
         "paymentCycle": "26722587",
         "rewardProgram": {"id": "0x2F57D4cf81c87A92dd5f0686fEc6e02887662d07"},
         "timestamp": "1653387365",
+        "rootHash": "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
     }
 ]
 
