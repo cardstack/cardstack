5190be1c010ac2a46196004eee662dca0104200f
Luke Melia <luke@lukemelia.com>
2022-07-20 12:21:12 -0400

    Update ember-cli-deprecation-workflow

--- packages/web-client/config/deprecation-workflow.js
+++ packages/web-client/config/deprecation-workflow.js
@@ -1,8 +1,24 @@
 /*global self */
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
+  throwOnUnhandled: true,
   workflow: [
-    { handler: 'silence', matchId: 'ember-source.deprecation-without-for' },
-    { handler: 'silence', matchId: 'ember-source.deprecation-without-since' },
+    {
+      handler: 'silence',
+      matchId: 'deprecated-run-loop-and-computed-dot-access',
+    },
+    { handler: 'silence', matchId: 'routing.transition-methods' },
+    {
+      handler: 'silence',
+      matchId: 'argument-less-helper-paren-less-invocation',
+    },
+    { handler: 'silence', matchId: 'ember-modifier.use-destroyables' },
+    { handler: 'silence', matchId: 'ember-modifier.use-modify' },
+    { handler: 'silence', matchId: 'ember-modifier.no-args-property' },
+    { handler: 'silence', matchId: 'ember-modifier.no-element-property' },
+    { handler: 'silence', matchId: 'ember-modifier.function-based-options' },
+    { handler: 'silence', matchId: 'setting-on-hash' },
+    { handler: 'silence', matchId: 'ensure-safe-component.string' },
+    { handler: 'silence', matchId: 'autotracking.mutation-after-consumption' },
   ],
 };
