5f8823629d66d8f9c6d3097f408c729f2af62f81
Buck Doyle <buck.doyle@cardstack.com>
2022-07-21 10:01:17 -0500

    hub: Fix Prisma warning about excess instances in tests (#3092)
    This updates the Prisma service to construct PrismaClient when the
    service is loaded instead of instantiated. This fixes a warning
    discussed at the link below. The previous implementation would construct
    a new Prisma client for each test, which would quickly exceed the
    database connexion limit.
    
    https://www.prisma.io/docs/concepts/components/prisma-client/working-with-prismaclient/instantiate-prisma-client#use-a-single-shared-instance-of-prismaclient

--- packages/hub/services/prisma-manager.ts
+++ packages/hub/services/prisma-manager.ts
@@ -7,51 +7,60 @@ import {
 } from './prisma-extensions/push-notification-registration';
 import { ExtendedLatestEventBlock, getLatestEventBlockExtension } from './prisma-extensions/latest-event-block';
 import { ExtendedExchangeRate, getExchangeRateExtension } from './prisma-extensions/exchange-rate';
+
 export interface ExtendedPrismaClient extends PrismaClient {
   exchangeRate: ExtendedExchangeRate;
   pushNotificationRegistration: ExtendedPushNotificationRegistration;
   latestEventBlock: ExtendedLatestEventBlock;
 }
 
+let dbConfig: Record<string, any> = config.get('db');
+
+// Prisma client should be a singleton to avoid this problem:
+// https://www.prisma.io/docs/concepts/components/prisma-client/working-with-prismaclient/instantiate-prisma-client#the-number-of-prismaclient-instances-matters
+
+let singletonClient = new PrismaClient({
+  datasources: { db: { url: dbConfig.url } },
+  log: dbConfig.prismaLog,
+});
+
 export default class PrismaManager {
-  private client?: ExtendedPrismaClient;
   private prismaTestingHelper?: PrismaTestingHelper<PrismaClient>;
 
-  dbConfig: Record<string, any> = config.get('db');
+  async ready() {
+    // In tests, do not add extensions until the in-transaction proxy client exists
+
+    if (!dbConfig.useTransactionalRollbacks) {
+      addCardstackPrismaExtensions(singletonClient);
+    }
+  }
 
   async getClient() {
-    if (!this.client) {
-      let client = new PrismaClient({
-        datasources: { db: { url: this.dbConfig.url } },
-        log: this.dbConfig.prismaLog,
-      });
-
-      if (this.dbConfig.useTransactionalRollbacks) {
-        this.prismaTestingHelper = new PrismaTestingHelper(client);
+    if (dbConfig.useTransactionalRollbacks) {
+      // Set up transactional test helper with extensions if it doesn’t yet exist
+      if (!this.prismaTestingHelper) {
+        this.prismaTestingHelper = new PrismaTestingHelper(singletonClient);
         await this.prismaTestingHelper.startNewTransaction();
-        client = this.prismaTestingHelper.getProxyClient();
+
+        let proxyClientToExtend = this.prismaTestingHelper.getProxyClient() as ExtendedPrismaClient;
+        addCardstackPrismaExtensions(proxyClientToExtend);
       }
 
-      this.addCardstackPrismaExtensions(client);
-
-      this.client = client as ExtendedPrismaClient;
+      return this.prismaTestingHelper.getProxyClient() as ExtendedPrismaClient;
     }
 
-    return this.client;
+    return singletonClient as ExtendedPrismaClient;
   }
 
   async teardown() {
     this.prismaTestingHelper?.rollbackCurrentTransaction();
-    // TODO CS-4254
-    // warn(prisma-client) There are already 10 instances of Prisma Client actively running.
-    return this.client?.$disconnect();
   }
+}
 
-  private addCardstackPrismaExtensions(client: PrismaClient) {
-    Object.assign(client.exchangeRate, getExchangeRateExtension(client));
-    Object.assign(client.pushNotificationRegistration, getPushNotificationRegistrationExtension(client));
-    Object.assign(client.latestEventBlock, getLatestEventBlockExtension(client));
-  }
+function addCardstackPrismaExtensions(client: PrismaClient) {
+  Object.assign(client.exchangeRate, getExchangeRateExtension(client));
+  Object.assign(client.pushNotificationRegistration, getPushNotificationRegistrationExtension(client));
+  Object.assign(client.latestEventBlock, getLatestEventBlockExtension(client));
 }
 
 declare module '@cardstack/di' {
