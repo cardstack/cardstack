9d9227b3332ddeeed97802fa1bbe42b086d56193
Paulin Alcoser <50881828+pac469@users.noreply.github.com>
2022-07-27 08:54:07 -0400

    Issue with hypothesis positional parameters

--- packages/cardpay-reward-programs/tests/rules/test_staking.py
+++ packages/cardpay-reward-programs/tests/rules/test_staking.py
@@ -1,11 +1,32 @@
 import pytest
 import pandas as pd
 import duckdb
-from cardpay_reward_programs.rules import safe_ownership, staking
 import math
+import hypothesis.strategies as st
+import hypothesis.strategies as tuples
+from cardpay_reward_programs.rules import safe_ownership, staking
+from hypothesis import given
+from hypothesis.extra.pandas import data_frames, columns
 
 token_holder_table = "_TOKEN_HOLDER"
 safe_ownership_table = "_SAFE_OWNERSHIP"
+token_holder_columns = ["_block_number", "token", "safe", "balance_uint64"]
+token_holder_df = data_frames(columns = columns(token_holder_columns, dtype = str),
+                                rows = st.tuples(
+                                    st.integers(min_value = 1000000, max_value = 18446744073709551615),
+                                    st.from_regex("card\-[0-9]", fullmatch = True),
+                                    st.from_regex("safe\-[0-9]", fullmatch = True),
+                                    st.floats(min_value = 22995968, max_value = 24032768)
+                                )
+)
+safe_owner_columns = ["safe", "owner"]
+safe_owner_df = data_frames(columns = columns(safe_owner_columns, dtype = str),
+                            rows = st.tuples(
+                                st.from_regex("owner\-[0-9]", fullmatch = True),
+                                st.from_regex("safe\-[0-9]", fullmatch = True),
+                            )
+                            
+                )
 
 def create_rule(
     monkeypatch, fake_data_token_holder, fake_data_safe_owner, core_config_overrides={}, user_config_overrides={}
@@ -272,7 +293,7 @@ def test_correctly_manages_first_deposit_in_cycle(monkeypatch):
     assert pytest.approx(get_amount(result, "owner1", 0)) == 120
 
 
-def test_correct_calc_rewards_in_cycle(monkeypatch):
+def test_correct_calc_rewards_in_cycle(monkeypatch, i = 0, j = 0):
     fake_data_token_holder = pd.DataFrame([
         {
             "_block_number": 0,
@@ -296,27 +317,13 @@ def test_correct_calc_rewards_in_cycle(monkeypatch):
     
     assert pytest.approx(get_amount(result, "owner1", 0)) == 60
 
-def max_balance(monkeypatch):
-    fake_data_token_holder = pd.DataFrame([
-        {
-            "_block_number": 0,
-            "token": "card",
-            "safe": "safe1",
-            "balance_uint64": 18446744073709551615
-        }
-    ])
+@given(token_holder_df, safe_owner_df)
+def test_hypothesis(token_holder_df, safe_owner_df):
+    assert len(token_holder_df) > -1 
 
-    fake_data_safe_owner = pd.DataFrame([
-        {
-            "safe":"safe1",
-            "owner":"owner1"
-        }
-    ])
+@given(st.integers(), st.integers())
+def test_b(x, y, monkeypatch):
+    print(x, y)
+    assert 0 == 0
+
     
-    rule = create_rule(
-        monkeypatch, fake_data_token_holder, fake_data_safe_owner
-    )
-
-    result = rule.run(30, "0x0")
-    expected_value = 1.1068 * (10**18)
-    assert get_amount(result, "owner1", 0) - expected_value < 4619683561473
