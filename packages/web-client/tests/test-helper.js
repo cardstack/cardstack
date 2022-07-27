fa6b0d2ac5b1167a09bffc8ca7fd57257e8ea58f
Buck Doyle <buck.doyle@cardstack.com>
2022-07-27 15:14:46 -0500

    Add workaround for router/visit bug with query

--- packages/web-client/tests/test-helper.js
+++ packages/web-client/tests/test-helper.js
@@ -1,7 +1,7 @@
 import Application from '@cardstack/web-client/app';
 import config from '@cardstack/web-client/config/environment';
 import * as QUnit from 'qunit';
-import { setApplication } from '@ember/test-helpers';
+import { setApplication, visit } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 import setupSinon from 'ember-sinon-qunit';
@@ -13,3 +13,8 @@ setup(QUnit.assert);
 setupSinon();
 
 start();
+
+export async function visitWithQueryFix(url) {
+  await visit('/');
+  await visit(url);
+}
