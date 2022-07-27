1d71f2f59d130e7973d3f9ca69069d8ec2b05173
Luke Melia <luke@lukemelia.com>
2022-07-27 11:10:42 -0700

    Eliminate ember-global deprecation from boxel

--- packages/boxel/tests/dummy/config/deprecation-workflow.js
+++ packages/boxel/tests/dummy/config/deprecation-workflow.js
@@ -3,7 +3,7 @@ self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   throwOnUnhandled: true,
   workflow: [
-    { handler: 'silence', matchId: 'ember-global' },
+    { handler: 'throw', matchId: 'ember-global' },
     { handler: 'silence', matchId: 'ember-modifier.use-modify' },
     { handler: 'silence', matchId: 'ember-modifier.function-based-options' },
     { handler: 'silence', matchId: 'ember-modifier.no-args-property' },
