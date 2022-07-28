b298fdd4bb3905def4911c26ba2d3ff1b8dc41da
Paulin Alcoser <50881828+pac469@users.noreply.github.com>
2022-07-28 15:42:22 -0400

    negative rewards test staking

--- packages/cardpay-reward-programs/tests/rules/test_staking.py
+++ packages/cardpay-reward-programs/tests/rules/test_staking.py
@@ -1,3 +1,5 @@
+from re import M
+from tkinter import END
 import pytest
 import pandas as pd
 import duckdb
@@ -5,36 +7,52 @@ import math
 import hypothesis.strategies as st
 import hypothesis.strategies as tuples
 from cardpay_reward_programs.rules import safe_ownership, staking
-from hypothesis import given
-from hypothesis.extra.pandas import data_frames, columns
+from hypothesis import given, settings
+from hypothesis.extra.pandas import data_frames, columns, range_indexes, column
+from pytest import MonkeyPatch
 
+START_BLOCK = 30
+END_BLOCK = 60
+CYCLE_LENGTH = 30
 token_holder_table = "_TOKEN_HOLDER"
-safe_ownership_table = "_SAFE_OWNERSHIP"
-token_holder_columns = ["_block_number", "token", "safe", "balance_uint64"]
-token_holder_df = data_frames(columns = columns(token_holder_columns, dtype = str),
-                                rows = st.tuples(
-                                    st.integers(min_value = 1000000, max_value = 18446744073709551615),
-                                    st.from_regex("card\-[0-9]", fullmatch = True),
-                                    st.from_regex("safe\-[0-9]", fullmatch = True),
-                                    st.floats(min_value = 22995968, max_value = 24032768)
-                                )
+safe_ownership_table = "_SAFE_OWNERSHIP"               
+
+owner_st = st.from_regex("owner\-[0-2]", fullmatch = True)
+safe_st =  st.from_regex("safe\-[0-2]", fullmatch = True)
+card_st = st.from_regex("card\-[0-0]", fullmatch = True)
+# card_st = st.text("card")
+block_number_st = st.integers(min_value = START_BLOCK - 5, max_value = END_BLOCK + 5)
+balance_st = st.integers(min_value = 22995968, max_value = 24032768)
+
+safe_owner_columns = {
+    "owner": {"elements":owner_st, "unique":True},
+    "safe": {"elements":safe_st, "unique":True}
+}
+
+token_holder_columns = {
+    "_block_number":{"elements": block_number_st, "unique":True},
+    "safe":{"elements":safe_st},
+    "token":{"elements": card_st},
+    "balance_uint64":{"elements": balance_st, "unique":True}
+}
+
+safe_owner_df = data_frames(
+    index=range_indexes(min_size= 3, max_size=3),
+    columns=[column(key, **value) for key, value in safe_owner_columns.items()],
 )
-safe_owner_columns = ["safe", "owner"]
-safe_owner_df = data_frames(columns = columns(safe_owner_columns, dtype = str),
-                            rows = st.tuples(
-                                st.from_regex("owner\-[0-9]", fullmatch = True),
-                                st.from_regex("safe\-[0-9]", fullmatch = True),
-                            )
-                            
-                )
+
+token_holder_df = data_frames(
+    index=range_indexes(min_size=3, max_size=4),
+    columns=[column(key, **value) for key, value in token_holder_columns.items()],
+)
 
 def create_rule(
     monkeypatch, fake_data_token_holder, fake_data_safe_owner, core_config_overrides={}, user_config_overrides={}
 ):
     core_config = {
-        "payment_cycle_length": 30,
-        "start_block": 0,
-        "end_block": 30,
+        "payment_cycle_length": CYCLE_LENGTH,
+        "start_block": START_BLOCK,
+        "end_block": END_BLOCK,
         "subgraph_config_locations": {
             "prepaid_card_payment": "s3://cardpay-staging-partitioned-graph-data/data/prepaid_card_payments/0.0.3/",
             "spend_accumulation": "s3://tall-data-dev/paulin/spend_accumulation/0.0.1/",
@@ -44,7 +62,7 @@ def create_rule(
 
     core_config.update(core_config_overrides)
     user_config = {
-        "token": "card",
+        "token": "card-0",
         "duration": 30,
         "interest_rate_monthly": 0.06
     }
@@ -78,107 +96,8 @@ def get_amount(result, payee, idx):
     idx = result.index[result["payee"] == payee].tolist()[0]
     return result.loc[idx, "amount"]
    
-   
-def test_correctly_compound_interest(monkeypatch):
-    fake_data_token_holder = pd.DataFrame(
-        [
-            {
-                "_block_number": 0,
-                "id": 1,
-                "token": "card",
-                "safe": "safe1",
-                "balance_uint64": 1000
-            },
-            {
-                "_block_number": 10,
-                "id": 1,
-                "token": "card",
-                "safe": "safe1",
-                "balance_uint64": 1500
-            },
-            {
-                "_block_number": 15,
-                "id": 1,
-                "token": "card",
-                "safe": "safe1",
-                "balance_uint64": 1200
-            },
-            {
-                "_block_number": 20,
-                "id": 1,
-                "token": "card",
-                "safe": "safe1",
-                "balance_uint64": 1700
-            },
-            {
-                "_block_number": 25,
-                "id": 1,
-                "token": "card",
-                "safe": "safe1",
-                "balance_uint64": 1900
-            },
-            {
-                "_block_number": 0,
-                "id": 2,
-                "token": "card",
-                "safe": "safe2",
-                "balance_uint64": 1500
-            },
-            {
-                "_block_number": 0,
-                "id": 2,
-                "token": "card",
-                "safe": "safe2",
-                "balance_uint64": 1500
-            },   
-            {
-                "_block_number": 15,
-                "id": 2,
-                "token": "card",
-                "safe": "safe2",
-                "balance_uint64": 1200
-            },
-            {
-                "_block_number": 25,
-                "id": 2,
-                "token": "card",
-                "safe": "safe2",
-                "balance_uint64": 1000
-            }, 
-            {
-                "_block_number": 0,
-                "id": 3,
-                "token": "card",
-                "safe": "safe3",
-                "balance_uint64": 1500
-            }, 
-        ]    
-    )
 
-    fake_data_safe_owner = pd.DataFrame(
-        [
-            {
-                "safe":"safe1",
-                "owner":"owner1"
-            },
-            {
-                "safe": "safe2",
-                "owner": "owner2"
-            },
-            {
-                "safe": "safe3",
-                "owner": "owner3"
-            }
-        ]   
-    )
-
-    rule = create_rule(
-        monkeypatch, fake_data_token_holder, fake_data_safe_owner
-    )
-    result = rule.run(30, "0x0")
-    assert len(result) == 3
-
-def test_correct_calc_rewards(monkeypatch):
+def tst_correct_calc_rewards(monkeypatch):
     fake_data_token_holder = pd.DataFrame([
         {
             "_block_number": 0,
@@ -262,7 +181,7 @@ def test_correct_calc_rewards(monkeypatch):
     assert math.ceil(get_amount(result, "owner3", 2)) == math.ceil(60)
 
 
-def test_correctly_manages_first_deposit_in_cycle(monkeypatch):
+def tst_correctly_manages_first_deposit_in_cycle(monkeypatch):
     fake_data_token_holder = pd.DataFrame([
         {
             "_block_number": 0,
@@ -293,7 +212,7 @@ def test_correctly_manages_first_deposit_in_cycle(monkeypatch):
     assert pytest.approx(get_amount(result, "owner1", 0)) == 120
 
 
-def test_correct_calc_rewards_in_cycle(monkeypatch, i = 0, j = 0):
+def tst_correct_calc_rewards_in_cycle(monkeypatch):
     fake_data_token_holder = pd.DataFrame([
         {
             "_block_number": 0,
@@ -318,12 +237,51 @@ def test_correct_calc_rewards_in_cycle(monkeypatch, i = 0, j = 0):
     assert pytest.approx(get_amount(result, "owner1", 0)) == 60
 
 @given(token_holder_df, safe_owner_df)
-def test_hypothesis(token_holder_df, safe_owner_df):
-    assert len(token_holder_df) > -1 
+def test_num_of_safes_matches_results(token_holder_df, safe_owner_df):
+    """
+    testing that number of safes within range matches the len of payees
+    """
+    with MonkeyPatch.context() as monkeypatch:
+        rule = create_rule(
+            monkeypatch, token_holder_df, safe_owner_df
+        )
+        safes_set = set()
+        for _, instance in token_holder_df.iterrows():
+            if START_BLOCK <= instance["_block_number"] < END_BLOCK:
+                safes_set.add(instance["safe"])
+        
+        result = rule.run(END_BLOCK, "0x0")
+        assert len(result) == len(safes_set)
 
-@given(st.integers(), st.integers())
-def test_b(x, y, monkeypatch):
-    print(x, y)
-    assert 0 == 0
+@given(token_holder_df, safe_owner_df)  
+def test_all_stakers_receiving_rewards(token_holder_df, safe_owner_df):
+    """
+    testing the any owner with some staking during the cycle receive rewards
+    """
+    with MonkeyPatch.context() as monkeypatch:
+        rule = create_rule(
+            monkeypatch, token_holder_df, safe_owner_df
+        )
+        payees_list = []
+        safes_set = set()
+        for _, row in token_holder_df.iterrows():
+            cur_block_num = row["_block_number"]
+            if START_BLOCK <= cur_block_num <= END_BLOCK:
+                rewarded_safe = row["safe"]
+                if rewarded_safe in safes_set:
+                    return
+                safes_set.add(rewarded_safe)
+                owner = safe_owner_df.where(safe_owner_df["safe"] == rewarded_safe )["owner"][0]
+                if type(owner) == str:
+                    payees_list.append(owner)
+        result = rule.run(END_BLOCK, "0x0")
+        print(result)
+        all_positive_rewards = True
+        
+        for payee in payees_list:
+            if result.where(result["payee"] == payee)["amount"][0] <= 0:
+                all_positive_rewards = False
+        assert all_positive_rewards
 
-    
+
+
