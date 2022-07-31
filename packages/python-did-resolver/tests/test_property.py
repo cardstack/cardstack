d0118552025b61d9807c10251057b7c5974fbbde
tintinthong <justinthong93@gmail.com>
2022-07-31 12:11:59 +0100

    update test (added one unit test)

--- packages/python-did-resolver/tests/test_property.py
+++ packages/python-did-resolver/tests/test_property.py
@@ -1,6 +1,9 @@
+import json
 import re
+import uuid as _uu
 from datetime import timedelta
 from typing import get_args
+from uuid import UUID
 
 import pytest
 from did_resolver.resolver import Resolver
@@ -9,13 +12,15 @@ from hypothesis import strategies as st
 from python_did_resolver.main import (
     FLICKR_BASE58_ALPHABET,
     SHORT_TYPE,
+    UUIDV5_NAMESPACE,
     CardstackIdentifierType,
+    EncodeOptions,
     encode_did,
+    get_resolver,
     hash_func,
     is_flickr_base_58,
     is_valid_uuid,
     parse_identifier,
-    s3_resolution_method,
     su,
 )
 from python_did_resolver.utils import VersionOutOfRange, to_kebab_case
@@ -143,8 +148,7 @@ def test_compat_encode(o):
 def test_resolve(o):
     did = encode_did(o)
     path = to_kebab_case(o["type"])
-    registry = {"cardstack": s3_resolution_method}
-    r = Resolver(registry)
+    r = Resolver(get_resolver())
     resolved_doc = r.resolve(did)
     if "alsoKnownAs" in resolved_doc["didDocument"].keys() and "unique_id" in o.keys():
         uid = o["unique_id"]
@@ -157,8 +161,7 @@ def test_resolve(o):
 @given(encode_options)
 def test_resolve_invalid_checksum(o):
     did = encode_did(o) + "a"
-    registry = {"cardstack": s3_resolution_method}
-    r = Resolver(registry)
+    r = Resolver(get_resolver())
     with pytest.raises(
         Exception,
         match=r"Invalid DID identifier: checksum failed",
@@ -174,8 +177,7 @@ REGEX_NON_EXISTENT_SHORT_CODE = rf"^(?!(?:{SHORT_TYPES_DELIMITED})$)[a-z]\Z"
 def test_resolve_invalid_short_type(c):
     cardstack_identifier = "1" + c + su.random(length=22)
     did = f"did:cardstack:{cardstack_identifier}{hash_func(cardstack_identifier)}"
-    registry = {"cardstack": s3_resolution_method}
-    r = Resolver(registry)
+    r = Resolver(get_resolver())
     with pytest.raises(
         Exception,
         match=r"Invalid DID identifier: unknown type .*",
@@ -201,6 +203,42 @@ def test_resolve_js(o):
 @settings(deadline=timedelta(seconds=60))
 def test_compat_resolve(o):
     did = encode_did(o)
-    registry = {"cardstack": s3_resolution_method}
-    r = Resolver(registry)
+    r = Resolver(get_resolver())
     r.resolve(did) == js_resolve_did(did)
+
+
+def test_resolve_rule():
+    """
+    Unit test to ensure resolve rule
+    """
+    rules = [
+        {
+            "core": {
+                "payment_cycle_length": 2880,
+                "start_block": 22776480,
+                "end_block": 22776480,
+                "docker_image": "120317779495.dkr.ecr.us-east-1.amazonaws.com/flat_payment:latest",
+                "subgraph_config_locations": {},
+            },
+            "user_defined": {
+                "token": "0x52031d287Bb58E26A379A7Fec2c84acB54f54fe3",
+                "reward_per_user": 103000000000000000000,
+                "duration": 120960,
+                "accounts": [
+                    "0x159ADe032073d930E85f95AbBAB9995110c43C71",
+                    "0x7317f75E480f09b6d5e454dEc9F0B6Bb40eeAC4a",
+                ],
+            },
+        }
+    ]
+    _uu.NAMESPACE_DNS = UUID(UUIDV5_NAMESPACE)
+    json_content = json.dumps(rules)
+    uid = su.uuid(name=json_content)
+    encode_options: EncodeOptions = {
+        "version": 1,
+        "type": "RewardRule",
+        "uniqueId": uid,
+    }
+    computed_did = encode_did(encode_options)
+    did = "did:cardstack:1reoYsaoXq8nTe71jr57278A878793ec874afd28"
+    computed_did == did
