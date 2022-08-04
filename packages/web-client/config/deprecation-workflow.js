435a1e51fa0be52efb03e4e0bcaccaf97c2c623e
Luke Melia <luke@lukemelia.com>
2022-08-04 13:16:57 -0700

    Ember 4 upgrade deprecation tweaks

--- packages/web-client/config/deprecation-workflow.js
+++ packages/web-client/config/deprecation-workflow.js
@@ -3,6 +3,8 @@ self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   throwOnUnhandled: true,
   workflow: [
+    { handler: 'log', matchId: 'ember-polyfills.deprecate-assign' },
+
     // We're unable eliminate ember-modifier deprecations completely due to the current state
     // of ember-power-select. However, the ember-modifier deprecations are for upgrading to
     // ember-modifier 4.0, not ember-source 4.0, so I think we are OK leaving those that we
