b73e5a32bd983bb2228b1a90bdcbd6b89d216493
Buck Doyle <buck.doyle@cardstack.com>
2022-08-04 14:15:13 -0500

    Move fakeness message

--- packages/hub/config/test.cjs
+++ packages/hub/config/test.cjs
@@ -23,7 +23,9 @@ module.exports = {
     google: {
       serviceAccount: {
         client_email: 'test@example.com',
-        private_key: `-----BEGIN RSA PRIVATE KEY-----
+        private_key: `THIS IS FAKE
+
+-----BEGIN RSA PRIVATE KEY-----
 MIIBOgIBAAJBAOTUaxf5R6ctqGE7+VWfmmGHpcaONeP5EgIiKFFMgiQ5PzA+i8xI
 DDKl6YhPC4hx5WST98uae9F5RopsFQ9uBsUCAwEAAQJAei96Z7ixq/DTQeg2QKQS
 WRWHTThOSkaKeR0oDhEeRJxSR/+IJO5+pvKxw8XEioSW+UkzZ1Xar0bQ9vgQqi3x
@@ -31,9 +33,7 @@ gQIhAPOi/07ewCGKhqSAajAJKqANuS9MGW3YlsU6eWbF8GYVAiEA8HERpYHmaD1G
 sfhurg0LmCqunx4RCfR8BvvA0h3mefECIQCSFp2iLDzmn8qmXv3NOeHeQPxWmPny
 fNnC/5IGBxBkFQIgGC1mdtwPDSBMQY0XpAzPw6dXE8z0LaoV5qXeB8LV8CECIGmi
 04tL7zRgmzzrRK4LoXLwBTufCeFjlPcoP5TQsdFC
------END RSA PRIVATE KEY-----
-
-          THE ABOVE IS FAKE`,
+-----END RSA PRIVATE KEY-----`,
       },
     },
   },
