b39efa73612d84f6b290a5bb42ed187ae2931281
Ian Calvert <ianjcalvert@gmail.com>
2022-08-17 15:09:05 +0100

    Refactor staking query for clarity

--- packages/cardpay-reward-programs/cardpay_reward_programs/rules/staking.py
+++ packages/cardpay-reward-programs/cardpay_reward_programs/rules/staking.py
@@ -19,8 +19,9 @@ class Staking(Rule):
         token_holder_table = table_query
         safe_owner_table = aux_table_query
         return f"""
-            with filtered_safes as (
-                SELECT safe, _block_number, type, max(owner) as owner
+            -- Select only the safes we are rewarding, and their owner at the end of the cycle
+            with filtered_safes AS (
+                SELECT safe, max(owner) as owner
                 FROM {safe_owner_table} a
                 WHERE _block_number = (
                     SELECT MAX(_block_number)
@@ -28,48 +29,62 @@ class Staking(Rule):
                     WHERE a.safe = b.safe
                     AND _block_number::integer < $2::integer
                 )
-                group by safe, _block_number, type
+                AND type = 'depot'
+                GROUP BY safe
             ),
-            filtered_balances as (
-                select tht.safe, tht.balance_downscale_e9_uint64::int64 as balance_int64, tht._block_number, tht.token,
-                from {token_holder_table} as tht, filtered_safes as sot
-                where tht.safe = sot.safe
-                and sot.type = 'depot'
+            -- Get the balance history of the safes we are interested in, filtered to the token, 
+            -- joining to get the owner
+            filtered_balances AS (
+                SELECT tht.safe, sot.owner, tht.balance_downscale_e9_uint64::int64 AS balance_int64, tht._block_number
+                FROM {token_holder_table} AS tht
+                LEFT JOIN filtered_safes AS sot ON (tht.safe = sot.safe)
+                WHERE token = $3::text
+                AND sot.safe IS NOT NULL
             ),
-            balance_changes as (select
-                safe,
-                balance_int64 - lag(balance_int64, 1 ,0) over (partition by safe order by _block_number asc) as change,
-                $5::float+1 as interest_rate,
-                ($2::integer - _block_number::integer) / $4::float  as compounding_rate,
-                from filtered_balances
-                where _block_number::integer < $2::integer
-                and token = $3::text
-                and safe is not null
-                qualify
-                _block_number::integer >= $1::integer
+            -- Get the deposits & withdrawals by comparing balances to the previous balance
+            balance_changes AS (
+                SELECT
+                    safe,
+                    owner,
+                    -- Get the change by subtracting the balance before
+                    balance_int64 - LAG(balance_int64, 1 ,0) OVER (PARTITION BY safe ORDER BY _block_number asc) AS change,
+                    $5::float+1 AS interest_rate,
+                    -- This is the proportion of the 'month' remaining
+                    ($2::integer - _block_number::integer) / $4::float AS compounding_periods,
+                FROM filtered_balances
+                WHERE _block_number::integer < $2::integer
+                QUALIFY _block_number::integer >= $1::integer
                 ),
-            original_balances as (select safe,
-                last(balance_int64) as change,
-                $5::float+1 as interest_rate,
-                1 as compounding_rate,
-                from filtered_balances a
-                where _block_number::integer < $1::integer
-                and token = $3::text
-                and safe is not null
-                and _block_number = (
+            original_balances AS (
+                SELECT 
+                    safe, 
+                    owner,
+                    -- There is only one value here after the group but logically it is the "last" balance
+                    -- it is called "change" to match the balance_changes CTE so we can union them together
+                    LAST(balance_int64) AS change,
+                    $5::float+1 AS interest_rate,
+                    1 AS compounding_periods,
+                FROM filtered_balances a
+                WHERE _block_number::integer < $1::integer
+                AND _block_number = (
                     SELECT MAX(_block_number)
                     FROM filtered_balances b
                     WHERE a.safe = b.safe
                     AND _block_number::integer < $1::integer
                 )
-                group by safe
+                GROUP BY safe, owner
                 ),
-            all_data as (select * from original_balances union all select * from balance_changes)
 
-            select owner as payee, sum(change * ((interest_rate**compounding_rate) - 1)) as rewards
-            from all_data
-            left join filtered_safes using (safe)
-            group by payee
+            -- Combine the balances at the start of the period and 
+            all_data AS (SELECT * FROM original_balances UNION ALL SELECT * FROM balance_changes)
+
+            -- Aggregate the changes, each is treated as a compounding interest calculation
+            -- rate^(periods) gives you the total after growth, so we need to take 1 away to get just the growth
+            -- e.g. 5% APR for two years would be 1.05^2 - 1
+            -- and 5% APR for half a year would be 1.05^(1/2) - 1
+            SELECT owner AS payee, sum(change * ((interest_rate**compounding_periods) - 1)) AS rewards
+            FROM all_data
+            GROUP BY payee
         """
 
     def run(self, payment_cycle: int, reward_program_id: str):
