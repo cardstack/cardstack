5ed9ec300296fbb632d686e36e21aff0a550f4a0
Buck Doyle <buck.doyle@cardstack.com>
2022-08-18 15:49:48 -0500

    Add explanation for deprecation exception

--- packages/ssr-web/config/deprecation-workflow.js
+++ packages/ssr-web/config/deprecation-workflow.js
@@ -3,6 +3,7 @@ self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   throwOnUnhandled: true,
   workflow: [
+    // Fastboot is still using inject: https://github.com/ember-fastboot/ember-cli-fastboot/blob/2a6ed5d76b00da2106a9ba10f048aa2bef989a80/packages/ember-cli-fastboot/fastboot/initializers/ajax.js#L33-L34
     { handler: 'log', matchId: 'remove-owner-inject' },
     { handler: 'log', matchId: 'ember-polyfills.deprecate-assign' },
   ],
