6f84e3854457c5d3ae557fc0cd7e83d1f9be8d67
tintinthong <justinthong93@gmail.com>
2022-08-19 15:33:35 +0200

    update cli

--- packages/cardpay-cli/rewards/reward-balances.ts
+++ packages/cardpay-cli/rewards/reward-balances.ts
@@ -13,6 +13,10 @@ export default {
         type: 'string',
         description: 'The address that tally rewarded -- The owner of prepaid card.',
       })
+      .option('safeAddress', {
+        type: 'string',
+        description: 'safe address. Specify if you want gas estimates for each claim',
+      })
       .option('rewardProgramId', {
         type: 'string',
         description: 'The reward program id.',
@@ -20,14 +24,15 @@ export default {
       .option('network', NETWORK_OPTION_LAYER_2);
   },
   async handler(args: Arguments) {
-    let { network, address, rewardProgramId } = args as unknown as {
+    let { network, address, safeAddress, rewardProgramId } = args as unknown as {
       network: string;
       address: string;
+      safeAddress: string;
       rewardProgramId?: string;
     };
     let { web3 } = await getEthereumClients(network, getConnectionType(args));
     let rewardPool = await getSDK('RewardPool', web3);
-    const tokenBalances = await rewardPool.rewardTokenBalances(address, rewardProgramId);
+    const tokenBalances = await rewardPool.rewardTokenBalances(address, safeAddress, rewardProgramId);
     console.log(`Reward balances for ${address}`);
     displayRewardTokenBalance(tokenBalances);
   },
