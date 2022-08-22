4b3ab398d2c65885b3a9266def976f7905eb84af
Buck Doyle <buck.doyle@cardstack.com>
2022-08-22 11:31:30 -0500

    Restore hack
    ugh

--- packages/hub/services/clock.ts
+++ packages/hub/services/clock.ts
@@ -1,4 +1,5 @@
 import { format } from 'date-fns';
+import config from 'config';
 
 export class Clock {
   hrNow() {
@@ -9,6 +10,11 @@ export class Clock {
   }
   dateStringNow() {
     return format(this.now(), 'yyyy-MM-dd');
+  }
+
+  // FIXME remove
+  config() {
+    return config;
   }
 }
 
