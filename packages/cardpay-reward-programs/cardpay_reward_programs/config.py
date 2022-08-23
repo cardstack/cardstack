607061e8f35ef4cfc96c6cdd03fb1eab254f1b95
tintinthong <justinthong93@gmail.com>
2022-08-23 14:25:09 +0200

    update reward programs

--- packages/cardpay-reward-programs/cardpay_reward_programs/config.py
+++ packages/cardpay-reward-programs/cardpay_reward_programs/config.py
@@ -16,7 +16,7 @@ default_payment_list = pd.DataFrame(
 config = {
     "staging": {
         "tokens": {
-            "card": "0xB236ca8DbAB0644ffCD32518eBF4924ba866f7Ee",
+            "card": "0xB0427e9F03Eb448D030bE3EBC96F423857ceEb2f",
             "dai": "0xFeDc0c803390bbdA5C4C296776f4b574eC4F30D1",
         },
         "reward_program": "0x5E4E148baae93424B969a0Ea67FF54c315248BbA",
