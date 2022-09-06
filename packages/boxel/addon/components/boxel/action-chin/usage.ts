c67fccce5c8ce40afcafe9bcb8a001a3abba01b4
Buck Doyle <buck.doyle@cardstack.com>
2022-09-06 18:50:26 -0500

    Add usage for CSS variables in ActionChin

--- packages/boxel/addon/components/boxel/action-chin/usage.ts
+++ packages/boxel/addon/components/boxel/action-chin/usage.ts
@@ -13,6 +13,14 @@ export default class ActionChinUsage extends Component {
   @tracked unlockState = 'default';
   @tracked depositState = 'disabled';
 
+  @tracked paddingHorizontal = 'var(--boxel-sp-lg)';
+  @tracked paddingVertical = 'var(--boxel-sp-lg)';
+  @tracked backgroundColor = 'var(--boxel-purple-750)';
+  @tracked emphasisTextColor = 'var(--boxel-light)';
+  @tracked textColor = 'var(--boxel-purple-300)';
+  @tracked disabledBackground = 'rgb(54 52 65 / 90%)';
+  @tracked lockIconSize = '0.75rem';
+
   get depositIsDisabled(): boolean {
     return (
       this.unlockState !== 'memorialized' || this.depositState === 'disabled'
