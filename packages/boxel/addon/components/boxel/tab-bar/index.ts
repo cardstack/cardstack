5efa3d2c8767bc10374d1f22dc75b12a296b003a
Buck Doyle <buck.doyle@cardstack.com>
2022-09-06 11:34:05 -0500

    Move event-prevention into non-link branch
    This means the log action still works but links behave
    as expected.

--- packages/boxel/addon/components/boxel/tab-bar/index.ts
+++ packages/boxel/addon/components/boxel/tab-bar/index.ts
@@ -8,10 +8,8 @@ export default class TabBar extends Component {
   invokeMenuItemAction(actionOrLink: () => never, e: Event): void;
   invokeMenuItemAction(actionOrLink: Link, e: Event): void;
   @action invokeMenuItemAction(actionOrLink: unknown, e: Event): void {
-    e.preventDefault();
-    if (actionOrLink instanceof Link && actionOrLink.transitionTo) {
-      actionOrLink.transitionTo();
-    } else {
+    if (!(actionOrLink instanceof Link)) {
+      e.preventDefault();
       (actionOrLink as () => never)();
     }
   }
