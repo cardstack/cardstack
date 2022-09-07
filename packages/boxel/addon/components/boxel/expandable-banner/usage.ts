6b2a5402fe75828204e3b81283fe611e8d3246db
Buck Doyle <buck.doyle@cardstack.com>
2022-09-07 15:47:14 -0500

    Add usage for CSS variables in ExpandableBanner

--- packages/boxel/addon/components/boxel/expandable-banner/usage.ts
+++ packages/boxel/addon/components/boxel/expandable-banner/usage.ts
@@ -5,4 +5,11 @@ import './usage.css';
 export default class ExpandableBannerUsage extends Component {
   @tracked icon = 'payment';
   @tracked summary = 'Deposit funds to become a Supplier';
+
+  @tracked minHeight = '5rem';
+  @tracked minHeightOpen = '15rem';
+  @tracked textColor = 'var(--boxel-light)';
+  @tracked backgroundColor = 'var(--boxel-purple-400)';
+  @tracked verticalGap = 'var(--boxel-sp)';
+  @tracked horizontalGap = 'var(--boxel-sp-lg)';
 }
