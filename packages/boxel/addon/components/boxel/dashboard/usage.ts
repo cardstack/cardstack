d8f28c990524e7b7072af008366d80441c5b490a
Buck Doyle <buck.doyle@cardstack.com>
2022-09-07 11:52:04 -0500

    Add usage for CSS variables in Dashboard

--- packages/boxel/addon/components/boxel/dashboard/usage.ts
+++ packages/boxel/addon/components/boxel/dashboard/usage.ts
@@ -4,4 +4,8 @@ import { tracked } from '@glimmer/tracking';
 export default class BoxelDashboardComponent extends Component {
   @tracked displayLeftEdge = true;
   @tracked darkTheme = true;
+
+  @tracked leftEdgeWidth = '5rem';
+  @tracked backgroundColor = 'inherit';
+  @tracked color = 'inherit';
 }
