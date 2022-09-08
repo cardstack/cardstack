bd47275d064fdd34cd45e278de353c3bb2791f83
FadhlanR <fridhwanallah@gmail.com>
2022-09-08 16:15:07 +0700

    Update wording of enable SP module command (#3204)
    Co-authored-by: Luke Melia <luke@lukemelia.com>

--- packages/cardpay-cli/scheduled-payment/enable-module.ts
+++ packages/cardpay-cli/scheduled-payment/enable-module.ts
@@ -14,7 +14,7 @@ export default {
       })
       .positional('gasTokenAddress', {
         type: 'string',
-        description: 'The token address (defaults to Kovan DAI)',
+        description: 'The token address (defaults to Goerli DAI)',
       })
       .option('network', NETWORK_OPTION_ANY);
   },
@@ -28,7 +28,7 @@ export default {
     let scheduledPaymentModule = await getSDK('ScheduledPaymentModule', web3, signer);
     let blockExplorer = await getConstant('blockExplorer', web3);
 
-    console.log(`Enabling scheduled payment module to the safe ...`);
+    console.log(`Enabling scheduled payment module on safe ${safeAddress}`);
     let onTxnHash = (txnHash: string) =>
       console.log(`Transaction hash: ${blockExplorer}/tx/${txnHash}/token-transfers`);
     let { scheduledPaymentModuleAddress, metaGuardAddress } = await scheduledPaymentModule.enableModule(
@@ -36,8 +36,8 @@ export default {
       gasTokenAddress,
       { onTxnHash }
     );
-    console.log(`scheduled payment module deployed to: ${scheduledPaymentModuleAddress}`);
-    console.log(`meta guard deployed to: ${metaGuardAddress}`);
-    console.log('done');
+    console.log(`Scheduled payment module deployed to: ${scheduledPaymentModuleAddress}`);
+    console.log(`Meta guard deployed to: ${metaGuardAddress}`);
+    console.log(`Done. The module and guard have now been enabled on safe ${safeAddress}`);
   },
 } as CommandModule;
