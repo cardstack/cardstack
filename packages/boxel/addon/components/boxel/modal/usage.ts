6c6c0f67acd904471bdb23d90dbd16e39bd6f571
Luke Melia <luke@lukemelia.com>
2022-09-08 16:14:23 -0400

    Update modal from upstream changes

--- packages/boxel/addon/components/boxel/modal/usage.ts
+++ packages/boxel/addon/components/boxel/modal/usage.ts
@@ -4,11 +4,11 @@ import { action } from '@ember/object';
 import './usage.css';
 import { isBlank } from '@ember/utils';
 
-type SizeVariant = 'small' | 'medium' | 'large' | undefined;
+type ModalSize = 'small' | 'medium' | 'large' | undefined;
 
 export default class ModalUsage extends Component {
   @tracked isOpen = false;
-  @tracked size: SizeVariant;
+  @tracked size: ModalSize = undefined;
   @tracked offsetRight = '0px';
   @tracked offsetLeft = '0px';
   @tracked offsetTop = '0px';
@@ -20,16 +20,24 @@ export default class ModalUsage extends Component {
     small: '36.25rem',
     medium: '43.75rem',
     large: '65rem',
+    '<undefined>': '65rem',
   };
 
   @tracked maxWidthOverride: string | undefined;
 
   get maxWidth() {
     if (isBlank(this.maxWidthOverride)) {
-      return this.size ? this.maxWidths[this.size] : '65rem';
+      return this.maxWidths[this.sizeAsString || '<undefined>'];
     } else {
       return this.maxWidthOverride;
     }
+  }
+
+  get sizeAsString(): ModalSize | '<undefined>' {
+    return this.size ?? '<undefined>';
+  }
+  @action updateSize(val: ModalSize): void {
+    this.size = val;
   }
 
   @action
