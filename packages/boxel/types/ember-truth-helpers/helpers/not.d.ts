596f3c5489bb5a5e8338f90b9b54b192a8b67cdf
Luke Melia <luke@lukemelia.com>
2022-09-08 16:18:54 -0400

    Move improperly placed comment in truth-helper types

--- packages/boxel/types/ember-truth-helpers/helpers/not.d.ts
+++ packages/boxel/types/ember-truth-helpers/helpers/not.d.ts
@@ -1,12 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Helper from '@ember/component/helper';
 
-// NOTE: These types are somewhat imperfect.
-// For instance, the limit at 5 is arbitrary. Also, the actual helpers
-// use JavaScript truthiness along with special handling for empty arrays
-// and objects with an `isTruthy` method. In these cases, we just won't
-// narrow as much as we potentially could.
-
 interface NotHelperSignature {
   Args: { Positional: any[] };
   Return: boolean;
--- packages/boxel/types/ember-truth-helpers/helpers/or.d.ts
+++ packages/boxel/types/ember-truth-helpers/helpers/or.d.ts
@@ -1,6 +1,12 @@
 import Helper from '@ember/component/helper';
 import { Falsy, Maybe, UnsetValue } from './-private/shared';
 
+// NOTE: These types are somewhat imperfect.
+// For instance, the limit at 5 is arbitrary. Also, the actual helpers
+// use JavaScript truthiness along with special handling for empty arrays
+// and objects with an `isTruthy` method. In these cases, we just won't
+// narrow as much as we potentially could.
+
 type OrPair<A, B> = B extends UnsetValue
   ? A
   : Maybe<A> extends true
