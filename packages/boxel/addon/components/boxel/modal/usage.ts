059aab5e7a3c6f16ed565536cd1daa1ec7986d71
Buck Doyle <buck.doyle@cardstack.com>
2022-09-08 12:20:08 -0500

    Add usage for CSS variables in Modal
    This is semi-borrowed from #3171 but will need updating
    when that’s merged.

--- packages/boxel/addon/components/boxel/modal/usage.ts
+++ packages/boxel/addon/components/boxel/modal/usage.ts
@@ -2,16 +2,35 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import './usage.css';
+import { isBlank } from '@ember/utils';
+
+type SizeVariant = 'small' | 'medium' | 'large' | undefined;
 
 export default class ModalUsage extends Component {
   @tracked isOpen = false;
-  @tracked size = 'n/a';
+  @tracked size: SizeVariant;
   @tracked offsetRight = '0px';
   @tracked offsetLeft = '0px';
   @tracked offsetTop = '0px';
   @tracked layer = 'default';
   @tracked isDefaultOpen = false;
   @tracked isUrgentOpen = false;
+
+  maxWidths = {
+    small: '36.25rem',
+    medium: '43.75rem',
+    large: '65rem',
+  };
+
+  @tracked maxWidthOverride: string | undefined;
+
+  get maxWidth() {
+    if (isBlank(this.maxWidthOverride)) {
+      return this.size ? this.maxWidths[this.size] : '65rem';
+    } else {
+      return this.maxWidthOverride;
+    }
+  }
 
   @action
   onClose(): void {
