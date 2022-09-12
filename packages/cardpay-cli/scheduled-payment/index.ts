c194a83312b5f4b149d2623d5dd10c8634b4a5e0
FadhlanR <fridhwanallah@gmail.com>
2022-09-12 11:08:42 +0700

    Estimate scheduled payment execution (#3205)

--- packages/cardpay-cli/scheduled-payment/index.ts
+++ packages/cardpay-cli/scheduled-payment/index.ts
@@ -2,9 +2,10 @@ import type { Argv } from 'yargs';
 export const command = 'scheduled-payment <command>';
 export const desc = 'Commands to interact with the scheduled payment module';
 import enableModule from './enable-module';
+import estimateExecution from './estimate-execution';
 
 export const builder = function (yargs: Argv) {
-  return yargs.command([enableModule] as any);
+  return yargs.command([enableModule, estimateExecution] as any);
 };
 
 export function handler(/* argv: Argv */) {
