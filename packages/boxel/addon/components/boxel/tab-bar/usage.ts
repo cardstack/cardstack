75cd02cba585e7a13df9a630fce3eab4f676aa26
Buck Doyle <buck.doyle@cardstack.com>
2022-09-14 12:24:45 -0500

    Add preliminary spread and icon

--- packages/boxel/addon/components/boxel/tab-bar/usage.ts
+++ packages/boxel/addon/components/boxel/tab-bar/usage.ts
@@ -1,7 +1,10 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class TabBarUsage extends Component {
+  @tracked spread = false;
+
   @action log(message: string): void {
     // eslint-disable-next-line no-console
     console.log(message);
