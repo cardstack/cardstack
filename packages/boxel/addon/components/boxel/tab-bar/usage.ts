1d522238c39914aeeef833eb5dfa2cd70ff61b79
Buck Doyle <buck.doyle@cardstack.com>
2022-09-14 14:31:16 -0500

    Add font overrides

--- packages/boxel/addon/components/boxel/tab-bar/usage.ts
+++ packages/boxel/addon/components/boxel/tab-bar/usage.ts
@@ -5,6 +5,9 @@ import { tracked } from '@glimmer/tracking';
 export default class TabBarUsage extends Component {
   @tracked backgroundColor = 'inherit';
   @tracked borderBottom = '1px solid var(--boxel-light-500)';
+  @tracked font = 'inherit';
+  @tracked fontWeightHover = '600';
+
   @tracked spread = false;
 
   @action log(message: string): void {
