d3ef7b786abc15979206e5e2e168fbb85d0f38ad
FadhlanR <fridhwanallah@gmail.com>
2022-09-15 12:53:16 +0700

    sdk: Add SDK & CLI command to create SP hash (#3220)

--- packages/cardpay-cli/scheduled-payment/index.ts
+++ packages/cardpay-cli/scheduled-payment/index.ts
@@ -1,5 +1,6 @@
 import type { Argv } from 'yargs';
 import createSafe from './create-safe';
+import createSpHash from './create-sp-hash';
 export const command = 'scheduled-payment <command>';
 export const desc = 'Commands to interact with the scheduled payment module';
 import enableModule from './enable-module';
@@ -7,7 +8,7 @@ import estimateExecution from './estimate-execution';
 import schedulePayment from './schedule-payment';
 
 export const builder = function (yargs: Argv) {
-  return yargs.command([createSafe, enableModule, estimateExecution, schedulePayment] as any);
+  return yargs.command([createSafe, createSpHash, enableModule, estimateExecution, schedulePayment] as any);
 };
 
 export function handler(/* argv: Argv */) {
