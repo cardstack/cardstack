d4e838fbef3989c01d43cc727a2e37d83940f088
Buck Doyle <buck.doyle@cardstack.com>
2022-09-21 17:22:19 -0500

    Add documentation for @name

--- packages/boxel/addon/components/boxel/toggle-button-group/usage.ts
+++ packages/boxel/addon/components/boxel/toggle-button-group/usage.ts
@@ -3,6 +3,7 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class RadioInputUsage extends Component {
+  @tracked name = 'example-toggle-button-group-usage';
   @tracked groupDescription = 'Select one';
   @tracked disabled = false;
 
