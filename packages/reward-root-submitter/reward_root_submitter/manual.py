9db3f55151c7d5e7ca273a26a6fafa974dec27f3
tintinthong <justinthong93@gmail.com>
2022-09-22 18:44:04 +0800

    fixup

--- packages/reward-root-submitter/reward_root_submitter/manual.py
+++ packages/reward-root-submitter/reward_root_submitter/manual.py
@@ -3,12 +3,10 @@ import sys
 from cloudpathlib import AnyPath
 
 from .config import Config
-from .main import process_file, setup_logging
+from .main import get_all_unsubmitted_roots, process_file, setup_logging
 
 config = Config()
 setup_logging(config)
 
 if __name__ == "__main__":
-    file_path = sys.argv[1]
-    print(file_path)
-    process_file(AnyPath(file_path), Config())
+    get_all_unsubmitted_roots(config)
