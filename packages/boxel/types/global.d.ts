a058c9c050ca7871da58a7e38d51ff1f01fb2a3e
danibonilha <dani.bonilha@live.com>
2022-09-22 19:14:10 -0300

    [CS-4566]: Add types to BasicDropdown

--- packages/boxel/types/global.d.ts
+++ packages/boxel/types/global.d.ts
@@ -12,7 +12,7 @@ import OrHelper from 'ember-truth-helpers/helpers/or';
 import NotHelper from 'ember-truth-helpers/helpers/not';
 import GtHelper from 'ember-truth-helpers/helpers/gt';
 import OptionalHelper from 'ember-composable-helpers/helpers/optional';
-import { HelperLike } from '@glint/template';
+import { ComponentLike, HelperLike } from '@glint/template';
 import { svgJar } from '@cardstack/boxel/utils/svg-jar';
 import cssUrl from '@cardstack/boxel/helpers/css-url';
 import cssVar from '@cardstack/boxel/helpers/css-var';
@@ -21,6 +21,15 @@ import { Link, LinkParams } from 'ember-link';
 import menuDivider from '@cardstack/boxel/helpers/menu-divider';
 import { MenuItem } from '@cardstack/boxel/helpers/menu-item';
 import AddHelper from 'ember-math-helpers/helpers/add';
+import BasicDropdown, {
+  DropdownActions,
+  Dropdown,
+} from 'ember-basic-dropdown/addon/components/basic-dropdown';
+import Component from '@glimmer/component';
+import BasicDropdownTrigger from 'ember-basic-dropdown/addon/components/basic-dropdown-trigger';
+import BasicDropdownContent from 'ember-basic-dropdown/addon/components/basic-dropdown-content';
+
+type ArgsFromComponent<TClass> = TClass extends Component<infer U> ? U : never;
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
@@ -33,7 +42,7 @@ declare module '@glint/environment-ember-loose/registry' {
     optional: typeof OptionalHelper;
     cn: HelperLike<{
       Args: {
-        Positional: string[];
+        Positional: Array<string | undefined>;
         Named: Record<string, string | boolean | undefined>;
       };
       Return: string;
@@ -74,5 +83,30 @@ declare module '@glint/environment-ember-loose/registry' {
     }>;
     'svg-jar': typeof svgJar;
     'unique-id': HelperLike<{ Args: EmptyObject; Return: string }>;
+    // TODO: Move to own file on .gts migration
+    BasicDropdown: ComponentLike<{
+      Element: HTMLDivElement;
+      Args: ArgsFromComponent<BasicDropdown>;
+      Blocks: {
+        default: [
+          {
+            Trigger: ComponentLike<{
+              Element: HTMLDivElement;
+              Args: Partial<ArgsFromComponent<BasicDropdownTrigger>>;
+              Blocks: { default: [] };
+            }>;
+            Content: ComponentLike<{
+              Element: HTMLDivElement;
+              Args: Partial<ArgsFromComponent<BasicDropdownContent>>;
+              Blocks: { default: [] };
+            }>;
+            actions: DropdownActions;
+            uniqueId: Dropdown['uniqueId'];
+            disabled: Dropdown['disabled'];
+            isOpen: Dropdown['isOpen'];
+          }
+        ];
+      };
+    }>;
   }
 }
