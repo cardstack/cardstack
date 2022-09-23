e032dc671092da29d1da80d54c2b24425eae24c3
danibonilha <dani.bonilha@live.com>
2022-09-23 09:54:28 -0300

    [CS-4566]: Extract basic dd types into its own file

--- packages/boxel/types/global.d.ts
+++ packages/boxel/types/global.d.ts
@@ -12,7 +12,7 @@ import OrHelper from 'ember-truth-helpers/helpers/or';
 import NotHelper from 'ember-truth-helpers/helpers/not';
 import GtHelper from 'ember-truth-helpers/helpers/gt';
 import OptionalHelper from 'ember-composable-helpers/helpers/optional';
-import { ComponentLike, HelperLike } from '@glint/template';
+import { HelperLike } from '@glint/template';
 import { svgJar } from '@cardstack/boxel/utils/svg-jar';
 import cssUrl from '@cardstack/boxel/helpers/css-url';
 import cssVar from '@cardstack/boxel/helpers/css-var';
@@ -21,15 +21,11 @@ import { Link, LinkParams } from 'ember-link';
 import menuDivider from '@cardstack/boxel/helpers/menu-divider';
 import { MenuItem } from '@cardstack/boxel/helpers/menu-item';
 import AddHelper from 'ember-math-helpers/helpers/add';
-import BasicDropdown, {
-  DropdownActions,
-  Dropdown,
-} from 'ember-basic-dropdown/addon/components/basic-dropdown';
 import Component from '@glimmer/component';
-import BasicDropdownTrigger from 'ember-basic-dropdown/addon/components/basic-dropdown-trigger';
-import BasicDropdownContent from 'ember-basic-dropdown/addon/components/basic-dropdown-content';
 
-type ArgsFromComponent<TClass> = TClass extends Component<infer U> ? U : never;
+export type ArgsFromComponent<TClass> = TClass extends Component<infer U>
+  ? U
+  : never;
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
@@ -83,30 +79,5 @@ declare module '@glint/environment-ember-loose/registry' {
     }>;
     'svg-jar': typeof svgJar;
     'unique-id': HelperLike<{ Args: EmptyObject; Return: string }>;
-    // TODO: Move to own file on .gts migration
-    BasicDropdown: ComponentLike<{
-      Element: HTMLDivElement;
-      Args: ArgsFromComponent<BasicDropdown>;
-      Blocks: {
-        default: [
-          {
-            Trigger: ComponentLike<{
-              Element: HTMLDivElement;
-              Args: Partial<ArgsFromComponent<BasicDropdownTrigger>>;
-              Blocks: { default: [] };
-            }>;
-            Content: ComponentLike<{
-              Element: HTMLDivElement;
-              Args: Partial<ArgsFromComponent<BasicDropdownContent>>;
-              Blocks: { default: [] };
-            }>;
-            actions: DropdownActions;
-            uniqueId: Dropdown['uniqueId'];
-            disabled: Dropdown['disabled'];
-            isOpen: Dropdown['isOpen'];
-          }
-        ];
-      };
-    }>;
   }
 }
