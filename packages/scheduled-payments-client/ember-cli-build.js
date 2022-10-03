10f0315e0ff6265cdba1a4edae60ea6f2fbfdf86
danibonilha <dani.bonilha@live.com>
2022-10-03 17:06:09 -0300

    [CS-4652][sch-pay]: setup embroider

--- packages/scheduled-payments-client/ember-cli-build.js
+++ packages/scheduled-payments-client/ember-cli-build.js
@@ -1,24 +1,15 @@
 'use strict';
 
+process.env.EMBROIDER_REBUILD_ADDONS = '@cardstack/boxel';
+
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { Webpack } = require('@embroider/webpack');
+const { compatBuild } = require('@embroider/compat');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
-  return app.toTree();
+  return compatBuild(app, Webpack);
 };
