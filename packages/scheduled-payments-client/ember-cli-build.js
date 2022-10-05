733c6c0fdf2ae7cc7c323293b14c1a7d8763d748
Luke Melia <luke@lukemelia.com>
2022-10-05 09:46:51 -0400

    Add boxel dependency and necessary config and deps to scheduled-payments-client

--- packages/scheduled-payments-client/ember-cli-build.js
+++ packages/scheduled-payments-client/ember-cli-build.js
@@ -6,11 +6,73 @@ process.env.EMBROIDER_REBUILD_ADDONS = '@cardstack/boxel';
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const { Webpack } = require('@embroider/webpack');
 const { compatBuild } = require('@embroider/compat');
+const svgoUniqueId = require('svgo-unique-id');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    svgJar: {
+      strategy: 'inline',
+      sourceDirs: ['public'],
+      optimizer: {
+        svgoModule: require('svgo'),
+        plugins: [
+          { removeTitle: false },
+          { removeDesc: { removeAny: false } },
+          { removeViewBox: false },
+          {
+            cleanupIDs: { minify: false },
+          },
+          {
+            prefixIds: false,
+          },
+          {
+            uniqueID: svgoUniqueId,
+          },
+        ],
+      },
+    },
   });
 
-  return compatBuild(app, Webpack);
+  return compatBuild(app, Webpack, {
+    packagerOptions: {
+      webpackConfig: {
+        output: {
+          assetModuleFilename: '[path][name]-[contenthash][ext]',
+        },
+        module: {
+          rules: [
+            {
+              test: /\.(png|jpg|gif|woff|woff2|eot|ttf|otf|flac)$/i,
+              type: 'asset/resource',
+            },
+            {
+              test: /\.svg$/i,
+              type: 'asset/resource',
+              use: [
+                {
+                  loader: '@hyperbola/svgo-loader',
+                  options: {
+                    plugins: [
+                      { removeTitle: false },
+                      { removeDesc: { removeAny: false } },
+                      { removeViewBox: false },
+                      {
+                        cleanupIDs: { minify: false },
+                      },
+                      {
+                        prefixIds: false,
+                      },
+                      {
+                        uniqueID: svgoUniqueId,
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+  });
 };
