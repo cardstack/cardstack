813e4e7e945c11778af588361e62be1ac918a9e3
tintinthong <justinthong93@gmail.com>
2022-10-11 20:38:17 +0800

    add command

--- packages/cardpay-cli/rewards/index.ts
+++ packages/cardpay-cli/rewards/index.ts
@@ -15,6 +15,7 @@ import withdrawGasEstimate from './withdraw-gas-estimate';
 import checkClaimParams from './check-claim-params';
 import claimAllRewardsGasEstimate from './claim-all-rewards-gas-estimate';
 import claimAll from './claim-all';
+import getExplanation from './get-explanation';
 
 export const command = 'rewards <command>';
 export const desc = 'Commands to get interact with the reward programs and the reward manager contract';
@@ -38,6 +39,7 @@ export const builder = function (yargs: Argv) {
       checkClaimParams,
       claimAll,
       claimAllRewardsGasEstimate,
+      getExplanation,
     ] as any)
     .demandCommand(1, 'You must specify a valid subcommand');
 };
