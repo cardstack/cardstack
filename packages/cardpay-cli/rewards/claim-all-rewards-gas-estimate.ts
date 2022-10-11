d9937b5eb01cb9876020e27e65c507a23d9e9cb8
tintinthong <justinthong93@gmail.com>
2022-10-11 10:35:12 +0800

    update claim-all-gas-estimate

--- packages/cardpay-cli/rewards/claim-all-rewards-gas-estimate.ts
+++ packages/cardpay-cli/rewards/claim-all-rewards-gas-estimate.ts
@@ -6,7 +6,7 @@ import Web3 from 'web3';
 const { fromWei } = Web3.utils;
 
 export default {
-  command: 'claim-all-rewards-gas-estimate <rewardSafe> <tokenAddress> [rewardProgramId]',
+  command: 'claim-all-rewards-gas-estimate <rewardSafe> <rewardProgramId> <tokenAddress> ',
   describe: 'Obtain a gas estimate to claim all rewards corresponding to a rewardProgrmaId and tokenAddress',
   builder(yargs: Argv) {
     return yargs
@@ -14,13 +14,13 @@ export default {
         type: 'string',
         description: 'The address of the rewardSafe which will receive the rewards',
       })
+      .positional('rewardProgramId', {
+        type: 'string',
+        description: 'The reward program id.',
+      })
       .positional('tokenAddress', {
         type: 'string',
         description: 'The address of the tokens that are being claimed as rewards',
-      })
-      .option('rewardProgramId', {
-        type: 'string',
-        description: 'The reward program id.',
       })
       .option('network', NETWORK_OPTION_LAYER_2);
   },
@@ -29,13 +29,13 @@ export default {
       network: string;
       address: string;
       rewardSafe: string;
-      rewardProgramId?: string;
+      rewardProgramId: string;
       tokenAddress: string;
     };
     let { web3 } = await getEthereumClients(network, getConnectionType(args));
     let rewardPool = await getSDK('RewardPool', web3);
     let assets = await getSDK('Assets', web3);
-    const gasEstimate = await rewardPool.claimAllGasEstimate(rewardSafe, tokenAddress, rewardProgramId);
+    const gasEstimate = await rewardPool.claimAllGasEstimate(rewardSafe, rewardProgramId, tokenAddress);
     let { symbol } = await assets.getTokenInfo(tokenAddress);
     console.log(
       `The gas estimate for claiming ALL ${symbol} to reward safe ${rewardSafe} is ${fromWei(
