f539a2c00e926877d7f374ab64b4fd7877047fe1
Chi Jun <pcjun97@gmail.com>
2022-10-14 17:14:39 +0800

    Update deploy config to use env vars

--- packages/safe-tools-client/config/deploy.js
+++ packages/safe-tools-client/config/deploy.js
@@ -4,22 +4,20 @@ module.exports = function (deployTarget) {
   let ENV = {
     s3: {
       allowOverwrite: true,
+      bucket: process.env.AWS_S3_BUCKET,
     },
     cloudfront: {
       objectPaths: ['/*'],
+      distribution: process.env.AWS_CLOUDFRONT_DISTRIBUTION,
     },
   };
 
   if (deployTarget === 'staging') {
     ENV.build.environment = 'production';
-    ENV.s3.bucket = 'cardstack-safe-tools-client-staging';
-    ENV.cloudfront.distribution = 'PLACEHOLDER';
   }
 
   if (deployTarget === 'production') {
     ENV.build.environment = 'production';
-    ENV.s3.bucket = 'cardstack-safe-tools-client-production';
-    ENV.cloudfront.distribution = 'PLACEHOLDER';
   }
 
   return ENV;
