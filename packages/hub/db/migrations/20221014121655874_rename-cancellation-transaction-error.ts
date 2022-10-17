b705d2933eb31dad5b7f1e3506ec8032787a844c
Fadhlan Ridhwanallah <fridhwanallah@gmail.com>
2022-10-17 15:21:01 +0700

    linter fixes

--- packages/hub/db/migrations/20221014121655874_rename-cancellation-transaction-error.ts
+++ packages/hub/db/migrations/20221014121655874_rename-cancellation-transaction-error.ts
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/camelcase */
 import { MigrationBuilder } from 'node-pg-migrate';
 const TABLE = 'scheduled_payments';
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-    pgm.renameColumn(TABLE, 'cancellation_transaction_error', 'cancelation_transaction_error');
+  pgm.renameColumn(TABLE, 'cancellation_transaction_error', 'cancelation_transaction_error');
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
-    pgm.renameColumn(TABLE, 'cancelation_transaction_error', 'cancellation_transaction_error');
+  pgm.renameColumn(TABLE, 'cancelation_transaction_error', 'cancellation_transaction_error');
 }
