329fa3a9c685ec498e019329a14596f77dee3e95
danibonilha <dani.bonilha@live.com>
2022-10-17 12:40:54 -0300

    [Bug]: Fix missing styles on reload

--- packages/safe-tools-client/app/controllers/schedule.ts
+++ packages/safe-tools-client/app/controllers/schedule.ts
@@ -2,6 +2,8 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
+import '../css/schedule.css';
+
 export default class Schedule extends Controller {
   @tracked isSetupSafeModalOpen = false;
 
