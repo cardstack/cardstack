f39607f665fef84238e8af1fb8896f38cfbea78e
Buck Doyle <buck.doyle@cardstack.com>
2022-10-19 14:32:23 -0500

    Add ConnectButton to sidebar

--- packages/safe-tools-client/app/controllers/schedule.ts
+++ packages/safe-tools-client/app/controllers/schedule.ts
@@ -1,10 +1,13 @@
-import Controller from '@ember/controller';
+import Controller, { inject as controller } from '@ember/controller';
+import ApplicationController from '@cardstack/safe-tools-client/controllers/application';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 import '../css/schedule.css';
 
 export default class Schedule extends Controller {
+  @controller declare application: ApplicationController;
+
   @tracked isSetupSafeModalOpen = false;
 
   @action openSetupSafeModal() {
