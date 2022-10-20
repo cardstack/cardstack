fc4af3e16e3c6f71d522a851bfd0b4dc4d08b869
tintinthong <justinthong93@gmail.com>
2022-10-20 11:36:02 +0800

    update flat payment

--- packages/cardpay-reward-programs/cardpay_reward_programs/rules/flat_payment.py
+++ packages/cardpay-reward-programs/cardpay_reward_programs/rules/flat_payment.py
@@ -8,8 +8,16 @@ class FlatPayment(Rule):
     This rule pays a fixed amount to all specified users.
     """
 
-    def __init__(self, core_parameters, user_defined_parameters):
-        super(FlatPayment, self).__init__(core_parameters, user_defined_parameters)
+    def __init__(
+        self,
+        core_parameters,
+        user_defined_parameters,
+        explanation_block={},
+        metadata={},
+    ):
+        super(FlatPayment, self).__init__(
+            core_parameters, user_defined_parameters, explanation_block, metadata
+        )
 
     def set_user_defined_parameters(self, reward_per_user, token, accounts):
         self.token = token
@@ -22,7 +30,7 @@ class FlatPayment(Rule):
     def run(self, payment_cycle: int, reward_program_id: str):
         if len(self.accounts) == 0:
             return default_payment_list
-        return pd.DataFrame.from_records(
+        o = pd.DataFrame.from_records(
             {
                 "payee": account,
                 "rewardProgramID": reward_program_id,
@@ -34,3 +42,12 @@ class FlatPayment(Rule):
             }
             for account in self.accounts
         )
+        return o
+
+    def get_explanation_data_arr(self, payment_list):
+        explanation_data_arr = []
+        for payment in payment_list:
+            explanation_data_arr.append(
+                {"amount": payment["amount"], "token": payment["token"]}
+            )
+        return explanation_data_arr
