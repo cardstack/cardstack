dae534827e1100a03c53e22a64861bb75263c074
Dani Bonilha <dani.bonilha@live.com>
2022-10-20 15:41:23 -0300

    [CS-4658]: Add safe info to control panel (#3334)
    * [CS-4658]: Add safe info to control panel
    
    * [CR]: Use action to set modal flags

--- packages/safe-tools-client/app/controllers/schedule.ts
+++ packages/safe-tools-client/app/controllers/schedule.ts
@@ -1,6 +1,8 @@
 import Controller, { inject as controller } from '@ember/controller';
-import ApplicationController from '@cardstack/safe-tools-client/controllers/application';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+import ApplicationController from '@cardstack/safe-tools-client/controllers/application';
 
 import '../css/schedule.css';
 
@@ -9,6 +11,35 @@ export default class Schedule extends Controller {
 
   // modified with set helper
   @tracked isSetupSafeModalOpen = false;
+  @tracked isDepositModalOpen = false;
+
+  get safe() {
+    // Should we grab the network nativeToken info here, or expect the sdk method to return it within safeInfo?
+
+    //TODO: get safe info from sdk and format it
+    return {
+      address: '0xABCD...EFFF',
+      token: {
+        address: 'eth',
+        name: 'Ethereum',
+        symbol: 'ETH',
+        decimals: 18,
+        balance: 0,
+      },
+    };
+  }
+
+  get safeButtonLabel() {
+    return this.safe ? 'Add Funds' : 'Create Safe';
+  }
+
+  @action onSafeButtonClick() {
+    if (this.safe) {
+      this.isDepositModalOpen = true;
+    } else {
+      this.isSetupSafeModalOpen = true;
+    }
+  }
 }
 
 declare module '@ember/controller' {
