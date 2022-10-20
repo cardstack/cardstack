33e4dabee011784c3940329bd5365feb51442124
Buck Doyle <buck.doyle@cardstack.com>
2022-10-20 15:37:21 -0500

    Add reconnect on load

--- packages/safe-tools-client/app/services/wallet.ts
+++ packages/safe-tools-client/app/services/wallet.ts
@@ -12,12 +12,16 @@ import walletProviders, {
 import Web3 from 'web3';
 import { timeout } from 'ember-concurrency';
 
+const CHAIN_NAME_FIXME = 'mainnet';
+const CHAIN_ID_FIXME = 1;
+
 export default class Wallet extends Service {
   @tracked isConnected = false;
   @tracked providerId: WalletProviderId | undefined;
   @tracked address: string | undefined;
 
-  chainConnectionManager = new ChainConnectionManager('mainnet'); // FIXME generalise
+  web3 = new Web3();
+  chainConnectionManager = new ChainConnectionManager(CHAIN_NAME_FIXME);
 
   walletProviders = walletProviders.map((w) =>
     w.id === 'metamask'
@@ -38,6 +42,15 @@ export default class Wallet extends Service {
       this.isConnected = true;
       this.address = accounts[0];
     });
+
+    const providerId =
+      ChainConnectionManager.getProviderIdForChain(CHAIN_ID_FIXME);
+    if (providerId !== 'wallet-connect' && providerId !== 'metamask') {
+      return;
+    }
+
+    this.providerId = providerId;
+    this.chainConnectionManager.reconnect(this.web3, this.providerId!);
   }
 
   @action connect() {
@@ -47,8 +60,7 @@ export default class Wallet extends Service {
   }
 
   @task *connectWalletTask() {
-    const web3 = new Web3();
-    yield this.chainConnectionManager.connect(web3, this.providerId!);
+    yield this.chainConnectionManager.connect(this.web3, this.providerId!);
     yield timeout(500); // allow time for strategy to verify connected chain -- it might not accept the connection
   }
 
