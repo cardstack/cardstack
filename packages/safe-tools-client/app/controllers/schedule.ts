641d4ac5cf2fdd981cc1f2dcc745a557568f2cda
Dani Bonilha <dani.bonilha@live.com>
2022-10-24 14:32:36 -0300

    [CS-4662][safe-tools]: Add deposit modal (#3355)
    * [CS-4662]: Add deposit modal no payments yet
    
    * [CS-4662]: Add size option to icon accessory
    
    * [CS-4662]: Update copy icon
    
    * [CS-4662]: Add custom color to boxel info icon
    
    * [CS-4662]: Add yellow banner to deposit modal
    
    * [CS-4662]: Add deposit modal to schedule tab
    
    * [CS-4662]: Update address
    
    * [CS-4662]: Conditionally render yellow banner
    
    * [CS-4662][CR]: Apply same apostrophe char

--- packages/safe-tools-client/app/controllers/schedule.ts
+++ packages/safe-tools-client/app/controllers/schedule.ts
@@ -18,7 +18,7 @@ export default class Schedule extends Controller {
 
     //TODO: get safe info from sdk and format it
     return {
-      address: '0xABCD...EFFF',
+      address: '0x8a40AFffb53f4F953a204cAE087219A28771df9d',
       token: {
         address: 'eth',
         name: 'Ethereum',
@@ -39,6 +39,46 @@ export default class Schedule extends Controller {
     } else {
       this.isSetupSafeModalOpen = true;
     }
+  }
+
+  // TODO: add selected network
+  get network() {
+    return 'Ethereum';
+  }
+
+  get scheduledPaymentsTokensToCover() {
+    const hasScheduledPayments = false;
+    // TODO: Add helper functions to map amounts, use fromWei function
+    return hasScheduledPayments
+      ? {
+          nextMonth: {
+            tokens: [
+              {
+                symbol: 'ETH',
+                amountToCover: 0,
+              },
+              {
+                symbol: 'USDC',
+                amountToCover: 0,
+              },
+              {
+                symbol: 'USDT',
+                amountToCover: 0,
+              },
+            ],
+            hasEnoughBalance: true,
+          },
+          nextSixMonths: {
+            tokens: [
+              {
+                symbol: 'DAI',
+                amountToCover: 0,
+              },
+            ],
+            hasEnoughBalance: true,
+          },
+        }
+      : undefined;
   }
 }
 
