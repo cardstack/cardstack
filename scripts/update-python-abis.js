58c270a8d7af8a7a56fb4b87c9bea842de5915af
Ian Calvert <ianjcalvert@gmail.com>
2022-10-25 10:25:13 +0100

    Linting issues

--- scripts/update-python-abis.js
+++ scripts/update-python-abis.js
@@ -1,29 +1,26 @@
-const glob = require("glob");
+const glob = require('glob');
 const path = require('path');
-const { readFileSync, outputFile } = require("fs-extra");
-const prettier = require("prettier");
-const kebabCase = require("lodash/kebabCase");
+// eslint-disable-next-line node/no-extraneous-require
+const { readFileSync, outputFile } = require('fs-extra');
 
-const targetDir = "abi/";
+const globPattern = 'packages/cardpay-sdk/contracts/abi/latest/*.ts';
 
-const globPattern = "packages/cardpay-sdk/contracts/abi/latest/*.ts";
-
-const pythonPackages = ["cardpay-reward-api", "cardpay-reward-scheduler", "reward-root-submitter"]
+const pythonPackages = ['cardpay-reward-api', 'cardpay-reward-scheduler', 'reward-root-submitter'];
 
 glob(globPattern, {}, (err, files) => {
   if (err) {
     throw new Error(err);
   }
   files.map((file) => {
-    let o;
     const typescriptModuleContents = readFileSync(file).toString();
-    const contractAbi = typescriptModuleContents.replace('export default ', '').replace('function noop() {};', '').replace('noop();', '');
+    const contractAbi = typescriptModuleContents
+      .replace('export default ', '')
+      .replace('function noop() {};', '')
+      .replace('noop();', '');
     const jsonAbi = JSON.stringify(eval(contractAbi), null, 2);
     let fileName = path.basename(file, '.ts') + '.json';
     pythonPackages.map((pythonPackage) => {
-        outputFile(
-            path.join("packages", pythonPackage, "abis", fileName), jsonAbi
-        );
+      outputFile(path.join('packages', pythonPackage, 'abis', fileName), jsonAbi);
     });
   });
 });
