87648a5c48449b4cb4b9a12995f7bb11629766e6
Luke Melia <luke@lukemelia.com>
2022-10-25 22:13:33 -0400

    Fix linting

--- packages/ember-helpers/addon/.eslintrc.js
+++ packages/ember-helpers/addon/.eslintrc.js
@@ -39,6 +39,11 @@ module.exports = {
       },
       plugins: ['node'],
       extends: ['plugin:node/recommended'],
+      rules: {
+        // this can be removed once the following is fixed
+        // https://github.com/mysticatea/eslint-plugin-node/issues/77
+        'node/no-unpublished-require': 'off',
+      },
     },
   ],
 };
