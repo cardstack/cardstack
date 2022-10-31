cb564df867a5f6804715ff770154c9fa3efa04ae
tintinthong <justinthong93@gmail.com>
2022-10-31 20:03:08 +0800

    add read parquet file

---
+++ packages/cardpay-reward-programs/scripts/read_parquet.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import typer
+from cloudpathlib import AnyPath
+
+
+def read_parquet(
+    reward_program_id: str, 
+    payment_cycle: int
+):
+    path = AnyPath(
+        f"s3://cardpay-staging-reward-programs/rewardProgramID={reward_program_id}/paymentCycle={payment_cycle}/results.parquet"
+    )
+    o = pd.read_parquet(path)
+    print(o)
+
+
+if __name__ == "__main__":
+    typer.run(read_parquet)
