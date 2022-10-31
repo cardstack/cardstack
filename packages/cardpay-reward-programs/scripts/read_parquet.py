a077a2af154b02bab5a187a53f502c66749ecd49
tintinthong <justinthong93@gmail.com>
2022-10-31 20:16:29 +0800

    black fo lint

--- packages/cardpay-reward-programs/scripts/read_parquet.py
+++ packages/cardpay-reward-programs/scripts/read_parquet.py
@@ -3,10 +3,7 @@ import typer
 from cloudpathlib import AnyPath
 
 
-def read_parquet(
-    reward_program_id: str, 
-    payment_cycle: int
-):
+def read_parquet(reward_program_id: str, payment_cycle: int):
     path = AnyPath(
         f"s3://cardpay-staging-reward-programs/rewardProgramID={reward_program_id}/paymentCycle={payment_cycle}/results.parquet"
     )
