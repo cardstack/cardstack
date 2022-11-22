e207ac21f84a4b5eb101b09b9f8743fcc4f890a3
Matic Jurglič <matic@jurglic.si>
2022-11-22 10:34:07 +0100

    Add test cases

--- packages/hub/node-tests/tasks/scheduled-payment-on-chain-cancelation-waiter-test.ts
+++ packages/hub/node-tests/tasks/scheduled-payment-on-chain-cancelation-waiter-test.ts
@@ -1,5 +1,7 @@
 import { expect } from 'chai';
+import { subDays } from 'date-fns';
 import ScheduledPaymentOnChainCancelationWaiter from '../../tasks/scheduled-payment-on-chain-cancelation-waiter';
+import { nowUtc } from '../../utils/dates';
 import { registry, setupHub } from '../helpers/server';
 
 let sdkError: Error | null = null;
@@ -36,6 +38,72 @@ describe('ScheduledPaymentOnChainCancelationWaiterTask', function () {
   });
 
   let { getPrisma, getContainer } = setupHub(this);
+
+  it('returns an error if there is no cancelation tx hash', async function () {
+    let prisma = await getPrisma();
+    await prisma.scheduledPayment.create({
+      data: {
+        id: '73994d4b-bb3a-4d73-969f-6fa24da16fb4',
+        senderSafeAddress: '0xc0ffee254729296a45a3885639AC7E10F9d54979',
+        moduleAddress: '0x7E7d0B97D663e268bB403eb4d72f7C0C7650a6dd',
+        tokenAddress: '0xa455bbB2A81E09E0337c13326BBb302Cb37D7cf6',
+        gasTokenAddress: '0x6A50E3807FB9cD0B07a79F64e561B9873D3b132E',
+        amount: '100',
+        payeeAddress: '0x821f3Ee0FbE6D1aCDAC160b5d120390Fb8D2e9d3',
+        executionGasEstimation: 100000,
+        maxGasPrice: '1000000000',
+        feeFixedUsd: 0,
+        feePercentage: 0,
+        salt: '54lt',
+        payAt: new Date(),
+        spHash: '0x123',
+        chainId: 1,
+        userAddress: '0x57022DA74ec3e6d8274918C732cf8864be7da833',
+        cancelationTransactionHash: null,
+      },
+    });
+    let task = await getContainer().instantiate(ScheduledPaymentOnChainCancelationWaiter);
+    let result = await task.perform({ scheduledPaymentId: '73994d4b-bb3a-4d73-969f-6fa24da16fb4' });
+
+    expect(result).to.deep.equal({
+      status: 'failure',
+      message: 'Missing transaction hash',
+    });
+  });
+
+  it('returns an error if scheduled payment has been canceled more than a day ago and the cancelation transaction still has not been mined', async function () {
+    let prisma = await getPrisma();
+    await prisma.scheduledPayment.create({
+      data: {
+        id: '73994d4b-bb3a-4d73-969f-6fa24da16fb4',
+        senderSafeAddress: '0xc0ffee254729296a45a3885639AC7E10F9d54979',
+        moduleAddress: '0x7E7d0B97D663e268bB403eb4d72f7C0C7650a6dd',
+        tokenAddress: '0xa455bbB2A81E09E0337c13326BBb302Cb37D7cf6',
+        gasTokenAddress: '0x6A50E3807FB9cD0B07a79F64e561B9873D3b132E',
+        amount: '100',
+        payeeAddress: '0x821f3Ee0FbE6D1aCDAC160b5d120390Fb8D2e9d3',
+        executionGasEstimation: 100000,
+        maxGasPrice: '1000000000',
+        feeFixedUsd: 0,
+        feePercentage: 0,
+        salt: '54lt',
+        payAt: null,
+        canceledAt: subDays(nowUtc(), 2),
+        spHash: '0x123',
+        chainId: 1,
+        userAddress: '0x57022DA74ec3e6d8274918C732cf8864be7da833',
+        cancelationTransactionHash: '0x123',
+      },
+    });
+    let task = await getContainer().instantiate(ScheduledPaymentOnChainCancelationWaiter);
+    let result = await task.perform({ scheduledPaymentId: '73994d4b-bb3a-4d73-969f-6fa24da16fb4' });
+
+    expect(result).to.deep.equal({
+      status: 'failure',
+      message:
+        'Scheduled payment has been canceled more than a day ago and the cancelation transaction still has not been mined',
+    });
+  });
 
   it('waits for the transaction to be mined and updates block number', async function () {
     let prisma = await getPrisma();
--- packages/hub/tasks/scheduled-payment-on-chain-cancelation-waiter.ts
+++ packages/hub/tasks/scheduled-payment-on-chain-cancelation-waiter.ts
@@ -3,12 +3,17 @@ import * as Sentry from '@sentry/node';
 import { isBefore, subDays } from 'date-fns';
 import { nowUtc } from '../utils/dates';
 
+interface TaskResult {
+  status: 'success' | 'failure';
+  message: string;
+}
+
 export default class ScheduledPaymentOnChainCancelationWaiter {
   prismaManager = inject('prisma-manager', { as: 'prismaManager' });
   cardpay = inject('cardpay');
   ethersProvider = inject('ethers-provider', { as: 'ethersProvider' });
 
-  async perform(payload: { scheduledPaymentId: string }) {
+  async perform(payload: { scheduledPaymentId: string }): Promise<TaskResult> {
     let prisma = await this.prismaManager.getClient();
     let scheduledPayment = await prisma.scheduledPayment.findFirstOrThrow({
       where: { id: payload.scheduledPaymentId },
@@ -19,44 +24,44 @@ export default class ScheduledPaymentOnChainCancelationWaiter {
 
     if (scheduledPayment.cancelationBlockNumber) {
       // Did we somehow spawn this task after it already updated cancelationBlockNumber previously?
-      return Sentry.captureException(
-        new Error('Task run for scheduled payment that already has a cancelation block number'),
-        {
-          tags: {
-            action: 'scheduled-payment-task',
-            scheduledPaymentId: scheduledPayment.id,
-          },
-        }
-      );
-    }
-
-    if (!scheduledPayment.cancelationTransactionHash) {
-      // This should never happen because we spawn this task only after updating cancelationTransactionHash in the route. But just in case...
-      return Sentry.captureException(new Error('Missing transaction hash'), {
+      let errorMessage = 'Task run for scheduled payment that already has a cancelation block number';
+      Sentry.captureException(new Error(errorMessage), {
         tags: {
           action: 'scheduled-payment-task',
           scheduledPaymentId: scheduledPayment.id,
         },
       });
+
+      return { status: 'failure', message: errorMessage };
+    }
+
+    if (!scheduledPayment.cancelationTransactionHash) {
+      // This should never happen because we spawn this task only after updating cancelationTransactionHash in the route. But just in case...
+      let errorMessage = 'Missing transaction hash';
+      Sentry.captureException(new Error(errorMessage), {
+        tags: {
+          action: 'scheduled-payment-task',
+          scheduledPaymentId: scheduledPayment.id,
+        },
+      });
+      return { status: 'failure', message: errorMessage };
     }
 
     if (isBefore(scheduledPayment.canceledAt!, subDays(nowUtc(), 1))) {
-      return Sentry.captureException(
-        new Error(
-          'Scheduled payment has been canceled more than a day ago and the cancelation transaction still has not been mined'
-        ),
-        {
-          tags: {
-            action: 'scheduled-payment-task',
-            scheduledPaymentId: scheduledPayment.id,
-          },
-        }
-      );
+      let errorMessage =
+        'Scheduled payment has been canceled more than a day ago and the cancelation transaction still has not been mined';
+      Sentry.captureException(new Error(errorMessage), {
+        tags: {
+          action: 'scheduled-payment-task',
+          scheduledPaymentId: scheduledPayment.id,
+        },
+      });
+      return { status: 'failure', message: errorMessage };
     }
 
     try {
       let receipt = await scheduledPaymentModule.cancelPaymentOnChain(scheduledPayment.cancelationTransactionHash);
-      return await prisma.scheduledPayment.update({
+      await prisma.scheduledPayment.update({
         data: {
           cancelationBlockNumber: receipt.blockNumber, // To let us know that the scheduled payment has been created on chain and that the process of scheduling the payment was completed
         },
@@ -64,6 +69,7 @@ export default class ScheduledPaymentOnChainCancelationWaiter {
           id: scheduledPayment.id,
         },
       });
+      return { status: 'success', message: 'Scheduled payment cancelation transaction mined' };
     } catch (error: any) {
       if (error.message.includes('revert')) {
         // Error message from the waitUntilTransactionMined in the SDK will be: Transaction with hash "${txnHash}" was reverted
@@ -76,7 +82,8 @@ export default class ScheduledPaymentOnChainCancelationWaiter {
           },
         });
 
-        return; // We don't want to throw an error here because we don't want the task to be retried - the transaction was obviously reverted
+        // We don't want to throw an error here because we don't want the task to be retried - the transaction was obviously reverted
+        return { status: 'failure', message: error.message };
       }
 
       // At this point, we don't know what error to expect. Let's throw the error so that the task will restart
