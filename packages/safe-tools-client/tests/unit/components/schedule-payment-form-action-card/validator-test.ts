c9931f09626d2d04258cf68eeacea499835daa9d
Fadhlan Ridhwanallah <fridhwanallah@gmail.com>
2022-12-13 22:03:40 +0700

    lint fixes

--- packages/safe-tools-client/tests/unit/components/schedule-payment-form-action-card/validator-test.ts
+++ packages/safe-tools-client/tests/unit/components/schedule-payment-form-action-card/validator-test.ts
@@ -31,6 +31,9 @@ module('Unit | SchedulePaymentFormActionCard Validator', function (hooks) {
           'https://assets-cdn.trustwallet.com/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
       },
       maxGasFee: 'normal',
+      minPaymentDate: new Date(),
+      minPaymentTime: new Date(),
+      minMonthlyUntil: new Date(),
     };
   });
 
