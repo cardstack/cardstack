12eadf7dcda22776093824fe30b962fc026978cc
tintinthong <justinthong93@gmail.com>
2022-12-15 21:46:08 +0800

    add reward proofs migration

---
+++ packages/hub/db/migrations/20221215134519797_reward-proofs.ts
@@ -0,0 +1,22 @@
+import { MigrationBuilder } from 'node-pg-migrate';
+
+const TABLE = 'reward_proofs';
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createTable(TABLE, {
+    reward_program_id: { type: 'string', notNull: true },
+    payee: { type: 'string', notNull: true },
+    leaf: { type: 'string', unique: true, notNull: true, primaryKey: true },
+    payment_cycle: { type: 'integer', notNull: true },
+    proof: { type: 'text[]', notNull: true },
+    token_type: { type: 'integer', notNull: true },
+    valid_from: { type: 'integer', notNull: true },
+    valid_to: { type: 'integer', notNull: true },
+    explanation_id: { type: 'string' },
+    explanation_data: { type: 'json' },
+  });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable(TABLE);
+}
