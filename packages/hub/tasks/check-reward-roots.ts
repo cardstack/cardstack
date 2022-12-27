eaffe4409c1f322fec81d466cc8f20c377039ad0
tintinthong <justinthong93@gmail.com>
2022-12-27 18:01:43 +0800

    change index size to 1000

--- packages/hub/tasks/check-reward-roots.ts
+++ packages/hub/tasks/check-reward-roots.ts
@@ -2,39 +2,43 @@ import { inject } from '@cardstack/di';
 import WorkerClient from '../services/worker-client';
 import { RewardPrograms, RewardRoots } from '../services/subgraph';
 
-const MAX_INDEX_SIZE_PROGRAM = 100;
+const MAX_INDEX_SIZE_PROGRAM = 1000;
 
 export default class CheckRewardRoots {
   subgraph = inject('subgraph');
   databaseManager = inject('database-manager', { as: 'databaseManager' });
   workerClient: WorkerClient = inject('worker-client', { as: 'workerClient' });
   async perform(max_index_size_per_program = MAX_INDEX_SIZE_PROGRAM) {
-    let db = await this.databaseManager.getClient();
-    const r1: RewardPrograms = await this.subgraph.getRewardPrograms();
-    const rewardProgramIds = r1?.data?.rewardPrograms.map((o) => o.id);
-    for (const rewardProgramId of rewardProgramIds) {
-      const LAST_INDEXED_BLOCK_QUERY = `SELECT COALESCE(MAX(payment_cycle),0) as last_indexed_block_number FROM reward_root_index WHERE reward_program_id='${rewardProgramId}';`;
-      const {
-        rows: [{ last_indexed_block_number }],
-      } = await db.query(LAST_INDEXED_BLOCK_QUERY);
-      const r2: RewardRoots = await this.subgraph.getRewardRoots(
-        rewardProgramId,
-        last_indexed_block_number,
-        max_index_size_per_program
-      );
-      let files: S3FileInfo[] = r2?.data?.merkleRootSubmissions.map((root) => {
-        return {
-          paymentCycle: root.paymentCycle,
-          rewardProgramId: root.rewardProgram.id,
-        };
-      });
-      for (const file of files) {
-        // Process 1 parquet (rewardProgramId, paymentCycle) file at one time
-        await this.workerClient.addJob('process-reward-root', file, {
-          jobKey: file.rewardProgramId + '-' + file.paymentCycle,
-          maxAttempts: 1,
+    try {
+      let db = await this.databaseManager.getClient();
+      const r1: RewardPrograms = await this.subgraph.getRewardPrograms();
+      const rewardProgramIds = r1?.data?.rewardPrograms.map((o) => o.id);
+      for (const rewardProgramId of rewardProgramIds) {
+        const LAST_INDEXED_BLOCK_QUERY = `SELECT COALESCE(MAX(payment_cycle),0) as last_indexed_block_number FROM reward_root_index WHERE reward_program_id='${rewardProgramId}';`;
+        const {
+          rows: [{ last_indexed_block_number }],
+        } = await db.query(LAST_INDEXED_BLOCK_QUERY);
+        const r2: RewardRoots = await this.subgraph.getRewardRoots(
+          rewardProgramId,
+          last_indexed_block_number,
+          max_index_size_per_program
+        );
+        let files: S3FileInfo[] = r2?.data?.merkleRootSubmissions.map((root) => {
+          return {
+            paymentCycle: root.paymentCycle,
+            rewardProgramId: root.rewardProgram.id,
+          };
         });
+        for (const file of files) {
+          // Process 1 parquet (rewardProgramId, paymentCycle) file at one time
+          await this.workerClient.addJob('process-reward-root', file, {
+            jobKey: file.rewardProgramId + '-' + file.paymentCycle,
+            maxAttempts: 1,
+          });
+        }
       }
+    } catch (e) {
+      console.log(e);
     }
   }
 }
