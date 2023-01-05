b44828895cb3ce71ccc3c3e54833649e7ffff3e1
Buck Doyle <buck.doyle@cardstack.com>
2023-01-05 14:33:47 -0600

    Remove hardcoded Hub URL

--- packages/ssr-web/tests/fastboot/visit-profile-test.js
+++ packages/ssr-web/tests/fastboot/visit-profile-test.js
@@ -4,12 +4,13 @@ import {
   visit,
   mockServer,
 } from 'ember-cli-fastboot-testing/test-support';
+import config from '@cardstack/ssr-web/config/environment';
 
 module('FastBoot | profile', function (hooks) {
   setup(hooks);
 
   test('it renders a profile', async function (assert) {
-    await mockServer.get('http://example.com/api/profiles/mandello', {
+    await mockServer.get(`${config.hubURL}/api/profiles/mandello`, {
       meta: { network: 'sokol' },
       data: {
         id: 'db7d5245-15be-4c24-9392-e5316387c5e4',
