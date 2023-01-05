3a68a412431c6e98fbf192d72a0bf13ee5efe45f
Buck Doyle <buck.doyle@cardstack.com>
2023-01-05 13:30:08 -0600

    Change server dist path to match

--- packages/ssr-web/deployment/fastboot-server.js
+++ packages/ssr-web/deployment/fastboot-server.js
@@ -22,7 +22,7 @@ Sentry.init({
 
 let server = new FastBootAppServer({
   log: false,
-  distPath: 'dist',
+  distPath: '/tmp/deploy-dist',
   gzip: true, // Optional - Enables gzip compression.
   host: '0.0.0.0', // Optional - Sets the host the server listens on.
   port: 4000, // Optional - Sets the port the server listens on (defaults to the PORT env var or 3000).
