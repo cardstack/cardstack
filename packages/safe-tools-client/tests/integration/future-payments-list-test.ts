c93566c61ec317e10d9fb094efa2813a1fbb0224
Fadhlan Ridhwanallah <fridhwanallah@gmail.com>
2023-01-06 16:13:43 +0700

    Update time windows

--- packages/safe-tools-client/tests/integration/future-payments-list-test.ts
+++ packages/safe-tools-client/tests/integration/future-payments-list-test.ts
@@ -2,7 +2,11 @@
 import Service from '@ember/service';
 import { render, TestContext } from '@ember/test-helpers';
 import percySnapshot from '@percy/ember';
-import { addMinutes, addMonths, addHours } from 'date-fns';
+import { addDays, addHours, startOfDay, endOfDay, endOfMonth } from 'date-fns';
+import {
+  setupFakeDateService,
+  FakeDateService,
+} from 'ember-date-service/test-support';
 
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
@@ -16,14 +20,29 @@ class HubAuthenticationServiceStub extends Service {
   isAuthenticated = true;
 }
 
+const NOW = new Date(2023, 0, 1);
+
 let returnEmptyScheduledPayments = false;
+let returnScheduledPaymentsUntilTomorrow = false;
+let dateService: FakeDateService;
 
 class ScheduledPaymentsStub extends Service {
   fetchScheduledPayments = (chainId: number, minPayAt?: Date) => {
     if (returnEmptyScheduledPayments || !minPayAt) {
       return Promise.resolve([]);
     }
-    return Promise.resolve([
+
+    const startOfToday = startOfDay(minPayAt);
+    const endOfToday = endOfDay(minPayAt);
+    const startOfTomorrow = startOfDay(addDays(minPayAt, 1));
+    const endOfTomorrow = endOfDay(addDays(minPayAt, 1));
+    const endOfThisMonth = endOfMonth(minPayAt);
+
+    // 3 SPs are today
+    // 2 Sps are tomorrow
+    // 1 Sp is this month
+    // 1 Sp is later
+    const scheduledPayments = [
       {
         amount: '10000000',
         feeFixedUSD: '0',
@@ -32,7 +51,7 @@ class ScheduledPaymentsStub extends Service {
         tokenAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
         chainId,
         payeeAddress: '0xeBCC5516d44FFf5E9aBa2AcaeB65BbB49bC3EBe1',
-        payAt: addMinutes(addHours(minPayAt, 1), 5),
+        payAt: startOfToday,
       },
       {
         amount: '10000000',
@@ -42,7 +61,7 @@ class ScheduledPaymentsStub extends Service {
         tokenAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
         chainId,
         payeeAddress: '0xeBCC5516d44FFf5E9aBa2AcaeB65BbB49bC3EBe1',
-        payAt: addMinutes(addHours(minPayAt, 1), 20),
+        payAt: addHours(startOfToday, 2),
       },
       {
         amount: '11000000',
@@ -52,36 +71,80 @@ class ScheduledPaymentsStub extends Service {
         tokenAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
         chainId,
         payeeAddress: '0xeBCC5516d44FFf5E9aBa2AcaeB65BbB49bC3EBe1',
-        payAt: addMonths(minPayAt, 1),
+        payAt: endOfToday,
       },
       {
-        amount: '20000000',
+        amount: '11000000',
         feeFixedUSD: '0',
         feePercentage: '0',
         gasTokenAddress: '0x123',
         tokenAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
         chainId,
         payeeAddress: '0xeBCC5516d44FFf5E9aBa2AcaeB65BbB49bC3EBe1',
-        payAt: addMonths(minPayAt, 3),
+        payAt: startOfTomorrow,
       },
-    ]);
+      {
+        amount: '11000000',
+        feeFixedUSD: '0',
+        feePercentage: '0',
+        gasTokenAddress: '0x123',
+        tokenAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+        chainId,
+        payeeAddress: '0xeBCC5516d44FFf5E9aBa2AcaeB65BbB49bC3EBe1',
+        payAt: endOfTomorrow,
+      },
+      {
+        amount: '11000000',
+        feeFixedUSD: '0',
+        feePercentage: '0',
+        gasTokenAddress: '0x123',
+        tokenAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+        chainId,
+        payeeAddress: '0xeBCC5516d44FFf5E9aBa2AcaeB65BbB49bC3EBe1',
+        payAt: endOfThisMonth,
+      },
+      {
+        amount: '11000000',
+        feeFixedUSD: '0',
+        feePercentage: '0',
+        gasTokenAddress: '0x123',
+        tokenAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+        chainId,
+        payeeAddress: '0xeBCC5516d44FFf5E9aBa2AcaeB65BbB49bC3EBe1',
+        payAt: addDays(endOfThisMonth, 1),
+      },
+    ];
+
+    if (returnScheduledPaymentsUntilTomorrow) {
+      return Promise.resolve(
+        scheduledPayments.filter((sp) => sp.payAt <= endOfTomorrow)
+      );
+    }
+
+    return Promise.resolve(scheduledPayments);
   };
 }
 
 module('Integration | Component | future-payments-list', function (hooks) {
   setupRenderingTest(hooks);
+  setupFakeDateService(hooks);
 
-  hooks.beforeEach(function (this: TestContext) {
+  hooks.beforeEach(async function (this: TestContext) {
     this.owner.register('service:wallet', WalletServiceStub);
     this.owner.register('service:scheduled-payments', ScheduledPaymentsStub);
     this.owner.register(
       'service:hub-authentication',
       HubAuthenticationServiceStub
     );
+
+    dateService = this.owner.lookup('service:date') as FakeDateService;
+    await dateService.setNow(NOW.getTime());
   });
 
   hooks.afterEach(function () {
     returnEmptyScheduledPayments = false;
+    returnScheduledPaymentsUntilTomorrow = false;
+    dateService.reset();
   });
 
   test('It renders no payments display if no future payments', async function (assert) {
@@ -98,7 +161,7 @@ module('Integration | Component | future-payments-list', function (hooks) {
   });
 
   test('It renders future payments list', async function (assert) {
-    assert.expect(5);
+    assert.expect(6);
     this.set('onDepositClick', () => {});
     await render(hbs`
       <FuturePaymentsList @onDepositClick={{this.onDepositClick}} />
@@ -109,21 +172,54 @@ module('Integration | Component | future-payments-list', function (hooks) {
     assert.dom('[data-test-future-payments-list]').isVisible();
     assert.strictEqual(
       document.querySelectorAll(
-        `[data-test-time-bracket='next hour'] [data-test-scheduled-payment-card]`
+        `[data-test-time-bracket='today'] [data-test-scheduled-payment-card]`
+      ).length,
+      3
+    );
+    assert.strictEqual(
+      document.querySelectorAll(
+        `[data-test-time-bracket='tomorrow'] [data-test-scheduled-payment-card]`
       ).length,
       2
     );
     assert.strictEqual(
       document.querySelectorAll(
-        `[data-test-time-bracket='next month'] [data-test-scheduled-payment-card]`
+        `[data-test-time-bracket='this month'] [data-test-scheduled-payment-card]`
       ).length,
       1
     );
     assert.strictEqual(
       document.querySelectorAll(
-        `[data-test-time-bracket='next few months'] [data-test-scheduled-payment-card]`
+        `[data-test-time-bracket='later'] [data-test-scheduled-payment-card]`
       ).length,
       1
     );
+  });
+
+  test('It renders future payments list, with no this month and later time windows', async function (assert) {
+    assert.expect(6);
+    returnScheduledPaymentsUntilTomorrow = true;
+    this.set('onDepositClick', () => {});
+    await render(hbs`
+      <FuturePaymentsList @onDepositClick={{this.onDepositClick}} />
+    `);
+
+    await percySnapshot(assert);
+    assert.dom('[data-test-no-future-payments-list]').isNotVisible();
+    assert.dom('[data-test-future-payments-list]').isVisible();
+    assert.strictEqual(
+      document.querySelectorAll(
+        `[data-test-time-bracket='today'] [data-test-scheduled-payment-card]`
+      ).length,
+      3
+    );
+    assert.strictEqual(
+      document.querySelectorAll(
+        `[data-test-time-bracket='tomorrow'] [data-test-scheduled-payment-card]`
+      ).length,
+      2
+    );
+    assert.dom(`[data-test-time-bracket='this month']`).isNotVisible();
+    assert.dom(`[data-test-time-bracket='later']`).isNotVisible();
   });
 });
