d3c9029988dcba1c7c12409b15c106d8ee9e5072
Chi Jun <pcjun97@gmail.com>
2023-01-12 22:31:35 +0800

    Fix eslint errors

---
+++ scripts/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  extends: '@cardstack/eslint-config',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+      rules: {
+        '@typescript-eslint/consistent-type-exports': 'error',
+        '@typescript-eslint/ban-types': [
+          'error',
+          {
+            types: {
+              TransactionReceipt:
+                'Use SuccessfulTransactionReceipt for SDK return types. Unsuccessful transaction receipts should throw.',
+            },
+          },
+        ],
+      },
+    },
+  ],
+  env: {
+    commonjs: true,
+    node: true,
+    mocha: true,
+  },
+};
