2353eb8d5c579cde6d3a955f473a14876ec1fdfe
Matic Jurglič <matic@jurglic.si>
2023-01-23 12:02:54 +0100

    Ensure payments are executed at least 1min after payAt (defence against block time manipulation effects)

--- packages/hub/services/scheduled-payments/fetcher.ts
+++ packages/hub/services/scheduled-payments/fetcher.ts
@@ -1,6 +1,6 @@
 import { inject } from '@cardstack/di';
 import { ScheduledPayment } from '@prisma/client';
-import { startOfDay, subDays } from 'date-fns';
+import { startOfDay, subDays, subMinutes } from 'date-fns';
 import { nowUtc } from '../../utils/dates';
 
 export default class ScheduledPaymentsFetcherService {
@@ -8,8 +8,7 @@ export default class ScheduledPaymentsFetcherService {
   validForDays = 3;
 
   // This query fetches scheduled payments that are due to be executed now.
-  // Will prioritize scheduled payments with no payment attempts.
-  // After that scheduled payments with the earlier failed payment attempts.
+  // Will prioritize scheduled payments with no payment attempts, and fter that scheduled payments with the earlier failed payment attempts.
   // Payment scheduler will periodically try to fetch these payments at regular intervals and execute them if they are due.
   // This query mostly relies on the payAt field, which is set to the time when the payment should be executed.
   // In case of one-time payments, payAt is set only initially, and then never changed.
@@ -17,6 +16,10 @@ export default class ScheduledPaymentsFetcherService {
   // It is also important to note that validForDays tells us for how many days the payment is still valid to retry in
   // case it is failing for some reason.
 
+  // We only want to fetch scheduled payments which that have payAt older or equal a minute ago. This is to
+  // satisfy the requirement in the smart contract (https://github.com/cardstack/cardstack-module-scheduled-payment/blob/main/contracts/ScheduledPaymentModule.sol)
+  // that protects against potential undesired effects of when miners manipulate the timestamp of the block in which the payment is executed, for example executing payments ahead of time
+  // or bunching many transactions together to get a bigger mining reward in the time window that block time can be manipulated (which is around 1 minute)
   async fetchScheduledPayments(limit = 10): Promise<ScheduledPayment[]> {
     let prisma = await this.prismaManager.getClient();
     let _nowUtc = nowUtc();
@@ -37,7 +40,10 @@ export default class ScheduledPaymentsFetcherService {
           scheduled_payments.canceled_at IS NULL 
           AND scheduled_payments.creation_block_number > 0 
           AND scheduled_payments.pay_at > ${startOfDay(subDays(_nowUtc, this.validForDays))}
-          AND scheduled_payments.pay_at <= ${_nowUtc}
+          AND scheduled_payments.pay_at <= ${subMinutes(
+            _nowUtc,
+            1
+          ).toISOString()}::timestamp -- 1 minute safety buffer due to a requirement in the ScheduledPayment module contract to avoid undesired effects of block time manipulation; find more details in the description above
           AND (
             (
               scheduled_payments.recurring_day_of_month IS NULL 
