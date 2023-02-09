7923e8b039e68fcb211c72f3b89030edb44f24cf
Matic Jurglič <matic@jurglic.si>
2023-02-09 11:43:27 +0000

    Assure number type because we want to use chain id since there can be naming mismatches between ethers networks and ours (i.e polygon, matic)

--- packages/hub/services/ethers-provider.ts
+++ packages/hub/services/ethers-provider.ts
@@ -2,7 +2,7 @@ import config from 'config';
 import { getWeb3ConfigByNetwork, JsonRpcProvider } from '@cardstack/cardpay-sdk';
 
 export default class EthersProvider {
-  getInstance(chainId: number | string) {
+  getInstance(chainId: number) {
     let rpcUrl = getWeb3ConfigByNetwork({ web3: config.get('web3') }, chainId).rpcNodeHttpsUrl;
     return new JsonRpcProvider(rpcUrl, chainId);
   }
