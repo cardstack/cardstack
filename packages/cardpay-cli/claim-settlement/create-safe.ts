5b75b2ec43c719c2e3d75d8d423763d6ad05c05a
tintinthong <justinthong93@gmail.com>
2023-02-10 18:08:25 +0800

    add cli to create safe

--- packages/cardpay-cli/claim-settlement/create-safe.ts
+++ packages/cardpay-cli/claim-settlement/create-safe.ts
@@ -1,5 +1,5 @@
 import { Argv } from 'yargs';
-import { getSDK } from '@cardstack/cardpay-sdk';
+import { getConstant, getSDK } from '@cardstack/cardpay-sdk';
 import { getEthereumClients, getConnectionType, NETWORK_OPTION_ANY } from '../utils';
 import { Arguments, CommandModule } from 'yargs';
 
@@ -15,26 +15,26 @@ export default {
       .option('network', NETWORK_OPTION_ANY);
   },
   async handler(args: Arguments) {
-    let { network } = args as unknown as {
+    let { network, txnHash } = args as unknown as {
       network: string;
       txnHash: string;
     };
     let { ethersProvider, signer } = await getEthereumClients(network, getConnectionType(args));
-    let ClaimSettlementModule = await getSDK('ClaimSettlementModule', ethersProvider, signer);
-    // let blockExplorer = await getConstant('blockExplorer', ethersProvider);
-    const o = ClaimSettlementModule.name;
-    console.log(o);
+    let claimSettlementModule = await getSDK('ClaimSettlementModule', ethersProvider, signer);
+    let blockExplorer = await getConstant('blockExplorer', ethersProvider);
 
-    // console.log(`Create a new safe and install SP module and meta guard...`);
-    // let onTxnHash = (txnHash: string) => console.log(`Transaction hash: ${blockExplorer}/tx/${txnHash}`);
-    // let { safeAddress, scheduledPaymentModuleAddress, metaGuardAddress } =
-    //   await scheduledPaymentModule.createSafeWithModuleAndGuard(txnHash, { onTxnHash });
-    // if (!safeAddress) {
-    //   console.log(`Failed to deploy new safe`);
-    // } else {
-    //   console.log(`Safe deployed to: ${safeAddress}`);
-    //   console.log(`Scheduled payment module deployed to: ${scheduledPaymentModuleAddress}`);
-    //   console.log(`Meta guard deployed to: ${metaGuardAddress}`);
-    // }
+    console.log(`Create a new safe and install SP module and meta guard...`);
+    let onTxnHash = (txnHash: string) => console.log(`Transaction hash: ${blockExplorer}/tx/${txnHash}`);
+    let { safeAddress, moduleAddress, metaGuardAddress } = await claimSettlementModule.createSafeWithModuleAndGuard(
+      txnHash,
+      { onTxnHash }
+    );
+    if (!safeAddress) {
+      console.log(`Failed to deploy new safe`);
+    } else {
+      console.log(`Safe deployed to: ${safeAddress}`);
+      console.log(`Scheduled payment module deployed to: ${moduleAddress}`);
+      console.log(`Meta guard deployed to: ${metaGuardAddress}`);
+    }
   },
 } as CommandModule;
