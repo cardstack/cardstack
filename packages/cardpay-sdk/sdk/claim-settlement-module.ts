ec670b2b605b0d1040e2e046ebb77f6bbf470fb3
tintinthong <justinthong93@gmail.com>
2023-02-11 15:21:14 +0800

    update claim settlement module

--- packages/cardpay-sdk/sdk/claim-settlement-module.ts
+++ packages/cardpay-sdk/sdk/claim-settlement-module.ts
@@ -4,16 +4,19 @@ import JsonRpcProvider from '../providers/json-rpc-provider';
 import SafeModule from './safe-module';
 import { Signer } from 'ethers';
 import ClaimSettlementABI from '../contracts/abi/modules/claim-settlement-module';
-// import { SuccessfulTransactionReceipt } from './utils/successful-transaction-receipt';
-// import { getAddress } from '../contracts/addresses';
-
-// import ClaimSettlemntABI from '../contracts/abi/modules/claim-settlement-module';
-// import { ethSignSignatureToRSVForSafe, signTypedData } from './utils/signing-utils';
+import { SetupArgs } from './utils/module-utils';
 
 export default class ClaimSettlementModule extends SafeModule {
   salt = 'cardstack-cs-create-safe';
   abi = ClaimSettlementABI;
   constructor(ethersProvider: JsonRpcProvider, signer?: Signer) {
     super(ethersProvider, signer);
+  }
+
+  async setupArgs(safeAddress: string, safeOwners: string[]): Promise<SetupArgs> {
+    return {
+      types: ['address', 'address', 'address[]', 'address'],
+      values: [safeAddress, safeAddress, safeOwners, safeAddress],
+    };
   }
 }
