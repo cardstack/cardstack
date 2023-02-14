70ba859d5756e8575f29c2b7f99ffcc3814c19e8
Matic Jurglič <matic@jurglic.si>
2023-02-14 10:28:40 +0000

    Improve payment error messages

--- packages/safe-tools-client/app/helpers/payment-error-message.ts
+++ packages/safe-tools-client/app/helpers/payment-error-message.ts
@@ -13,16 +13,23 @@ export function paymentErrorMessage([failureReason]: PositionalArgs) {
   // Try to match the error message to a known error explained in a way that makes sense to the user
   if (failureReason) {
     if (failureReason.includes('InvalidPeriod'))
-      return 'payment attempted at the wrong time'; // Could be the crank's fault. Not much the user can do about it except report it to support
+      // The crank attempted this payment at the time that the scheduled payment module smart contract did not agree with.
+      // This is indicative of a scheduling bug in the crank or in the contract.
+      // The user can't do anything to fix it but report it to support.
+      return 'attempted at the incorrect time - contact support';
     if (failureReason.includes('ExceedMaxGasPrice'))
-      return 'current gas price exceeds the maximum specified';
+      return 'gas price exceeded the maximum specified';
     if (failureReason.includes('PaymentExecutionFailed'))
-      return 'insufficient funds to execute the payment'; // This error covers 3 different failure scenarios and it is currently not possible to know which we are dealing with - insufficient funds for either of the: transfer amount, gas fee, service fee
+      // This error covers 3 different failure scenarios and it is currently not possible to know which we are
+      // dealing with. It means insufficient funds for either of the: transfer amount, gas fee, service fee
+      return 'insufficient funds to execute the payment';
     if (failureReason.includes('OutOfGas'))
-      return 'execution gas is too low to execute the payment'; // Probably our fault where we estimate execution gas. Not much the user can do about it except report it to support
+      // There is something wrong with our payment execution gas estimation. This is indicative of a bug in the smart contract or the SDK.
+      // The user can't do anything to fix it but report it to support.
+      return 'incorrect gas estimation - contact support';
   }
 
-  return 'unrecognized error - contact support';
+  return 'unknown error - contact support';
 }
 
 export default helper<Signature>(paymentErrorMessage);
