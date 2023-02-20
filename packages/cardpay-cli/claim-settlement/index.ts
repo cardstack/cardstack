63b2c96d470502c03c2d700e6b97bccd9ce36c40
tintinthong <justinthong93@gmail.com>
2023-02-20 15:18:30 +0800

    add details and enable module command

--- packages/cardpay-cli/claim-settlement/index.ts
+++ packages/cardpay-cli/claim-settlement/index.ts
@@ -1,11 +1,13 @@
 import type { Argv } from 'yargs';
 import createSafe from './create-safe';
+import enableModule from './enable-module';
+import details from './details';
 
 export const command = 'claim-settlement <command>';
 export const desc = 'Commands to interact with the claim settlement module';
 
 export const builder = function (yargs: Argv) {
-  return yargs.command([createSafe] as any);
+  return yargs.command([createSafe, enableModule, details] as any);
 };
 
 export function handler(/* argv: Argv */) {
