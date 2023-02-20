734486d757e1a355b575552eafde2b92a0c6589f
tintinthong <justinthong93@gmail.com>
2023-02-20 16:30:57 +0800

    add execute to index

--- packages/cardpay-cli/claim-settlement/index.ts
+++ packages/cardpay-cli/claim-settlement/index.ts
@@ -2,12 +2,13 @@ import type { Argv } from 'yargs';
 import createSafe from './create-safe';
 import enableModule from './enable-module';
 import details from './details';
+import execute from './execute';
 
 export const command = 'claim-settlement <command>';
 export const desc = 'Commands to interact with the claim settlement module';
 
 export const builder = function (yargs: Argv) {
-  return yargs.command([createSafe, enableModule, details] as any);
+  return yargs.command([createSafe, enableModule, details, execute] as any);
 };
 
 export function handler(/* argv: Argv */) {
