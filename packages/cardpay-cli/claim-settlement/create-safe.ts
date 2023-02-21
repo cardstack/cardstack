cb44afbdce4e590a291a765a8e42c7c6ef82b5a1
tintinthong <justinthong93@gmail.com>
2023-02-21 10:52:25 +0800

    use this command

--- packages/cardpay-cli/claim-settlement/create-safe.ts
+++ packages/cardpay-cli/claim-settlement/create-safe.ts
@@ -25,16 +25,14 @@ export default {
 
     console.log(`Create a new safe and install Claim Settlement module and meta guard...`);
     let onTxnHash = (txnHash: string) => console.log(`Transaction hash: ${blockExplorer}/tx/${txnHash}`);
-    let { safeAddress, moduleAddress, metaGuardAddress } = await claimSettlementModule.createSafeWithModuleAndGuard(
-      txnHash,
-      { onTxnHash }
-    );
+    let { safeAddress, moduleAddress } = await claimSettlementModule.createSafeWithModule(txnHash, {
+      onTxnHash,
+    });
     if (!safeAddress) {
       console.log(`Failed to deploy new safe`);
     } else {
       console.log(`Safe deployed to: ${safeAddress}`);
       console.log(`Claim settlement module deployed to: ${moduleAddress}`);
-      console.log(`Meta guard deployed to: ${metaGuardAddress}`);
     }
   },
 } as CommandModule;
