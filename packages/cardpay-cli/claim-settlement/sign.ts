d7f77f56739aa1b1ff4b7361671271fbdf8c3f01
tintinthong <justinthong93@gmail.com>
2023-02-28 19:37:12 +0800

    update cli

--- packages/cardpay-cli/claim-settlement/sign.ts
+++ packages/cardpay-cli/claim-settlement/sign.ts
@@ -3,43 +3,42 @@ import { getEthereumClients, getConnectionType, NETWORK_OPTION_ANY } from '../ut
 import { getConstant, getSDK } from '@cardstack/cardpay-sdk';
 
 export default {
-  command: 'execute <moduleAddress> [payeeSafeAddress] [gasTokenAddress]',
-  describe: 'Execute a claim settlement',
+  command: 'sign <moduleAddress> <payeeAddress> <tokenAddress> <amountInEth> [validitySeconds]',
+  describe: 'Sign claim',
   builder(yargs: Argv) {
     return yargs
       .positional('moduleAddress', {
         type: 'string',
         description: 'Module address enabled on safe',
       })
-      .option('payeeSafeAddress', {
+      .positional('payeeAddress', {
         type: 'string',
         description: 'The address receiving assets from claim',
       })
-      .option('gasTokenAddress', {
+      .positional('tokenAddress', {
         type: 'string',
-        description: 'The address of gas token',
+        description: 'The address of token being claimed',
+      })
+      .positional('amountInEth', {
+        type: 'string',
+        description: 'The amount of token being claimed',
       })
       .option('network', NETWORK_OPTION_ANY);
   },
   async handler(args: Arguments) {
-    let { network, moduleAddress, payeeSafeAddress, gasTokenAddress } = args as unknown as {
+    let { network, moduleAddress, payeeAddress, tokenAddress, amountInEth } = args as unknown as {
       network: string;
       moduleAddress: string;
-      payeeSafeAddress: string;
-      gasTokenAddress: string;
+      payeeAddress: string;
+      tokenAddress: string;
+      amountInEth: string;
     };
     let { ethersProvider, signer } = await getEthereumClients(network, getConnectionType(args));
     let claimSettlementModule = await getSDK('ClaimSettlementModule', ethersProvider, signer);
+    let claim = await claimSettlementModule.stakingClaim(moduleAddress, payeeAddress, tokenAddress, amountInEth);
+    let { signature, encoded } = await claimSettlementModule.sign(claim);
     let blockExplorer = await getConstant('blockExplorer', ethersProvider);
     let onTxnHash = (txnHash: string) => console.log(`Transaction hash: ${blockExplorer}/tx/${txnHash}`);
-    let claim = await claimSettlementModule.defaultStakingClaim(moduleAddress, payeeSafeAddress);
-    let signedClaim = await claimSettlementModule.sign(claim);
-    if (payeeSafeAddress) {
-      await claimSettlementModule.executeSafe(moduleAddress, payeeSafeAddress, signedClaim, gasTokenAddress, {
-        onTxnHash,
-      });
-    } else {
-      await claimSettlementModule.executeEOA(moduleAddress, signedClaim, { onTxnHash });
-    }
+    await claimSettlementModule.executeEOA(moduleAddress, { signature, encoded }, { onTxnHash });
   },
 } as CommandModule;
