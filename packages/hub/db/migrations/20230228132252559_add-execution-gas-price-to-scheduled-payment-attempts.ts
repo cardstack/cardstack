b808db989f89d4d4a37e23b71c0d90db0b4b05d2
Matic Jurglič <matic@jurglic.si>
2023-03-06 14:15:25 +0100

    Add default value to execution gas price to avoid not null constraint problem for historic attempts

--- packages/hub/db/migrations/20230228132252559_add-execution-gas-price-to-scheduled-payment-attempts.ts
+++ packages/hub/db/migrations/20230228132252559_add-execution-gas-price-to-scheduled-payment-attempts.ts
@@ -5,7 +5,7 @@ const EXECUTION_GAS_PRICE = 'execution_gas_price';
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.addColumn(TABLE, {
-    [EXECUTION_GAS_PRICE]: { type: 'string', notNull: true },
+    [EXECUTION_GAS_PRICE]: { type: 'string', notNull: true, default: '0' },
   });
 }
 
