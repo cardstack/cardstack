ed422fde03a7d05da569fdae3bf94c56c0428498
Fadhlan Ridhwanallah <fridhwanallah@gmail.com>
2023-03-15 10:12:43 +0000

    testcase fix

--- packages/safe-tools-client/tests/integration/components/future-payments-list-test.ts
+++ packages/safe-tools-client/tests/integration/components/future-payments-list-test.ts
@@ -58,8 +58,14 @@ class TokenToUsdServiceStub extends TokenToUsdService {
       tokenInAddress: '0x0',
       tokenOutAddress: '0x0',
       tokenInDecimals: 6,
-      tokenOutDecimals: 18,
-      rate: FixedNumber.from('0.001'),
+      tokenOutDecimals:
+          tokenAddress === '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+            ? 6
+            : 18,
+      rate:
+          tokenAddress === '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+            ? FixedNumber.from('1')
+            : FixedNumber.from('0.001'),
     });
   }
 }
