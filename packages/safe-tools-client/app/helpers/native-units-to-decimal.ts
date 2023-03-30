2552a187231751f47eb9b6d7ecaa0393b1c99f1c
Matic Jurglič <matic@jurglic.si>
2023-03-30 13:13:45 +0200

    Add decimal rounding case for very small numbers (i.e. 0.000001)

--- packages/safe-tools-client/app/helpers/native-units-to-decimal.ts
+++ packages/safe-tools-client/app/helpers/native-units-to-decimal.ts
@@ -10,16 +10,35 @@ interface Signature {
   Return: string;
 }
 
+export function roundDecimals(number: number, decimals: number) {
+  // Below is a special case for numbers betweeon 0 and 1 whose first significant decimal digit comes after the rounding point.
+  // For example we want to round to to 3 decimals, but the number is 0.0000001. In this case if we use toFixed(3),
+  // we will get 0, but in this case we want to keep 0.0000001 to not throw away the significant part.
+  if (number < 1 && number % 1 !== 0) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    const match = number.toString().match(/(\.0*)/);
+    if (match) {
+      const leadingZerosInDecimal = match[0].length - 1;
+      if (leadingZerosInDecimal > decimals) {
+        return number.toFixed(leadingZerosInDecimal + 1);
+      }
+    }
+  }
+
+  return number.toFixed(decimals);
+}
+
 export function nativeUnitsToDecimal([
   amount,
   tokenDecimals,
   decimals = undefined,
 ]: PositionalArgs) {
   const number = parseFloat(ethersUtils.formatUnits(amount, tokenDecimals));
+
   if (decimals) {
-    return number.toFixed(decimals);
+    return roundDecimals(number, decimals);
   } else {
-    return String(number % 1 === 0 ? number : number.toFixed(3));
+    return String(number % 1 === 0 ? number : roundDecimals(number, 3));
   }
 }
 
