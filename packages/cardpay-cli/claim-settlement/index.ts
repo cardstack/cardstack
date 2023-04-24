2bcee859f0bd00d81b75ab6292ecda71ae5ebcfa
tintinthong <justinthong93@gmail.com>
2023-04-24 23:23:45 +0800

    add set did configuration

--- packages/cardpay-cli/claim-settlement/index.ts
+++ packages/cardpay-cli/claim-settlement/index.ts
@@ -6,12 +6,22 @@ import execute from './execute';
 import addValidator from './add-validator';
 import registerAccount from './register-account';
 import sign from './sign';
+import setDid from './set-did';
 
 export const command = 'claim-settlement <command>';
 export const desc = 'Commands to interact with the claim settlement module';
 
 export const builder = function (yargs: Argv) {
-  return yargs.command([createSafe, enableModule, details, execute, addValidator, registerAccount, sign] as any);
+  return yargs.command([
+    createSafe,
+    enableModule,
+    details,
+    execute,
+    addValidator,
+    registerAccount,
+    sign,
+    setDid,
+  ] as any);
 };
 
 export function handler(/* argv: Argv */) {
