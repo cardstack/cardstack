bbcaac2efbbf9a5c3e95d100b7e7c3c83bd80af9
tintinthong <justinthong93@gmail.com>
2023-06-08 18:18:04 +0800

    add browser build

---
+++ packages/cardpay-sdk/scripts/browser-build.js
@@ -0,0 +1,23 @@
+const esbuild = require('esbuild');
+const { NodeModulesPolyfillPlugin } = require('@esbuild-plugins/node-modules-polyfill');
+const { NodeGlobalsPolyfillPlugin } = require('@esbuild-plugins/node-globals-polyfill');
+
+esbuild
+  .build({
+    entryPoints: ['index.ts'],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    platform: 'browser',
+    target: ['es2020'],
+    outfile: 'dist/browser.js',
+    format: 'esm',
+    plugins: [
+      NodeGlobalsPolyfillPlugin({
+        buffer: true,
+      }),
+      NodeModulesPolyfillPlugin(),
+    ],
+  })
+  .then(console.log('build succesful'))
+  .catch((e) => console.error('Build failed with error:', e));
